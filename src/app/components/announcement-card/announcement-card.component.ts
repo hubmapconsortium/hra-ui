0013dc7a412d174cbcc554d56519eba5e48f79b4
VigneshReddy20 <89468415+VigneshReddy20@users.noreply.github.com>
2022-12-14 21:53:35 -0500

    Vignesh route fix (#76)
    * Fix routing issues
    
    * Fixes for route

--- src/app/components/announcement-card/announcement-card.component.ts
+++ src/app/components/announcement-card/announcement-card.component.ts
@@ -14,7 +14,7 @@ export class AnnouncementCardComponent {
   @Input() messages: Announcement[] = [
     {
       message: "The 4th Release of the Human Reference Atlas (v1.3) is now available! See what's new in HRA v1.3 in",
-      route: 'release-notes/v1.3',
+      route: 'v1.3',
       routeText: '4th Release Notes'
     },
     {
