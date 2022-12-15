06d485e9d9d7fdbd02c75d35b7417da4f8339386
Bruce W. Herr II <bherr@indiana.edu>
2022-12-15 09:10:15 -0500

    Typo

--- src/app/shared/simple-tile-items.ts
+++ src/app/shared/simple-tile-items.ts
@@ -5,6 +5,6 @@ export const TileDefinition: TileItems[] = [
         title: 'A <b>Common Coordinate Framework (CCF)</b> for a human body provides a unique address for each cell in the human body. It is similar to the latitude-longtitude system used to navigate a world map.'
     },
     {
-        title: 'A <b>Human Reference Atlas (HRA)</b> is a comprehensive, high-resolution, three-dimensional atlas of all the cells in the healthy human body. The Human Reference Atlas provides standard terminologies and data structures for describing spicimens, biological structures, and spatial positions linked to existing ontologies.'
+        title: 'A <b>Human Reference Atlas (HRA)</b> is a comprehensive, high-resolution, three-dimensional atlas of all the cells in the healthy human body. The Human Reference Atlas provides standard terminologies and data structures for describing specimens, biological structures, and spatial positions linked to existing ontologies.'
     }
 ];
