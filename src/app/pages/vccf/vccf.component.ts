6bcec2daf8b8d6baf721069983617fedf65159ef
Bruce W. Herr II <bherr@indiana.edu>
2023-01-04 08:59:16 -0500

    Add terms of use and other minor updates to the VCCF page

--- src/app/pages/vccf/vccf.component.ts
+++ src/app/pages/vccf/vccf.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PageDataItems } from 'src/app/components/page-data/page-data';
 import { PageHeaderItems } from 'src/app/components/page-header/page-header-items';
@@ -12,6 +12,7 @@ export class VccfComponent {
   pageHeader: PageHeaderItems[];
   overviewData: PageDataItems[];
   vccfDataFiles: PageDataItems[];
+  termsOfUse: PageDataItems[];
   license: PageDataItems[];
   citation: PageDataItems[];
   acknowledgments: PageDataItems[];
@@ -19,12 +20,13 @@ export class VccfComponent {
 
   constructor(route: ActivatedRoute) {
     const data = route.snapshot.data['content'];
-    this.pageHeader = data.pageHeader
-    this.overviewData = data.overviewData
-    this.vccfDataFiles = data.vccfDataFiles
-    this.license = data.license
-    this.citation = data.citation
-    this.acknowledgments = data.acknowledgments
-    this.references = data.references
+    this.pageHeader = data.pageHeader;
+    this.overviewData = data.overviewData;
+    this.vccfDataFiles = data.vccfDataFiles;
+    this.termsOfUse = data.termsOfUse;
+    this.license = data.license;
+    this.citation = data.citation;
+    this.acknowledgments = data.acknowledgments;
+    this.references = data.references;
   }
 }
