69bad4d6ac213851dfc5050e2d7a0e1e43a0778c
Daniel Bolin <axbolin@iu.edu>
2023-02-07 14:16:12 -0500

    Update test config

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,14 @@
 const nxPreset = require('@nrwl/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: 85,
+    },
+  },
+};
