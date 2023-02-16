31c77c35e89b40ee8a530c6b51152c7d2949e9a0
jagrut-18 <jagrutchaudhari18@gmail.com>
2023-02-16 14:10:46 -0500

    adds source list implementation

--- libs/components/molecules/src/lib/source-list/source-list.component.ts
+++ libs/components/molecules/src/lib/source-list/source-list.component.ts
@@ -1,12 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
+
+export interface SourceListItem {
+  title: string;
+  link: string;
+}
 
 @Component({
   selector: 'hra-source-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatListModule, MatTableModule],
   templateUrl: './source-list.component.html',
   styleUrls: ['./source-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SourceListComponent {}
+export class SourceListComponent {
+  @Input() sources: SourceListItem[] = [];
+}
