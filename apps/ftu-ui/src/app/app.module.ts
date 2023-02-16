51b2caec6be8b5a3c1135cf31895c2ddff1d0e4f
rchavan10 <rajchavan1099@gmail.com>
2023-02-16 10:31:32 -0500

    created routing setup

--- apps/ftu-ui/src/app/app.module.ts
+++ apps/ftu-ui/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
