4953d47e2ca92c9074a4673563e9d0cfd71ec569
bhushankhope <bhushankhope@gmail.com>
2023-02-17 11:40:41 -0500

    created service to download png,svg

--- src/app/components/two-dim-image/two-dim-image.component.ts
+++ src/app/components/two-dim-image/two-dim-image.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output, TemplateRef } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { FileDownloadService } from '../../services/file-download/file-download.service';
 import { OrganData } from './two-dim-image';
 
 @Component({
@@ -16,9 +17,14 @@ export class TwoDimImageComponent {
   
   @Output() infoRoute = new EventEmitter<OrganData>;
   
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog, private readonly downloader: FileDownloadService) { }
 
   openImageViewer(content: TemplateRef<unknown>): void {
     this.dialog.open(content, { panelClass: 'two-dim-image-modal' })
+  }
+
+  downloadClick(event: Event, url: string): void {
+    event.preventDefault();
+    this.downloader.download(url);
   }
 }
