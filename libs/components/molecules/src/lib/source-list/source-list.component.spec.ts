6c353a82f31f4a4c18d1dbd9d9d874f49c2c87be
jagrut-18 <jagrutchaudhari18@gmail.com>
2023-02-17 14:35:45 -0500

    update test file

--- libs/components/molecules/src/lib/source-list/source-list.component.spec.ts
+++ libs/components/molecules/src/lib/source-list/source-list.component.spec.ts
@@ -1,10 +1,11 @@
+import { MatTableModule } from '@angular/material/table';
 import { Shallow } from 'shallow-render';
 import { SourceListComponent } from './source-list.component';
 
 describe('SourceListComponent', () => {
   let shallow: Shallow<SourceListComponent>;
   beforeEach(() => {
-    shallow = new Shallow(SourceListComponent);
+    shallow = new Shallow(SourceListComponent).dontMock(MatTableModule);
   });
 
   it('should create', async () => {
