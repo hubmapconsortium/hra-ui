f6ca04f7ac9d6e7a7365af623a5ac2d46c8cfc8c
rchavan10 <rajchavan1099@gmail.com>
2023-02-17 12:16:49 -0500

    removed unnescessary modules

--- libs/components/molecules/src/lib/header/header.component.ts
+++ libs/components/molecules/src/lib/header/header.component.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'hra-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
