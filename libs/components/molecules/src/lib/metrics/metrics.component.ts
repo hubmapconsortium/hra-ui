0b7b2ddf18ca87f1594445492fbeaf5ec71901a4
Aditya Rudrawar <adirudrawar17@gmail.com>
2023-02-17 15:16:20 -0500

    Add metrics component - Add input in stories -  Add HTML - Add SCSS - Pass the input to HTML

--- libs/components/molecules/src/lib/metrics/metrics.component.ts
+++ libs/components/molecules/src/lib/metrics/metrics.component.ts
@@ -1,12 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+export interface MetricItem {
+  icon: string;
+  description: string;
+  value: string;
+}
 
 @Component({
-  selector: 'hra-metrics',
+  selector: "hra-metrics",
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './metrics.component.html',
-  styleUrls: ['./metrics.component.scss'],
+  templateUrl: "./metrics.component.html",
+  styleUrls: ["./metrics.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MetricsComponent {}
+export class MetricsComponent {
+  /**
+   * Input  Title of metrics component
+   */
+  @Input() title = "";
+
+  /**
+   * Input  Logo of metrics component
+   */
+  @Input() logo = "";
+
+  /**
+   * Input  List of metrics component
+   */
+  @Input() metrics: MetricItem[] = [];
+}
