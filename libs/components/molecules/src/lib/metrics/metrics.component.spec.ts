62eb47f7e15678090cf820daa21a45b31691266a
Aditya Rudrawar <adirudrawar17@gmail.com>
2023-02-17 17:13:36 -0500

    Add test

--- libs/components/molecules/src/lib/metrics/metrics.component.spec.ts
+++ libs/components/molecules/src/lib/metrics/metrics.component.spec.ts
@@ -1,5 +1,14 @@
-import { MetricsComponent } from './metrics.component';
+import { MetricsComponent } from "./metrics.component";
+import { Shallow } from "shallow-render";
 
-describe('MetricsComponent', () => {
-  // TODO
+describe("MetricsComponent", () => {
+  let shallow: Shallow<MetricsComponent>;
+
+  beforeEach(() => {
+    shallow = new Shallow(MetricsComponent);
+  });
+
+  it("should create MetricsComponent", async () => {
+    await expect(shallow.render()).resolves.toBeDefined();
+  });
 });
