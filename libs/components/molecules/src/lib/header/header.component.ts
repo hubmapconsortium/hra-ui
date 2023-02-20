f1ddb08e0274edf772668e572769ea9ac4bf775e
rchavan10 <rajchavan1099@gmail.com>
2023-02-20 12:04:33 -0500

    doc coverage

--- libs/components/molecules/src/lib/header/header.component.ts
+++ libs/components/molecules/src/lib/header/header.component.ts
@@ -2,6 +2,9 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 
+/**
+ * Header component, displays on top of every page.
+ */
 @Component({
   selector: 'hra-header',
   standalone: true,
@@ -11,7 +14,18 @@ import { MatIconModule } from '@angular/material/icon';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
+  /**
+   * Input for product logo URL to displayed on the left side.
+   */
   @Input() productLogoUrl = '';
+
+  /**
+   * Input for product title to displayed on the left side.
+   */
   @Input() productTitle = '';
+
+  /**
+   * Input for app title to show on the left side.
+   */
   @Input() appTitle = '';
 }
