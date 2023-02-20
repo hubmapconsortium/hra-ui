933a1e51ff89972d7abfca9c3d68dd61545b2460
edlu77 <eddd.lu@gmail.com>
2023-02-20 14:41:43 -0500

    Resolve conflicts

--- apps/ftu-ui/src/app/app.module.ts
+++ apps/ftu-ui/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { InteractiveSvgComponent } from '@hra-ui/components/molecules';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
--- libs/components/molecules/src/index.ts
+++ libs/components/molecules/src/index.ts
@@ -1,4 +1,5 @@
-export * from './lib/interactive-svg/interactive-svg.component';
 export * from './lib/landing-page-intro/landing-page-intro.component';
+export * from './lib/interactive-svg/interactive-svg.component';
+
 /** Placeholder to make build and tests pass until any actual library code is created */
 export const placeholder = undefined;
