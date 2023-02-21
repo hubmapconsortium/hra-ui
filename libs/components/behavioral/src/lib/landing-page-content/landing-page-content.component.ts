563f0d2abb21eb75161dd84bed66ba4bfd3d1ceb
Sai Kiran Jella <jsaikiran19@gmail.com>
2023-02-21 14:30:26 -0500

    adds a resource file for content

--- libs/components/behavioral/src/lib/landing-page-content/landing-page-content.component.ts
+++ libs/components/behavioral/src/lib/landing-page-content/landing-page-content.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageIntroComponent } from '@hra-ui/components/molecules';
+import { LANDING_PAGE_CONTENT } from '../../resources/landing-page-content';
 
 @Component({
   selector: 'ftu-landing-page-content',
@@ -11,20 +12,6 @@ import { LandingPageIntroComponent } from '@hra-ui/components/molecules';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LandingPageContentComponent {
-  introData = {
-    title: 'Welcome to the Functional Tissue Unit Explorer',
-
-    description: `A functional tissue unit (FTU) is the smallest tissue organization that performs a unique physiologic function and
-    is replicated multiple times in a whole organ. Explore medical illustrations of FTUs to view cell type populations
-    by gene, protein, and lipid biomarkers. The FTU Explorer features experimental datasets from
-    <a href='' target='_blank'>HuBMAP</a>, <a href='' target='_blank'>CellxGene</a>,
-    <a href='' target='_blank'>NeMO</a>, and <a href='' target='_blank'>GTEx</a>.`,
-
-    partners: `A special thanks to our partners: The FTU Explorer was designed in close collaboration with
-    <a href='' target='_blank'>Kidney Precision Medicine Project</a> and <a href=''>European Bioinformatics Institute.</a>`,
-
-    moreText: 'Explore FTUs',
-
-    img: '../../../../molecules/src/assets/welcome.svg',
-  };
+  /** Introduction Data for landing-page-intro component */
+  introData = LANDING_PAGE_CONTENT.introData;
 }
