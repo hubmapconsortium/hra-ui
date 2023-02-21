21d246fc3497be3896786a01487664136639bdd6
anbadrin <anita1397@gmail.com>
2023-02-21 17:13:15 -0500

    Fixing code smells

--- libs/components/molecules/src/lib/screen-size-notice/screen-size-notice.component.spec.ts
+++ libs/components/molecules/src/lib/screen-size-notice/screen-size-notice.component.spec.ts
@@ -1,4 +1,3 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Shallow } from 'shallow-render';
 import { ScreenSizeNoticeComponent } from './screen-size-notice.component';
 
