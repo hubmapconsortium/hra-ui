b8002c0a1bcd48f21ebb6d6ea0f4bb5d74805471
Aditya Rudrawar <adirudrawar17@gmail.com>
2023-02-22 14:03:02 -0500

    Remove conflict

--- libs/components/molecules/src/index.ts
+++ libs/components/molecules/src/index.ts
@@ -1,6 +1,3 @@
 export * from './lib/landing-page-intro/landing-page-intro.component';
 export * from './lib/source-list/source-list.component';
 export * from './lib/metrics/metrics.component';
-
-/** Placeholder to make build and tests pass until any actual library code is created */
-export const placeholder = undefined;
