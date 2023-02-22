9ef90f1f3ff8b1ba0d0c2a84c180583bf8a38e06
Aditya Rudrawar <adirudrawar17@gmail.com>
2023-02-22 14:56:38 -0500

    Address PR comments

--- libs/components/molecules/src/lib/metrics/metrics.component.ts
+++ libs/components/molecules/src/lib/metrics/metrics.component.ts
@@ -1,33 +1,35 @@
-import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
+/**
+  An interface representing a single metric with an icon, value and description.
+ */
 export interface MetricItem {
+  /** Represents the icon associated with the metric. */
   icon: string;
-  description: string;
+  /** Describes the numberical value of the metric. */
   value: string;
+  /** Describes the type of metric. */
+  description: string;
 }
-
+/**
+ * Describes the metrics for the HuBMAP project.
+ */
 @Component({
-  selector: "hra-metrics",
+  selector: 'hra-metrics',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: "./metrics.component.html",
-  styleUrls: ["./metrics.component.scss"],
+  templateUrl: './metrics.component.html',
+  styleUrls: ['./metrics.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MetricsComponent {
-  /**
-   * Input  Title of metrics component
-   */
-  @Input() title = "";
+  /** Main title of metrics component. */
+  @Input() title = '';
 
-  /**
-   * Input  Logo of metrics component
-   */
-  @Input() logo = "";
+  /** Logo of metrics component. */
+  @Input() logo = '';
 
-  /**
-   * Input  List of metrics component
-   */
+  /** List of metrics that will be shown to the user. */
   @Input() metrics: MetricItem[] = [];
 }
