868801febe5a33494ee154f44ccf0b86b4e6f265
jagrut-18 <jagrutchaudhari18@gmail.com>
2023-02-22 12:31:24 -0500

    requested changes

--- libs/components/molecules/src/lib/biomarker-table-data-card/biomarker-table-data-card.component.spec.ts
+++ libs/components/molecules/src/lib/biomarker-table-data-card/biomarker-table-data-card.component.spec.ts
@@ -1,22 +1,14 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatTableModule } from '@angular/material/table';
+import { Shallow } from 'shallow-render';
 import { BiomarkerTableDataCardComponent } from './biomarker-table-data-card.component';
 
 describe('BiomarkerTableDataCardComponent', () => {
-  let component: BiomarkerTableDataCardComponent;
-  let fixture: ComponentFixture<BiomarkerTableDataCardComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [BiomarkerTableDataCardComponent],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(BiomarkerTableDataCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  let shallow: Shallow<BiomarkerTableDataCardComponent>;
+  beforeEach(() => {
+    shallow = new Shallow(BiomarkerTableDataCardComponent).dontMock(MatTableModule);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create', async () => {
+    await expect(shallow.render()).resolves.toBeDefined();
   });
 });
