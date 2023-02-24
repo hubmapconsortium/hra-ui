07306db6badfc484ebad796d7f5c08c29a383e19
Sumedh Salvi <sumedhsalvi25@gmail.com>
2023-02-23 20:07:01 -0500

    product logo url and markdown

--- libs/components/molecules/src/lib/info-modal/info-modal.component.ts
+++ libs/components/molecules/src/lib/info-modal/info-modal.component.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from 
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MarkdownModule } from 'ngx-markdown';
 
 @Component({
   selector: 'hra-info-modal',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MarkdownModule],
   templateUrl: './info-modal.component.html',
   styleUrls: ['./info-modal.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,4 +20,12 @@ export class InfoModalComponent {
 
   /** Emitted when the close icon is clicked */
   @Output() readonly closeClick = new EventEmitter<void>();
+
+  /**
+   * Input for product logo URL to displayed on the left side.
+   */
+  @Input() productLogoUrl = '';
+
+  /** Description for FTU Info Modal */
+  @Input() description = '';
 }
