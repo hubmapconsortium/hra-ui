67e80644b89044424f8804d8e7b6f84994da4d03
tanukansalgit <tanukansal167@gmail.com>
2023-02-24 01:59:43 -0500

    mat Tree hierarchial data display

--- libs/components/molecules/src/lib/tissue-tree-list/tissue-tree-list.component.ts
+++ libs/components/molecules/src/lib/tissue-tree-list/tissue-tree-list.component.ts
@@ -2,16 +2,18 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTreeModule, MatTreeNestedDataSource } from '@angular/material/tree';
 import { NestedTreeControl } from '@angular/cdk/tree';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 interface TissueTreeGroup {
-  label: string;
-  items?: TissueTreeGroup[];
+  name: string;
+  children?: TissueTreeGroup[];
 }
 
 @Component({
   selector: 'hra-tissue-tree-list',
   standalone: true,
-  imports: [CommonModule, MatTreeModule],
+  imports: [CommonModule, MatTreeModule, MatIconModule, MatExpansionModule],
   templateUrl: './tissue-tree-list.component.html',
   styleUrls: ['./tissue-tree-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,33 +21,45 @@ interface TissueTreeGroup {
 export class TissueTreeListComponent {
   @Input() treeList: TissueTreeGroup[];
 
-  treeControl = new NestedTreeControl<TissueTreeGroup>((node) => node.items);
+  treeControl = new NestedTreeControl<TissueTreeGroup>((node) => node.children);
 
   dataSource = new MatTreeNestedDataSource<TissueTreeGroup>();
 
   constructor() {
     this.treeList = [
       {
-        label: 'Kidney',
-        items: [
-          { label: 'Ascending thin limb' },
-          { label: 'Cortical collecting duct' },
-          { label: 'Collecting duct(inner medulla)' },
+        name: 'Kidney',
+        children: [
+          { name: 'Ascending thin limb' },
+          { name: 'Cortical collecting duct' },
+          { name: 'Collecting duct(inner medulla)' },
         ],
       },
       {
-        label: 'Large Intestine',
-        items: [{ label: 'Crypt of Lieberkuhn' }],
+        name: 'Large Intestine',
+        children: [{ name: 'Crypt of Lieberkuhn' }],
       },
       {
-        label: 'Liver',
-        items: [{ label: 'Liver lobule' }],
+        name: 'Liver',
+        children: [{ name: 'Liver lobule' }],
       },
     ];
 
     this.dataSource.data = this.treeList;
-    console.log('dataSprce==', this.dataSource);
   }
 
-  hasChild = (_: number, node: TissueTreeGroup) => !!node.items && node.items.length > 0;
+  hasChild = (_: number, node: TissueTreeGroup) => !!node.children && node.children.length > 0;
+
+  getChildren(node: TissueTreeGroup): TissueTreeGroup[] {
+    console.log('hererree');
+    return node.children || [];
+  }
+
+  isExpanded(node: TissueTreeGroup): boolean {
+    console.log('in funtio');
+    console.log('node==', node);
+    console.log('reduklt==', this.treeControl.isExpanded(node));
+    // return this.treeControl.isExpanded(node);
+    return true;
+  }
 }
