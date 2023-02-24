b3cc6c37f6dd2539d8f863b60557ed94ff3f3df2
Himanshu Joshi <hsjoshi28@gmail.com>
2023-02-24 16:46:53 -0500

    features add on

--- libs/components/molecules/src/lib/contact-modal/contact-modal.component.ts
+++ libs/components/molecules/src/lib/contact-modal/contact-modal.component.ts
@@ -1,12 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EmailInputComponent } from '@hra-ui/components/atoms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'hra-contact-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, EmailInputComponent],
   templateUrl: './contact-modal.component.html',
   styleUrls: ['./contact-modal.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ContactModalComponent {}
+export class ContactModalComponent {
+  @Input() placeHolder = 'Enter Email Here';
+  /**
+   * Input for product logo URL to displayed on the left side.
+   */
+  @Input() productLogoUrl = '';
+
+  /**
+   * Input for product title to displayed on the left side.
+   */
+  @Input() productTitle = '';
+
+  @Output() emailChange = new EventEmitter<string | null>();
+
+  email(event: string | null) {
+    console.log(event);
+    this.emailChange.emit(event);
+  }
+}
