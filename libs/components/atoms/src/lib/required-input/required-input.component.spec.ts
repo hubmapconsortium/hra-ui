4466c6430d494597b3e3c97800804d595a3353c6
Darwante, Sanket Atmaram <sdarwant@iu.edu>
2023-02-24 19:23:08 -0500

    added validation and functionality code

--- libs/components/atoms/src/lib/required-input/required-input.component.spec.ts
+++ libs/components/atoms/src/lib/required-input/required-input.component.spec.ts
@@ -1,22 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Shallow } from 'shallow-render';
 import { RequiredInputComponent } from './required-input.component';
 
-describe('RequiredInputComponent', () => {
-  let component: RequiredInputComponent;
-  let fixture: ComponentFixture<RequiredInputComponent>;
+describe('TextFieldComponent', () => {
+  let shallow: Shallow<RequiredInputComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RequiredInputComponent],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(RequiredInputComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    shallow = new Shallow(RequiredInputComponent);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create', async () => {
+    const { instance } = await shallow.render();
+    expect(instance).toBeTruthy();
+  });
+
+  it('should show error message if input is empty', async () => {
+    const { find, instance } = await shallow.render({ bind: { label: 'Test Label', error: 'This field is required' } });
+    const input = find('input');
+
+    input.nativeElement.value = '';
+    input.nativeElement.dispatchEvent(new Event('input'));
+    expect(instance.formControl.invalid).toBe(true);
+    expect(find('mat-error').nativeElement.textContent.trim()).toBe('This field is required');
+  });
+
+  it('should not show error message if input is not empty', async () => {
+    const { find, instance } = await shallow.render({ bind: { label: 'Test Label', error: 'This field is required' } });
+    const input = find('input');
+
+    input.nativeElement.value = 'hello';
+    input.nativeElement.dispatchEvent(new Event('input'));
+    expect(instance.formControl.invalid).toBe(false);
   });
 });
--- libs/components/atoms/src/lib/required-input/required-input.component.ts
+++ libs/components/atoms/src/lib/required-input/required-input.component.ts
@@ -1,12 +1,43 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
+/**
+ * A reusable text field component that can be used to input text.
+ */
 @Component({
   selector: 'hra-required-input',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './required-input.component.html',
   styleUrls: ['./required-input.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RequiredInputComponent {}
+export class RequiredInputComponent {
+  /**
+   * The label to display for the text field.
+   */
+  @Input() label = '';
+
+  /**
+   * The error message to display if the input is invalid.
+   */
+  @Input() error = '';
+
+  /**
+   * Input Value  of required input component
+   */
+  @Input() inputValue = '';
+
+  /**
+   * An event emitter that emits the input value when it changes.
+   */
+  @Output() inputChanged = new EventEmitter<string>();
+
+  /**
+   * Form control of required input component
+   */
+  formControl = new FormControl(this.inputValue, Validators.required);
+}
