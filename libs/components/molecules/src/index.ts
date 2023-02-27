e55a425bfba5ee3996f3535c28a616be13de7220
Anita Badri Narayanan <97862753+anbadrin@users.noreply.github.com>
2023-02-27 14:23:59 -0500

    Initial code changes for ftu-screen-size-notice #64 (#77)
    * Initial code changes for ftu-screen-size-notice
    
    * Update in ftu-screen-size-notice
    
    * Fix package.json
    
    * pulled from dev
    
    * Fixing code smells
    
    * Updating contentUrl to stories
    
    * Updating css
    
    * Updating css
    
    * Adding outputs to buttons
    
    * Update screen-size-notice.component.ts
    
    ---------
    
    Co-authored-by: Daniel Bolin <axbolin@iu.edu>

--- libs/components/molecules/src/index.ts
+++ libs/components/molecules/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/landing-page-intro/landing-page-intro.component';
+export * from './lib/screen-size-notice/screen-size-notice.component';
 export * from './lib/source-list/source-list.component';
