320bc50def2e4eff78eefdfb4ca3937fa1f3ecf2
Aditya Rudrawar <30310911+adityarudrawar@users.noreply.github.com>
2023-02-27 16:34:40 -0500

    ftu-metrics (#71)
    * Intiliaze metrics component
    
    * Add logos
    
    * Add cell types svg
    
    * Add metrics component
    - Add input in stories
    -  Add HTML
    - Add SCSS
    - Pass the input to HTML
    
    * Remove extra div
    
    * Change padding and margin
    
    * Change px to rem
    
    * Add empty alt attribute to the img tag
    
    * Add test
    
    * Remove conflict
    
    * Address PR comments
    
    * Format

--- libs/components/molecules/src/index.ts
+++ libs/components/molecules/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/landing-page-intro/landing-page-intro.component';
 export * from './lib/screen-size-notice/screen-size-notice.component';
 export * from './lib/source-list/source-list.component';
+export * from './lib/metrics/metrics.component';
