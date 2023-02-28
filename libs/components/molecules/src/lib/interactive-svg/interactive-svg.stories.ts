4f805476b6e1f64e61a5f09df8703ef394904de8
edlu77 <eddd.lu@gmail.com>
2023-02-28 10:50:34 -0500

    Formatting

--- libs/components/molecules/src/lib/interactive-svg/interactive-svg.stories.ts
+++ libs/components/molecules/src/lib/interactive-svg/interactive-svg.stories.ts
@@ -11,5 +11,5 @@ const Template: Story<InteractiveSvgComponent> = (args) => ({ props: args });
 
 export const Default = Template.bind({});
 Default.args = {
-  fileName: 'alveoli'
+  fileName: 'alveoli',
 };
