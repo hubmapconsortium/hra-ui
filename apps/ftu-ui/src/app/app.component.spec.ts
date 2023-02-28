94900bbdb54d700b204d54412e1a89679d448004
edlu77 <eddd.lu@gmail.com>
2023-02-28 10:48:14 -0500

    Reset some files

--- apps/ftu-ui/src/app/app.component.spec.ts
+++ apps/ftu-ui/src/app/app.component.spec.ts
@@ -14,4 +14,17 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  it(`should have as title 'ftu-ui'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('ftu-ui');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome ftu-ui');
+  });
 });
--- apps/ftu-ui/src/app/app.module.ts
+++ apps/ftu-ui/src/app/app.module.ts
@@ -1,12 +1,11 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule ],
+  declarations: [AppComponent, NxWelcomeComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
