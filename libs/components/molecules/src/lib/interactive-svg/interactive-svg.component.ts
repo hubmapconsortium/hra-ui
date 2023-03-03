824dbb998c1ca8cd8eb3cdb3742fab8508de5467
edlu77 <eddd.lu@gmail.com>
2023-03-03 02:43:09 -0500

    Refactor hovering logic

--- libs/components/molecules/src/lib/interactive-svg/interactive-svg.component.ts
+++ libs/components/molecules/src/lib/interactive-svg/interactive-svg.component.ts
@@ -1,17 +1,30 @@
+import { OverlayModule } from '@angular/cdk/overlay';
 import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   EventEmitter,
+  inject,
   Input,
   OnDestroy,
   Output,
+  Renderer2,
   ViewEncapsulation,
 } from '@angular/core';
 import { InlineSVGModule, SVGScriptEvalMode } from 'ng-inline-svg-2';
-import { fromEvent, Subject, takeUntil, tap } from 'rxjs';
+import { BehaviorSubject, debounce, fromEventPattern, map, Observable, Subject, takeUntil, timer } from 'rxjs';
+import { NodeEventHandler } from 'rxjs/internal/observable/fromEvent';
 
 import { svgDataSet, SvgNodeData } from './svg-models';
+
+const HOVER_DELAY = 300;
+
+export interface NodeTooltipData {
+  /** Node reference */
+  node: SvgNodeData;
+  /** Center point of hovered node in screen coordinates */
+  origin: { x: number; y: number };
+}
 
 /**
  * Interactive SVG component
@@ -19,7 +32,7 @@ import { svgDataSet, SvgNodeData } from './svg-models';
 @Component({
   selector: 'hra-interactive-svg',
   standalone: true,
-  imports: [CommonModule, InlineSVGModule],
+  imports: [CommonModule, InlineSVGModule, OverlayModule],
   templateUrl: './interactive-svg.component.html',
   styleUrls: ['./interactive-svg.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -38,8 +51,16 @@ export class InteractiveSvgComponent implements OnDestroy {
   /** SVG script eval mode */
   readonly scriptEvalMode = SVGScriptEvalMode.NEVER;
 
+  private readonly renderer = inject(Renderer2);
+
   /** Destroys */
   private destroy$ = new Subject<void>();
+
+  /** Observable of node hover data or undefined when there is no active hover */
+  readonly nodeHoverr = new BehaviorSubject<NodeTooltipData | undefined>(undefined);
+
+  /** Observable of node hover with a timer */
+  readonly nodeHover$ = this.nodeHoverr.pipe(debounce((event) => timer(event ? HOVER_DELAY : 0)));
 
   /**
    * Clears observables on destroy
@@ -57,7 +78,7 @@ export class InteractiveSvgComponent implements OnDestroy {
     const crosswalkEl = el.querySelector('[id^="Crosswalk"]');
     if (crosswalkEl) {
       // Move to front (i.e. last child in svg)
-      el.appendChild(crosswalkEl);
+      this.renderer.appendChild(el, crosswalkEl);
       this.attachCrosswalkHover(crosswalkEl);
     }
   }
@@ -67,18 +88,40 @@ export class InteractiveSvgComponent implements OnDestroy {
    * @param el element
    */
   private attachCrosswalkHover(el: Element): void {
-    fromEvent(el, 'mouseover')
-      .pipe(takeUntil(this.destroy$), tap(this.onCrosswalkHover.bind(this)))
-      .subscribe();
+    this.attachEvent(el, 'mouseover').subscribe(this.onCrosswalkHover.bind(this));
+    this.attachEvent(el, 'mouseout')
+      .pipe(map(() => undefined))
+      .subscribe(this.nodeHoverr); // TODO check that mouseout is the correct event
   }
 
   /**
    * Finds matching node in data from a hovered element
    * @param ev
    */
-  private onCrosswalkHover(ev: Event): void {
+  private onCrosswalkHover(ev: MouseEvent): void {
     const target = ev.target as SVGElement;
-    this.findHoverMatch(target);
+    const data = this.findCrosswalkHoverTargetData(target);
+    if (data) {
+      this.nodeHover.emit(data.node_name);
+      this.nodeHoverr.next({
+        node: data,
+        origin: {
+          x: 0,
+          y: 0,
+        },
+      });
+    }
+  }
+
+  private findCrosswalkHoverTargetData(target: SVGElement): SvgNodeData | undefined {
+    let id = target.id;
+    if (!id) {
+      const parent = target.parentElement as HTMLElement;
+      const index = Array.from(parent.children).indexOf(target);
+      id = `${parent.id}_${index + 1}`;
+    }
+    id = this.decodeId(id).toLowerCase();
+    return this.svgNodeData.find((data) => data.node_name.toLowerCase() === id);
   }
 
   /**
@@ -90,29 +133,15 @@ export class InteractiveSvgComponent implements OnDestroy {
     this.destroy$ = new Subject();
   }
 
-  /**
-   * Checks for nodes on mouse hover and emits the matching node from mapping file
-   * @param target target element
-   */
-  findHoverMatch(target: SVGElement): void {
-    const targetId = target.id;
+  private decodeId(id: string): string {
+    const replacer = (_match: string, hex: string) => String.fromCharCode(Number.parseInt(hex, 16));
+    return id.replace(/_x([\da-f]+)_/gi, replacer);
+  }
 
-    const parentElement = target.parentElement;
-    const parentId = parentElement ? parentElement.id : '';
-    const parsedParentId = parentId.split('x5F_').join('');
-    const parentNodeMatch = this.svgNodeData.find(
-      (entry) =>
-        parsedParentId === entry['node_name'] ||
-        parsedParentId.includes(entry['node_name']) ||
-        entry['node_name'].includes(parsedParentId)
-    );
-
-    if (targetId) {
-      const parsedTargetId = targetId.split('x5F_').join('');
-      const targetNodeMatch = this.svgNodeData.find((entry) => parsedTargetId === entry['node_name']);
-      this.nodeHover.emit(targetNodeMatch?.node_name);
-    } else if (parsedParentId !== '' && parentNodeMatch) {
-      this.nodeHover.emit(parentNodeMatch.node_name);
-    }
+  private attachEvent<K extends keyof SVGElementEventMap>(el: Element, event: K): Observable<SVGElementEventMap[K]> {
+    const { renderer, destroy$ } = this;
+    const add = (handler: NodeEventHandler) => renderer.listen(el, event, handler);
+    const remove = (_handler: NodeEventHandler, unlisten: () => void) => unlisten();
+    return fromEventPattern<SVGElementEventMap[K]>(add, remove).pipe(takeUntil(destroy$));
   }
 }
--- libs/components/molecules/src/lib/interactive-svg/interactive-svg.stories.ts
+++ libs/components/molecules/src/lib/interactive-svg/interactive-svg.stories.ts
@@ -11,5 +11,5 @@ const Template: Story<InteractiveSvgComponent> = (args) => ({ props: args });
 
 export const Default = Template.bind({});
 Default.args = {
-  url: 'https://hubmapconsortium.github.io/ccf-releases/v1.3/2d-ftu/2d-ftu-lung-pulmonary-alveolus.svg',
+  url: 'assets/2d-ftu/2d-ftu-kidney-descending-thin-loop-of-henle.svg',
 };
