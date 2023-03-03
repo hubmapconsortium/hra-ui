76b7061b5ff586bc434a45a8e2a50b27e4772179
axdanbol <axdanbol@users.noreply.github.com>
2023-03-03 18:25:54 +0000

    Staging deploy -- 60c96c6b422c1e5102efa1578935d3d9b790b149

---
+++ compodoc/components-atoms/js/menu-wc.js
@@ -0,0 +1,106 @@
+'use strict';
+
+customElements.define('compodoc-menu', class extends HTMLElement {
+    constructor() {
+        super();
+        this.isNormalMode = this.getAttribute('mode') === 'normal';
+    }
+
+    connectedCallback() {
+        this.render(this.isNormalMode);
+    }
+
+    render(isNormalMode) {
+        let tp = lithtml.html(`
+        <nav>
+            <ul class="list">
+                <li class="title">
+                    <a href="index.html" data-type="index-link">components-atoms</a>
+                </li>
+
+                <li class="divider"></li>
+                ${ isNormalMode ? `<div id="book-search-input" role="search"><input type="text" placeholder="Type to search"></div>` : '' }
+                <li class="chapter">
+                    <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
+                    <ul class="links">
+                        <li class="link">
+                            <a href="overview.html" data-type="chapter-link">
+                                <span class="icon ion-ios-keypad"></span>Overview
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="index.html" data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>README
+                            </a>
+                        </li>
+                                <li class="link">
+                                    <a href="dependencies.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-list"></span>Dependencies
+                                    </a>
+                                </li>
+                                <li class="link">
+                                    <a href="properties.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-apps"></span>Properties
+                                    </a>
+                                </li>
+                    </ul>
+                </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#components-links"' :
+                            'data-target="#xs-components-links"' }>
+                            <span class="icon ion-md-cog"></span>
+                            <span>Components</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="components-links"' : 'id="xs-components-links"' }>
+                            <li class="link">
+                                <a href="components/EmailInputComponent.html" data-type="entity-link" >EmailInputComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/EmptyBiomarkerComponent.html" data-type="entity-link" >EmptyBiomarkerComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/GradientLegendComponent.html" data-type="entity-link" >GradientLegendComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/LabelBoxComponent.html" data-type="entity-link" >LabelBoxComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/RequiredInputComponent.html" data-type="entity-link" >RequiredInputComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/RequiredTextboxComponent.html" data-type="entity-link" >RequiredTextboxComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/SizeLegendComponent.html" data-type="entity-link" >SizeLegendComponent</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#interfaces-links"' :
+                            'data-target="#xs-interfaces-links"' }>
+                            <span class="icon ion-md-information-circle-outline"></span>
+                            <span>Interfaces</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
+                            <li class="link">
+                                <a href="interfaces/GradientPoint.html" data-type="entity-link" >GradientPoint</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/SizeLegend.html" data-type="entity-link" >SizeLegend</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="divider"></li>
+                    <li class="copyright">
+                        Documentation generated using <a href="https://compodoc.app/" target="_blank">
+                            <img data-src="images/compodoc-vectorise.png" class="img-responsive" data-type="compodoc-logo">
+                        </a>
+                    </li>
+            </ul>
+        </nav>
+        `);
+        this.innerHTML = tp.strings;
+    }
+});
