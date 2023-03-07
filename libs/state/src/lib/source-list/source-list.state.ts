15477f8c4c9e6f6fed922f5ab670d19f156bae7a
Sumedh Salvi <sumedhsalvi25@gmail.com>
2023-03-06 19:04:08 -0500

    add source list action and initialized interface model

--- libs/state/src/lib/source-list/source-list.state.ts
+++ libs/state/src/lib/source-list/source-list.state.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
 import { State } from '@ngxs/store';
 
+export interface SourceListModel {
+  title: string;
+  link: string;
+}
+
 /** State handling source list data */
 @State<SourceListModel>({
   name: 'sourceList',
   defaults: {
-    sourceList: [],
+    title: '',
+    link: '',
   },
 })
 @Injectable()
