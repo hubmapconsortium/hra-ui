6255b2b79c02f9a92cdad5218c744d39dce83819
jagrut-18 <jagrutchaudhari18@gmail.com>
2023-03-07 16:04:31 -0500

    update test case

--- libs/components/molecules/src/lib/fullscreen/fullscreen-container.component.spec.ts
+++ libs/components/molecules/src/lib/fullscreen/fullscreen-container.component.spec.ts
@@ -1,31 +1,38 @@
+import { Component, Input } from '@angular/core';
 import { Shallow } from 'shallow-render';
+import { FullscreenContainerComponent } from './fullscreen-container.component';
 import { FullscreenContentComponent } from './fullscreen-content.component';
 
-import { FullscreenContainerComponent } from './fullscreen-container.component';
-import { NgModule } from '@angular/core';
-
-@NgModule({
-  imports: [FullscreenContentComponent],
-  exports: [FullscreenContentComponent],
+@Component({
+  selector: 'hra-test',
+  standalone: true,
+  imports: [FullscreenContainerComponent, FullscreenContentComponent],
+  template: `
+    <hra-fullscreen-container [fullscreen]="fullscreen">
+      Some content
+      <hra-fullscreen-content> Other content </hra-fullscreen-content>
+    </hra-fullscreen-container>
+  `,
 })
-class FullscreenTestModule {}
+class TestComponent {
+  @Input() fullscreen = false;
+}
 
 describe('FullscreenContainerComponent', () => {
-  let shallow: Shallow<FullscreenContainerComponent>;
+  let shallow: Shallow<TestComponent>;
 
   beforeEach(() => {
-    shallow = new Shallow(FullscreenContainerComponent).import(FullscreenTestModule);
+    shallow = new Shallow(TestComponent).dontMock(FullscreenContainerComponent, FullscreenContentComponent);
   });
 
   it('should create', async () => {
-    const template = `
-      <hra-fullscreen-container>
-        Some content
-        <hra-fullscreen-content>
-          Other content
-        </hra-fullscreen-content>
-      </hra-fullscreen-container>
-    `;
-    await expect(shallow.render(template)).resolves.toBeDefined();
+    await expect(shallow.render()).resolves.toBeDefined();
+  });
+
+  it('should update fullscreen mode of child components', async () => {
+    const { findComponent } = await shallow.render({ bind: { fullscreen: true } });
+    const child = findComponent(FullscreenContentComponent);
+    expect(child).toHaveFoundOne();
+    expect(child.isFullScreen).toBeTruthy();
   });
 });
