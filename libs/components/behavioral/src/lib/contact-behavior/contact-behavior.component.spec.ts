e06452dcc5aa9c2d1110e4d629416e8950413f2f
Himanshu Joshi <hsjoshi28@gmail.com>
2023-03-07 16:01:55 -0500

    test cases

--- libs/components/behavioral/src/lib/contact-behavior/contact-behavior.component.spec.ts
+++ libs/components/behavioral/src/lib/contact-behavior/contact-behavior.component.spec.ts
@@ -1,22 +1,65 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { OverlayModule } from '@angular/cdk/overlay';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Store } from '@ngxs/store';
+import { mock } from 'jest-mock-extended';
+import { Shallow } from 'shallow-render';
 import { ContactBehaviorComponent } from './contact-behavior.component';
 
 describe('ContactBehaviorComponent', () => {
-  let component: ContactBehaviorComponent;
-  let fixture: ComponentFixture<ContactBehaviorComponent>;
+  const testEmail = 'example@domain.com';
+  const testContactData = {
+    email: testEmail,
+    subject: 'Test Subject',
+    message: 'Test Message',
+  };
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ContactBehaviorComponent],
-    }).compileComponents();
+  const selfRef = mock<MatDialogRef<void>>();
+  const postRef = mock<MatDialogRef<void>>();
+  const dialog = mock<MatDialog>();
+  dialog.open.mockReturnValue(postRef);
 
-    fixture = TestBed.createComponent(ContactBehaviorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  let shallow: Shallow<ContactBehaviorComponent>;
+
+  beforeEach(() => {
+    shallow = new Shallow(ContactBehaviorComponent)
+      .dontMock(OverlayModule)
+      .provideMock({ provide: Store, useValue: mock() })
+      .provideMock({ provide: MatDialog, useValue: dialog });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  afterEach(() => jest.clearAllMocks());
+
+  it('should create', async () => {
+    await expect(shallow.render()).resolves.toBeDefined();
+  });
+
+  describe('submit', () => {
+    it('should submit the message and open a dialog of acknowledgement', async () => {
+      const { instance } = await shallow.provideMock({ provide: MatDialogRef, useValue: selfRef }).render();
+      instance.submit(testContactData);
+      expect(dialog.open).toHaveBeenCalled();
+    });
+  });
+
+  describe('close', () => {
+    it('should close self if inside a dialog', async () => {
+      const { instance } = await shallow.provideMock({ provide: MatDialogRef, useValue: selfRef }).render();
+      instance.close();
+      expect(selfRef.close).toHaveBeenCalled();
+    });
+
+    it('should close the post dialog if open', async () => {
+      const { instance } = await shallow.render();
+      instance.submit(testContactData);
+      instance.close();
+      expect(postRef.close).toHaveBeenCalled();
+    });
+
+    it('should do nothing if not inside a dialog and post is not open', async () => {
+      const { instance } = await shallow.render();
+      instance.close();
+      expect(selfRef.close).not.toHaveBeenCalled();
+      expect(postRef.close).not.toHaveBeenCalled();
+    });
   });
 });
