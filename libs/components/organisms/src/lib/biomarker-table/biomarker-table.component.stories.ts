fd3c4cef4ab4c4e30710e7d5491ae358a8da46ef
Aditya Rudrawar <adirudrawar17@gmail.com>
2023-03-08 17:03:28 -0500

    Add base table component

--- libs/components/organisms/src/lib/biomarker-table/biomarker-table.component.stories.ts
+++ libs/components/organisms/src/lib/biomarker-table/biomarker-table.component.stories.ts
@@ -1,5 +1,5 @@
 import { moduleMetadata, Story, Meta } from '@storybook/angular';
-import { BiomarkerTableComponent } from './biomarker-table.component';
+import { BiomarkerTableComponent, DataCell } from './biomarker-table.component';
 
 export default {
   title: 'BiomarkerTableComponent',
@@ -9,11 +9,26 @@ export default {
       imports: [],
     }),
   ],
-} as Meta<BiomarkerTableComponent>;
+} as Meta<BiomarkerTableComponent<DataCell>>;
 
-const Template: Story<BiomarkerTableComponent> = (args: BiomarkerTableComponent) => ({
+const Template: Story<BiomarkerTableComponent<DataCell>> = (args: BiomarkerTableComponent<DataCell>) => ({
   props: args,
 });
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  columns: [
+    { label: 'Cell Type', dataType: 'text' },
+    { label: 'Cell Count', dataType: 'numeric' },
+    { label: 'RGMB', dataType: 'object' },
+    { label: 'SOX9', dataType: 'object' },
+    { label: 'CD44', dataType: 'object' },
+    { label: 'LGR5', dataType: 'object' },
+  ],
+  data: [
+    ['absorptive cell', 2764, undefined, undefined],
+    ['enteroendocrine cell', 17, undefined, undefined],
+    ['epithelial stem cell', 187, 'object', 'object'],
+    ['goblet cell', undefined, undefined, undefined],
+  ],
+};
--- libs/components/organisms/src/lib/biomarker-table/biomarker-table.component.ts
+++ libs/components/organisms/src/lib/biomarker-table/biomarker-table.component.ts
@@ -1,12 +1,47 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+
+export type DataType = 'text' | 'numeric' | 'object';
+
+export interface Column {
+  label: string;
+  dataType: DataType;
+}
+
+export interface DataCell {
+  name: string;
+  uberonId: string;
+  numberOfDatasets: number;
+  cellTypeName: string;
+  clId: string;
+  numberOfCells: string;
+  geneName: string;
+  hgncId: string;
+  meanExpressionValue: number;
+}
 
 @Component({
   selector: 'hra-biomarker-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './biomarker-table.component.html',
   styleUrls: ['./biomarker-table.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BiomarkerTableComponent {}
+export class BiomarkerTableComponent<T extends DataCell> implements OnChanges {
+  @Input() columns: Column[] = [];
+  @Input() data: (string | number | T | undefined)[][] = [];
+
+  get columnIds(): string[] {
+    return this.columns.map((column) => column.label);
+  }
+
+  readonly dataSource = new MatTableDataSource<unknown[]>([]);
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if ('data' in changes) {
+      this.dataSource.data = this.data;
+    }
+  }
+}
