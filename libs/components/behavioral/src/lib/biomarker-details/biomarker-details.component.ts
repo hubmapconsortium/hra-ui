976bcb9c073aa91e5fd4161597d9151acd9068d0
anbadrin <anita1397@gmail.com>
2023-03-09 14:44:22 -0500

    Documentation coverage and code smell fixes

--- libs/components/behavioral/src/lib/biomarker-details/biomarker-details.component.ts
+++ libs/components/behavioral/src/lib/biomarker-details/biomarker-details.component.ts
@@ -1,17 +1,22 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BiomarkerTableDataCardComponent } from '@hra-ui/components/molecules';
-import { LabelBoxComponent } from '@hra-ui/components/atoms';
-import { GradientLegendComponent } from '@hra-ui/components/atoms';
-import { SizeLegendComponent } from '@hra-ui/components/atoms';
-import { SourceListComponent } from '@hra-ui/components/molecules';
-import { DataItem } from '@hra-ui/components/molecules';
-import { GradientPoint } from '@hra-ui/components/atoms';
-import { SizeLegend } from '@hra-ui/components/atoms';
-import { SourceListItem } from '@hra-ui/components/molecules';
+import {
+  BiomarkerTableDataCardComponent,
+  SourceListComponent,
+  DataItem,
+  SourceListItem,
+} from '@hra-ui/components/molecules';
+import {
+  LabelBoxComponent,
+  GradientLegendComponent,
+  SizeLegendComponent,
+  GradientPoint,
+  SizeLegend,
+} from '@hra-ui/components/atoms';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
 
+/** The component displays the biomarker details which includes the details, gradient legends, size legends and source lists*/
 @Component({
   selector: 'ftu-biomarker-details',
   standalone: true,
