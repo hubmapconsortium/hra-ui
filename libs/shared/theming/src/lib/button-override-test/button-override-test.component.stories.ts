6f9f2b68eb5287222f2868014bce26c5b35e1256
rchavan10 <rajchavan1099@gmail.com>
2023-03-09 11:29:01 -0500

    overrides for button

--- libs/shared/theming/src/lib/button-override-test/button-override-test.component.stories.ts
+++ libs/shared/theming/src/lib/button-override-test/button-override-test.component.stories.ts
@@ -25,15 +25,9 @@ const Template =
   });
 
 //Basic button
-export const Basic = Template(`<button mat-button color="primary">Test</button>`);
-export const Basic_Secondary = Template(`<button mat-stroked-button color="primary">Test</button>`);
-//export const Raised = Template(`<button mat-raised-button>Test</button>`)
-export const Link = Template(`<a mat-button>Test</a>`);
-//export const Disabled = Template(`<button mat-button disabled>Test</button>`)
-
-//On Hover
-export const Hover = Template(`<button mat-button [ngClass]=" 'hover' " color="primary">Test</button>`);
-export const Hover_Secondary = Template(
-  `<button mat-stroked-button [ngClass]=" 'hover' " color="primary">Test</button>`
-);
-export const Hover_Link = Template(`<a mat-button [ngClass]= " 'hover-link' ">Test</a>`);
+export const Basic = Template(`<button mat-button [color]="palette" [disabled]="disabled">Test</button>`);
+Basic.args = {
+  palette: 'primary',
+  disabled: false,
+};
+//export const Link = Template(`<a mat-button>Test</a>`);
