b536f51ed89e5ed6adbe45ea890219392862df3c
anbadrin <anita1397@gmail.com>
2023-03-09 16:21:08 -0500

    Fixing sonar issue-data duplication

--- libs/components/behavioral/src/lib/biomarker-details/biomarker-details.component.stories.ts
+++ libs/components/behavioral/src/lib/biomarker-details/biomarker-details.component.stories.ts
@@ -1,6 +1,11 @@
 import { moduleMetadata, Story, Meta } from '@storybook/angular';
 import { BiomarkerDetailsComponent } from './biomarker-details.component';
 import { MatIconModule } from '@angular/material/icon';
+import { DataItem } from '@hra-ui/components/molecules';
+
+function createDataItem(label: string, value: string): DataItem {
+  return { label, value };
+}
 
 export default {
   title: 'BiomarkerDetailsComponent',
@@ -16,49 +21,14 @@ const Template: Story<BiomarkerDetailsComponent> = (args: BiomarkerDetailsCompon
   props: args,
 });
 
-export const data = [
+const data = [
   [
-    {
-      label: 'New Functional Tissue Unit Name',
-      value: 'New Crypt of Lieberkuhn',
-    },
-    {
-      label: 'New Uberon ID',
-      value: 'UBERON1:0011185',
-    },
-    {
-      label: '#Datasets1',
-      value: '4',
-    },
+    createDataItem('Functional Tissue Unit Name', 'Liver lobule'),
+    createDataItem('New Uberon ID', 'UBERON1:0022285'),
+    createDataItem('#Datasets1', '4'),
   ],
-  [
-    {
-      label: 'New Cell Type Name',
-      value: 'epithelial stem cell 1',
-    },
-    {
-      label: 'CL ID',
-      value: 'CL:0009017',
-    },
-    {
-      label: 'Number of Cells',
-      value: '186',
-    },
-  ],
-  [
-    {
-      label: 'New Gene Name',
-      value: 'RGMB',
-    },
-    {
-      label: 'HGNC ID',
-      value: 'HGNC:26897',
-    },
-    {
-      label: 'New Mean Expression Value',
-      value: '0.0643',
-    },
-  ],
+  [createDataItem('Cell Type Name', 'absorptive cell'), createDataItem('CL ID', 'CL:1119017')],
+  [createDataItem('New Gene Name', 'RGMB'), createDataItem('Mean Expression Value', '0.0643')],
 ];
 
 export const Primary = Template.bind({});
