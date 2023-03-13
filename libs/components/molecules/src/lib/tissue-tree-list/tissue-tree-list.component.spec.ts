120e9b55511dfd1b79f6d5ae4efd112c08e39616
tanukansalgit <tanukansal167@gmail.com>
2023-03-13 16:52:46 -0400

    100% test cases coverage

--- libs/components/molecules/src/lib/tissue-tree-list/tissue-tree-list.component.spec.ts
+++ libs/components/molecules/src/lib/tissue-tree-list/tissue-tree-list.component.spec.ts
@@ -6,6 +6,25 @@ describe('TissueTreeListComponent', () => {
   let component: TissueTreeListComponent;
   let fixture: ComponentFixture<TissueTreeListComponent>;
 
+  const treeList = [
+    {
+      name: 'Kidney',
+      children: [
+        { name: 'Ascending thin limb' },
+        { name: 'Cortical collecting duct' },
+        { name: 'Collecting duct(inner medulla)' },
+      ],
+    },
+    {
+      name: 'Large Intestine',
+      children: [{ name: 'Crypt of Lieberkuhn' }],
+    },
+    {
+      name: 'Liver',
+      children: [{ name: 'Liver lobule' }],
+    },
+  ];
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [TissueTreeListComponent],
@@ -18,5 +37,45 @@ describe('TissueTreeListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('check if child for the item exists', () => {
+    const component = new TissueTreeListComponent();
+    component.treeList = treeList;
+    expect(component.hasChild(1, treeList[0])).toBe(true);
+  });
+
+  it('select item', () => {
+    const component = new TissueTreeListComponent();
+    const targetItem = treeList[0]['children'][0];
+    component.treeList = treeList;
+    component.selectItem(targetItem);
+    expect(component.selectedItem).toBe(targetItem);
+  });
+
+  it('check if item is selected for false input', () => {
+    const component = new TissueTreeListComponent();
+    const selectedItem = treeList[0]['children'][0];
+    component.treeList = treeList;
+    component.selectItem(selectedItem);
+
+    const isItemSelected = component.isSelected(treeList[0]);
+    expect(isItemSelected).toBe(false);
+  });
+
+  it('check if item is selected for correct input', () => {
+    const component = new TissueTreeListComponent();
+    const selectedItem = treeList[0]['children'][0];
+    component.treeList = treeList;
+    component.selectItem(selectedItem);
+
+    const isItemSelected = component.isSelected(selectedItem);
+    expect(isItemSelected).toBe(true);
+  });
+
+  it('unselects the item', () => {
+    const component = new TissueTreeListComponent();
+    component.unselectItem();
+    expect(component.selectedItem).toBe(undefined);
   });
 });
