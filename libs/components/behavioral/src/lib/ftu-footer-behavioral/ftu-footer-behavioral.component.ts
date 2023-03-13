c2a298cc010a24f777e6903edf317f3966a18667
Hanuhitha Dondapati <hanuhitha.99@gmail.com>
2023-03-13 19:25:57 -0400

    adding footer component

--- libs/components/behavioral/src/lib/ftu-footer-behavioral/ftu-footer-behavioral.component.ts
+++ libs/components/behavioral/src/lib/ftu-footer-behavioral/ftu-footer-behavioral.component.ts
@@ -1,12 +1,24 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DownloadFormat, FooterComponent } from '@hra-ui/components/molecules';
 
 @Component({
   selector: 'ftu-ftu-footer-behavioral',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FooterComponent],
   templateUrl: './ftu-footer-behavioral.component.html',
   styleUrls: ['./ftu-footer-behavioral.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FtuFooterBehavioralComponent {}
+export class FtuFooterBehavioralComponent<T extends DownloadFormat = DownloadFormat> {
+  /** Input for product logo URL to displayed on the left side. */
+  @Input() productLogoUrl = '';
+
+  /** Input for product title to displayed on the left side. */
+  @Input() productTitle = '';
+
+  /** Different download formats options displayed to the user */
+  @Input() downloadFormats: T[] = [];
+
+  @Input() size: any;
+}
--- libs/components/behavioral/src/lib/ftu-footer-behavioral/ftu-footer-behavioral.stories.ts
+++ libs/components/behavioral/src/lib/ftu-footer-behavioral/ftu-footer-behavioral.stories.ts
@@ -6,9 +6,27 @@ export default {
   component: FtuFooterBehavioralComponent,
 } as Meta;
 
+const DownloadFormat = [
+  { label: 'Download XLXS' },
+  { label: 'Download JSON-LD' },
+  { label: 'Download PDF' },
+  { label: 'Download AI' },
+  { label: 'Download PNG' },
+];
 const Template: Story<FtuFooterBehavioralComponent> = (args: FtuFooterBehavioralComponent) => ({
   props: args,
 });
 
-export const Default = Template.bind({});
-Default.args = {};
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  downloadFormats: DownloadFormat,
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  productLogoUrl: 'assets/icons/logo-icon.svg',
+  productTitle: 'Human Reference Atlas',
+  downloadFormats: DownloadFormat,
+};
