8a11ddf4438c9905c6b287947662b85b97e70c7e
Himanshu Joshi <hsjoshi28@gmail.com>
2023-03-14 05:27:00 -0400

    updated test cases

--- libs/components/behavioral/src/lib/header-behavior/header-behavior.component.spec.ts
+++ libs/components/behavioral/src/lib/header-behavior/header-behavior.component.spec.ts
@@ -1,28 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { dispatch, selectQuerySnapshot } from '@hra-ui/cdk/injectors';
+import { Shallow } from 'shallow-render';
 
 import { HeaderBehaviorComponent } from './header-behavior.component';
 
 jest.mock('@hra-ui/cdk/injectors');
 
 describe('HeaderBehaviorComponent', () => {
-  let component: HeaderBehaviorComponent;
-  let fixture: ComponentFixture<HeaderBehaviorComponent>;
+  let shallow: Shallow<HeaderBehaviorComponent>;
 
   jest.mocked(selectQuerySnapshot).mockReturnValue(jest.fn());
   jest.mocked(dispatch).mockReturnValue(jest.fn());
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HeaderBehaviorComponent],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(HeaderBehaviorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    shallow = new Shallow(HeaderBehaviorComponent);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create', async () => {
+    await expect(shallow.render()).resolves.toBeDefined();
   });
 });
