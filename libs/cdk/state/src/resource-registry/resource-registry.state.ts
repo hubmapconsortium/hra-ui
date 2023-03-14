6731ff9445d6390245c0f5f42d823f1450cfa5ba
Daniel Bolin <axbolin@iu.edu>
2023-03-14 16:52:18 -0400

    Add docs

--- libs/cdk/state/src/resource-registry/resource-registry.state.ts
+++ libs/cdk/state/src/resource-registry/resource-registry.state.ts
@@ -21,16 +21,32 @@ export class ResourceRegistryState {
   /** Http service for resource loading */
   private readonly http = inject(HttpClient);
 
+  /**
+   * Add a single entry
+   * @param ctx State context
+   * @param action Action with id and entry to add
+   */
   @Action(Add)
   addOne(ctx: ResourceRegistryContext, { id, entry }: Add): void {
     this.addMany(ctx, new AddMany({ [id]: entry }));
   }
 
+  /**
+   * Add multiple entries
+   * @param ctx State context
+   * @param action Action with entries to add
+   */
   @Action(AddMany)
   addMany(ctx: ResourceRegistryContext, { entries }: AddMany): void {
     ctx.patchState(entries);
   }
 
+  /**
+   * Parse and add entries from yaml
+   * @param ctx State context
+   * @param action Action with raw yaml data
+   * @param filename Optional url/filename from which the data was loaded (for improved error messages)
+   */
   @Action(AddFromYaml)
   addYaml(ctx: ResourceRegistryContext, { yaml }: AddFromYaml, filename?: string): void {
     const data = load(yaml, { filename });
@@ -38,6 +54,12 @@ export class ResourceRegistryState {
     this.addMany(ctx, new AddMany(entries));
   }
 
+  /**
+   * Load and add entries from an external yaml file
+   * @param ctx State context
+   * @param action Action with the external file url
+   * @returns An observable that completes when the entries has been added
+   */
   @Action(LoadFromYaml)
   loadYaml(ctx: ResourceRegistryContext, { url }: LoadFromYaml): Observable<void> {
     return this.http
@@ -45,6 +67,12 @@ export class ResourceRegistryState {
       .pipe(map((data) => this.addYaml(ctx, new AddFromYaml(data), url)));
   }
 
+  /**
+   * Adds a markdown entry with content loaded from an external file
+   * @param ctx State context
+   * @param action Action with id and url to the external markdown
+   * @returns An observable that completes when the entry has been added
+   */
   @Action(LoadMarkdown)
   loadMarkdown(ctx: ResourceRegistryContext, { id, url }: LoadMarkdown): Observable<void> {
     return this.http.get(url, { responseType: 'text' }).pipe(
