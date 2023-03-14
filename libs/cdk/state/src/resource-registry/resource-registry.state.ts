d3eb0fe00364216d44eeb332badbf4a3cfc74bb0
Daniel Bolin <axbolin@iu.edu>
2023-03-14 12:44:31 -0400

    Refactor resource registry

--- libs/cdk/state/src/resource-registry/resource-registry.state.ts
+++ libs/cdk/state/src/resource-registry/resource-registry.state.ts
@@ -1,57 +1,55 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Action, State } from '@ngxs/store';
-import { Observable, tap } from 'rxjs';
-import { AddResource, LoadMarkdown } from './resource-registry.actions';
+import { load } from 'js-yaml';
+import { map, Observable } from 'rxjs';
+import { Add, AddFromYaml, AddMany, LoadFromYaml, LoadMarkdown } from './resource-registry.actions';
 import {
-  ResourceEntry,
-  ResourceId,
   ResourceRegistryContext,
   ResourceRegistryModel,
   ResourceType,
+  RESOURCE_REGISTRY_SCHEMA,
 } from './resource-registry.model';
 
-/**
- * Resource registry state
- */
+/** State keeping track of global resources */
 @State<ResourceRegistryModel>({
   name: 'resourceRegistry',
   defaults: {},
 })
 @Injectable()
 export class ResourceRegistryState {
-  /** injects httpClient */
+  /** Http service for resource loading */
   private readonly http = inject(HttpClient);
 
-  /**
-   * Action for adding a resource to state
-   * @param ctx State context
-   * @param param1 addResource action object containing id and entry
-   */
-  @Action(AddResource)
-  addResource(ctx: ResourceRegistryContext, { id, entry }: AddResource): void {
-    this.addResourceEntry(ctx, id, entry);
+  @Action(Add)
+  addOne(ctx: ResourceRegistryContext, { id, entry }: Add): void {
+    this.addMany(ctx, new AddMany({ [id]: entry }));
   }
 
-  /**
-   * action for loading markdown
-   * @param ctx State context
-   * @param param1 loadMarkdown object containing id and url
-   */
-  @Action(LoadMarkdown)
-  loadMarkdown(ctx: ResourceRegistryContext, { id, url }: LoadMarkdown): Observable<string> {
+  @Action(AddMany)
+  addMany(ctx: ResourceRegistryContext, { entries }: AddMany): void {
+    ctx.patchState(entries);
+  }
+
+  @Action(AddFromYaml)
+  addYaml(ctx: ResourceRegistryContext, { yaml }: AddFromYaml, filename?: string): void {
+    const data = load(yaml, { filename });
+    const entries = RESOURCE_REGISTRY_SCHEMA.parse(data);
+    this.addMany(ctx, new AddMany(entries));
+  }
+
+  @Action(LoadFromYaml)
+  loadYaml(ctx: ResourceRegistryContext, { url }: LoadFromYaml): Observable<void> {
     return this.http
       .get(url, { responseType: 'text' })
-      .pipe(tap((data) => this.addResourceEntry(ctx, id, { type: ResourceType.Markdown, markdown: data })));
+      .pipe(map((data) => this.addYaml(ctx, new AddFromYaml(data), url)));
   }
 
-  /**
-   * method to add a resource entry to the state
-   * @param param0 State context
-   * @param id id of the resource
-   * @param entry resource entry to be added
-   */
-  private addResourceEntry({ patchState }: ResourceRegistryContext, id: ResourceId, entry: ResourceEntry): void {
-    patchState({ [id]: entry });
+  @Action(LoadMarkdown)
+  loadMarkdown(ctx: ResourceRegistryContext, { id, url }: LoadMarkdown): Observable<void> {
+    return this.http.get(url, { responseType: 'text' }).pipe(
+      map((markdown) => new Add(id, { type: ResourceType.Markdown, markdown })),
+      map((action) => this.addOne(ctx, action))
+    );
   }
 }
