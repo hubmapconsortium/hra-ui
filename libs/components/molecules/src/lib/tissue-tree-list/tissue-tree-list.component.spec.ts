9b31c12248558abf9865dce8ebd2ba9402f8baca
tanukansalgit <tanukansal167@gmail.com>
2023-03-14 15:51:04 -0400

    unnecessary variables removed

--- libs/components/molecules/src/lib/tissue-tree-list/tissue-tree-list.component.spec.ts
+++ libs/components/molecules/src/lib/tissue-tree-list/tissue-tree-list.component.spec.ts
@@ -1,4 +1,3 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Shallow } from 'shallow-render';
 import { TissueTreeListComponent } from './tissue-tree-list.component';
 
