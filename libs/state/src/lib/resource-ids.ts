77444d3ae2c9b38aec09d71b347f2b394cdcfeb1
Daniel Bolin <axbolin@iu.edu>
2023-03-15 11:05:59 -0400

    Add resource ids

---
+++ libs/state/src/lib/resource-ids.ts
@@ -0,0 +1,4 @@
+import { createResourceId } from '@hra-ui/cdk/state';
+
+export const ProductTitle = createResourceId('ProductTitle');
+export const ProductLogoUrl = createResourceId('ProductLogoUrl');
