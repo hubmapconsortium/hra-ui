835fb531207f710d64cc83fb54e11c860b9f6236
Sai Kiran Jella <jsaikiran19@gmail.com>
2023-03-16 14:31:03 -0400

    fix test case

--- apps/ftu-ui/src/app/app.component.spec.ts
+++ apps/ftu-ui/src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
@@ -6,6 +7,7 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent, NxWelcomeComponent],
+      imports: [RouterModule],
     }).compileComponents();
   });
 
