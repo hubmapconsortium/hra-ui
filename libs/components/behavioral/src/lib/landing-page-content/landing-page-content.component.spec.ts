e20fa6f9fcd2c797552e3edc69b9bc275272edba
Sai Kiran Jella <jsaikiran19@gmail.com>
2023-03-16 11:02:51 -0400

    adds test for landing page behavior

--- libs/components/behavioral/src/lib/landing-page-content/landing-page-content.component.spec.ts
+++ libs/components/behavioral/src/lib/landing-page-content/landing-page-content.component.spec.ts
@@ -1,21 +1,35 @@
+import { dispatch, selectQuerySnapshot } from '@hra-ui/cdk/injectors';
 import { LandingPageIntroComponent } from '@hra-ui/components/molecules';
 import { Shallow } from 'shallow-render';
 
 import { LandingPageContentComponent } from './landing-page-content.component';
-
+jest.mock('@hra-ui/cdk/injectors');
 describe('LandingPageContentComponent', () => {
   let shallow: Shallow<LandingPageContentComponent>;
+  jest.mocked(selectQuerySnapshot).mockReturnValue(jest.fn());
+  jest.mocked(dispatch).mockReturnValue(jest.fn());
   beforeEach(async () => {
     shallow = new Shallow(LandingPageContentComponent);
   });
+  afterEach(() => jest.clearAllMocks());
 
   it('should create LandingPageContentComponent', async () => {
     await expect(shallow.render()).resolves.toBeDefined();
   });
 
   it('should call exploreFTU function on moreClick event emit', async () => {
-    const { findComponent } = await shallow.render();
+    const { instance, findComponent } = await shallow.render();
     const landingPageIntroComponent = findComponent(LandingPageIntroComponent);
+    jest.spyOn(instance, 'exploreFTU');
     landingPageIntroComponent.moreClick.emit();
+    expect(instance.exploreFTU).toHaveBeenCalled();
+  });
+
+  it('should get metricItems', async () => {
+    jest.mocked(selectQuerySnapshot).mockReturnValue(() => {
+      return { metrics: [{ type: 'metrics', text: 'test' }] };
+    });
+    const { instance } = await shallow.render();
+    expect(instance.metricItems).toEqual([{ type: 'metrics', text: 'test' }]);
   });
 });
