f5ba71ad4f37517d720a80f40adfc2151aa9c1ff
Aditya Rudrawar <adirudrawar17@gmail.com>
2023-03-17 12:32:48 -0400

    Remove placeholder

--- libs/components/organisms/src/index.ts
+++ libs/components/organisms/src/index.ts
@@ -1,5 +1,1 @@
-// Remove once other exports are available
-/** Placeholder to make build and tests pass until any actual library code is created */
-export const placeholder = undefined;
-
 export * from './lib/biomarker-table/biomarker-table.component';
