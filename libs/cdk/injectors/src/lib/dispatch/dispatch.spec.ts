2d08b5e83afe278cb3a8a0d46fb2fff242c50074
Daniel Bolin <axbolin@iu.edu>
2023-03-24 14:20:17 -0400

    Improve dispatch injectors

--- libs/cdk/injectors/src/lib/dispatch/dispatch.spec.ts
+++ libs/cdk/injectors/src/lib/dispatch/dispatch.spec.ts
@@ -1,20 +1,25 @@
 import { inject } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { mock, mockFn } from 'jest-mock-extended';
-import { dispatch } from './dispatch';
+import { lastValueFrom, of } from 'rxjs';
+import { dispatch, dispatch$ } from './dispatch';
 
 jest.mock('@angular/core');
 
-describe(dispatch, () => {
-  const actionInstance = { type: 'test' };
-  const actionType = mockFn().mockReturnValue(actionInstance);
-  const store = mock<Store>();
-  jest.mocked(inject).mockReturnValue(store);
+const boundArgs = ['q', {}, 'w'];
+const args = [1, 'a', true];
+const actionInstance = { type: 'test' };
+const actionType = mockFn().mockReturnValue(actionInstance);
+const store = mock<Store>();
+jest.mocked(inject).mockReturnValue(store);
+store.dispatch.mockReturnValue(of(undefined));
 
-  afterEach(() => jest.clearAllMocks());
+afterEach(() => jest.clearAllMocks());
+
+describe(dispatch, () => {
+  const fn = dispatch(actionType);
 
   it('should create a new action instance on each call', () => {
-    const fn = dispatch(actionType);
     fn();
     expect(actionType).toHaveBeenCalledTimes(1);
     fn();
@@ -22,20 +27,53 @@ describe(dispatch, () => {
   });
 
   it('should forward arguments to the action constructor', () => {
-    const args = [1, 'a', true];
-    const fn = dispatch(actionType);
     fn(...args);
     expect(actionType).toHaveBeenCalledWith(...args);
   });
 
+  it('should forward bound arguments to the action constructor', () => {
+    const fn2 = dispatch(actionType, ...boundArgs);
+    fn2(...args);
+    expect(actionType).toHaveBeenCalledWith(...boundArgs, ...args);
+  });
+
   it('should dispatch the action', () => {
-    const fn = dispatch(actionType);
     fn();
     expect(store.dispatch).toHaveBeenCalledWith(actionInstance);
   });
 
   it('should return the action instance', () => {
-    const fn = dispatch(actionType);
     expect(fn()).toBe(actionInstance);
+  });
+});
+
+describe(dispatch$, () => {
+  const fn = dispatch$(actionType);
+
+  it('should create a new action instance on each call', () => {
+    fn();
+    expect(actionType).toHaveBeenCalledTimes(1);
+    fn();
+    expect(actionType).toHaveBeenCalledTimes(2);
+  });
+
+  it('should forward arguments to the action constructor', () => {
+    fn(...args);
+    expect(actionType).toHaveBeenCalledWith(...args);
+  });
+
+  it('should forward bound arguments to the action constructor', () => {
+    const fn2 = dispatch$(actionType, ...boundArgs);
+    fn2(...args);
+    expect(actionType).toHaveBeenCalledWith(...boundArgs, ...args);
+  });
+
+  it('should dispatch the action', () => {
+    fn();
+    expect(store.dispatch).toHaveBeenCalledWith(actionInstance);
+  });
+
+  it('should return the action instance', async () => {
+    await expect(lastValueFrom(fn())).resolves.toBe(actionInstance);
   });
 });
--- libs/cdk/injectors/src/lib/dispatch/dispatch.ts
+++ libs/cdk/injectors/src/lib/dispatch/dispatch.ts
@@ -1,25 +1,41 @@
 import { inject } from '@angular/core';
-import type { AnyConstructor } from '@hra-ui/utils/types';
+import type { Any } from '@hra-ui/utils/types';
 import { Store } from '@ngxs/store';
-
-/**
- * Function type returned by `dispatch(actionType)`.
- * Accepts the same arguments as the passed action constructor and
- * returns an instance of the action.
- */
-export type Dispatcher<A extends AnyConstructor> = (...args: ConstructorParameters<A>) => InstanceType<A>;
+import { map, Observable } from 'rxjs';
 
 /**
  * Wraps an action constructor with automatic dispatching on each call to the function.
  *
- * @param actionType Action constructor to create new instances
+ * @param type Action constructor to create new instances
  * @returns A function that dispatches an action on the store each time it is called
  */
-export function dispatch<A extends AnyConstructor>(actionType: A): Dispatcher<A> {
+export function dispatch<A, BoundArgs extends Any[], DispatchArgs extends Any[]>(
+  type: new (...args: [...BoundArgs, ...DispatchArgs]) => A,
+  ...boundArgs: BoundArgs
+): (...args: DispatchArgs) => A {
   const store = inject(Store);
   return (...args) => {
-    const instance = new actionType(...args);
+    const instance = new type(...boundArgs, ...args);
     store.dispatch(instance);
     return instance;
+  };
+}
+
+/**
+ * Wraps an action constructor with automatic dispatching on each call to the function.
+ * Each call to the wrapper returns an observable that emits the action once the
+ * action dispatch has completed.
+ *
+ * @param type Action constructor to create new instances
+ * @returns A function that dispatches an action on the store each time it is called
+ */
+export function dispatch$<A, BoundArgs extends Any[], DispatchArgs extends Any[]>(
+  type: new (...args: [...BoundArgs, ...DispatchArgs]) => A,
+  ...boundArgs: BoundArgs
+): (...args: DispatchArgs) => Observable<A> {
+  const store = inject(Store);
+  return (...args) => {
+    const instance = new type(...boundArgs, ...args);
+    return store.dispatch(instance).pipe(map(() => instance));
   };
 }
