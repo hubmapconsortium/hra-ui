9e6a66917314e9975db68a12453d467ee10729c2
Sumedh Salvi <sumedhsalvi25@gmail.com>
2023-03-24 20:49:40 -0400

    Added product info inputs

--- libs/components/behavioral/src/lib/footer-behavior/footer-behavior.component.ts
+++ libs/components/behavioral/src/lib/footer-behavior/footer-behavior.component.ts
@@ -1,12 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { selectQuerySnapshot } from '@hra-ui/cdk/injectors';
+import { ResourceRegistrySelectors as RR } from '@hra-ui/cdk/state';
+import { ResourceIds as Ids } from '@hra-ui/state';
+import { FooterComponent } from '@hra-ui/components/molecules';
 
 @Component({
   selector: 'ftu-footer-behavior',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FooterComponent],
   templateUrl: './footer-behavior.component.html',
   styleUrls: ['./footer-behavior.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterBehaviorComponent {}
+export class FooterBehaviorComponent {
+  /** Input for product logo URL to displayed on the left side. */
+  readonly productLogoUrl = selectQuerySnapshot(RR.url, Ids.ProductLogoUrl);
+
+  /** Input for product title to displayed on the left side. */
+  readonly productTitle = selectQuerySnapshot(RR.anyText, Ids.ProductTitle);
+}
