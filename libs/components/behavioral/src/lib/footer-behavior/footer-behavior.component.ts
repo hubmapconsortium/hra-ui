3152f9541d603e7a78b92727067686f525fb3d6e
Sumedh Salvi <sumedhsalvi25@gmail.com>
2023-03-24 21:12:22 -0400

    contact modal behavior

--- libs/components/behavioral/src/lib/footer-behavior/footer-behavior.component.ts
+++ libs/components/behavioral/src/lib/footer-behavior/footer-behavior.component.ts
@@ -1,22 +1,37 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, TemplateRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { selectQuerySnapshot } from '@hra-ui/cdk/injectors';
 import { ResourceRegistrySelectors as RR } from '@hra-ui/cdk/state';
 import { ResourceIds as Ids } from '@hra-ui/state';
 import { FooterComponent } from '@hra-ui/components/molecules';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'ftu-footer-behavior',
   standalone: true,
-  imports: [CommonModule, FooterComponent],
+  imports: [CommonModule, MatDialogModule, FooterComponent],
   templateUrl: './footer-behavior.component.html',
   styleUrls: ['./footer-behavior.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FooterBehaviorComponent {
+  /** A template to post a message */
+  @ViewChild('postContact') readonly postContactTemplate!: TemplateRef<void>;
+
   /** Input for product logo URL to displayed on the left side. */
   readonly productLogoUrl = selectQuerySnapshot(RR.url, Ids.ProductLogoUrl);
 
   /** Input for product title to displayed on the left side. */
   readonly productTitle = selectQuerySnapshot(RR.anyText, Ids.ProductTitle);
+
+  /** A dialog box which shows contact modal after clicking on contact */
+  private readonly dialog = inject(MatDialog);
+
+  /** Dialog box which references the contact modal dialog box */
+  private postRef?: MatDialogRef<void>;
+
+  /** A function which opens the contact modal dialog box */
+  contact(): void {
+    this.postRef = this.dialog.open(this.postContactTemplate);
+  }
 }
