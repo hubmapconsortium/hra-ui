09099dd871f3a79eb901c33b542d2a89b7314b7b
jagrut-18 <jagrutchaudhari18@gmail.com>
2023-03-27 16:14:20 -0400

    add documentation

--- libs/state/src/lib/cell-summary/cell-summary.actions.ts
+++ libs/state/src/lib/cell-summary/cell-summary.actions.ts
@@ -5,21 +5,15 @@ import { CELL_SUMMARY_SCHEMA } from './cell-summary.model';
 /** Base action factory */
 const Action = ActionGroup('ResourceRegistry');
 
+/** Set data to the store */
 export class SetData extends Action('SetData') {
-  /**
-   * Set data to the store
-   * @param data array of objects
-   */
   constructor(readonly data: object) {
     super();
   }
 }
 
+/** Computes aggregrate for the data */
 export class ComputeAggregate extends Action('ComputeAggregate') {
-  /**
-   * Computes aggregrate for the data
-   * @param summaries array of the aggregate data
-   */
   constructor(readonly summaries: z.infer<typeof CELL_SUMMARY_SCHEMA>) {
     super();
   }
--- libs/state/src/lib/cell-summary/cell-summary.model.ts
+++ libs/state/src/lib/cell-summary/cell-summary.model.ts
@@ -5,11 +5,19 @@ export type CellSummary = z.infer<typeof CELL_SUMMARY_SCHEMA>;
 export type Aggregate = z.infer<typeof CELL_SUMMARY_AGGREGATE_SCHEMA>;
 export type AggregateRow = Aggregate[string]['rows'][number];
 
+/**
+ * The CellSummaryStateModel interface describes the overall shape of the state for this feature.
+ * It has two properties, summaries and aggregate, both of which are of their corresponding types.
+ */
 export interface CellSummaryStateModel {
   summaries: CellSummary;
   aggregate: Aggregate;
 }
 
+/**
+ * The CELL_SCHEMA has an object that contains an id and a label for the cell,
+ * another object that contains an id and a label for the biomarker, and a count and a percentage that are both numbers.
+ */
 export const CELL_SCHEMA = z.object({
   cell: z.object({
     id: z.string(),
@@ -23,6 +31,10 @@ export const CELL_SCHEMA = z.object({
   percentage: z.number(),
 });
 
+/**
+ * The CELL_SUMMARY_SCHEMA constant is a record of CellSummary objects,
+ * where each CellSummary object contains a label and an array of CELL_SCHEMA objects.
+ */
 export const CELL_SUMMARY_SCHEMA = z.record(
   z.object({
     label: z.string(),
@@ -30,6 +42,10 @@ export const CELL_SUMMARY_SCHEMA = z.record(
   })
 );
 
+/**
+ * The CELL_SUMMARY_AGGREGATE_SCHEMA is a record of Aggregate objects, where each Aggregate object contains a label,
+ * an array of columns that are strings, and an array of rows that are AggregateRow objects.
+ */
 export const CELL_SUMMARY_AGGREGATE_SCHEMA = z.record(
   z.object({
     label: z.string(),
