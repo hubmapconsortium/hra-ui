52786ee0ff98ab84fb9c2ee81cbacdcc25a628a8
jagrut-18 <jagrutchaudhari18@gmail.com>
2023-03-27 16:32:46 -0400

    update documentation

--- libs/state/src/lib/cell-summary/cell-summary.actions.ts
+++ libs/state/src/lib/cell-summary/cell-summary.actions.ts
@@ -5,21 +5,23 @@ import { CELL_SUMMARY_SCHEMA } from './cell-summary.model';
 /** Base action factory */
 const Action = ActionGroup('ResourceRegistry');
 
-/**
-Defines the SetData action, which sets data to the store.
-@param data - The data to set to the store.
-*/
+/** Set data to store */
 export class SetData extends Action('SetData') {
+  /**
+  Defines the SetData action, which sets data to the store.
+  @param data - The data to set to the store.
+  */
   constructor(readonly data: object) {
     super();
   }
 }
 
-/**
+/** Compute aggregate of the given data and store to state */
+export class ComputeAggregate extends Action('ComputeAggregate') {
+  /**
 Defines the ComputeAggregate action, which computes aggregate data for the given summaries.
 @param summaries - The cell summary data to compute aggregate for.
 */
-export class ComputeAggregate extends Action('ComputeAggregate') {
   constructor(readonly summaries: z.infer<typeof CELL_SUMMARY_SCHEMA>) {
     super();
   }
