c433b55d1d55eceda5c420508f00d65bb114c496
rchavan10 <rajchavan1099@gmail.com>
2023-03-28 10:22:03 -0400

    added action to load tissue

--- libs/state/src/lib/tissue-library/tissue-library.actions.ts
+++ libs/state/src/lib/tissue-library/tissue-library.actions.ts
@@ -1,8 +1,8 @@
 import { Action } from '@hra-ui/cdk/state';
 import { TissueLibraryService } from '@hra-ui/services';
 
+export class Load extends Action('') {}
+
 export class SetActive extends Action('') {}
 
 export class ClearActive extends Action('') {}
-
-export class Load extends Action('') {}
--- libs/state/src/lib/tissue-library/tissue-library.state.ts
+++ libs/state/src/lib/tissue-library/tissue-library.state.ts
@@ -2,15 +2,15 @@ import { inject, Injectable } from '@angular/core';
 import { TissueLibraryService } from '@hra-ui/services';
 import { Action, State, StateContext } from '@ngxs/store';
 import { Observable } from 'rxjs';
-import { SetActive } from './tissue-library.actions';
+import { Load } from './tissue-library.actions';
 
 @State<void>({ name: 'tissue-library' })
 @Injectable()
 export class TissueLibraryState {
   private readonly tissue = inject(TissueLibraryService);
 
-  @Action(SetActive)
+  @Action(Load)
   setActive(ctx: StateContext<void>): Observable<void> {
-    return this.tissue.setActiveTissue();
+    return this.tissue.getTissues();
   }
 }
