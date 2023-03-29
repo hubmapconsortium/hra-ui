ce2b87e2ee8d21956529a0c4e07e28427b21f31b
rchavan10 <rajchavan1099@gmail.com>
2023-03-29 09:56:14 -0400

    initial commit for tissue data

--- libs/services/src/lib/tissue-library/tissue-library.service.ts
+++ libs/services/src/lib/tissue-library/tissue-library.service.ts
@@ -1,5 +1,36 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { z } from 'zod';
+
+// export interface TissueData {
+//   root: string;
+//   Nodes: {
+//     iri: {
+//       id: string;
+//       type: string;
+//       label: string;
+//       children: Array<string>;
+//       parent?: string;
+//     }
+//   }
+// }
+
+export type TissueData = z.infer<typeof TISSUE_DATA_SCHEMA>;
+
+const IRI = z.string().url();
+
+export const TISSUE_DATA_SCHEMA = z.object({
+  root: IRI,
+  Nodes: z.record(
+    IRI,
+    z.object({
+      '@id': IRI,
+      '@type': z.string(),
+      label: z.string(),
+      // TODO children
+    })
+  ),
+});
 
 @Injectable()
 export abstract class TissueLibraryService {
