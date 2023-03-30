6bf888c86688469130db689fb0adc06e426a20a1
rchavan10 <rajchavan1099@gmail.com>
2023-03-30 12:28:06 -0400

    Observable returns Tissue Data

--- libs/state/src/lib/tissue-library/tissue-library.state.ts
+++ libs/state/src/lib/tissue-library/tissue-library.state.ts
@@ -3,6 +3,7 @@ import { TissueLibraryService } from '@hra-ui/services';
 import { Action, State, StateContext } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { Load } from './tissue-library.actions';
+import { TissueData } from '@hra-ui/services';
 
 @State<void>({ name: 'tissue-library' })
 @Injectable()
@@ -10,7 +11,7 @@ export class TissueLibraryState {
   private readonly tissue = inject(TissueLibraryService);
 
   @Action(Load)
-  setActive(ctx: StateContext<void>): Observable<void> {
+  setActive(ctx: StateContext<void>): Observable<TissueData> {
     return this.tissue.getTissues();
   }
 }
