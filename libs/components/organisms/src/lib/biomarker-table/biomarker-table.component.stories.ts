c025027cda0950bc4ee5ef7a86ce9f3da83aba50
Neerudu, Pranathi <pneerudu@iu.edu>
2023-04-04 12:37:47 -0400

    Intial push

--- libs/components/organisms/src/lib/biomarker-table/biomarker-table.component.stories.ts
+++ libs/components/organisms/src/lib/biomarker-table/biomarker-table.component.stories.ts
@@ -25,7 +25,50 @@ Primary.args = {
       {
         color: '#9ca5ee',
         size: 0.56,
-        data: [],
+        data: [
+          [
+            {
+              label: 'Functional Tissue Unit Name',
+              value: 'Crypt of Lieberkuhn',
+            },
+            {
+              label: 'Uberon ID',
+              value: 'UBERON:0011184',
+            },
+            {
+              label: '#Datasets',
+              value: '3',
+            },
+          ],
+          [
+            {
+              label: 'Cell Type Name',
+              value: 'epithelial stem cell',
+            },
+            {
+              label: 'CL ID',
+              value: 'CL:0009016',
+            },
+            {
+              label: 'Number of Cells',
+              value: '187',
+            },
+          ],
+          [
+            {
+              label: 'Gene Name',
+              value: 'RGMB',
+            },
+            {
+              label: 'HGNC ID',
+              value: 'HGNC:26896',
+            },
+            {
+              label: 'Mean Expression Value',
+              value: '0.0642',
+            },
+          ],
+        ],
       },
       undefined,
       {
