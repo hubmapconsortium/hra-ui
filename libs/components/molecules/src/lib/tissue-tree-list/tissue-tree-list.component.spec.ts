4fc0d76626e0a4f3613ddf403ffc7031e84206c3
rchavan10 <rajchavan1099@gmail.com>
2023-04-05 15:41:09 -0400

    test cases for selectNode function

--- libs/components/molecules/src/lib/tissue-tree-list/tissue-tree-list.component.spec.ts
+++ libs/components/molecules/src/lib/tissue-tree-list/tissue-tree-list.component.spec.ts
@@ -1,47 +1,66 @@
+import { MatTreeModule } from '@angular/material/tree';
 import { Shallow } from 'shallow-render';
-import { TissueTreeListComponent } from './tissue-tree-list.component';
+import { DataNode, TissueTreeListComponent } from './tissue-tree-list.component';
 
 describe('TissueTreeListComponent', () => {
-  let shallow: Shallow<TissueTreeListComponent>;
-  const tissueTree = [
-    {
+  let shallow: Shallow<TissueTreeListComponent<DataNode>>;
+  const nodes: Record<string, DataNode> = {
+    id1: {
       label: 'Kidney',
-      tissues: [
-        { label: 'Ascending thin limb' },
-        { label: 'Cortical collecting duct' },
-        { label: 'Collecting duct(inner medulla)' },
-      ],
+      children: ['id2', 'id3'],
     },
-    {
+    id2: {
+      label: 'Ascending thin limb',
+    },
+    id3: {
+      label: 'Cortical collecting duct',
+      children: ['id4', 'id6'],
+    },
+    id4: {
       label: 'Large Intestine',
-      tissues: [{ label: 'Crypt of Lieberkuhn' }],
+      children: ['id5'],
     },
-    {
+    id5: {
+      label: 'Crypt of Lieberkuhn',
+    },
+    id6: {
       label: 'Liver',
-      tissues: [{ label: 'Liver lobule' }],
+      children: ['id7'],
     },
-  ];
+    id7: {
+      label: 'Liver lobule',
+    },
+  };
 
   beforeEach(async () => {
-    shallow = new Shallow(TissueTreeListComponent);
+    shallow = new Shallow(TissueTreeListComponent).dontMock(MatTreeModule);
   });
 
   it('creates', async () => {
-    await expect(shallow.render({ bind: { tissueTree: tissueTree } })).resolves.toBeDefined();
+    await expect(shallow.render({ bind: { nodes } })).resolves.toBeDefined();
   });
 
-  it('check if child for the item exists', async () => {
-    const { instance } = await shallow.render({ bind: { tissueTree: tissueTree } });
-    instance.tissueTree = tissueTree;
-    expect(instance.hasChild(1, tissueTree[0])).toBe(true);
-  });
+  describe('selectNode(node)', () => {
+    const internalNode = {
+      label: '',
+      expandable: false,
+      level: 0,
+      data: nodes[0],
+    };
 
-  it('select item and again', async () => {
-    const { instance } = await shallow.render({ bind: { tissueTree: tissueTree } });
-    const targetItem = tissueTree[0]['tissues'][0];
-    instance.selectNode(targetItem);
-    expect(instance.selected).toBe(targetItem);
-    instance.selectNode(targetItem);
-    expect(instance.selected).toBe(undefined);
+    it('should set the selected node', async () => {
+      const { instance, outputs } = await shallow.render({ bind: { nodes } });
+      instance.selectNode(internalNode);
+      expect(instance.selected).toBe(internalNode.data);
+      expect(outputs.selectedChange.emit).toHaveBeenCalledWith(internalNode.data);
+    });
+
+    it('should clear the selected node if called with the current selection', async () => {
+      const { instance, outputs } = await shallow.render({ bind: { nodes } });
+      instance.selected = internalNode.data;
+      instance.selectNode(internalNode);
+      expect(instance.selected).toBeUndefined();
+      expect(outputs.selectedChange.emit).toHaveBeenCalledWith(undefined);
+    });
   });
 });
