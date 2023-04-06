a8ffd389960b4955983310e77e364be0212843a4
Hanuhitha Dondapati <hanuhitha.99@gmail.com>
2023-04-06 16:19:54 -0400

    download and notification action

--- libs/state/src/lib/state-analytics-plugin/default-loggable-actions.ts
+++ libs/state/src/lib/state-analytics-plugin/default-loggable-actions.ts
@@ -4,3 +4,11 @@ import { ContactActions } from '../contact';
  * Default loggable actions
  */
 export const DEFAULT_LOGGABLE_ACTIONS: unknown[] = [ContactActions.SendMessage];
+/**
+ * Default download action
+ */
+export const DEFAULT_DOWNLOAD_ACTIONS: unknown[] = [ContactActions.SendMessage];
+/**
+ * Default notification action
+ */
+export const DEFAULT_NOTIFICATION_ACTIONS: unknown[] = [ContactActions.SendMessage];
