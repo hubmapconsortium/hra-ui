d825b8611586a8956e310cd2481ef521e0408030
Aditya Rudrawar <adirudrawar17@gmail.com>
2023-04-07 14:46:17 -0400

    Correct the SourceListSelector

--- libs/state/src/lib/source-list/source-list.selectors.ts
+++ libs/state/src/lib/source-list/source-list.selectors.ts
@@ -1,5 +1,6 @@
 import { Selector } from '@ngxs/store';
 import { Source, SourceListModel } from './source-list.model';
+import { SourceListState } from './source-list.state';
 
 /** Selector class for retrieving data from the SourceListState */
 export class SourceListSelectors {
@@ -8,7 +9,7 @@ export class SourceListSelectors {
    * @param state The current state of the SourceListState
    * @returns array of source objects
    */
-  @Selector()
+  @Selector([SourceListState])
   static getSourceList(state: SourceListModel): Source[] {
     return state;
   }
