43f4a89c045cc7292cf8a1febdc245502f13f540
tanukansalgit <tanukansal167@gmail.com>
2023-04-11 13:31:56 -0400

    Document -Provide 100% document for the theming

--- libs/shared/theming/src/lib/state/theming.model.ts
+++ libs/shared/theming/src/lib/state/theming.model.ts
@@ -2,8 +2,17 @@ import { StateContext } from '@ngxs/store';
 import { MaterialCssVariables } from 'angular-material-css-vars';
 import { z } from 'zod';
 
+/**
+ * Model for theming
+ */
 export type ThemingModel = z.infer<typeof THEMING_FILE_SCHEMA>;
 
+/**
+ * State context for theming, with reference of model
+ */
 export type ThemingContext = StateContext<ThemingModel>;
 
+/**
+ * Theming mdoel schema, having records with key as material-css-vars pallete keys and string values
+ */
 export const THEMING_FILE_SCHEMA = z.record(z.nativeEnum(MaterialCssVariables), z.string());
--- libs/shared/theming/src/lib/state/theming.state.ts
+++ libs/shared/theming/src/lib/state/theming.state.ts
@@ -7,15 +7,39 @@ import { map, Observable, tap } from 'rxjs';
 import { Load } from './theming.actions';
 import { ThemingContext, ThemingModel, THEMING_FILE_SCHEMA } from './theming.model';
 
+/**
+ * State for keeping track of theming applied
+ */
 @State<ThemingModel>({
+  /**
+   * name of the state
+   */
   name: 'theming',
+  /**
+   * state variables default value
+   */
   defaults: {},
 })
+/**
+ * Theming state class
+ */
 @Injectable()
 export class ThemingState {
+  /**
+   * http service for link loading
+   */
   private readonly http = inject(HttpClient);
+  /**
+   * Material-vars service for setting theming variables
+   */
   private readonly materialVars = inject(MaterialCssVarsService);
 
+  /**
+   * Action for loading yaml, reading data from yaml, setting state variables, and set material-css-vars variables for dynamic theming.
+   * @param ctx State Context
+   * @param action Action with url(yaml url) to load
+   * @returns asyncronous data from the url
+   */
   @Action(Load)
   load(ctx: ThemingContext, { url }: Load): Observable<unknown> {
     return this.http.get(url, { responseType: 'text' }).pipe(
@@ -26,6 +50,10 @@ export class ThemingState {
     );
   }
 
+  /**
+   * Set values to the material-css-vars pallete keys
+   * @param data key-value pairs set in state
+   */
   private setVariables(data: ThemingModel): void {
     for (const [key, value] of Object.entries(data)) {
       this.materialVars.setVariable(key as MaterialCssVariables, value);
