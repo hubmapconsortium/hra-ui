0230a6012e0ac5c0a080a2029712bee33e3e177e
jagrut-18 <jagrutchaudhari18@gmail.com>
2023-04-12 14:02:03 -0400

    Export values, classes, etc.

--- libs/state/src/index.ts
+++ libs/state/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/cell-summary';
 export * from './lib/contact/contact.actions';
 export * from './lib/contact/contact.state';
 export * as ResourceIds from './lib/resource-ids';
---
+++ libs/state/src/lib/cell-summary/index.ts
@@ -0,0 +1,3 @@
+export * as CellSummaryActions from './cell-summary.actions';
+export { Cell, CellSummary, Aggregate, AggregateRow, AggregateRowEntry, GradientPoint } from './cell-summary.model';
+export { CellSummaryState } from './cell-summary.state';
