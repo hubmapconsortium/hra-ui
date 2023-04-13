b6c227962a5a9b9a21b63a14783b361953a77692
edlu77 <eddd.lu@gmail.com>
2023-04-13 18:23:21 -0400

    Fix test

--- libs/state/src/lib/medical-illustration/medical-illustration.selectors.spec.ts
+++ libs/state/src/lib/medical-illustration/medical-illustration.selectors.spec.ts
@@ -4,7 +4,7 @@ import { MedicalIllustrationSelectors } from './medical-illustration.selectors';
 describe('MedicalIllustrationSelectors', () => {
   const state: MedicalIllustrationModel = { url: 'test' };
   it('should return the current url', () => {
-    const result = MedicalIllustrationSelectors.getUrl(state);
+    const result = MedicalIllustrationSelectors.url(state);
     expect(result).toEqual('test');
   });
 });
--- libs/state/src/lib/medical-illustration/medical-illustration.state.spec.ts
+++ libs/state/src/lib/medical-illustration/medical-illustration.state.spec.ts
@@ -4,12 +4,42 @@ import { mock } from 'jest-mock-extended';
 import { SetActiveNode, SetUri } from './medical-illustration.actions';
 import { MedicalIllustrationModel } from './medical-illustration.model';
 import { MedicalIllustrationState } from './medical-illustration.state';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
 
 describe('MedicalIllustrationState', () => {
+  const testNode = {
+    organ_label: 'Kidney',
+    organ_id: 'UBERON:0002113',
+    anatomical_structure_of: '#FTUCorticalCollectingDuct',
+    source_spatial_entity: '#2DRefObjects',
+    node_name: 'Cortical_Collecting_Duct_Principal_Cell_1',
+    label: 'kidney cortex collecting duct principal cell',
+    OntologyID: 'CL:1000714',
+    representation_of: 'http://purl.obolibrary.org/obo/CL_1000714',
+    'svg file of single 2DFTU': '2d-ftu-kidney-cortical-collecting-duct',
+    exist_asctb: '1',
+    type: 'CT',
+    'REF/1': '',
+    'REF/1/DOI': '',
+    'REF/1/NOTES': '',
+    'Inset #': '',
+  };
   const testAction1 = new SetUri('test');
-  const testAction2 = new SetActiveNode({ item: 'test' });
+  const testAction2 = new SetActiveNode(testNode);
   const ctx = mock<StateContext<MedicalIllustrationModel>>();
-  const state = new MedicalIllustrationState();
+  let state: MedicalIllustrationState;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [MedicalIllustrationState],
+    });
+
+    state = TestBed.inject(MedicalIllustrationState);
+  });
 
   afterEach(() => jest.clearAllMocks());
 
@@ -23,7 +53,7 @@ describe('MedicalIllustrationState', () => {
   it('should save the current active node', async () => {
     state.setActiveNode(ctx, testAction2);
     expect(ctx.patchState).toHaveBeenCalledWith({
-      node: { item: 'test' },
+      node: testNode,
     });
   });
 });
