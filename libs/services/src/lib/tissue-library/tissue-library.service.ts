ad9e88aa294522c25bb6d20cbdbc20ebac829948
rchavan10 <rajchavan1099@gmail.com>
2023-04-13 11:34:18 -0400

    updated service

--- libs/services/src/lib/tissue-library/tissue-library.service.ts
+++ libs/services/src/lib/tissue-library/tissue-library.service.ts
@@ -2,35 +2,9 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { z } from 'zod';
 
-// export interface TissueData {
-//   root: string;
-//   Nodes: {
-//     iri: {
-//       id: string;
-//       type: string;
-//       label: string;
-//       children: Array<string>;
-//       parent?: string;
-//     }
-//   }
-// }
-
 export type TissueData = z.infer<typeof TISSUE_DATA_SCHEMA>;
 
-const IRI = z.string().url().brand('IRI');
-
-// export const TISSUE_DATA_SCHEMA = z.object({
-//   root: IRI,
-//   Nodes: z.record(
-//     IRI,
-//     z.object({
-//       '@id': IRI,
-//       '@type': z.string(),
-//       label: z.string(),
-//       // TODO children
-//     })
-//   ),
-// });
+export const IRI = z.string().url().brand('IRI');
 
 export const TISSUE_DATA_SCHEMA = z.object({
   root: IRI,
