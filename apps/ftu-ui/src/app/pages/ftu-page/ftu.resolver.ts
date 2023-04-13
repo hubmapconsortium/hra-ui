aa6cc4e4aa9830d8fc03dbfb6cceffe961756b10
Sai Kiran Jella <jsaikiran19@gmail.com>
2023-04-13 12:49:12 -0400

    changed if condition

--- apps/ftu-ui/src/app/pages/ftu-page/ftu.resolver.ts
+++ apps/ftu-ui/src/app/pages/ftu-page/ftu.resolver.ts
@@ -5,7 +5,7 @@ export const ftuResolver: ResolveFn<unknown> = (route) => {
   const id = route.queryParamMap.get('id');
   const uberon = route.queryParamMap.get('uberon');
 
-  if (!id || !uberon) {
+  if (!id) {
     return of({ error: 'Missing required params' });
   }
 
