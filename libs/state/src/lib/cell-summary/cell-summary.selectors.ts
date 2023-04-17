206cb4ee536922bbfed10952293f13c5634b09f4
jagrut-18 <jagrutchaudhari18@gmail.com>
2023-04-17 14:59:44 -0400

    rename selector names

--- libs/state/src/lib/cell-summary/cell-summary.selectors.ts
+++ libs/state/src/lib/cell-summary/cell-summary.selectors.ts
@@ -1,18 +1,19 @@
 import { Selector } from '@ngxs/store';
 import { Aggregate, CellSummaryStateModel } from './cell-summary.model';
 import { CellSummary } from './cell-summary.model';
+import { CellSummaryState } from './cell-summary.state';
 
 /** selectors for the CellSummary state */
 export class CellSummarySelectors {
   /** get the aggregate data from the state */
-  @Selector()
-  static getAggregateData(state: CellSummaryStateModel): Aggregate {
+  @Selector([CellSummaryState])
+  static aggregates(state: CellSummaryStateModel): Aggregate {
     return state.aggregate;
   }
 
   /** get the summaries data from the state */
-  @Selector()
-  static getSummariesData(state: CellSummaryStateModel): CellSummary {
+  @Selector([CellSummaryState])
+  static summaries(state: CellSummaryStateModel): CellSummary {
     return state.summaries;
   }
 }
