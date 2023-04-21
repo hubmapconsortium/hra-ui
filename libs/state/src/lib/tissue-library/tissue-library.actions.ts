1eb53af2aded122f1f0e107e55de780e5e8bfd27
rchavan10 <rajchavan1099@gmail.com>
2023-04-21 12:47:50 -0400

    added doc coverage

--- libs/state/src/lib/tissue-library/tissue-library.actions.ts
+++ libs/state/src/lib/tissue-library/tissue-library.actions.ts
@@ -1,3 +1,4 @@
 import { Action } from '@hra-ui/cdk/state';
 
+/** action of loading tissue data into the TissueLibrary state */
 export class Load extends Action('[TissueLibrary] Load Tissue Data') {}
--- libs/state/src/lib/tissue-library/tissue-library.state.ts
+++ libs/state/src/lib/tissue-library/tissue-library.state.ts
@@ -5,13 +5,19 @@ import { Observable, tap } from 'rxjs';
 import { Load } from './tissue-library.actions';
 import { TissueLibraryContext, TissueLibraryModel } from './tissue-library.model';
 
+/** State handling tissue data*/
 @State<TissueLibraryModel>({
   name: 'tissueLibrary',
 })
 @Injectable()
 export class TissueLibraryState {
+  /** injects the TissueLibraryService into a private readonly property */
   private readonly dataService = inject(TissueLibraryService);
-
+  /**
+   * Loads the tissue data into the current state
+   * @param ctx The state context instance
+   * @returns data The tissue data to be added to the state
+   */
   @Action(Load)
   setActive(ctx: TissueLibraryContext): Observable<unknown> {
     return this.dataService.getTissues().pipe(tap((data) => ctx.setState(data)));
