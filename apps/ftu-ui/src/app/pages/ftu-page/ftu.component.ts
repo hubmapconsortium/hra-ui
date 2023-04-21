bdbceb4bf398b2c5e9a492e6e6705722ce1dfe38
Daniel Bolin <axbolin@iu.edu>
2023-04-21 16:57:32 -0400

    Add ftu page content

--- apps/ftu-ui/src/app/pages/ftu-page/ftu.component.ts
+++ apps/ftu-ui/src/app/pages/ftu-page/ftu.component.ts
@@ -1,12 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FooterBehaviorComponent } from '@hra-ui/components/behavioral';
+import {
+  BiomarkerDetailsComponent,
+  FooterBehaviorComponent,
+  MedicalIllustrationBehaviorComponent,
+} from '@hra-ui/components/behavioral';
 import { FullscreenContainerComponent, FullscreenContentComponent } from '@hra-ui/components/molecules';
 
 @Component({
   selector: 'ftu-ftu-page',
   standalone: true,
-  imports: [CommonModule, FullscreenContainerComponent, FullscreenContentComponent, FooterBehaviorComponent],
+  imports: [
+    CommonModule,
+
+    BiomarkerDetailsComponent,
+    FooterBehaviorComponent,
+    FullscreenContainerComponent,
+    FullscreenContentComponent,
+    MedicalIllustrationBehaviorComponent,
+  ],
   templateUrl: './ftu.component.html',
   styleUrls: ['./ftu.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
