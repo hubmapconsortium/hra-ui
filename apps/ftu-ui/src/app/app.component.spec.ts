2da007adee9242a9bc9a06d323030305dadb9cd9
Sai Kiran Jella <39052532+jsaikiran19@users.noreply.github.com>
2023-04-21 11:21:12 -0400

    app setup (#153)
    * -adds header to landing and ftu
    -imports modules into app
    
    * [*] adds a file for states
    
    * updates spec file
    
    * Update data resolver
    
    * Remove autogenerated welcome page
    
    * Update page templates
    
    * spec for resolver
    
    * Remove unnecessary default values in templates
    
    * cdk state module
    
    * Copy resources
    
    * Load resources on app init
    
    * Fix circular dependency
    
    * Skip coverage for routing modules
    
    * Test app initialization
    
    ---------
    
    Co-authored-by: Daniel Bolin <axbolin@iu.edu>

--- apps/ftu-ui/src/app/app.component.spec.ts
+++ apps/ftu-ui/src/app/app.component.spec.ts
@@ -1,30 +1,38 @@
-import { TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { dispatchAction$ } from '@hra-ui/cdk/injectors';
+import { of } from 'rxjs';
+import { Shallow } from 'shallow-render';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { initFactory } from './app.init';
+import { AppModule } from './app.module';
+
+jest.mock('@hra-ui/cdk/injectors');
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AppComponent, NxWelcomeComponent],
-    }).compileComponents();
+  let shallow: Shallow<AppComponent>;
+
+  beforeEach(() => {
+    shallow = new Shallow(AppComponent, AppModule)
+      .replaceModule(RouterModule, RouterTestingModule)
+      .replaceModule(BrowserAnimationsModule, NoopAnimationsModule);
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('should create component', async () => {
+    expect(shallow.render()).resolves.toBeDefined();
   });
+});
 
-  it(`should have as title 'ftu-ui'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ftu-ui');
-  });
+describe('initFactory()', () => {
+  const dispatcher = jest.fn().mockReturnValue(of(undefined));
+  jest.mocked(dispatchAction$).mockReturnValue(dispatcher);
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome ftu-ui');
+  afterEach(() => jest.clearAllMocks());
+
+  it('should dispatch actions', () => {
+    const init = initFactory();
+    init();
+    expect(dispatcher).toHaveBeenCalled();
   });
 });
---
+++ apps/ftu-ui/src/app/app.init.ts
@@ -0,0 +1,24 @@
+import { dispatchAction$ } from '@hra-ui/cdk/injectors';
+import { LinkRegistryActions, LinkType, ResourceRegistryActions } from '@hra-ui/cdk/state';
+import { LinkIds } from '@hra-ui/state';
+import { Observable } from 'rxjs';
+
+/**
+ * Factory for APP_INITIALIZER to initialize the application
+ * @returns A initialization function
+ */
+export function initFactory(): () => Observable<unknown> {
+  const dispatch = dispatchAction$();
+  return () =>
+    dispatch([
+      new LinkRegistryActions.AddMany({
+        [LinkIds.LandingPage]: {
+          type: LinkType.Internal,
+          commands: ['/'],
+        },
+      }),
+
+      new LinkRegistryActions.LoadFromYaml('assets/links.yml'),
+      new ResourceRegistryActions.LoadFromYaml('assets/resources.yml'),
+    ]);
+}
