15510e49594cbe77dcdde8cb4f294fe11b82559a
Sai Kiran Jella <jsaikiran19@gmail.com>
2023-04-25 13:03:58 -0400

    test case

--- libs/services/src/lib/tissue-ftu/tissue-ftu.service.spec.ts
+++ libs/services/src/lib/tissue-ftu/tissue-ftu.service.spec.ts
@@ -1,16 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TissueFtuService } from './tissue-ftu.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('TissueFtuService', () => {
   let service: TissueFtuService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(TissueFtuService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return an observable on getReferenceOrgans', () => {
+    const res = service.getReferenceOrgans();
+    expect(res).toBeDefined();
   });
 });
