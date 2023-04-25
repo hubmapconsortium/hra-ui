468e757c7f900d2fe3c30622f80a59c2549348b8
Sai Kiran Jella <jsaikiran19@gmail.com>
2023-04-25 14:10:45 -0400

    docs

--- libs/services/src/lib/tissue-ftu/tissue-ftu.service.ts
+++ libs/services/src/lib/tissue-ftu/tissue-ftu.service.ts
@@ -2,12 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 
+/** Service to fetch reference organs */
 @Injectable({
   providedIn: 'root',
 })
 export class TissueFtuService {
+  /** injects http client */
   private http = inject(HttpClient);
 
+  /** Method to fetch reference organs */
   getReferenceOrgans(): Observable<unknown> {
     return this.http.get('');
   }
