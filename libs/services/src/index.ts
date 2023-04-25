114887d4c7ac103a6222c25ac19f3f718196defa
rchavan10 <rajchavan1099@gmail.com>
2023-04-25 10:23:33 -0400

    configured storybook

--- libs/services/src/index.ts
+++ libs/services/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/contact/contact.service';
 export * from './lib/contact/contact.mock';
 export * from './lib/tissue-library/tissue-library.service';
+export * from './lib/tissue-library/tissue-library.mock';
