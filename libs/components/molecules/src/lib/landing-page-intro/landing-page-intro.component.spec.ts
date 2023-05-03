5c329feca2c798f17af2ebdab8511225ecb2d746
Daniel Bolin <axbolin@iu.edu>
2023-05-03 12:50:53 -0400

    Update intro to use hraLink

--- libs/components/molecules/src/lib/landing-page-intro/landing-page-intro.component.spec.ts
+++ libs/components/molecules/src/lib/landing-page-intro/landing-page-intro.component.spec.ts
@@ -1,12 +1,16 @@
+import { LinkDirective } from '@hra-ui/cdk';
+import { selectQuerySnapshot } from '@hra-ui/cdk/injectors';
 import { Shallow } from 'shallow-render';
-
 import { LandingPageIntroComponent } from './landing-page-intro.component';
+
+jest.mock('@hra-ui/cdk/injectors');
+jest.mocked(selectQuerySnapshot).mockReturnValue(() => undefined as never);
 
 describe('LandingPageIntroComponent', () => {
   let shallow: Shallow<LandingPageIntroComponent>;
 
   beforeEach(() => {
-    shallow = new Shallow(LandingPageIntroComponent);
+    shallow = new Shallow(LandingPageIntroComponent).dontMock(LinkDirective);
   });
 
   it('should create LandingPageIntroComponent', async () => {
--- libs/components/molecules/src/lib/landing-page-intro/landing-page-intro.component.ts
+++ libs/components/molecules/src/lib/landing-page-intro/landing-page-intro.component.ts
@@ -1,14 +1,15 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { CommonModule } from '@angular/common';
+import { EMPTY_LINK, LinkDirective } from '@hra-ui/cdk';
 import { MarkdownModule } from 'ngx-markdown';
 
 /** Component for LandingPage Intro */
 @Component({
   selector: 'hra-landing-page-intro',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule, MarkdownModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MarkdownModule, LinkDirective],
   templateUrl: './landing-page-intro.component.html',
   styleUrls: ['./landing-page-intro.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -29,6 +30,6 @@ export class LandingPageIntroComponent {
   /** Text for more button */
   @Input() moreText = '';
 
-  /** Emits when the more button is clicked */
-  @Output() moreClick = new EventEmitter<void>();
+  /** Link to navigate to on more button click */
+  @Input() moreLink = EMPTY_LINK;
 }
