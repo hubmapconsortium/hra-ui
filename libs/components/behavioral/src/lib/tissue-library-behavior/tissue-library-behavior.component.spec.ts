fdc6c32d41a2014ff24779ed892837183ccfba4a
rchavan10 <rajchavan1099@gmail.com>
2023-05-04 01:24:44 -0400

    fixed bugs

--- libs/components/behavioral/src/lib/tissue-library-behavior/tissue-library-behavior.component.spec.ts
+++ libs/components/behavioral/src/lib/tissue-library-behavior/tissue-library-behavior.component.spec.ts
@@ -1,20 +1,31 @@
-import { dispatch, selectSnapshot } from '@hra-ui/cdk/injectors';
+import { select$, selectSnapshot } from '@hra-ui/cdk/injectors';
+import { EMPTY } from 'rxjs';
 import { Shallow } from 'shallow-render';
 import { TissueLibraryBehaviorComponent } from './tissue-library-behavior.component';
+import { subscribe } from 'diagnostics_channel';
+import { Store } from '@ngxs/store';
+import { MedicalIllustrationActions, MedicalIllustrationSelectors } from '@hra-ui/state';
 
 jest.mock('@hra-ui/cdk/injectors');
 
 describe('TissueLibraryBehaviorComponent', () => {
   let shallow: Shallow<TissueLibraryBehaviorComponent>;
+  let mockSelect$: jest.Mock;
 
   jest.mocked(selectSnapshot).mockReturnValue(jest.fn().mockReturnValue([]));
-  jest.mocked(dispatch).mockReturnValue(jest.fn());
+  jest.mocked(select$).mockReturnValue(EMPTY);
 
   beforeEach(async () => {
+    mockSelect$ = jest.fn(() => EMPTY);
+    jest.spyOn(Store.prototype, 'select').mockImplementation(mockSelect$);
     shallow = new Shallow(TissueLibraryBehaviorComponent);
   });
 
   it('should create', async () => {
     await expect(shallow.render()).resolves.toBeDefined();
+  });
+
+  it('should subscribe to the correct observable in constructor', () => {
+    expect(mockSelect$).toHaveBeenCalledWith(MedicalIllustrationSelectors.url);
   });
 });
--- libs/components/behavioral/src/lib/tissue-library-behavior/tissue-library-behavior.component.ts
+++ libs/components/behavioral/src/lib/tissue-library-behavior/tissue-library-behavior.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { selectSnapshot } from '@hra-ui/cdk/injectors';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { select$, selectSnapshot } from '@hra-ui/cdk/injectors';
 import { TissueTreeListComponent } from '@hra-ui/components/molecules';
-import { TissueLibrarySelectors } from '@hra-ui/state';
+import { TissueItem } from '@hra-ui/services';
+import { MedicalIllustrationSelectors, TissueLibrarySelectors } from '@hra-ui/state';
 
 /**
  * Component for Tissue Library Behavior
@@ -20,4 +21,20 @@ export class TissueLibraryBehaviorComponent {
    * Input for tissues data
    */
   readonly tissues = selectSnapshot(TissueLibrarySelectors.tissues);
+  /**
+   * Selected  of tissue library behavior component
+   */
+  selected?: TissueItem;
+
+  /**
+   * Sets the TissueItem instance as undefined if
+   * the url is undefined
+   */
+  constructor() {
+    select$(MedicalIllustrationSelectors.url).subscribe((url) => {
+      if (url === undefined) {
+        this.selected = undefined;
+      }
+    });
+  }
 }
