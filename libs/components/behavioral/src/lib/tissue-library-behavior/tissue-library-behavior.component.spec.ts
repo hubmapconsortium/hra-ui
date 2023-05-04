7f754c59a795de2184d79de981f4305189bcbc64
Daniel Bolin <axbolin@iu.edu>
2023-05-04 11:09:05 -0400

    Fix tests

--- libs/components/behavioral/src/lib/tissue-library-behavior/tissue-library-behavior.component.spec.ts
+++ libs/components/behavioral/src/lib/tissue-library-behavior/tissue-library-behavior.component.spec.ts
@@ -1,22 +1,18 @@
 import { select$, selectSnapshot } from '@hra-ui/cdk/injectors';
-import { EMPTY } from 'rxjs';
+import { Subject } from 'rxjs';
 import { Shallow } from 'shallow-render';
 import { TissueLibraryBehaviorComponent } from './tissue-library-behavior.component';
-import { Store } from '@ngxs/store';
-import { MedicalIllustrationSelectors } from '@hra-ui/state';
 
 jest.mock('@hra-ui/cdk/injectors');
 
 describe('TissueLibraryBehaviorComponent', () => {
+  const urlSubject = new Subject<string | undefined>();
   let shallow: Shallow<TissueLibraryBehaviorComponent>;
-  let mockSelect$: jest.Mock;
 
   jest.mocked(selectSnapshot).mockReturnValue(jest.fn().mockReturnValue([]));
-  jest.mocked(select$).mockReturnValue(EMPTY);
+  jest.mocked(select$).mockReturnValue(urlSubject);
 
   beforeEach(async () => {
-    mockSelect$ = jest.fn(() => EMPTY);
-    jest.spyOn(Store.prototype, 'select').mockImplementation(mockSelect$);
     shallow = new Shallow(TissueLibraryBehaviorComponent);
   });
 
@@ -24,7 +20,10 @@ describe('TissueLibraryBehaviorComponent', () => {
     await expect(shallow.render()).resolves.toBeDefined();
   });
 
-  it('should subscribe to the correct observable in constructor', () => {
-    expect(mockSelect$).toHaveBeenCalledWith(MedicalIllustrationSelectors.url);
+  it('should clear the selection when the medical illustration url becomes undefined', async () => {
+    const { instance } = await shallow.render();
+    instance.selected = {} as never;
+    urlSubject.next(undefined);
+    expect(instance.selected).toBeUndefined();
   });
 });
