d153c1744ab3a9e1b650d3eb13081b66402de1a7
rchavan10 <rajchavan1099@gmail.com>
2023-06-05 17:45:44 -0400

    added dialog box functionality

--- apps/ftu-ui/src/app/pages/ftu-page/ftu.component.ts
+++ apps/ftu-ui/src/app/pages/ftu-page/ftu.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, inject, OnInit } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { dispatch, injectDestroy$ } from '@hra-ui/cdk/injectors';
 import {
@@ -7,7 +8,11 @@ import {
   FooterBehaviorComponent,
   MedicalIllustrationBehaviorComponent,
 } from '@hra-ui/components/behavioral';
-import { FullscreenContainerComponent, FullscreenContentComponent } from '@hra-ui/components/molecules';
+import {
+  FullscreenContainerComponent,
+  FullscreenContentComponent,
+  ScreenSizeNoticeComponent,
+} from '@hra-ui/components/molecules';
 import { MedicalIllustrationActions } from '@hra-ui/state';
 import { takeUntil } from 'rxjs';
 
@@ -17,22 +22,41 @@ import { takeUntil } from 'rxjs';
   standalone: true,
   imports: [
     CommonModule,
-
     BiomarkerDetailsComponent,
     FooterBehaviorComponent,
     FullscreenContainerComponent,
     FullscreenContentComponent,
     MedicalIllustrationBehaviorComponent,
+    ScreenSizeNoticeComponent,
+    MatDialogModule,
   ],
   templateUrl: './ftu.component.html',
   styleUrls: ['./ftu.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FtuComponent {
+export class FtuComponent implements OnInit {
   /** Set the illustration from the id query parameter */
-  constructor() {
+  constructor(private dialog: MatDialog) {
     const queryParams$ = inject(ActivatedRoute).queryParams.pipe(takeUntil(injectDestroy$()));
     const setUriFromIri = dispatch(MedicalIllustrationActions.SetUriFromIRI);
     queryParams$.subscribe((queryParams) => setUriFromIri(queryParams['id']));
+  }
+
+  ngOnInit(): void {
+    this.openModalIfViewportLessThan480px();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(): void {
+    this.openModalIfViewportLessThan480px();
+  }
+
+  openModalIfViewportLessThan480px(): void {
+    const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+    if (viewportWidth < 480) {
+      this.dialog.open(ScreenSizeNoticeComponent, {
+        width: '500px',
+      });
+    }
   }
 }
