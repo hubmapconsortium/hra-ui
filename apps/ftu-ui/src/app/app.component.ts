839ce8e80a7bcbd669b8ab90c88759b5526de733
rchavan10 <rajchavan1099@gmail.com>
2023-06-06 15:03:47 -0400

    added dialog functionality to the app

--- apps/ftu-ui/src/app/app.component.ts
+++ apps/ftu-ui/src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, HostListener, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { ScreenSizeNoticeComponent } from '@hra-ui/components/molecules';
 
 @Component({
   selector: 'ftu-ui-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  providers: [MatDialog, MatDialogModule],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   @HostBinding('class.mat-typography') readonly matTypography = true;
+  isModalDisplayed = false;
+
+  constructor(private dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    this.openModalIfViewportLessThan480px();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(): void {
+    this.openModalIfViewportLessThan480px();
+  }
+
+  openModalIfViewportLessThan480px(): void {
+    const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+    if (viewportWidth < 481 && !this.isModalDisplayed) {
+      this.isModalDisplayed = true;
+      const dialogConfig: MatDialogConfig = {
+        width: '312px',
+        disableClose: false,
+        panelClass: 'custom-overlay',
+      };
+
+      const dialogRef: MatDialogRef<ScreenSizeNoticeComponent> = this.dialog.open(
+        ScreenSizeNoticeComponent,
+        dialogConfig
+      );
+
+      dialogRef.afterClosed().subscribe(() => {
+        this.isModalDisplayed = false;
+      });
+    } else if (viewportWidth >= 481 && this.isModalDisplayed) {
+      this.dialog.closeAll();
+    }
+  }
 }
