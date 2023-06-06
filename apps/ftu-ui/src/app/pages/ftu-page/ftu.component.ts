7c08340ac04f984f549b952d4412b46952b32741
rchavan10 <rajchavan1099@gmail.com>
2023-06-06 15:04:17 -0400

    fixed code

--- apps/ftu-ui/src/app/pages/ftu-page/ftu.component.ts
+++ apps/ftu-ui/src/app/pages/ftu-page/ftu.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, HostListener, inject, OnInit } from '@angular/core';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { dispatch, injectDestroy$ } from '@hra-ui/cdk/injectors';
 import {
@@ -8,11 +8,7 @@ import {
   FooterBehaviorComponent,
   MedicalIllustrationBehaviorComponent,
 } from '@hra-ui/components/behavioral';
-import {
-  FullscreenContainerComponent,
-  FullscreenContentComponent,
-  ScreenSizeNoticeComponent,
-} from '@hra-ui/components/molecules';
+import { FullscreenContainerComponent, FullscreenContentComponent } from '@hra-ui/components/molecules';
 import { MedicalIllustrationActions } from '@hra-ui/state';
 import { takeUntil } from 'rxjs';
 
@@ -27,36 +23,16 @@ import { takeUntil } from 'rxjs';
     FullscreenContainerComponent,
     FullscreenContentComponent,
     MedicalIllustrationBehaviorComponent,
-    ScreenSizeNoticeComponent,
-    MatDialogModule,
   ],
   templateUrl: './ftu.component.html',
   styleUrls: ['./ftu.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FtuComponent implements OnInit {
+export class FtuComponent {
   /** Set the illustration from the id query parameter */
   constructor(private dialog: MatDialog) {
     const queryParams$ = inject(ActivatedRoute).queryParams.pipe(takeUntil(injectDestroy$()));
     const setUriFromIri = dispatch(MedicalIllustrationActions.SetUriFromIRI);
     queryParams$.subscribe((queryParams) => setUriFromIri(queryParams['id']));
-  }
-
-  ngOnInit(): void {
-    this.openModalIfViewportLessThan480px();
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onWindowResize(): void {
-    this.openModalIfViewportLessThan480px();
-  }
-
-  openModalIfViewportLessThan480px(): void {
-    const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
-    if (viewportWidth < 480) {
-      this.dialog.open(ScreenSizeNoticeComponent, {
-        width: '500px',
-      });
-    }
   }
 }
