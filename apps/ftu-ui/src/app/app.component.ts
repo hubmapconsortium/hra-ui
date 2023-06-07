3cb378e846d42fab191f4bfce27700039eb2fffa
rchavan10 <rajchavan1099@gmail.com>
2023-06-07 18:14:33 -0400

    optimized code

--- apps/ftu-ui/src/app/app.component.ts
+++ apps/ftu-ui/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, HostBinding, HostListener, OnInit } from '@angular/core';
+import { Component, HostBinding, HostListener, OnInit, inject } from '@angular/core';
 import { MatDialog, MatDialogConfig, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
-import { ScreenSizeNoticeComponent } from '@hra-ui/components/molecules';
+import { selectQuerySnapshot } from '@hra-ui/cdk/injectors';
+import { StorageId, StorageSelectors } from '@hra-ui/cdk/state';
+import { ScreenNoticeBehaviorComponent } from '@hra-ui/components/behavioral';
 
 @Component({
   selector: 'ftu-ui-root',
@@ -10,9 +12,16 @@ import { ScreenSizeNoticeComponent } from '@hra-ui/components/molecules';
 })
 export class AppComponent implements OnInit {
   @HostBinding('class.mat-typography') readonly matTypography = true;
-  isModalDisplayed = false;
 
-  constructor(private dialog: MatDialog) {}
+  screenSizeNoticeOpen = false;
+
+  private readonly screenSizeNoticeShown = selectQuerySnapshot(
+    StorageSelectors.get,
+    StorageId.Local,
+    'screen-size-notice'
+  );
+
+  private readonly dialog = inject(MatDialog);
 
   ngOnInit(): void {
     this.openModalIfViewportLessThan480px();
@@ -25,24 +34,16 @@ export class AppComponent implements OnInit {
 
   openModalIfViewportLessThan480px(): void {
     const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
-    if (viewportWidth < 481 && !this.isModalDisplayed) {
-      this.isModalDisplayed = true;
+    if (viewportWidth <= 480 && !this.screenSizeNoticeShown()) {
       const dialogConfig: MatDialogConfig = {
         width: '312px',
         disableClose: false,
         panelClass: 'custom-overlay',
       };
 
-      const dialogRef: MatDialogRef<ScreenSizeNoticeComponent> = this.dialog.open(
-        ScreenSizeNoticeComponent,
-        dialogConfig
-      );
-
-      dialogRef.afterClosed().subscribe(() => {
-        this.isModalDisplayed = false;
-      });
-    } else if (viewportWidth >= 481 && this.isModalDisplayed) {
-      this.dialog.closeAll();
+      const ref = this.dialog.open(ScreenNoticeBehaviorComponent, dialogConfig);
+      ref.afterClosed().subscribe(() => (this.screenSizeNoticeOpen = false));
+      this.screenSizeNoticeOpen = true;
     }
   }
 }
