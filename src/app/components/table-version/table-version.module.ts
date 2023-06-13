78a063f0c33aaee1542a7b3f225573030feba542
bhushankhope <bhushankhope@gmail.com>
2023-06-13 16:51:35 -0400

    added blur to right of table

--- src/app/components/table-version/table-version.module.ts
+++ src/app/components/table-version/table-version.module.ts
@@ -3,12 +3,14 @@ import { TableVersionComponent } from './table-version.component';
 import { ChooseVersionModule } from '../choose-version/choose-version.module';
 import { TableModule } from '../table/table.module';
 import { CommonModule } from '@angular/common';
+import { CdkScrollableModule } from '@angular/cdk/scrolling';
 
 
 @NgModule({
     declarations: [TableVersionComponent],
     imports: [
         CommonModule,
+        CdkScrollableModule,
         ChooseVersionModule,
         TableModule
     ],
