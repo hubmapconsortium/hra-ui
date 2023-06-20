03994a3ce1c40175d9a026d04b8e4b64889b9441
rchavan10 <rajchavan1099@gmail.com>
2023-06-20 17:29:18 -0400

    Updated Cell Summary State

--- libs/state/src/lib/cell-summary/cell-summary.actions.ts
+++ libs/state/src/lib/cell-summary/cell-summary.actions.ts
@@ -1,9 +1,9 @@
 import { ActionGroup } from '@hra-ui/cdk/state';
-import { z } from 'zod';
-import { CELL_SUMMARY_SCHEMA } from './cell-summary.model';
+import { Iri } from '@hra-ui/services';
+import { CellSummaryModel } from './cell-summary.model';
 
 /** Base action factory */
-const Action = ActionGroup('ResourceRegistry');
+const Action = ActionGroup('CellSummary');
 
 /** Set data to store */
 export class SetData extends Action('SetData') {
@@ -16,13 +16,21 @@ export class SetData extends Action('SetData') {
   }
 }
 
+export class Load extends Action('Load') {
+  constructor(readonly iri: Iri) {
+    super();
+  }
+}
+
 /** Compute aggregate of the given data and store to state */
 export class ComputeAggregate extends Action('ComputeAggregate') {
   /**
    * Defines the ComputeAggregate action, which computes aggregate data for the given summaries.
    * @param summaries - The cell summary data to compute aggregate for.
    */
-  constructor(readonly summaries: z.infer<typeof CELL_SUMMARY_SCHEMA>) {
+  constructor(readonly summaries: CellSummaryModel) {
     super();
   }
 }
+
+export class Reset extends Action('Reset') {}
--- libs/state/src/lib/cell-summary/cell-summary.selectors.ts
+++ libs/state/src/lib/cell-summary/cell-summary.selectors.ts
@@ -1,18 +1,19 @@
 import { Selector } from '@ngxs/store';
-import { Aggregate, CellSummaryStateModel, CellSummary } from './cell-summary.model';
+import { CellSummary } from '@hra-ui/services';
+import { CellSummaryModel, CellSummaryAggregate } from './cell-summary.model';
 import { CellSummaryState } from './cell-summary.state';
 
 /** selectors for the CellSummary state */
 export class CellSummarySelectors {
   /** get the aggregate data from the state */
   @Selector([CellSummaryState])
-  static aggregates(state: CellSummaryStateModel): Aggregate {
-    return state.aggregate;
+  static aggregates(state: CellSummaryModel): CellSummaryAggregate[] {
+    return state.aggregates;
   }
 
   /** get the summaries data from the state */
   @Selector([CellSummaryState])
-  static summaries(state: CellSummaryStateModel): CellSummary {
+  static summaries(state: CellSummaryModel): CellSummary[] {
     return state.summaries;
   }
 }
