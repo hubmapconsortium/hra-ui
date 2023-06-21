b7a8834855fda8a9695e31f4307b40b5014f7e3b
rchavan10 <rajchavan1099@gmail.com>
2023-06-21 15:52:46 -0400

    updated index

--- libs/state/src/lib/cell-summary/index.ts
+++ libs/state/src/lib/cell-summary/index.ts
@@ -1,3 +1,4 @@
 export * as CellSummaryActions from './cell-summary.actions';
+export { CellSummaryAggregate, CellSummaryAggregateRow } from './cell-summary.model';
 export * from './cell-summary.selectors';
 export { CellSummaryState } from './cell-summary.state';
