fb06395a9f9f2fdf66564f9b2f85d8f0825f7049
rchavan10 <rajchavan1099@gmail.com>
2023-06-22 10:08:00 -0400

    updated to return SvgReference

--- libs/services/src/lib/ftu-data/ftu-data.service.ts
+++ libs/services/src/lib/ftu-data/ftu-data.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Iri, Url } from '../shared/common.model';
-import { CellSummary, DataFileReference, SourceReference } from './ftu-data.model';
+import { CellSummary, DataFileReference, SourceReference, SvgReference } from './ftu-data.model';
 
 /** Service for loading all data related to a single ftu */
 @Injectable()
 export abstract class FtuDataService {
   abstract getIllustrationUrl(iri: Iri): Observable<Url>;
-  abstract getIllustrationMapping(iri: Iri): Observable<void>; // TODO type
+  abstract getIllustrationMapping(iri: Iri): Observable<SvgReference[]>; // TODO return type and array of name and label objects
   abstract getCellSummaries(iri: Iri): Observable<CellSummary[]>;
   abstract getDataFileReferences(iri: Iri): Observable<DataFileReference[]>;
   abstract getSourceReferences(iri: Iri): Observable<SourceReference[]>;
