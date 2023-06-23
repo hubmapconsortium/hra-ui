985ec51f71190c12b32a8e5c10a84400629d95cd
rchavan10 <rajchavan1099@gmail.com>
2023-06-23 14:18:20 -0700

    added doc coverage

--- libs/services/src/lib/ftu-data/ftu-data.service.ts
+++ libs/services/src/lib/ftu-data/ftu-data.service.ts
@@ -1,14 +1,37 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Iri, Url } from '../shared/common.model';
-import { CellSummary, DataFileReference, SourceReference, SvgReference } from './ftu-data.model';
+import { CellSummary, DataFileReference, IllustrationMappingItem, SourceReference } from './ftu-data.model';
 
 /** Service for loading all data related to a single ftu */
 @Injectable()
 export abstract class FtuDataService {
+  /**
+   * This method takes
+   * @param iri and @returns an observable url
+   */
   abstract getIllustrationUrl(iri: Iri): Observable<Url>;
-  abstract getIllustrationMapping(iri: Iri): Observable<SvgReference[]>; // TODO return type and array of name and label objects
+  /**
+   * This method takes
+   * @param iri and @returns observable of IllustrationMappingItem
+   */
+  abstract getIllustrationMapping(iri: Iri): Observable<IllustrationMappingItem[]>;
+  /**
+   * Gets the
+   * @param iri and
+   * @returns observable of cell summaries
+   */
   abstract getCellSummaries(iri: Iri): Observable<CellSummary[]>;
+  /**
+   * Gets the
+   * @param iri
+   * @returns obeservable of data file references
+   */
   abstract getDataFileReferences(iri: Iri): Observable<DataFileReference[]>;
+  /**
+   * Gets the
+   * @param iri
+   * @returns obeservable of source references
+   */
   abstract getSourceReferences(iri: Iri): Observable<SourceReference[]>;
 }
