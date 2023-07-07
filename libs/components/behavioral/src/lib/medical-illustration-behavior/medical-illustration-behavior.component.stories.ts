e7dbe45cb1463d21404533d2fd4c33e99b0936d6
Knight7561 <rohithvr3@gmail.com>
2023-07-07 12:19:21 -0400

    storybook fix

--- libs/components/behavioral/src/lib/medical-illustration-behavior/medical-illustration-behavior.component.stories.ts
+++ libs/components/behavioral/src/lib/medical-illustration-behavior/medical-illustration-behavior.component.stories.ts
@@ -1,20 +1,16 @@
-import { MedicalIllustrationActions, MedicalIllustrationState } from '@hra-ui/state';
+import { IllustratorActions, IllustratorState } from '@hra-ui/state';
 import { Meta, Story } from '@storybook/angular';
 
 import { MedicalIllustrationBehaviorComponent } from './medical-illustration-behavior.component';
+import { Iri } from '@hra-ui/services';
 
 export default {
   title: 'MedicalIllustrationBehaviorComponent',
   component: MedicalIllustrationBehaviorComponent,
   parameters: {
     state: {
-      states: [MedicalIllustrationState],
-      actions: [
-        new MedicalIllustrationActions.SetMapping('assets/TEMP/mapping.csv'),
-        new MedicalIllustrationActions.SetUri(
-          'https://hubmapconsortium.github.io/ccf-releases/v1.3/2d-ftu/2d-ftu-large-intestine-crypt-lieberkuhn.svg'
-        ),
-      ],
+      states: [IllustratorState],
+      actions: [new IllustratorActions.Load('https://example.com/Kidney/Descending_Thin_Loop_of_Henle' as Iri)],
     },
   },
 } as Meta<MedicalIllustrationBehaviorComponent>;
