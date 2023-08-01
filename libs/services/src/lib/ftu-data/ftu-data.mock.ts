dde56b98690748d98394ad39d90cdadeda4661ac
Knight7561 <rohithvr3@gmail.com>
2023-08-01 13:11:10 -0400

    Dummy Data

--- libs/services/src/lib/ftu-data/ftu-data.mock.ts
+++ libs/services/src/lib/ftu-data/ftu-data.mock.ts
@@ -4,6 +4,47 @@ import { Iri, Url } from '../shared/common.model';
 import { MOCK_TISSUE_DATA } from '../tissue-library/tissue-library.mock';
 import { CellSummary, DataFileReference, IllustrationMappingItem, SourceReference } from './ftu-data.model';
 import { FtuDataService } from './ftu-data.service';
+import { MOCK_SUMMARIES } from '../tissue-ftu/tissue-ftu.mock';
+
+const sourceReferences = [
+  {
+    label: 'Owner Title',
+    link: 'google.com',
+  },
+  {
+    label:
+      '[Dataset Owner Title but extremely long and wraps around to the next line as you can see here in this example]',
+    link: 'google.com',
+  },
+];
+
+const CELL_SUMMARY_DATA: CellSummary[] = Object.values(MOCK_SUMMARIES).map((summary) => ({
+  label: summary.label,
+  cells: summary.entries.map((entry) => ({
+    id: entry.cell.id as Iri,
+    label: entry.cell.label,
+  })),
+  biomarkers: summary.entries.map((entry) => ({
+    id: entry.biomarker.id as Iri,
+    label: entry.biomarker.label,
+  })),
+  summaries: summary.entries.map((entry) => ({
+    cell: entry.cell.id as Iri,
+    biomarker: entry.biomarker.id as Iri,
+    count: entry.count,
+    percentage: entry.percentage,
+  })),
+}));
+
+const testNode = {
+  name: 'Cortical_Collecting_Duct_Principal_Cell_1',
+  label: 'kidney cortex collecting duct principal cell',
+};
+const testMapping = [
+  testNode,
+  { ...testNode, name: 'Cortical_Collecting_Duct_Principal_Cell_2' },
+  { ...testNode, name: 'Cortical_Collecting_Duct_Principal_Cell_3' },
+];
 
 /**
 This class represents a mock implementation of the FtuDataService class.
@@ -28,7 +69,7 @@ export class MockFtuDataService extends FtuDataService {
   @returns An Observable that emits an IllustrationMappingItem array.
   */
   override getIllustrationMapping(iri: Iri): Observable<IllustrationMappingItem[]> {
-    return of([]);
+    return of(testMapping);
   }
 
   /**
@@ -37,7 +78,7 @@ export class MockFtuDataService extends FtuDataService {
   @returns An Observable that emits an CellSummary array.
   */
   override getCellSummaries(iri: Iri): Observable<CellSummary[]> {
-    return of([]);
+    return of(CELL_SUMMARY_DATA);
   }
   /**
   Overrides the getDataFileReferences method to return an DataFileReference array.
@@ -58,6 +99,6 @@ export class MockFtuDataService extends FtuDataService {
   @returns An Observable that emits an empty array.
   */
   override getSourceReferences(iri: Iri): Observable<SourceReference[]> {
-    return of([]);
+    return of(sourceReferences);
   }
 }
