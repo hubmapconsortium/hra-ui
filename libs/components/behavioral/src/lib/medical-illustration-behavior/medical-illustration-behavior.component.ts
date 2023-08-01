1cfd9f660514693a6a20e19385745a75f6b82dda
Knight7561 <rohithvr3@gmail.com>
2023-08-01 13:03:28 -0400

    text box and Source Data

--- libs/components/behavioral/src/lib/medical-illustration-behavior/medical-illustration-behavior.component.ts
+++ libs/components/behavioral/src/lib/medical-illustration-behavior/medical-illustration-behavior.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { dispatch, selectSnapshot } from '@hra-ui/cdk/injectors';
+import { LabelBoxComponent } from '@hra-ui/components/atoms';
 import { InteractiveSvgComponent } from '@hra-ui/components/molecules';
-import { IllustratorActions, IllustratorSelectors } from '@hra-ui/state';
+import { ActiveFtuSelectors, IllustratorActions, IllustratorSelectors, TissueLibrarySelectors } from '@hra-ui/state';
 
 /**
  * Behavior component for medical illustration component
@@ -10,7 +11,7 @@ import { IllustratorActions, IllustratorSelectors } from '@hra-ui/state';
 @Component({
   selector: 'ftu-medical-illustration-behavior',
   standalone: true,
-  imports: [CommonModule, InteractiveSvgComponent],
+  imports: [CommonModule, InteractiveSvgComponent, LabelBoxComponent],
   templateUrl: './medical-illustration-behavior.component.html',
   styleUrls: ['./medical-illustration-behavior.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -25,6 +26,16 @@ export class MedicalIllustrationBehaviorComponent {
    * Current mapping file
    */
   readonly mapping = selectSnapshot(IllustratorSelectors.mapping);
+
+  readonly iri = selectSnapshot(ActiveFtuSelectors.iri);
+
+  readonly tissues = selectSnapshot(TissueLibrarySelectors.tissues);
+
+  get tissueTitle(): string {
+    const iri = this.iri();
+    const tissues = this.tissues();
+    return iri ? tissues[iri].label : '';
+  }
 
   /**
    * Updates the active node on node hover
