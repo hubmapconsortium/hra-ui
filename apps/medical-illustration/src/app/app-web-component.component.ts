5cc987559da1762e6e5c687a078dad0cc1786033
edlu77 <eddd.lu@gmail.com>
2023-08-02 21:03:29 -0400

    Add inputs and outputs attempt

--- apps/medical-illustration/src/app/app-web-component.component.ts
+++ apps/medical-illustration/src/app/app-web-component.component.ts
@@ -1,5 +1,11 @@
 /* eslint-disable @angular-eslint/no-output-on-prefix */
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { InteractiveSvgComponent, NodeMapEntry } from '@hra-ui/components/molecules';
+import { parse } from 'papaparse';
+
+export interface IllustrationData {
+  url: string;
+}
 
 @Component({
   selector: 'hra-root-wc',
@@ -7,4 +13,24 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppWebComponent {}
+export class AppWebComponent implements OnInit {
+  @ViewChild('illustration', { static: true }) readonly illustration?: InteractiveSvgComponent<NodeMapEntry>;
+
+  @Input() url = '';
+
+  @Input() mapping: unknown[] = [];
+
+  @Output() readonly onHover = new EventEmitter<NodeMapEntry>();
+
+  ngOnInit() {
+    if (this.illustration) {
+      this.illustration.url = this.url;
+      parse('../assets/TEMP/mapping.csv', {
+        download: true,
+        header: true,
+        skipEmptyLines: true,
+        complete: (result) => (this.mapping = result.data),
+      });
+    }
+  }
+}
