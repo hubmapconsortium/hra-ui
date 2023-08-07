818b8bbcee6bc08bb645ee07378d47d77912b2ae
Knight7561 <rohithvr3@gmail.com>
2023-08-07 16:48:51 -0400

    Color and Size encoding fix

--- libs/components/organisms/src/lib/biomarker-table/biomarker-table.component.ts
+++ libs/components/organisms/src/lib/biomarker-table/biomarker-table.component.ts
@@ -7,6 +7,12 @@ import {
   DataItem,
 } from '@hra-ui/components/molecules';
 import { HoverDirective } from '@hra-ui/cdk';
+import { GradientPoint, SizeLegend } from '@hra-ui/components/atoms';
+
+/**
+ * RGBTriblet of type RGB to store color
+ */
+type RGBTriplet = [number, number, number];
 
 /**
  * An interface representing a single cell of the table.
@@ -45,6 +51,12 @@ export class BiomarkerTableComponent<T extends DataCell> implements OnChanges {
   /** Rows of the table */
   @Input() data: DataRow<T>[] = [];
 
+  /** Gradient colors along with their stop points */
+  @Input() gradient: GradientPoint[] = [];
+
+  /** Taking input for the radius of the circle and the label to be displayed. */
+  @Input() sizes: SizeLegend[] = [];
+
   /** Getter method to provide the definations of the columns */
   get columnsWithTypeAndCount(): string[] {
     return ['type', 'count', ...this.columns];
@@ -61,5 +73,86 @@ export class BiomarkerTableComponent<T extends DataCell> implements OnChanges {
     if ('data' in changes) {
       this.dataSource.data = this.data;
     }
+  }
+
+  /** Lerp function to give value beween min and max value based on the given value
+   *
+   * @param value
+   * @param min
+   * @param max
+   * @returns
+   */
+  lerp(value: number, min: number, max: number): number {
+    return min * (1 - value) + max * value;
+  }
+
+  /**
+   * Converts HexCode to RGB
+   * @param hex
+   * @returns
+   */
+  hex2rgb(hex: string): RGBTriplet {
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5, 7), 16);
+    return [r, g, b];
+  }
+
+  /**
+   * Gets Min and Max color grade based on the meanExpression value
+   * @param meanExpression
+   * @returns
+   */
+  getMinMaxColor(meanExpression: number): { minColor: RGBTriplet; maxColor: RGBTriplet } {
+    const index = this.gradient.findIndex((item, i, arr) => {
+      return meanExpression >= item.percentage && meanExpression <= arr[i + 1]?.percentage;
+    });
+
+    const minColor: RGBTriplet = this.hex2rgb(this.gradient[index]?.color ?? this.gradient[0].color);
+    const maxColor: RGBTriplet = this.hex2rgb(
+      this.gradient[index + 1]?.color ?? this.gradient[this.gradient.length - 1].color
+    );
+
+    return { minColor, maxColor };
+  }
+
+  /**
+   * Gets Min and Max size grade based on the Percentage value
+   * @param percentage
+   * @returns
+   */
+  getMinMaxSize(percentage: number): { minSize: number; maxSize: number } {
+    const index = this.sizes.findIndex((item, i, arr) => {
+      return percentage >= parseFloat(item.label) / 100 && percentage <= parseFloat(arr[i + 1]?.label) / 100;
+    });
+    const minSize: number = this.sizes[index]?.radius ?? this.sizes[0].radius;
+    const maxSize: number = this.sizes[index + 1]?.radius ?? this.sizes[this.sizes.length - 1].radius;
+    return { minSize, maxSize };
+  }
+
+  /**
+   * Calculates the color of this value on this gradient
+   * @param value
+   * @returns
+   */
+  getColor(value: number): string {
+    const { minColor, maxColor } = this.getMinMaxColor(value);
+    return (
+      '#' +
+      minColor
+        .map((min, index) => this.lerp(value, min, maxColor[index]))
+        .map((component) => Math.round(component).toString(16))
+        .join('')
+    );
+  }
+
+  /**
+   * gets Size of the Cell based on the percentage value
+   * @param value
+   * @returns
+   */
+  getSize(value: number): number {
+    const { minSize, maxSize } = this.getMinMaxSize(value);
+    return this.lerp(value, minSize, maxSize);
   }
 }
