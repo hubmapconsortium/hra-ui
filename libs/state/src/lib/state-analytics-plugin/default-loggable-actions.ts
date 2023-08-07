40fa0145f076229e81b43b186624d1b2cd833be3
rchavan10 <rajchavan1099@gmail.com>
2023-08-07 14:36:47 -0400

    removed uneccesary actions

--- libs/state/src/lib/state-analytics-plugin/default-loggable-actions.ts
+++ libs/state/src/lib/state-analytics-plugin/default-loggable-actions.ts
@@ -2,11 +2,8 @@ import { LinkRegistryActions } from '@hra-ui/cdk/state';
 import { ContactActions } from '../contact';
 import { DownloadActions } from '../download';
 import { ActiveFtuActions } from '../active-ftu';
-import { CellSummaryActions } from '../cell-summary';
 import { IllustratorActions } from '../illustrator';
 import { ScreenModeAction } from '../screen-mode';
-import { SourceRefsActions } from '../source-refs';
-import { TissueLibraryActions } from '../tissue-library';
 
 /**
  * Default loggable actions
@@ -16,9 +13,6 @@ export const DEFAULT_LOGGABLE_ACTIONS: unknown[] = [
   DownloadActions.Download,
   LinkRegistryActions.Navigate,
   ActiveFtuActions.Load,
-  CellSummaryActions.ComputeAggregates,
   IllustratorActions.SetSelection,
   ScreenModeAction.Set,
-  SourceRefsActions.Load,
-  TissueLibraryActions.Load,
 ];
