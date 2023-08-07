7547e03178e2b7211b1632a6ac2e7ed37510a703
rchavan10 <rajchavan1099@gmail.com>
2023-08-07 13:16:55 -0400

    added more default actions

--- libs/state/src/lib/state-analytics-plugin/default-loggable-actions.ts
+++ libs/state/src/lib/state-analytics-plugin/default-loggable-actions.ts
@@ -1,6 +1,12 @@
 import { LinkRegistryActions } from '@hra-ui/cdk/state';
 import { ContactActions } from '../contact';
 import { DownloadActions } from '../download';
+import { ActiveFtuActions } from '../active-ftu';
+import { CellSummaryActions } from '../cell-summary';
+import { IllustratorActions } from '../illustrator';
+import { ScreenModeAction } from '../screen-mode';
+import { SourceRefsActions } from '../source-refs';
+import { TissueLibraryActions } from '../tissue-library';
 
 /**
  * Default loggable actions
@@ -9,4 +15,10 @@ export const DEFAULT_LOGGABLE_ACTIONS: unknown[] = [
   ContactActions.SendMessage,
   DownloadActions.Download,
   LinkRegistryActions.Navigate,
+  ActiveFtuActions.Load,
+  CellSummaryActions.ComputeAggregates,
+  IllustratorActions.SetSelection,
+  ScreenModeAction.Set,
+  SourceRefsActions.Load,
+  TissueLibraryActions.Load,
 ];
