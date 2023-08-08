6661dc12ac640f9bd900c26877289fffe9529e3d
Knight7561 <rohithvr3@gmail.com>
2023-08-08 13:06:45 -0400

    fix tests

--- libs/components/organisms/src/lib/biomarker-table/biomarker-table.component.spec.ts
+++ libs/components/organisms/src/lib/biomarker-table/biomarker-table.component.spec.ts
@@ -2,6 +2,7 @@ import { BiomarkerTableComponent, DataCell, DataRow } from './biomarker-table.co
 import { Shallow } from 'shallow-render';
 import { MatTableModule } from '@angular/material/table';
 import { HoverDirective } from '@hra-ui/cdk';
+import { GradientPoint, SizeLegend } from '@hra-ui/components/atoms';
 
 describe('BiomarkerTableComponent', () => {
   const columns = ['RGMB', 'SOX9', 'CD44', 'LGR5'];
@@ -24,6 +25,25 @@ describe('BiomarkerTableComponent', () => {
     ],
     ['goblet cell', undefined, undefined, undefined],
   ];
+  const gradient: GradientPoint[] = [
+    {
+      color: '#00000f',
+      percentage: 0,
+    },
+    {
+      color: '#00ffff',
+      percentage: 0,
+    },
+    {
+      color: '#ffffff',
+      percentage: 100,
+    },
+  ];
+  const sizes: SizeLegend[] = [
+    { label: '0%', radius: 0.1 },
+    { label: '50%', radius: 0.5 },
+    { label: '100%', radius: 1.0 },
+  ];
 
   let shallow: Shallow<BiomarkerTableComponent<DataCell>>;
 
@@ -32,11 +52,16 @@ describe('BiomarkerTableComponent', () => {
   });
 
   it('should create BiomarkerTableComponent', async () => {
-    await expect(shallow.render({ bind: { columns: columns, data: data } })).resolves.toBeDefined();
+    await expect(shallow.render({ bind: { columns: columns, data: data, gradient, sizes } })).resolves.toBeDefined();
   });
 
   it('should update dataSource', async () => {
-    const { instance } = await shallow.render({ bind: { columns: columns, data: data } });
+    const { instance } = await shallow.render({ bind: { columns: columns, data: data, gradient, sizes } });
     expect(instance.dataSource.data).toBe(data);
+  });
+
+  it('returns a size - getMinMaxSize function', async () => {
+    const { instance } = await shallow.render({ bind: { columns: columns, data: data, gradient, sizes } });
+    expect(instance.getColor(0.9)).toBeDefined();
   });
 });
