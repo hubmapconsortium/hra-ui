ab12fae54b019cb1dd0ac0ad09c96edee2143fc2
Knight7561 <rohithvr3@gmail.com>
2023-08-21 17:59:56 -0400

    Initial Project Setup

---
+++ apps/ftu-ui-small-wc/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'hra-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ftu-ui-small-wc';
+}
