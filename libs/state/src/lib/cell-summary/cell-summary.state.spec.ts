c19d86cc2081f2a51d313760f3d8743a09bc7989
rchavan10 <rajchavan1099@gmail.com>
2023-08-21 16:08:48 -0400

    intial test case covered

--- libs/state/src/lib/cell-summary/cell-summary.state.spec.ts
+++ libs/state/src/lib/cell-summary/cell-summary.state.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { FtuDataService, Iri } from '@hra-ui/services';
+import { CellSummary, FtuDataService, Iri } from '@hra-ui/services';
 import { MockProxy, mock } from 'jest-mock-extended';
 import { CellSummaryState } from './cell-summary.state';
 import { of } from 'rxjs';
@@ -9,21 +9,24 @@ import { Load } from './cell-summary.actions';
 
 describe('CellSummaryState', () => {
   const testIri = 'https://www.example.com/test-iri' as Iri;
+  let state: CellSummaryState;
+  const mockModel: CellSummaryModel = {
+    summaries: [],
+    aggregates: [],
+  };
 
   let ctx: MockProxy<StateContext<CellSummaryModel>>;
-  let state: CellSummaryState;
   let dataService: MockProxy<FtuDataService>;
 
   beforeEach(() => {
     TestBed.overrideProvider(FtuDataService, {
       useValue: (dataService = mock()),
     });
-
     state = TestBed.runInInjectionContext(() => new CellSummaryState());
     dataService.getCellSummaries.mockReturnValue(of([]));
     ctx = mock();
 
-    ctx.getState.mockReturnValue({ summaries: undefined, aggregates: undefined });
+    ctx.getState.mockReturnValue({ summaries: [], aggregates: [] });
   });
 
   describe('load(ctx, action)', () => {
@@ -35,23 +38,24 @@ describe('CellSummaryState', () => {
     it('should load the cell summaries', () => {
       const summaries: any[] = [];
       dataService.getCellSummaries.mockReturnValue(of(summaries));
-      state.load(ctx, new Load(testIri));
-      expect(ctx.patchState).toHaveBeenCalledWith({ aggregates: [], summaries: summaries });
+      state.load(ctx, new Load(testIri)).subscribe();
+      expect(ctx.patchState).toHaveBeenCalledWith({ summaries: summaries, aggregates: [] });
     });
   });
 
-  describe('computeAggregates', () => {
-    it('should compute aggregrate data and update state', () => {
-      ctx.getState.mockReturnValue({ summaries });
-      state.computeAggregates(ctx);
-      expect(ctx.patchState).toHaveBeenCalledWith();
-    });
-  });
+  // describe('computeAggregates', () => {
+  //   it('should compute aggregrate data and update state', () => {
+  //     const mockSummaries: CellSummaryModel = []
+  //     ctx.getState.mockReturnValue({ summaries: mockSummaries });
+  //     state.computeAggregates(ctx);
+  //     expect(ctx.patchState).toHaveBeenCalledWith();
+  //   });
+  // });
 
-  describe('reset', () => {
-    it('should reset summaries and aggregates', () => {
-      state.reset(ctx);
-      expect(ctx.patchState).toHaveBeenCalledWith({ aggregates: [], summaries: [] });
-    });
-  });
+  // describe('reset', () => {
+  //   it('should reset summaries and aggregates', () => {
+  //     state.reset(ctx);
+  //     expect(ctx.patchState).toHaveBeenCalledWith({ aggregates: [], summaries: [] });
+  //   });
+  // });
 });
