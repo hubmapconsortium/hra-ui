9d70da8e50dc8ca4f71c4efda054f1836956733b
rchavan10 <rajchavan1099@gmail.com>
2023-08-21 10:28:42 -0400

    test coverage

--- libs/state/src/lib/cell-summary/cell-summary.state.spec.ts
+++ libs/state/src/lib/cell-summary/cell-summary.state.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-import { FtuDataService } from '@hra-ui/services';
+import { FtuDataService, Iri } from '@hra-ui/services';
 import { MockProxy, mock } from 'jest-mock-extended';
 import { CellSummaryState } from './cell-summary.state';
 import { of } from 'rxjs';
+import { StateContext } from '@ngxs/store';
+import { CellSummaryModel } from './cell-summary.model';
+import { Load } from './cell-summary.actions';
 
 describe('CellSummaryState', () => {
+  const testIri = 'https://www.example.com/test-iri' as Iri;
+
+  let ctx: MockProxy<StateContext<CellSummaryModel>>;
   let state: CellSummaryState;
   let dataService: MockProxy<FtuDataService>;
 
@@ -15,5 +21,37 @@ describe('CellSummaryState', () => {
 
     state = TestBed.runInInjectionContext(() => new CellSummaryState());
     dataService.getCellSummaries.mockReturnValue(of([]));
+    ctx = mock();
+
+    ctx.getState.mockReturnValue({ summaries: undefined, aggregates: undefined });
+  });
+
+  describe('load(ctx, action)', () => {
+    it('should load the correct iri', () => {
+      state.load(ctx, new Load(testIri));
+      expect(dataService.getCellSummaries).toHaveBeenCalledWith(testIri);
+    });
+
+    it('should load the cell summaries', () => {
+      const summaries: any[] = [];
+      dataService.getCellSummaries.mockReturnValue(of(summaries));
+      state.load(ctx, new Load(testIri));
+      expect(ctx.patchState).toHaveBeenCalledWith({ aggregates: [], summaries: summaries });
+    });
+  });
+
+  describe('computeAggregates', () => {
+    it('should compute aggregrate data and update state', () => {
+      ctx.getState.mockReturnValue({ summaries });
+      state.computeAggregates(ctx);
+      expect(ctx.patchState).toHaveBeenCalledWith();
+    });
+  });
+
+  describe('reset', () => {
+    it('should reset summaries and aggregates', () => {
+      state.reset(ctx);
+      expect(ctx.patchState).toHaveBeenCalledWith({ aggregates: [], summaries: [] });
+    });
   });
 });
