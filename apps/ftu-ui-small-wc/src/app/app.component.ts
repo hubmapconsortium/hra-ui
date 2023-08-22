8db51c5a647e87b8de923517775c7701ac3a489d
Knight7561 <rohithvr3@gmail.com>
2023-08-22 11:32:32 -0400

    app module and component refactor

--- apps/ftu-ui-small-wc/src/app/app.component.ts
+++ apps/ftu-ui-small-wc/src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-  standalone: true,
-  imports: [],
   selector: 'hra-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
