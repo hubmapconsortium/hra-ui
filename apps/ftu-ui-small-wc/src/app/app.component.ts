4fae6f92b8b6e3f4bb66778abfe081026731d9f6
Knight7561 <rohithvr3@gmail.com>
2023-08-30 14:14:00 -0400

    Build chnages single bundle

--- apps/ftu-ui-small-wc/src/app/app.component.ts
+++ apps/ftu-ui-small-wc/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { selectSnapshot } from '@hra-ui/cdk/injectors';
+import { ChangeDetectionStrategy, Component, Input, Output, ViewEncapsulation } from '@angular/core';
+import { dispatch, select$, selectSnapshot } from '@hra-ui/cdk/injectors';
+import { LinkRegistryActions, ResourceRegistryActions, createLinkId } from '@hra-ui/cdk/state';
 import {
   BiomarkerDetailsWcComponent,
   FooterBehaviorComponent,
@@ -8,7 +9,7 @@ import {
   TissueLibraryBehaviorComponent,
 } from '@hra-ui/components/behavioral';
 import { FullscreenContainerComponent, FullscreenContentComponent } from '@hra-ui/components/molecules';
-import { ScreenModeSelectors } from '@hra-ui/state';
+import { ActiveFtuSelectors, IllustratorSelectors, ScreenModeSelectors } from '@hra-ui/state';
 
 @Component({
   selector: 'hra-root',
@@ -31,4 +32,26 @@ export class AppComponent {
   title = 'ftu-ui-small-wc';
 
   readonly isFullscreen = selectSnapshot(ScreenModeSelectors.isFullScreen);
+
+  @Input() set linksYamlUrl(url: string) {
+    this.loadLinks(url);
+  }
+
+  @Input() set resourcesYamlUrl(url: string) {
+    this.loadResources(url);
+  }
+
+  @Input() set organIri(iri: string) {
+    this.navigateToOrgan(createLinkId('FTU'), { queryParams: { id: iri } });
+  }
+
+  @Output() readonly organSelected = select$(ActiveFtuSelectors.iri);
+
+  @Output() readonly nodeHovered = select$(IllustratorSelectors.selected);
+
+  @Output() readonly nodeClicked = select$(IllustratorSelectors.selected);
+
+  private readonly loadLinks = dispatch(LinkRegistryActions.LoadFromYaml);
+  private readonly loadResources = dispatch(ResourceRegistryActions.LoadFromYaml);
+  private readonly navigateToOrgan = dispatch(LinkRegistryActions.Navigate);
 }
