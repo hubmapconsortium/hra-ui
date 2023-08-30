bc91731ac475df71ea576640e9a56970d94679e5
rchavan10 <rajchavan1099@gmail.com>
2023-08-30 11:28:01 -0400

    removed mock services

--- libs/services/src/lib/service.module.ts
+++ libs/services/src/lib/service.module.ts
@@ -1,27 +1,14 @@
 import { NgModule } from '@angular/core';
+import { ContactImplService } from './contact/contact.impl';
 import { ContactService } from './contact/contact.service';
 import { FTU_DATA_IMPL_ENDPOINTS, FtuDataImplService } from './ftu-data/ftu-data.impl';
 import { FtuDataService } from './ftu-data/ftu-data.service';
-import { MockTissueFtuService } from './tissue-ftu/tissue-ftu.mock';
-import { TissueFtuService } from './tissue-ftu/tissue-ftu.service';
-import { MockTissueLibraryService } from './tissue-library/tissue-library.mock';
-import { TissueLibraryService } from './tissue-library/tissue-library.service';
-import { ContactImplService } from './contact/contact.impl';
 
 @NgModule({
   providers: [
-    // TODO replace with real services
     {
       provide: ContactService,
       useExisting: ContactImplService,
-    },
-    {
-      provide: TissueLibraryService,
-      useExisting: MockTissueLibraryService,
-    },
-    {
-      provide: TissueFtuService,
-      useExisting: MockTissueFtuService,
     },
     {
       provide: FtuDataService,
