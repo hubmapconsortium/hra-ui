3d6321fe56534c3a8a4c5bec28e346fa6bd1d635
rchavan10 <rajchavan1099@gmail.com>
2023-08-31 14:23:21 -0400

    rollback changes

--- libs/components/behavioral/src/lib/footer-behavior/footer-behavior.component.ts
+++ libs/components/behavioral/src/lib/footer-behavior/footer-behavior.component.ts
@@ -2,11 +2,10 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatDialog, MatDialogConfig, MatDialogModule } from '@angular/material/dialog';
 import { dispatch, selectQuerySnapshot, selectSnapshot } from '@hra-ui/cdk/injectors';
-import { LinkRegistryActions, LinkType, ResourceRegistrySelectors as RR, createLinkId } from '@hra-ui/cdk/state';
+import { ResourceRegistrySelectors as RR } from '@hra-ui/cdk/state';
 import { FooterComponent } from '@hra-ui/components/molecules';
 import { DownloadActions, DownloadSelectors, ResourceIds as Ids, LinkIds } from '@hra-ui/state';
 import { ContactBehaviorComponent } from '../contact-behavior/contact-behavior.component';
-import { Router } from '@angular/router';
 
 /** A component for footer behavior which provides functionality for various buttons in footer */
 @Component({
@@ -27,17 +26,11 @@ export class FooterBehaviorComponent {
   /** Selects a snapshot of the current value of the available download formats */
   readonly downloadFormats = selectSnapshot(DownloadSelectors.formats);
 
-  /** A dispatch function to navigate to the Illustration */
-  readonly navigateToIllustration = dispatch(LinkRegistryActions.Navigate);
-
-  /** A dispatch function which adds the link to the registry */
-  readonly addLinkToRegistry = dispatch(LinkRegistryActions.Add);
-
-  /** Injecting the router */
-  private router = inject(Router);
-
   /** Hra portal link routing after clicking on HRA Portal */
   readonly hraPortal = LinkIds.Portal;
+
+  /** Metadata page link routing after clicking on Illustration */
+  readonly illustrationMetadata = LinkIds.Illustration;
 
   /** Embed link routing after clicking on Embed */
   readonly embed = LinkIds.Embed;
@@ -53,13 +46,5 @@ export class FooterBehaviorComponent {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
     this.dialog.open(ContactBehaviorComponent, dialogConfig);
-  }
-  /** A function which gets the organ and sets the Illustration link correctly */
-  illustrationLink(): void {
-    const organName = this.router.url.split('2F').slice(-1)[0];
-    const url = `https://hubmapconsortium.github.io/ccf-releases/v1.4/docs/2d-ftu/2d-ftu-${organName}.html`;
-    const IllustrationLink = createLinkId(organName);
-    this.addLinkToRegistry(IllustrationLink, { type: LinkType.External, url: url });
-    this.navigateToIllustration(IllustrationLink);
   }
 }
