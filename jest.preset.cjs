348fd60f932105223e1eb599cada3acdf3571a75
bhushankhope <bhushankhope@gmail.com>
2023-08-31 11:56:35 -0400

    Switch testing to jest!

---
+++ jest.preset.cjs
@@ -0,0 +1,15 @@
+const nxPreset = require('@nx/jest/preset').default;
+
+module.exports = {
+  ...nxPreset,
+  collectCoverage: true,
+  coveragePathIgnorePatterns: ['/node_modules/', 'index.ts', '.*-routing.module.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: 85,
+    },
+  },
+}
