ac4ac10cbcfc5ea24a072b348d595916ad02968f
Knight7561 <rohithvr3@gmail.com>
2023-09-07 15:39:59 -0400

    Aftu-ui index.html tweek

--- apps/ftu-ui/src/app/app.component.ts
+++ apps/ftu-ui/src/app/app.component.ts
@@ -1,7 +1,16 @@
-import { AfterContentInit, Component, HostBinding, Input, Output, inject } from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  HostBinding,
+  Injector,
+  Input,
+  Output,
+  SimpleChanges,
+  inject,
+} from '@angular/core';
 import { MatDialog, MatDialogConfig, MatDialogModule } from '@angular/material/dialog';
 import { Router } from '@angular/router';
-import { dispatch, select$, selectQuerySnapshot } from '@hra-ui/cdk/injectors';
+import { dispatch, dispatch$, select$, selectQuerySnapshot } from '@hra-ui/cdk/injectors';
 import {
   LinkRegistryActions,
   ResourceRegistryActions,
@@ -10,7 +19,16 @@ import {
   createLinkId,
 } from '@hra-ui/cdk/state';
 import { ScreenNoticeBehaviorComponent } from '@hra-ui/components/behavioral';
-import { ActiveFtuSelectors, IllustratorSelectors } from '@hra-ui/state';
+import { FtuDataImplEndpoints, FTU_DATA_IMPL_ENDPOINTS, Iri } from '@hra-ui/services';
+import {
+  ActiveFtuActions,
+  ActiveFtuSelectors,
+  IllustratorSelectors,
+  ScreenModeAction,
+  TissueLibraryActions,
+  TissueLibrarySelectors,
+} from '@hra-ui/state';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'ftu-ui-root',
@@ -22,6 +40,7 @@ export class AppComponent implements AfterContentInit {
   @HostBinding('class.mat-typography') readonly matTypography = true;
 
   readonly SMALL_VIEWPORT_THRESHOLD = 480; // In pixels
+  readonly tissues = select$(TissueLibrarySelectors.tissues);
 
   @Input() set linksYamlUrl(url: string) {
     this.loadLinks(url);
@@ -35,11 +54,18 @@ export class AppComponent implements AfterContentInit {
     this.navigateToOrgan(createLinkId('FTU'), { queryParams: { id: iri } });
   }
 
+  @Input() datasetUrl = '';
+  @Input() illustrationsUrl = '';
+  @Input() summariesUrl = '';
+
   @Output() readonly organSelected = select$(ActiveFtuSelectors.iri);
 
   @Output() readonly nodeHovered = select$(IllustratorSelectors.selectedOnHovered);
 
   @Output() readonly nodeClicked = select$(IllustratorSelectors.selectedOnClicked);
+
+  private readonly injector = inject(Injector);
+  private endpoints?: FtuDataImplEndpoints;
 
   screenSizeNoticeOpen = false;
 
@@ -52,6 +78,11 @@ export class AppComponent implements AfterContentInit {
   private readonly loadLinks = dispatch(LinkRegistryActions.LoadFromYaml);
   private readonly loadResources = dispatch(ResourceRegistryActions.LoadFromYaml);
   private readonly navigateToOrgan = dispatch(LinkRegistryActions.Navigate);
+  private readonly setScreenSmall = dispatch(ScreenModeAction.SetSize);
+  private readonly reloadDataSets = dispatch(TissueLibraryActions.Load);
+  private readonly reloadActiveFtu = dispatch(ActiveFtuActions.Load);
+
+  private readonly reset = dispatch$(ActiveFtuActions.Reset);
 
   private readonly dialog = inject(MatDialog);
 
@@ -75,5 +106,41 @@ export class AppComponent implements AfterContentInit {
       ref.afterClosed().subscribe(() => (this.screenSizeNoticeOpen = false));
       this.screenSizeNoticeOpen = true;
     }
+  }
+
+  ngOnInit() {
+    this.endpoints = this.injector.get(FTU_DATA_IMPL_ENDPOINTS);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.endpoints = this.injector.get(FTU_DATA_IMPL_ENDPOINTS);
+    if ('datasetUrl' in changes) {
+      this.endpoints.datasets = this.datasetUrl as Iri;
+      this.endpoints.illustrations = this.illustrationsUrl as Iri;
+      this.endpoints.summaries = this.summariesUrl as Iri;
+      this.reset()
+        .pipe(
+          tap(() => {
+            this.reloadDataSets();
+            this.reloadActiveFtu(changes['organIri'].currentValue);
+          })
+        )
+        .subscribe();
+    }
+  }
+
+  showDefaultIri() {
+    this.tissues
+      .pipe(
+        tap((nodes) => {
+          for (const [key, { children }] of Object.entries(nodes)) {
+            if (children.length > 0 && key) {
+              this.organIri = children[0];
+              break;
+            }
+          }
+        })
+      )
+      .subscribe();
   }
 }
