d31f9a5d5d2cc99ec6752930624c475a40fd9b62
bhushankhope <bhushankhope@gmail.com>
2023-09-11 14:05:12 -0400

    create tests for download ftu comp

--- apps/ccf/src/app/components/download-ftu/download-ftu.component.spec.ts
+++ apps/ccf/src/app/components/download-ftu/download-ftu.component.spec.ts
@@ -1,12 +1,31 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DownloadFtuComponent } from './download-ftu.component';
 import { DownloadFtuModule } from './download-ftu.module';
 
+import { ChooseVersion } from '../choose-version/choose-version';
 import { Shallow } from 'shallow-render';
 import { FtuVersionData } from './download-ftu';
 
 describe('DownloadFtuComponent', () => {
+  const testData: FtuVersionData[] = [
+    { version: '1.0', rows: [] },
+    {
+      version: '2.0', rows: [
+        {
+          label: '',
+          url: '',
+          links: [],
+          releaseVersion: '',
+          dot: ''
+        }
+      ]
+    },
+  ];
+  const testVersions: ChooseVersion[] = [
+    { version: '1.0', release: '1' },
+    { version: '2.0', release: '2' },
+    { version: '3.0', release: '3' }
+  ];
+
   let shallow: Shallow<DownloadFtuComponent>;
 
   beforeEach(async () => {
@@ -17,16 +36,44 @@ describe('DownloadFtuComponent', () => {
     expect(shallow.render()).toBeDefined();
   });
 
-  it('display metadata should be false', async () => {
+  it('display metadata should be false by default', async () => {
     const { instance } = await shallow.render();
     expect(instance.displayMetadata).toBe(false);
   })
 
-  it('should return an array of FtuVersionData', async () => {
-    const ftuVersionData: FtuVersionData[] = []
-    const { instance } = await shallow.render();
-    instance['_data'] = ftuVersionData;
-    const result = instance.data;
-    expect(result).toEqual(ftuVersionData);
+  describe('.data [get/set]', () => {
+    it('should update the selection when set', async () => {
+      const { instance } = await shallow.render();
+      const spy = spyOn(instance, 'updateSelection');
+      instance.data = testData;
+      expect(spy).toHaveBeenCalled();
+    })
+  });
+
+  describe('.displayedColumns [get]', () => {
+    it('should return additional columns when displayMetadata is true', async () => {
+      const { instance } = await shallow.render({ bind: { displayMetadata: true } });
+      expect(instance.displayedColumns).toEqual(['label', 'links', 'releaseVersion', 'digitalObjectType'])
+    });
   })
+
+  describe('updateSelection(selectedVersion?)', () => {
+    it('should return early when versions or data are empty', async () => {
+      const { instance } = await shallow.render();
+      instance.updateSelection();
+      expect(instance.selectedData.data).toEqual([]);
+    });
+
+    it('should update the current selection', async () => {
+      const { instance } = await shallow.render({ bind: { data: testData, versions: testVersions } });
+      instance.updateSelection(testVersions[1]);
+      expect(instance.selectedData.data).toEqual(testData[1].rows);
+    });
+
+    it('should update the current selection when versions do not match', async () => {
+      const { instance } = await shallow.render({ bind: { data: testData, versions: testVersions } });
+      instance.updateSelection(testVersions[2]);
+      expect(instance.selectedData.data).toEqual([]);
+    });
+  });
 });
