90f6b838d959072d1d3dd529acce41f9296399e0
bhushankhope <bhushankhope@gmail.com>
2023-09-12 12:09:03 -0400

    added tests for menu tree

--- apps/ccf/src/app/components/menu-tree/menu-tree.component.spec.ts
+++ apps/ccf/src/app/components/menu-tree/menu-tree.component.spec.ts
@@ -1,22 +1,64 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MenuTreeComponent } from './menu-tree.component';
+import { MenuTreeModule } from './menu-tree.module';
+import { Shallow } from 'shallow-render';
+import { NavItems } from '../toolbar/nav-items';
+import { Overlay } from '@angular/cdk/overlay';
 
 describe('MenuTreeComponent', () => {
-  let component: MenuTreeComponent;
-  let fixture: ComponentFixture<MenuTreeComponent>;
+  let shallow: Shallow<MenuTreeComponent>;
+  const testTreeItems: NavItems[] = [
+    {
+      menuName: 'test'
+    }
+  ]
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [MenuTreeComponent],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(MenuTreeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    shallow = new Shallow(MenuTreeComponent, MenuTreeModule).mock(Overlay, {
+      scrollStrategies: {
+        block: jest.fn()
+      }
+    })
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(shallow.render()).toBeDefined();
   });
+
+  describe('ngOnInit()', () => {
+    it('should assign tree items to datasource', async () => {
+      const { instance } = await shallow.render({ bind: { treeItems: testTreeItems } });
+      expect(instance.dataSource.data).toEqual(testTreeItems)
+    })
+  })
+
+  describe('hasChild', () => {
+    const testNode: NavItems = {
+      menuName: 'test',
+      children: []
+    }
+    it('should return if node has children', async () => {
+      const { instance } = await shallow.render();
+      const hasChildren = instance.hasChild(0, testNode);
+      expect(hasChildren).toEqual(false)
+
+    })
+  })
+
+  describe('externalWindow()', () => {
+    it('should open url in external window', async () => {
+      const testUrl = 'www.example.com';
+      const { instance } = await shallow.render();
+      const spy = jest.spyOn(window, 'open')
+      instance.externalWindow(testUrl)
+      expect(spy).toHaveBeenCalledWith(testUrl, "_blank");
+    })
+  })
+
+  describe('scroll to the section', () => {
+    it('should scroll to the section of the menu item', async () => {
+      const testId: string = 'intro';
+      const { instance } = await shallow.render();
+      
+    })
+  })
 });
