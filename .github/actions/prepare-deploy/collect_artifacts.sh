2287cef7db567a9103ca425baa77b36a7cf7d8a7
bhushankhope <bhushankhope@gmail.com>
2023-10-05 16:09:42 -0400

    Debugging build preview...

--- .github/actions/prepare-deploy/collect_artifacts.sh
+++ .github/actions/prepare-deploy/collect_artifacts.sh
@@ -18,6 +18,9 @@ out_dir="$1"
 mkdir -p "${out_dir}"
 shift
 
+echo dist/*
+echo $@
+
 while (( "$#" )); do
   if [[ -d $1 ]]; then
     # Extract directory name (https://stackoverflow.com/a/1371283)
