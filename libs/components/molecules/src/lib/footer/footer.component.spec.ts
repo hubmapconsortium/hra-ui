38c56ef319724d8b6d134c46dcc4ea7f91f93196
edlu77 <eddd.lu@gmail.com>
2023-12-11 19:54:36 -0500

    Fix download list toggling

--- libs/components/molecules/src/lib/footer/footer.component.spec.ts
+++ libs/components/molecules/src/lib/footer/footer.component.spec.ts
@@ -1,9 +1,10 @@
 import { OverlayModule } from '@angular/cdk/overlay';
 import { MatListModule } from '@angular/material/list';
-import { Shallow } from 'shallow-render';
-import { FooterComponent } from './footer.component';
-import { selectQuerySnapshot } from '@hra-ui/cdk/injectors';
 import { LinkDirective } from '@hra-ui/cdk';
+import { selectQuerySnapshot } from '@hra-ui/cdk/injectors';
+import { Shallow } from 'shallow-render';
+
+import { FooterComponent } from './footer.component';
 
 jest.mock('@hra-ui/cdk/injectors');
 jest.mocked(selectQuerySnapshot).mockReturnValue(() => undefined as never);
@@ -17,5 +18,25 @@ describe('FooterComponent', () => {
 
   it('should create', async () => {
     await expect(shallow.render()).resolves.toBeDefined();
+  });
+
+  it('should do nothing if download list is open and the download button is clicked', async () => {
+    const mockEvent = {
+      target: { innerText: 'download' } as unknown,
+    } as MouseEvent;
+    const { instance } = await shallow.render();
+    instance.downloadListOpen = true;
+    instance.handleOutsideClick(mockEvent);
+    expect(instance.downloadListOpen).toBeTruthy();
+  });
+
+  it('should close the download list if open and the user clicks somewhere besides the button', async () => {
+    const mockEvent = {
+      target: { innerText: 'Somewhere Else' } as unknown,
+    } as MouseEvent;
+    const { instance } = await shallow.render();
+    instance.downloadListOpen = true;
+    instance.handleOutsideClick(mockEvent);
+    expect(instance.downloadListOpen).toBeFalsy();
   });
 });
