e1559209b0b7de361d26e08029377c61f4b3d1a5
bherr2 <bherr2@users.noreply.github.com>
2023-12-14 13:34:17 +0000

    Staging deploy -- a2c9e6bb26f675c2a68a9c80f3d6b2f17a3468ee

--- compodoc/state/js/menu-wc.js
+++ compodoc/state/js/menu-wc.js
@@ -58,6 +58,9 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                             <li class="link">
                                 <a href="modules/HraStateModule.html" data-type="entity-link" >HraStateModule</a>
                             </li>
+                            <li class="link">
+                                <a href="modules/MouseTrackerModule.html" data-type="entity-link" >MouseTrackerModule</a>
+                            </li>
                 </ul>
                 </li>
                     <li class="chapter">
