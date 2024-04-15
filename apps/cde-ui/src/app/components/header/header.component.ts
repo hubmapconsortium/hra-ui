e2ba057b88051470dfa8dda53d1513e75b75f544
bhushankhope <bhushankhope@gmail.com>
2024-04-15 14:48:24 -0400

    create header

---
+++ apps/cde-ui/src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'cde-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HeaderComponent {}
