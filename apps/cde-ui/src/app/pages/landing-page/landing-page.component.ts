c9ca4fe0ce416cefcfacc3d1da34f9d6bd6b62d7
bhushankhope <bhushankhope@gmail.com>
2024-04-16 12:21:59 -0400

    add routes to cards

--- apps/cde-ui/src/app/pages/landing-page/landing-page.component.ts
+++ apps/cde-ui/src/app/pages/landing-page/landing-page.component.ts
@@ -4,6 +4,9 @@ import { VisualCard, VisualCardComponent } from '../../components/visual-card/vi
 import { MatIconModule } from '@angular/material/icon';
 import { YouTubePlayerModule } from '@angular/youtube-player';
 
+/**
+ * Landing Page Component
+ */
 @Component({
   selector: 'cde-landing-page',
   standalone: true,
@@ -13,21 +16,25 @@ import { YouTubePlayerModule } from '@angular/youtube-player';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LandingPageComponent {
-  data: VisualCard[] = [
+  /** Input data for card */
+  readonly data: VisualCard[] = [
     {
       image: '../../assets/examples/intestine.svg',
       label: 'Explore 2D Intestine Data',
-      url: 'https://www.google.com',
+      route: 'vis-app',
+      alt: 'Image for Intestine',
     },
     {
       image: '../../assets/examples/skin.svg',
       label: 'Explore 3D Skin Data',
-      url: 'https://www.google.com',
+      route: 'home',
+      alt: 'Image for Skin',
     },
     {
       image: '../../assets/examples/tonsil.svg',
       label: 'Create a Visualization',
-      url: 'https://www.google.com',
+      route: 'create',
+      alt: 'Image for Tonsil',
     },
   ];
 }
