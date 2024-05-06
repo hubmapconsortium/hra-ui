49cb59248967eab4a49ccb812b5b4c8b7e181f4a
bhushankhope <bhushankhope@gmail.com>
2024-05-06 11:10:11 -0700

    create cde-visualization wc

--- libs/cde-visualization/src/index.ts
+++ libs/cde-visualization/src/index.ts
@@ -1,1 +1,17 @@
+import { createApplication } from '@angular/platform-browser';
+import { createCustomElement } from '@angular/elements';
+import { CdeVisualizationComponent } from './lib/cde-visualization/cde-visualization.component';
+
+(async () => {
+  const app = await createApplication({
+    providers: [],
+  });
+
+  const visualizationElement = createCustomElement(CdeVisualizationComponent, {
+    injector: app.injector,
+  });
+
+  customElements.define('cde-visualization', visualizationElement);
+})();
+
 export * from './lib/cde-visualization/cde-visualization.component';
