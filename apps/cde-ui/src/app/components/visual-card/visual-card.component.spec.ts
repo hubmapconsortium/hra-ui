f69ea4061d7fc827c2ee3006eadb0ba3484d3238
bhushankhope <bhushankhope@gmail.com>
2024-05-10 07:58:53 -0700

    update landing page tests

--- apps/cde-ui/src/app/components/visual-card/visual-card.component.spec.ts
+++ apps/cde-ui/src/app/components/visual-card/visual-card.component.spec.ts
@@ -1,14 +1,17 @@
-import { VisualCardComponent } from './visual-card.component';
-import { Shallow } from 'shallow-render';
+import { VisualCardComponent, VisualCard } from './visual-card.component';
+import { render, screen } from '@testing-library/angular';
 
 describe('VisualCardComponent', () => {
-  let shallow: Shallow<VisualCardComponent>;
-
-  beforeEach(async () => {
-    shallow = new Shallow(VisualCardComponent);
-  });
-
-  it('should create', async () => {
-    await expect(shallow.render()).resolves.toBeDefined();
+  it('card data is present on the page', async () => {
+    const testCardData: VisualCard[] = [{ image: 'img.svg', label: 'label', route: 'test route', alt: 'alt text' }];
+    await render(VisualCardComponent, {
+      componentInputs: {
+        cardData: testCardData,
+      },
+    });
+    expect(screen.getByText(/img.svg/i)).toBeInTheDocument();
+    expect(screen.getByText(/label/i)).toBeInTheDocument();
+    expect(screen.getByText(/test route/i)).toBeInTheDocument();
+    expect(screen.getByText(/alt text/i)).toBeInTheDocument();
   });
 });
