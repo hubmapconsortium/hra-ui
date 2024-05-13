d61b167c38d2f68af61b019d132669155494bda7
Daniel Bolin <axbolin@iu.edu>
2024-05-13 17:08:43 -0400

    refactor(cde): :lipstick: Update the header along with the entire theming structure

--- apps/cde-ui/src/app/components/header/header.component.ts
+++ apps/cde-ui/src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 
 /**
@@ -8,7 +9,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'cde-header',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
