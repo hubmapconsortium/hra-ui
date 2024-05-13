cb918b860692473be41d7476d5ce486902896af9
Daniel Bolin <axbolin@iu.edu>
2024-05-13 18:28:33 -0400

    docs(cdk): :memo: Add code documentation for icon providers

--- libs/cdk/icons/src/lib/providers.ts
+++ libs/cdk/icons/src/lib/providers.ts
@@ -4,30 +4,49 @@ import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { createSvgIconResolver } from './resolvers';
 
+/** Font icon provider configuration */
 export interface FontIconsConfig {
+  /** Default classes to apply when no font set is specified */
   defaultClasses?: string[];
 }
 
+/** Svg icon provider configuration */
 export interface SvgIconsConfig {
+  /** Base directory containing icons and namespace folders */
   directory?: string;
 }
 
+/** Configuration when providing font icons and svg icon at the same time */
 export interface IconsConfig {
+  /** Font icon configuration */
   fontIcons?: FontIconsConfig;
+  /** Svg icon configuration */
   svgIcons?: SvgIconsConfig;
 }
 
+/** Injection token for font icon configuration */
 const FONT_ICONS_CONFIG = new InjectionToken<Required<FontIconsConfig>>('FONT_ICONS_CONFIG');
+
+/** Injection token for svg icon configuration */
 const SVG_ICONS_CONFIG = new InjectionToken<Required<SvgIconsConfig>>('SVG_ICONS_CONFIG');
 
+/** Default values for font icon configuration */
 const DEFAULT_FONT_ICONS_CONFIG: Required<FontIconsConfig> = {
   defaultClasses: [],
 };
 
+/** Default values for svg icon configuration */
 const DEFAULT_SVG_ICONS_CONFIG: Required<SvgIconsConfig> = {
   directory: 'assets/icons',
 };
 
+/**
+ * Factory for registering default font icon classes
+ *
+ * @param registry Material icon registry
+ * @param config Configuration object
+ * @returns Registration function
+ */
 function registerDefaultFontSetClassesFactory(
   registry: MatIconRegistry,
   { defaultClasses }: Required<FontIconsConfig>,
@@ -38,6 +57,15 @@ function registerDefaultFontSetClassesFactory(
   };
 }
 
+/**
+ * Factory for registering a svg icon resolver
+ *
+ * @param registry Material icon registry
+ * @param location Location service
+ * @param sanitizer Resource url sanitizer
+ * @param config Configuration object
+ * @returns Registration function
+ */
 function registerSvgIconResolverFactory(
   registry: MatIconRegistry,
   location: Location,
@@ -55,6 +83,12 @@ function registerSvgIconResolverFactory(
   };
 }
 
+/**
+ * Provides font icons
+ *
+ * @param config Configuration object
+ * @returns Application providers
+ */
 export function provideFontIcons(config?: FontIconsConfig): EnvironmentProviders {
   return makeEnvironmentProviders([
     {
@@ -70,6 +104,12 @@ export function provideFontIcons(config?: FontIconsConfig): EnvironmentProviders
   ]);
 }
 
+/**
+ * Provides svg icons
+ *
+ * @param config Configuration object
+ * @returns Application providers
+ */
 export function provideSvgIcons(config?: SvgIconsConfig): EnvironmentProviders {
   return makeEnvironmentProviders([
     {
@@ -85,6 +125,12 @@ export function provideSvgIcons(config?: SvgIconsConfig): EnvironmentProviders {
   ]);
 }
 
+/**
+ * Provides both font and svg icons
+ *
+ * @param config Configuration object
+ * @returns Application providers
+ */
 export function provideIcons(config?: IconsConfig): EnvironmentProviders {
   return makeEnvironmentProviders([provideFontIcons(config?.fontIcons), provideSvgIcons(config?.svgIcons)]);
 }
--- libs/cdk/icons/src/lib/resolvers.ts
+++ libs/cdk/icons/src/lib/resolvers.ts
@@ -2,29 +2,38 @@ import { Location } from '@angular/common';
 import { IconResolver } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 
+/** Configuration for a svg icon resolver */
 export interface SvgIconResolverConfig {
+  /** Location service used to resolve urls */
   location: Location;
+  /** Sanitizer to create resource urls */
   sanitizer: DomSanitizer;
+  /** Base directory of icons and namespace folders */
   directory: string;
 }
 
+/**
+ * Creates svg icon resolver with the specified configuration.
+ * The function resolves all svg icons to `directory/[namespace/]name.svg`
+ *
+ * @param config Configuration object
+ * @returns A resolver function
+ */
 export function createSvgIconResolver(config: SvgIconResolverConfig): IconResolver {
   const { location, sanitizer, directory } = config;
   return (name, namespace) => {
-    if (!isValidPathSegment(name) || !isValidPathSegment(namespace)) {
-      return null;
-    }
-
     const path = joinPath(directory, namespace, name) + '.svg';
     const url = location.prepareExternalUrl(path);
     return sanitizer.bypassSecurityTrustResourceUrl(url);
   };
 }
 
-function isValidPathSegment(segment: string): boolean {
-  return /^[\w\-$]+$/.test(segment);
-}
-
+/**
+ * Joins path segments into a single string. Removes empty segments and double '/' sequences.
+ *
+ * @param segments Segments, some of which may be the empty string
+ * @returns The joined path
+ */
 function joinPath(...segments: string[]): string {
   return segments.join('/').replace(/\/{2,}/g, '/');
 }
