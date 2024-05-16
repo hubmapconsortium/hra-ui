d1cf33d671ef823e6e249f34fa7f2520498f0d67
Bruce W. Herr II <bherr@indiana.edu>
2024-05-16 15:10:52 -0400

    Improved the ontology tree by removing unused functionality that was slowing down performance. (#382)

--- apps/ccf-eui/src/app/modules/ontology-exploration/ontology-tree/ontology-tree.module.ts
+++ apps/ccf-eui/src/app/modules/ontology-exploration/ontology-tree/ontology-tree.module.ts
@@ -6,19 +6,10 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatTreeModule } from '@angular/material/tree';
 
 import { OntologyTreeComponent } from './ontology-tree.component';
-import { OpacitySliderModule } from 'ccf-shared';
 import { ButtonToggleModule } from '../../../shared/components/button-toggle/button-toggle.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    MatButtonModule,
-    MatIconModule,
-    MatTooltipModule,
-    MatTreeModule,
-    OpacitySliderModule,
-    ButtonToggleModule,
-  ],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatTooltipModule, MatTreeModule, ButtonToggleModule],
   declarations: [OntologyTreeComponent],
   exports: [OntologyTreeComponent],
 })
