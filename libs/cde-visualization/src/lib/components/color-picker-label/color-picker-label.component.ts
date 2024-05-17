51b4d977985080297c5aebb923c4eaed7b1e1024
bhushankhope <bhushankhope@gmail.com>
2024-05-17 08:51:14 -0700

    add anchor input

--- libs/cde-visualization/src/lib/components/color-picker-label/color-picker-label.component.ts
+++ libs/cde-visualization/src/lib/components/color-picker-label/color-picker-label.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, input, model } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ColorPickerModule } from 'ngx-color-picker';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'cde-color-picker-label',
   standalone: true,
-  imports: [CommonModule, ColorPickerModule],
+  imports: [CommonModule, ColorPickerModule, MatIconModule],
   templateUrl: './color-picker-label.component.html',
   styleUrl: './color-picker-label.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -13,4 +14,5 @@ import { ColorPickerModule } from 'ngx-color-picker';
 export class ColorPickerLabelComponent {
   color = model.required<string>();
   label = input.required<string>();
+  isAnchor = input<boolean>(false);
 }
