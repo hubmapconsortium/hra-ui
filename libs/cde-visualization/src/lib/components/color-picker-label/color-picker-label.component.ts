0296fa25602ffb8d76f7d86a19a5edc649413bff
bhushankhope <bhushankhope@gmail.com>
2024-05-17 09:29:39 -0700

    add tooltip to color picker

--- libs/cde-visualization/src/lib/components/color-picker-label/color-picker-label.component.ts
+++ libs/cde-visualization/src/lib/components/color-picker-label/color-picker-label.component.ts
@@ -2,17 +2,28 @@ import { ChangeDetectionStrategy, Component, input, model } from '@angular/core'
 import { CommonModule } from '@angular/common';
 import { ColorPickerModule } from 'ngx-color-picker';
 import { MatIconModule } from '@angular/material/icon';
+import { ConnectionPositionPair, OverlayModule } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'cde-color-picker-label',
   standalone: true,
-  imports: [CommonModule, ColorPickerModule, MatIconModule],
+  imports: [CommonModule, ColorPickerModule, MatIconModule, OverlayModule],
   templateUrl: './color-picker-label.component.html',
   styleUrl: './color-picker-label.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ColorPickerLabelComponent {
-  color = model.required<string>();
-  label = input.required<string>();
-  isAnchor = input<boolean>(false);
+  readonly color = model.required<string>();
+  readonly label = input.required<string>();
+  readonly isAnchor = input<boolean>(false);
+  tooltipOpen = false;
+  readonly overlayPositions: ConnectionPositionPair[] = [
+    {
+      originX: 'end',
+      overlayX: 'start',
+      originY: 'top',
+      overlayY: 'top',
+      offsetY: -10,
+    },
+  ];
 }
