4db4f58a3395966b0cbe89a5d6deaeace1acc3fe
edlu77 <eddd.lu@gmail.com>
2024-05-22 15:27:51 -0400

    Tooltip changes

--- libs/cde-visualization/src/lib/components/color-picker-label/color-picker-label.component.ts
+++ libs/cde-visualization/src/lib/components/color-picker-label/color-picker-label.component.ts
@@ -1,8 +1,8 @@
-import { ChangeDetectionStrategy, Component, OnInit, computed, input, model } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ColorPickerModule } from 'ngx-color-picker';
-import { MatIconModule } from '@angular/material/icon';
 import { ConnectionPositionPair, OverlayModule } from '@angular/cdk/overlay';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, input, model } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { ColorPickerModule } from 'ngx-color-picker';
 import { Rgb, hexToRgb, rgbToHex } from '../../models/color';
 
 @Component({
@@ -13,38 +13,30 @@ import { Rgb, hexToRgb, rgbToHex } from '../../models/color';
   styleUrl: './color-picker-label.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ColorPickerLabelComponent implements OnInit {
+export class ColorPickerLabelComponent {
   readonly color = model.required<Rgb>();
   readonly label = input.required<string>();
-  readonly labelPos = input<string>('right');
   readonly isAnchor = input<boolean>(false);
 
   readonly hexColor = computed(() => rgbToHex(this.color()));
 
   tooltipOpen = false;
-  overlayPositions: ConnectionPositionPair[] = [
+  readonly overlayPositions: ConnectionPositionPair[] = [
+    {
+      originX: 'start',
+      overlayX: 'end',
+      originY: 'center',
+      overlayY: 'center',
+      offsetX: -26,
+    },
     {
       originX: 'end',
       overlayX: 'start',
-      originY: 'top',
-      overlayY: 'top',
-      offsetX: 70,
-      offsetY: -10,
+      originY: 'center',
+      overlayY: 'center',
+      offsetX: 4,
     },
   ];
-
-  ngOnInit() {
-    this.overlayPositions = [
-      {
-        originX: 'end',
-        overlayX: 'start',
-        originY: 'top',
-        overlayY: 'top',
-        offsetX: this.labelPos() === 'right' ? 70 : -280,
-        offsetY: -10,
-      },
-    ];
-  }
 
   updateColor(hex: string): void {
     this.color.set(hexToRgb(hex));
