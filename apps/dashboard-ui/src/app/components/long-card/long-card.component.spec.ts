1359b1efc2380be3d00c44e3b840d807849edd3f
bhushankhope <bhushankhope@gmail.com>
2024-05-23 13:19:02 -0700

    improve test coverage

--- apps/dashboard-ui/src/app/components/long-card/long-card.component.spec.ts
+++ apps/dashboard-ui/src/app/components/long-card/long-card.component.spec.ts
@@ -1,21 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { LongCardComponent } from './long-card.component';
-
+import { LongCard, LongCardComponent } from './long-card.component';
+import { render, screen } from '@testing-library/angular';
 describe('LongCardComponent', () => {
-  let component: LongCardComponent;
-  let fixture: ComponentFixture<LongCardComponent>;
+  const testCards: LongCard[] = [
+    { image: 'path/to/image1.jpg', label: 'Card 1', route: '/route1', alt: 'Image 1' },
+    { image: 'path/to/image2.jpg', label: 'Card 2', route: '/route2', alt: 'Image 2' },
+  ];
+  it('should display the cards', async () => {
+    await render(LongCardComponent, {
+      componentInputs: { cardData: testCards },
+    });
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [LongCardComponent],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(LongCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    const cards = screen.getAllByRole('img');
+    expect(cards.length).toEqual(2);
+    expect(screen.getByText('Card 1')).toBeInTheDocument();
+    expect(screen.getByText('Card 2')).toBeInTheDocument();
+    expect(cards[0]).toHaveAttribute('src', 'path/to/image1.jpg');
+    expect(cards[0]).toHaveAttribute('alt', 'Image 1');
+    expect(cards[1]).toHaveAttribute('src', 'path/to/image2.jpg');
+    expect(cards[1]).toHaveAttribute('alt', 'Image 2');
   });
 });
--- apps/dashboard-ui/src/app/pages/landing/landing.component.spec.ts
+++ apps/dashboard-ui/src/app/pages/landing/landing.component.spec.ts
@@ -1,21 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { render, screen } from '@testing-library/angular';
 import { LandingComponent } from './landing.component';
+import { RouterModule } from '@angular/router';
 
 describe('LandingComponent', () => {
-  let component: LandingComponent;
-  let fixture: ComponentFixture<LandingComponent>;
-
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [LandingComponent],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(LandingComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    await render(LandingComponent, {
+      providers: [RouterModule],
+    });
+  });
+  it('should render title', () => {
+    const title = screen.getByRole('heading', { level: 1 });
+    expect(title).toHaveTextContent('Explore the coverage and quality of the Human Referance Atlas');
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render the description', async () => {
+    const description = screen.getByText(
+      /The Human Reference Atlas \(HRA\) is constructed and used by experts from many fields of science from around the globe\./i,
+    );
+    expect(description).toBeInTheDocument();
+  });
+
+  it('should render the long cards', async () => {
+    const cards = screen.getAllByRole('link');
+    expect(cards).toHaveLength(5);
+    expect(cards[0]).toHaveTextContent('Data');
+    expect(cards[1]).toHaveTextContent('Usage');
+    expect(cards[2]).toHaveTextContent('Diversity & Inclusion');
+    expect(cards[3]).toHaveTextContent('Publications');
+    expect(cards[4]).toHaveTextContent('Experimental Data');
   });
 });
