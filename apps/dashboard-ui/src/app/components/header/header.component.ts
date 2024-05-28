056cb54e1f4a8349b0d84a6a0dbddee914416380
bhushankhope <bhushankhope@gmail.com>
2024-05-28 14:05:19 -0700

    create menu for smaller breakpoints

--- apps/dashboard-ui/src/app/components/header/header.component.ts
+++ apps/dashboard-ui/src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { RouterModule } from '@angular/router';
 
 /**
@@ -9,9 +11,14 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'hra-header',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, RouterModule],
+  imports: [CommonModule, MatButtonModule, RouterModule, MatMenuModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[class.menu-open]': 'menuOpen',
+  },
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  menuOpen = false;
+}
