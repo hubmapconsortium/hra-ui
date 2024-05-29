095b16ee5004d8d7d67ddcc98e2f47c8a08c7ea7
Daniel Bolin <axbolin@iu.edu>
2024-05-29 18:11:12 -0400

    fix(cde-visualization): :bug: Use absolute urls in the data loader

--- libs/cde-visualization/src/index.ts
+++ libs/cde-visualization/src/index.ts
@@ -1,7 +1,7 @@
 import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { createCustomElement } from '@hra-ui/webcomponents';
+import { InputProps, createCustomElement } from '@hra-ui/webcomponents';
 import { ColorPickerModule } from 'ngx-color-picker';
 import { CdeVisualizationComponent } from './lib/cde-visualization/cde-visualization.component';
 
@@ -11,8 +11,9 @@ export * from './lib/models/edge';
 export * from './lib/models/metadata';
 export * from './lib/models/node';
 
-export type CdeVisualizationElementConstructor = Awaited<typeof CdeVisualizationElement>;
 export type CdeVisualizationElement = InstanceType<CdeVisualizationElementConstructor>;
+export type CdeVisualizationElementProps = InputProps<CdeVisualizationComponent>;
+export type CdeVisualizationElementConstructor = Awaited<typeof CdeVisualizationElement>;
 
 export const CdeVisualizationElement = createCustomElement('cde-visualization', CdeVisualizationComponent, {
   providers: [provideHttpClient(), provideAnimations(), importProvidersFrom(ColorPickerModule)],
