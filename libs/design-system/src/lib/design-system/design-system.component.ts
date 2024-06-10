1f6f3b3216e8178dbf472011b358b6715cc443f8
bhushankhope <bhushankhope@gmail.com>
2024-06-10 10:50:42 -0400

    create design system

---
+++ libs/design-system/src/lib/design-system/design-system.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'hra-ui-design-system',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './design-system.component.html',
+  styleUrl: './design-system.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DesignSystemComponent {}
