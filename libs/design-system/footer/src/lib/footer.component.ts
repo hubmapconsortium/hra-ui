5a1c729938988ae8dbb1edaa7510770f653fe1d0
Bhushan Khope <53601863+bhushankhope@users.noreply.github.com>
2024-06-10 10:47:43 -0700

    create design system (#407)
    * create design system
    
    * create footer entrypoint
    
    * refactor: Cherry pick cdk/icons from cell distance explorer
    
    * create footer
    
    * fix test and lint
    
    * test: :white_check_mark: add tests for providers and resolvers

---
+++ libs/design-system/footer/src/lib/footer.component.ts
@@ -0,0 +1,19 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+
+/**
+ * Component for footer
+ */
+@Component({
+  selector: 'hra-ui-footer',
+  standalone: true,
+  imports: [CommonModule, MatIconModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FooterComponent {
+  /** HRA Logo url */
+  readonly logo = input.required<string>();
+}
