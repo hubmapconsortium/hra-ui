1d8a155b5dadde385b3316d6e800436697dd82eb
Daniel Bolin <axbolin@iu.edu>
2024-06-21 15:01:24 -0400

    refactor(cde): :fire: Removed unnecessary app component
    cde-visualization is already a web component so it doesn't need an app component

--- apps/cde-visualization-wc/src/main.ts
+++ apps/cde-visualization-wc/src/main.ts
@@ -1,11 +1,1 @@
-import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { createCustomElement } from '@hra-ui/webcomponents';
-import { ColorPickerModule } from 'ngx-color-picker';
-
-import { AppComponent } from './app/app.component';
-
-createCustomElement('cde-visualization-wc', AppComponent, {
-  providers: [provideHttpClient(), provideAnimations(), importProvidersFrom(ColorPickerModule)],
-});
+import '@hra-ui/cde-visualization';
