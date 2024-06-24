57ad9e4633df3032f982f28d34ebd742b67f0045
bhushankhope <bhushankhope@gmail.com>
2024-06-24 14:46:13 -0400

    Remove more debug

--- .github/actions/prepare-deploy/create-redirects.sh
+++ .github/actions/prepare-deploy/create-redirects.sh
@@ -4,7 +4,6 @@ set -e
 touch deploy/_redirects
 for dir in deploy/apps/*
 do
-  echo $dir
   if [ -d "$dir" ]; then
     APP="$(basename $dir)"
     echo "/apps/${APP}/* /apps/${APP}/index.html 200" >> deploy/_redirects
