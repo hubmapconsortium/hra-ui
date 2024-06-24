edb8338c1e8bdb35ea41a8e26371973d7110357d
Bhushan Khope <53601863+bhushankhope@users.noreply.github.com>
2024-06-24 14:47:54 -0400

    fix: :construction_worker: netlify routing (#485)
    * fix netlify routing
    
    * fix project.json
    
    * add shell script to create redirects
    
    * fix file name
    
    * update file path
    
    * update file url
    
    * revert initial changes
    
    * update build previews
    
    * update project.json: humanatlas.io
    
    * Update path and add DELETEMEs for testing temporarily
    
    * Update create-redirects
    
    * debug build preview
    
    * Change directory name
    
    * Remove debug information
    
    * Remove more debug
    
    ---------
    
    Co-authored-by: Bruce W. Herr II <bherr@indiana.edu>

---
+++ .github/actions/prepare-deploy/create-redirects.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+touch deploy/_redirects
+for dir in deploy/apps/*
+do
+  if [ -d "$dir" ]; then
+    APP="$(basename $dir)"
+    echo "/apps/${APP}/* /apps/${APP}/index.html 200" >> deploy/_redirects
+  fi
+done
