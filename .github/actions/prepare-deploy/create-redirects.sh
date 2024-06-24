cd67ace43215712f48f635034a334b62c72cd419
bhushankhope <bhushankhope@gmail.com>
2024-06-24 14:37:03 -0400

    debug build preview

--- .github/actions/prepare-deploy/create-redirects.sh
+++ .github/actions/prepare-deploy/create-redirects.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 
+echo $PWD
+echo $(ls dist/apps/*)
 touch dist/_redirects
 for dir in dist/apps/*
 do
+  echo $dir
   if [ -d "$dir" ]; then
     APP="$(basename $dir)"
     echo "/apps/${APP}/* /apps/${APP}/index.html 200" >> dist/_redirects
   fi
 done
+
+cat dist/_redirects
