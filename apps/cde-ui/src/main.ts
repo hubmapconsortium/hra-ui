8c1888920664195b7a47f8b800b560dc37e9ce9c
edlu77 <eddd.lu@gmail.com>
2024-06-28 12:19:05 -0400

    Import bold Metropolis font weight

--- apps/cde-ui/src/main.ts
+++ apps/cde-ui/src/main.ts
@@ -2,4 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import '@fontsource/metropolis/500.css';
+
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
