eb15d2d82d955e15036948b4142b8c96aa0d8afe
Daniel Bolin <axbolin@iu.edu>
2024-07-16 15:22:59 -0400

    refactor: Create ccf-scene-utils library
    New location of functions from ccf-database that are still in use. See #497

---
+++ libs/ccf-scene-utils/src/lib/ccf-scene-utils.spec.ts
@@ -0,0 +1,7 @@
+import { ccfSceneUtils } from './ccf-scene-utils';
+
+describe('ccfSceneUtils', () => {
+  it('should work', () => {
+    expect(ccfSceneUtils()).toEqual('ccf-scene-utils');
+  });
+});
---
+++ libs/ccf-scene-utils/src/lib/ccf-scene-utils.ts
@@ -0,0 +1,3 @@
+export function ccfSceneUtils(): string {
+  return 'ccf-scene-utils';
+}
