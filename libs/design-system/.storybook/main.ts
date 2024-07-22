49c7b1d1ef54c4ffc00f3a135367c5df0b262b79
Daniel Bolin <axbolin@iu.edu>
2024-07-22 15:35:58 -0400

    build(design-system): :package: Add storybook addons

--- libs/design-system/.storybook/main.ts
+++ libs/design-system/.storybook/main.ts
@@ -2,7 +2,13 @@ import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
   stories: ['../**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    '@storybook/addon-designs',
+    'storybook-addon-pseudo-states',
+  ],
   framework: {
     name: '@storybook/angular',
     options: {},
