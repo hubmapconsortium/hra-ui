4f38855ada352543c31dae477327447c813c2ed9
Daniel Bolin <axbolin@iu.edu>
2024-07-29 19:03:14 -0400

    test(design-system): :white_check_mark: Add tests & storybook for scrolling

--- libs/design-system/scrolling/src/lib/scroll-timeline/scroll-timeline.ts
+++ libs/design-system/scrolling/src/lib/scroll-timeline/scroll-timeline.ts
@@ -1,10 +1,19 @@
 import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 import { InjectionToken, PLATFORM_ID, Signal, inject, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { endWith, ignoreElements, interval, map, takeWhile } from 'rxjs';
+import { Observable, endWith, ignoreElements, interval, map, takeWhile } from 'rxjs';
 
 /** Basic signature of the ScrollTimeline constructor */
 export type ScrollTimelineFunc = new ({ source, axis }: { source: HTMLElement; axis: 'x' | 'y' }) => AnimationTimeline;
+
+/**
+ * Interval between scroll timeline availability checks.
+ * Primarily used to simplify testing.
+ */
+export const SCROLL_TIMELINE_QUERY_INTERVAL = new InjectionToken<Observable<number>>('SCROLL_TIMELINE_QUERY_INTERVAL', {
+  providedIn: 'root',
+  factory: () => interval(100),
+});
 
 /**
  * Provides ScrollTimeline as a signal.
@@ -20,12 +29,12 @@ export const SCROLL_TIMELINE = new InjectionToken<Signal<ScrollTimelineFunc | nu
       return signal(null);
     }
 
-    const isAvailable = () => 'ScrollTimeline' in window;
+    const isAvailable = () => 'ScrollTimeline' in window && typeof window.ScrollTimeline === 'function';
     if (isAvailable()) {
       return signal(window['ScrollTimeline']);
     }
 
-    const scrollTimeline$ = interval(100).pipe(
+    const scrollTimeline$ = inject(SCROLL_TIMELINE_QUERY_INTERVAL).pipe(
       takeWhile(() => !isAvailable()),
       ignoreElements(),
       endWith(null),
--- libs/design-system/scrolling/src/lib/scrolling.module.ts
+++ libs/design-system/scrolling/src/lib/scrolling.module.ts
@@ -1,8 +1,22 @@
 import { EnvironmentProviders, NgModule, makeEnvironmentProviders } from '@angular/core';
 import { provideStyleComponents } from '@hra-ui/cdk/styling';
-import { NgScrollbarModule, NgScrollbarOptions, provideScrollbarOptions } from 'ngx-scrollbar';
+import {
+  NgScrollbarModule,
+  NgScrollbarOptions,
+  provideScrollbarOptions,
+  provideScrollbarPolyfill,
+} from 'ngx-scrollbar';
 import { ScrollOverflowFadeDirective } from './scroll-overflow-fade/scroll-overflow-fade.directive';
 import { ScrollbarStylesComponent } from './scrollbar-styles/scrollbar-styles.component';
+
+/** Scrolling configuration */
+export interface ScollingOptions extends NgScrollbarOptions {
+  /** Url to the scroll timeline polyfill script */
+  polyfillUrl?: string;
+}
+
+/** Default scroll timeline polyfill url */
+const DEFAULT_POLYFILL_URL = 'assets/polyfills/scroll-timeline-polyfill.js';
 
 /**
  * Provide scrolling functionality to an application.
@@ -10,12 +24,10 @@ import { ScrollbarStylesComponent } from './scrollbar-styles/scrollbar-styles.co
  * @param options Scrollbar options
  * @returns An environment provider
  */
-export function provideScrolling(options?: NgScrollbarOptions): EnvironmentProviders {
+export function provideScrolling(options?: ScollingOptions): EnvironmentProviders {
   return makeEnvironmentProviders([
     provideStyleComponents(ScrollbarStylesComponent),
-
-    // TODO point polyfill url to 'cdn.humanatlas.io/path/to/polyfill.js'?
-
+    provideScrollbarPolyfill(options?.polyfillUrl ?? DEFAULT_POLYFILL_URL),
     provideScrollbarOptions({
       visibility: 'hover',
       appearance: 'compact',
