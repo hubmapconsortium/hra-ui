e4ce2262e5fc845b3428837dec2cb2904c6ab773
Daniel Bolin <axbolin@iu.edu>
2024-07-30 12:23:55 -0400

    docs(design-system): :memo: Add docs for code in design system's scrolling/testing entrypoint

--- libs/design-system/scrolling/testing/src/index.ts
+++ libs/design-system/scrolling/testing/src/index.ts
@@ -1,10 +1,16 @@
 import type { ScrollTimelineFunc } from '@hra-ui/design-system/scrolling';
 import { mock } from 'jest-mock-extended';
 
+/** Globals used by scrolling */
 interface ScrollingGlobals {
+  /** Scroll timeline constructor */
   ScrollTimeline?: ScrollTimelineFunc;
 }
 
+/**
+ * Setup the testing environment for scrolling to work.
+ * Adds global mocks for functions used by scrolling if they don't exist.
+ */
 export function setupScrollTesting(): void {
   const globals = globalThis as ScrollingGlobals;
   if (typeof globals.ScrollTimeline !== 'function') {
