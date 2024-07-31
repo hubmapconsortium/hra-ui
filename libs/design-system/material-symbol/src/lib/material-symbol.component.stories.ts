57f90b0c449c0b0605660cd4db48b9589ba56a7c
edlu77 <eddd.lu@gmail.com>
2024-07-30 20:34:22 -0400

    Add material symbol component

---
+++ libs/design-system/material-symbol/src/lib/material-symbol.component.stories.ts
@@ -0,0 +1,29 @@
+import { Meta, StoryObj } from '@storybook/angular';
+
+import { MaterialSymbolComponent } from './material-symbol.component';
+
+const meta: Meta<MaterialSymbolComponent> = {
+  component: MaterialSymbolComponent,
+  title: 'MaterialSymbolComponent',
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/gQEMLugLjweDvbsNNUVffD/Design-System-Repository?node-id=1790-54&t=7pLI9aOEH4FfwISb-4',
+    },
+  },
+};
+export default meta;
+type Story = StoryObj<MaterialSymbolComponent>;
+
+export const Default: Story = {
+  args: {
+    icon: 'search',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    small: true,
+    icon: 'search',
+  },
+};
---
+++ libs/design-system/material-symbol/src/lib/material-symbol.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+/**
+ * Material symbols used in HRA interfaces
+ */
+@Component({
+  selector: 'hra-material-symbol',
+  standalone: true,
+  imports: [CommonModule, MatIconModule],
+  templateUrl: './material-symbol.component.html',
+  styleUrl: './material-symbol.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[attr.small]': 'small()',
+  },
+})
+export class MaterialSymbolComponent {
+  /** Icon options:
+   * arrow_drop_up, arrow_drop_down, arrow_right, upload, download, arrow_right_alt,
+   * arrow_left_alt, arrow_upward_alt, arrow_downward_alt, play_circle, mail, code,
+   * fullscreen, fullscreen_exit, info, expand_all, collapse_all, more_vert, check_box,
+   * check_box_outline_blank, apps, close, keyboard_arrow_up, keyboard_arrow_down, remove,
+   * add, chevron_right, filter_list, link, copyright, search, api, database, menu,
+   * check, anchor, restart_alt
+   */
+  readonly icon = input.required<string>();
+
+  /** True if small size (20px) */
+  readonly small = input<boolean>(false);
+}
