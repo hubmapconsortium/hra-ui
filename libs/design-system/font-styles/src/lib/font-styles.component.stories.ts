777eebb54f07c7105958d235dba85e93e59d5f76
edlu77 <eddd.lu@gmail.com>
2024-08-07 17:38:47 -0400

    Add font styles to design system storybook

--- libs/design-system/font-styles/src/lib/font-styles.component.stories.ts
+++ libs/design-system/font-styles/src/lib/font-styles.component.stories.ts
@@ -1,9 +1,24 @@
-import type { Meta, StoryObj } from '@storybook/angular';
 import { FontStylesComponent } from './font-styles.component';
 
+import type { Meta, StoryObj } from '@storybook/angular';
 const meta: Meta<FontStylesComponent> = {
   component: FontStylesComponent,
   title: 'FontStylesComponent',
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/BCEJn9KCIbBJ5MzqnojKQp/Design-System-Components?node-id=192-39',
+    },
+  },
+  argTypes: {
+    typographyType: {
+      control: 'select',
+      options: ['display', 'headline', 'title', 'label', 'body', 'wordmark'],
+    },
+  },
+  args: {
+    typographyType: 'display',
+  },
 };
 export default meta;
 type Story = StoryObj<FontStylesComponent>;
--- libs/design-system/font-styles/src/lib/font-styles.component.ts
+++ libs/design-system/font-styles/src/lib/font-styles.component.ts
@@ -1,5 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+export type FontContent = {
+  title: string;
+  description: string;
+  typography: {
+    small: string;
+    medium?: string;
+    large: string;
+    splash?: string;
+    subheaderLarge?: string;
+    subheaderSmall?: string;
+  };
+};
 
 @Component({
   selector: 'hra-font-styles',
@@ -8,5 +21,84 @@ import { CommonModule } from '@angular/common';
   templateUrl: './font-styles.component.html',
   styleUrl: './font-styles.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[style.--hra-splash-font]': 'getCssFont("splash")',
+    '[style.--hra-splash-spacing]': 'getCssSpacing("splash")',
+    '[style.--hra-large-font]': 'getCssFont("large")',
+    '[style.--hra-large-spacing]': 'getCssSpacing("large")',
+    '[style.--hra-medium-font]': 'getCssFont("medium")',
+    '[style.--hra-medium-spacing]': 'getCssSpacing("medium")',
+    '[style.--hra-small-font]': 'getCssFont("small")',
+    '[style.--hra-small-spacing]': 'getCssSpacing("small")',
+  },
 })
-export class FontStylesComponent {}
+export class FontStylesComponent {
+  typographyType = input<string>('headline');
+
+  fonts: Record<string, FontContent> = {
+    display: {
+      title: 'Display',
+      description: 'Uses: Splash text headings',
+      typography: {
+        large: 'Large: Metropolis, Medium, 42/63, -1.25px',
+        medium: 'Medium: Metropolis, Medium, 38/57, -1.75px',
+        small: 'Small: Metropolis, Medium, 32/48, -1.25px',
+      },
+    },
+    headline: {
+      title: 'Headline',
+      description: 'Uses: Secondary headings',
+      typography: {
+        large: 'Large: Metropolis, Medium, 32/48, 0px',
+        medium: 'Medium: Metropolis, Medium, 28/42, 0px',
+        small: 'Small: Metropolis, Medium, 24/32, 0px',
+      },
+    },
+    title: {
+      title: 'Title',
+      description: 'Uses: Tertiary headings, titles in app components',
+      typography: {
+        large: 'Large: Metropolis, Medium, 24/36, 0px',
+        medium: 'Medium: Metropolis, Medium, 20/30, 0px',
+        small: 'Small: Metropolis Medium, 16/24, 0px ',
+      },
+    },
+    label: {
+      title: 'Label',
+      description: 'Uses: Buttons, application interfaces, data visualizations',
+      typography: {
+        large: 'Large: Metropolis, Medium, 16/24, 0px',
+        medium: 'Medium: Metropolis, Medium, 14/21, 0px ',
+        small: 'Small: Metropolis, Medium, 12/18, 0px',
+      },
+    },
+    body: {
+      title: 'Body',
+      description: 'Uses: Paragraph text',
+      typography: {
+        splash: 'Splash: Nunito Sans, 18/27, +0.5px',
+        large: 'Large: Nunito Sans, 18/27, +0.5px',
+        medium: 'Medium: Nunito Sans, 14/21, +0.5px',
+        small: 'Small: Nunito Sans, 12/18, +0.5px',
+      },
+    },
+    wordmark: {
+      title: 'Wordmark',
+      description: 'Uses: HRA Wordmark only',
+      typography: {
+        subheaderLarge: 'Wordmark 25px',
+        large: 'Metropolis, Regular, 25/36, 0.1px',
+        subheaderSmall: 'Wordmark 12px',
+        small: 'Metropolis, Regular, 12/18, 0px',
+      },
+    },
+  };
+
+  getCssFont(size: string): string {
+    return `var(--sys-${this.typographyType()}-${size})`;
+  }
+
+  getCssSpacing(size: string): string {
+    return `var(--sys-${this.typographyType()}-${size}-tracking)`;
+  }
+}
