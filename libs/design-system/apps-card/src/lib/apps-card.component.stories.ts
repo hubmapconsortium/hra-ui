1bce6ada72489897521d96ea06d839d48c8f3010
Bhushan Khope <bhushankhope@gmail.com>
2024-08-12 14:35:38 -0400

    added select menu in app card storybook

--- libs/design-system/apps-card/src/lib/apps-card.component.stories.ts
+++ libs/design-system/apps-card/src/lib/apps-card.component.stories.ts
@@ -10,6 +10,63 @@ const meta: Meta<AppsCardComponent> = {
       url: 'https://www.figma.com/design/BCEJn9KCIbBJ5MzqnojKQp/Design-System-Components?node-id=78-355',
     },
   },
+  argTypes: {
+    icon: {
+      control: 'select',
+      options: [
+        'assets/logo/hubmap.svg',
+        'assets/logo/data_portal.svg',
+        'assets/logo/hra_small.svg',
+        'assets/logo/eui.svg',
+        'assets/logo/asctb-reporter.svg',
+        'assets/logo/azimuth.svg',
+        'assets/logo/fusion.svg',
+        'assets/logo/antibody-validation-reports.svg',
+      ],
+    },
+    title: {
+      control: 'select',
+      options: [
+        'HuBMAP Consortium',
+        'HuBMAP Data Portal',
+        'Data Portal Workspaces',
+        'Human Reference Atlas',
+        'Exploration User Interface',
+        'ASCT+B Reporter',
+        'Azimuth',
+        'FUSION',
+        'Antibody Validation Reports',
+      ],
+    },
+    description: {
+      control: 'select',
+      options: [
+        'HuBMAP all access: Learn about us, our policies, data, and tools. Explore our publications and how to work with us.',
+        'Explore, visualize and download consortium-generated spatial and single cell data for the human body.',
+        'Access HuBMAP data in a lightweight exploration platform and perform analyses directly within the portal.',
+        'Use the HRA Portal to access atlas data, explore atlas functionality, and contribute to the Human Reference Atlas.',
+        'Explore and validate spatially registered single-cell datasets in three-dimensions across organs.',
+        'Explore and compare ASCT+B tables and construct validated panels for multiplexed antibody-based imaging (OMAPs) tables.',
+        'Azimuth uses a reference dataset to process, analyze, and interpret single-cell RNA-seq or ATAC-seq experiments.',
+        'Functional Unit State Identification and Navigation with WSI.',
+        'Provide antibody details for multiplex imaging assays and capture data requested by journals for manuscript submission.',
+      ],
+    },
+    link: {
+      control: 'select',
+      options: [
+        'https://hubmapconsortium.org/',
+        'https://portal.hubmapconsortium.org/',
+        'https://portal.hubmapconsortium.org/workspaces',
+        'https://humanatlas.io/',
+        'https://apps.humanatlas.io/eui/',
+        'https://hubmapconsortium.github.io/ccf-asct-reporter/',
+        'https://azimuth.hubmapconsortium.org/',
+        'http://fusion.hubmapconsortium.org/?utm_source=hubmap',
+        'https://avr.hubmapconsortium.org/',
+      ],
+    },
+  },
 };
 export default meta;
 type Story = StoryObj<AppsCardComponent>;
@@ -21,24 +78,5 @@ export const Primary: Story = {
     link: 'https://humanatlas.io/',
     description:
       'Use the HRA Portal to access atlas data, explore atlas functionality, and contribute to the Human Reference Atlas.',
-  },
-};
-
-export const DataPortalWorkspaces: Story = {
-  args: {
-    icon: 'assets/logo/data_portal.svg',
-    title: 'Data Portal Workspaces',
-    link: 'https://portal.hubmapconsortium.org/workspaces',
-    description:
-      'Access HuBMAP data in a lightweight exploration platform and perform analyses directly within the portal.',
-  },
-};
-
-export const ExplorationUserInterface: Story = {
-  args: {
-    icon: 'assets/logo/eui.svg',
-    title: 'Exploration User Interface',
-    link: 'https://apps.humanatlas.io/eui/',
-    description: 'Explore and validate spatially registered single-cell datasets in three-dimensions across organs.',
   },
 };
