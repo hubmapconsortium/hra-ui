ba6f8a8fdd1de87ee7cb2dc057fc7d74b85cb707
Daniel Bolin <axbolin@iu.edu>
2024-08-12 17:30:16 -0400

    fix(services): Readd ftu data mock service (used by stories)

--- libs/services/src/index.ts
+++ libs/services/src/index.ts
@@ -4,6 +4,7 @@ export { Iri, Url } from './lib/shared/common.model';
 export * from './lib/contact/contact.mock';
 export * from './lib/contact/contact.service';
 export * from './lib/ftu-data/ftu-data.impl';
+export * from './lib/ftu-data/ftu-data.mock';
 export * from './lib/ftu-data/ftu-data.service';
 export * from './lib/service.module';
 export * from './lib/shared/common.model';
