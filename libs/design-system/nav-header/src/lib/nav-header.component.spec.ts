62837aa518455d4e912dbc2211de1450d2089287
Bhushan Khope <bhushankhope@gmail.com>
2024-08-13 12:20:25 -0400

    improve test coverage

--- libs/design-system/nav-header/src/lib/nav-header.component.spec.ts
+++ libs/design-system/nav-header/src/lib/nav-header.component.spec.ts
@@ -1,22 +1,27 @@
 import { render, screen } from '@testing-library/angular';
-import { NavHeaderComponent, NavInfo } from './nav-header.component';
+import { NavHeaderComponent } from './nav-header.component';
 
 describe('NavHeaderComponent', () => {
-  const testNavHeaderData: NavInfo = {
-    link: 'example.com',
-    icon: 'icon.svg',
-    title: 'test title',
-  };
+  const link = 'example.com';
+  const icon = 'icon.svg';
+  const title = 'test title';
 
   beforeEach(async () => {
     await render(NavHeaderComponent, {
       componentInputs: {
-        navInfo: testNavHeaderData,
+        link: link,
+        icon: icon,
+        title: title,
+        description: 'Test Description',
       },
     });
   });
 
   it('should render header', () => {
     expect(screen.getByText('test title')).toBeInTheDocument();
+  });
+
+  it('should render description', () => {
+    expect(screen.getByText('Test Description')).toBeInTheDocument();
   });
 });
