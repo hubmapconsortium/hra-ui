32eded64d3b0eba508baf50f118845c514c45e41
Bhushan Khope <bhushankhope@gmail.com>
2024-08-15 12:08:17 -0400

    add text around divider

--- libs/design-system/src/lib/divider.stories.ts
+++ libs/design-system/src/lib/divider.stories.ts
@@ -1,7 +1,8 @@
 import { MatDividerModule } from '@angular/material/divider';
 import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
 
-const content = 'Lorem Ipsum';
+const content =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 const meta: Meta = {
   title: 'MatDivider',
