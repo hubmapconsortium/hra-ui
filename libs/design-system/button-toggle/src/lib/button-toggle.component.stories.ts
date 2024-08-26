c65fe29664e2f2e60a8d20bdb90461406a09cc3b
Bhushan Khope <bhushankhope@gmail.com>
2024-08-26 11:11:24 -0400

    added states to button toggle

--- libs/design-system/button-toggle/src/lib/button-toggle.component.stories.ts
+++ libs/design-system/button-toggle/src/lib/button-toggle.component.stories.ts
@@ -38,17 +38,17 @@ export const SingleSelect: Story = {
     <h2>Single Select</h2>
       <mat-button-toggle-group name="singleSelect" aria-label="Single Select"
       hraButtonToggleSize="${args['size']}">
-        <mat-button-toggle value="button1" checked>Button</mat-button-toggle>
-        <mat-button-toggle value="button2">Button</mat-button-toggle>
-        <mat-button-toggle value="button3">Button</mat-button-toggle>
+        <mat-button-toggle disableRipple value="button1" checked>Button</mat-button-toggle>
+        <mat-button-toggle disableRipple value="button2">Button</mat-button-toggle>
+        <mat-button-toggle disableRipple disabled value="button3">Button</mat-button-toggle>
       </mat-button-toggle-group>
 
       <h2>Multi Select</h2>
       <mat-button-toggle-group multiple name="multiSelect" aria-label="Multi Select"
       hraButtonToggleSize="${args['size']}">
-        <mat-button-toggle value="button1" checked>Button</mat-button-toggle>
-        <mat-button-toggle value="button2">Button</mat-button-toggle>
-        <mat-button-toggle value="button3">Button</mat-button-toggle>
+        <mat-button-toggle disableRipple value="button1" checked>Button</mat-button-toggle>
+        <mat-button-toggle disableRipple value="button2">Button</mat-button-toggle>
+        <mat-button-toggle disableRipple value="button3">Button</mat-button-toggle>
       </mat-button-toggle-group>
     `,
   }),
