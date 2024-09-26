b4b677ca17cb7de24761eb871a10a5aea46de56a
Bhushan Khope <bhushankhope@gmail.com>
2024-09-26 15:19:10 -0400

    improve test coverage for body ui library

--- libs/ccf-body-ui/src/lib/body-ui/body-ui.component.ts
+++ libs/ccf-body-ui/src/lib/body-ui/body-ui.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import {
+  booleanAttribute,
   Component,
   computed,
   effect,
@@ -7,6 +8,7 @@ import {
   ErrorHandler,
   inject,
   input,
+  numberAttribute,
   output,
   OutputEmitterRef,
   Signal,
@@ -24,10 +26,12 @@ export interface XYZTriplet<T = number> {
   z: T;
 }
 
+/** Type for setter methods of the BodyUI */
 type SetterMethods<ValueT> = keyof {
   [P in keyof BodyUI as BodyUI[P] extends (v: ValueT) => void ? P : never]: BodyUI[P];
 };
 
+/** Parses the input if it is a JSON String */
 function tryParseJson(value: unknown): unknown {
   try {
     if (typeof value === 'string') {
@@ -40,6 +44,7 @@ function tryParseJson(value: unknown): unknown {
   return value;
 }
 
+/** Utility function to use input data to set to relevant body ui setter */
 function connectInput<T>(
   bodyUi: Signal<BodyUI | undefined>,
   source: Signal<T | undefined>,
@@ -51,20 +56,31 @@ function connectInput<T>(
   }
 }
 
+/** Utility function to use output data to set to relevant body ui subject */
 function connectOutput<T>(out: OutputEmitterRef<T>, source: Observable<T>): Subscription {
   return source.subscribe((value) => out.emit(value));
 }
 
-// TODO: Try to improve this
+/** Zod for SPATIAL SCENE NODE  */
 export const SPATIAL_SCENE_NODE = z
   .object({})
   .passthrough()
   .refine((obj): obj is SpatialSceneNode => true);
 
+/** Zod for SPATIAL SCENE NODE array */
 export const SPATIAL_SCENE_NODE_ARRAY = z.array(SPATIAL_SCENE_NODE);
 
+/** Preprocesses the scene input */
 const SCENE_INPUT = z.preprocess(tryParseJson, z.union([z.literal(''), z.string().url(), SPATIAL_SCENE_NODE_ARRAY]));
+/** Bind scene input */
+const parseSceneInput = SCENE_INPUT.parse.bind(SCENE_INPUT);
 
+/** Preprocess the target input */
+const TARGET_INPUT = z.preprocess(tryParseJson, z.tuple([z.number(), z.number(), z.number()]));
+/** Bind target input */
+const parseTargetInput = TARGET_INPUT.parse.bind(TARGET_INPUT);
+
+/** HRA Body UI Component */
 @Component({
   standalone: true,
   imports: [],
@@ -73,27 +89,43 @@ const SCENE_INPUT = z.preprocess(tryParseJson, z.union([z.literal(''), z.string(
   styleUrl: './body-ui.component.scss',
 })
 export class BodyUiComponent {
-  readonly scene = input(undefined, { transform: SCENE_INPUT.parse.bind(SCENE_INPUT) });
+  /** Scene for the deck gl */
+  readonly scene = input(undefined, { transform: parseSceneInput });
 
-  readonly rotation = input<number>();
-  readonly rotationX = input<number>();
-  readonly zoom = input<number>();
-  readonly target = input<[number, number, number]>();
+  /** Rotation for the deck gl */
+  readonly rotation = input(undefined, { transform: numberAttribute });
+  /** Rotation X for the deck gl */
+  readonly rotationX = input(undefined, { transform: numberAttribute });
+  /** Zoom for the deck gl */
+  readonly zoom = input(undefined, { transform: numberAttribute });
+  /** Target for the deck gl */
+  readonly target = input(undefined, { transform: parseTargetInput });
+  /** Bounds for the deck gl */
   readonly bounds = input<XYZTriplet>();
+  /** Camera for the deck gl */
   readonly camera = input<string>();
-  readonly interactive = input<boolean>();
+  /** Flag for the interactive for deck gl */
+  readonly interactive = input(true, { transform: booleanAttribute });
 
+  /** Output for rotation change */
   readonly rotationChange = output<[number, number]>();
+  /** Output for node click */
   readonly nodeClick = output<NodeClickEvent>();
+  /** Output for node drag */
   readonly nodeDrag = output<NodeDragEvent>();
+  /** Output for node hover start */
   readonly nodeHoverStart = output<SpatialSceneNode>();
+  /** Output for node hover end */
   readonly nodeHoverStop = output<SpatialSceneNode>();
-  readonly initialized = output<void>();
 
+  /** Instance of HttpClient */
   private readonly http = inject(HttpClient);
+  /** Instance of Error Handler */
   private readonly errorHandler = inject(ErrorHandler);
 
+  /** Instance of canvas element */
   private readonly canvas = viewChild.required<ElementRef<HTMLCanvasElement>>('canvas');
+  /** Instance of BodyUI class */
   private readonly bodyUi = derivedAsync(
     async () => {
       const bodyUi = new BodyUI({
@@ -114,6 +146,7 @@ export class BodyUiComponent {
     { initialValue: undefined },
   );
 
+  /** Processed scene data for deck gl */
   private readonly sceneData = derivedAsync<SpatialSceneNode[]>(
     () => {
       const value = this.scene();
@@ -134,11 +167,13 @@ export class BodyUiComponent {
     { initialValue: [] },
   );
 
+  /** Returns the bounds zoom according to bounds input */
   private readonly boundsZoom = computed(() => {
     const bounds = this.bounds();
     return bounds ? this.getBoundsZoom(bounds) : undefined;
   });
 
+  /** Constructor for the component */
   constructor() {
     effect(() => connectInput(this.bodyUi, this.sceneData, 'setScene'));
     effect(() => connectInput(this.bodyUi, this.rotation, 'setRotation'));
@@ -160,11 +195,13 @@ export class BodyUiComponent {
     });
   }
 
+  /** Sets the deck gl zoom according to the provided bounds */
   zoomToBounds(bounds: XYZTriplet, margin = { x: 48, y: 48 }): void {
     const zoom = this.getBoundsZoom(bounds, margin);
     this.bodyUi()?.setZoom(zoom);
   }
 
+  /** Returns zoom value according to current bounds */
   private getBoundsZoom(bounds: XYZTriplet, margin = { x: 48, y: 48 }): number {
     const { width, height } = this.canvas().nativeElement;
     const pxRatio = window.devicePixelRatio;
