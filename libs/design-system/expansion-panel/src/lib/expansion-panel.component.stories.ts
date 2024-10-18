9a2283ca20cf225bb23842edcc7fc186c6f6b81e
Bhushan Khope <bhushankhope@gmail.com>
2024-10-18 12:17:48 -0400

    add animation to expansion panel

--- libs/design-system/expansion-panel/src/lib/expansion-panel.component.stories.ts
+++ libs/design-system/expansion-panel/src/lib/expansion-panel.component.stories.ts
@@ -1,6 +1,7 @@
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
+import { provideDesignSystem } from '@hra-ui/design-system';
+import { ButtonModule } from '@hra-ui/design-system/button';
+import { applicationConfig, moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
 import {
   ExpansionPanelActionsComponent,
   ExpansionPanelComponent,
@@ -12,13 +13,15 @@ const meta: Meta = {
   decorators: [
     moduleMetadata({
       imports: [
-        MatButtonModule,
         MatIconModule,
-        MatButtonModule,
+        ButtonModule,
         ExpansionPanelActionsComponent,
         ExpansionPanelComponent,
         ExpansionPanelHeaderContentComponent,
       ],
+    }),
+    applicationConfig({
+      providers: [provideDesignSystem()],
     }),
   ],
   render: (args) => ({
@@ -27,10 +30,10 @@ const meta: Meta = {
       <hra-expansion-panel [title]="'Title'">
         <hra-expansion-panel-actions>
           <button mat-icon-button>
-          <mat-icon class="material-symbols-rounded">
-            more_vert
-          </mat-icon>
-        </button>
+            <mat-icon class="material-symbols-rounded">
+              more_vert
+            </mat-icon>
+          </button>
         </hra-expansion-panel-actions>
         <hra-expansion-panel-header-content>
           Additional Actions
