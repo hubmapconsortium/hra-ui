23372ba7c8b027e0137b0e1a35cb013839b67837
Bhushan Khope <bhushankhope@gmail.com>
2024-10-23 13:42:00 -0400

    improve doc coverage for full screen component

--- libs/design-system/fullscreen/src/lib/fullscreen.component.ts
+++ libs/design-system/fullscreen/src/lib/fullscreen.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
+/** Fullscreen Component */
 @Component({
   selector: 'hra-fullscreen',
   standalone: true,
@@ -11,5 +12,6 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FullscreenComponent {
+  /** Reference to the mat dialog data */
   private readonly viewRef = inject(MAT_DIALOG_DATA);
 }
