44eed2cdb511f2c4e26dc661af7fa7705cded025
Bhushan Khope <bhushankhope@gmail.com>
2024-10-24 11:15:53 -0400

    use expansion panel for cell types module

--- libs/cde-visualization/src/lib/components/cell-types/cell-types.component.ts
+++ libs/cde-visualization/src/lib/components/cell-types/cell-types.component.ts
@@ -33,6 +33,11 @@ import { CellTypeEntry } from '../../models/cell-type';
 import { TOOLTIP_POSITION_RIGHT_SIDE } from '../../shared/tooltip-position';
 import { ColorPickerLabelComponent } from '../color-picker-label/color-picker-label.component';
 import { MicroTooltipDirective } from '@hra-ui/design-system/micro-tooltip';
+import {
+  ExpansionPanelActionsComponent,
+  ExpansionPanelComponent,
+  ExpansionPanelHeaderContentComponent,
+} from '@hra-ui/design-system/expansion-panel';
 /**
  * Cell Type Component
  */
@@ -56,6 +61,9 @@ import { MicroTooltipDirective } from '@hra-ui/design-system/micro-tooltip';
     TooltipCardComponent,
     MatSnackBarModule,
     MicroTooltipDirective,
+    ExpansionPanelComponent,
+    ExpansionPanelActionsComponent,
+    ExpansionPanelHeaderContentComponent,
   ],
   templateUrl: './cell-types.component.html',
   styleUrl: './cell-types.component.scss',
