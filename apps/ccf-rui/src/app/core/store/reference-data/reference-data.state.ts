b6009ef9ee103a0263ab2e5f21174f61c11f921b
Bruce W. Herr II <bherr@iu.edu>
2024-11-15 15:01:00 -0500

    fix: :bug: When uploading RUI registrations for editing, check the representation of against the organ list. Fixes #761

--- apps/ccf-rui/src/app/core/store/reference-data/reference-data.state.ts
+++ apps/ccf-rui/src/app/core/store/reference-data/reference-data.state.ts
@@ -176,7 +176,9 @@ export class ReferenceDataState extends NgxsImmutableDataRepository<ReferenceDat
 
     const name = entity.label ?? '';
     const organ = ALL_ORGANS.find(
-      (info) => name.endsWith(info.organ) && (!entity.side || entity.side.toLowerCase() === info.side),
+      (info) =>
+        (entity.representation_of == info.id || name.endsWith(info.organ)) &&
+        (!entity.side || entity.side.toLowerCase() === info.side),
     );
     if (!organ) {
       return undefined;
