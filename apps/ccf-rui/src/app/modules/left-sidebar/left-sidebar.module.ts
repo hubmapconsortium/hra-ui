79ad49acd459c93b1df9ffae900e9731526b234c
edlu77 <eddd.lu@gmail.com>
2024-12-10 23:00:45 -0500

    Add tooltips to page

--- apps/ccf-rui/src/app/modules/left-sidebar/left-sidebar.module.ts
+++ apps/ccf-rui/src/app/modules/left-sidebar/left-sidebar.module.ts
@@ -3,23 +3,22 @@ import { NgModule } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatMenuModule } from '@angular/material/menu';
+import { ButtonModule } from '@hra-ui/design-system/button';
+import { ExpansionPanelComponent, ExpansionPanelHeaderContentComponent } from '@hra-ui/design-system/expansion-panel';
+import { IconButtonSizeDirective } from '@hra-ui/design-system/icon-button';
+import { MicroTooltipDirective } from '@hra-ui/design-system/micro-tooltip';
 import { OpacitySliderModule } from 'ccf-shared';
 
-import { ExpansionPanelComponent, ExpansionPanelHeaderContentComponent } from '@hra-ui/design-system/expansion-panel';
 import { NameInputModule } from '../../shared/components/name-input/name-input.module';
 import { VisibilityMenuModule } from '../../shared/components/visibility-menu/visibility-menu.module';
 import { LeftSidebarComponent } from './left-sidebar.component';
-import { MatMenuModule } from '@angular/material/menu';
-import { IconButtonSizeDirective } from '@hra-ui/design-system/icon-button';
-import { ButtonModule } from '@hra-ui/design-system/button';
 
 @NgModule({
   imports: [
     CommonModule,
     MatExpansionModule,
     MatIconModule,
-    MatTooltipModule,
 
     MatDividerModule,
     NameInputModule,
@@ -30,6 +29,7 @@ import { ButtonModule } from '@hra-ui/design-system/button';
     MatMenuModule,
     IconButtonSizeDirective,
     ButtonModule,
+    MicroTooltipDirective,
   ],
   declarations: [LeftSidebarComponent],
   exports: [LeftSidebarComponent],
--- apps/ccf-rui/src/app/shared/components/visibility-menu/visibility-menu.module.ts
+++ apps/ccf-rui/src/app/shared/components/visibility-menu/visibility-menu.module.ts
@@ -5,9 +5,10 @@ import { OpacitySliderModule } from 'ccf-shared';
 import { MatRippleModule } from '@angular/material/core';
 
 import { VisibilityMenuComponent } from './visibility-menu.component';
+import { MicroTooltipDirective } from '@hra-ui/design-system/micro-tooltip';
 
 @NgModule({
-  imports: [CommonModule, MatIconModule, MatRippleModule, OpacitySliderModule],
+  imports: [CommonModule, MatIconModule, MatRippleModule, OpacitySliderModule, MicroTooltipDirective],
   declarations: [VisibilityMenuComponent],
   exports: [VisibilityMenuComponent],
 })
