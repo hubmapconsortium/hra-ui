2a8b883d395acffe2d4bf3070295dc3c97faeb69
Daniel Bolin <axbolin@iu.edu>
2025-01-13 11:45:59 -0500

    feat(app:apps.humanatlas.io): Create apps.humanatlas.io applications

---
+++ apps/apps.humanatlas.io/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  standalone: true,
+  imports: [RouterModule],
+  selector: 'hra-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {}
---
+++ apps/apps.humanatlas.io/src/app/app.config.ts
@@ -0,0 +1,8 @@
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideDesignSystem } from '@hra-ui/design-system';
+import { appRoutes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(appRoutes), provideDesignSystem()],
+};
