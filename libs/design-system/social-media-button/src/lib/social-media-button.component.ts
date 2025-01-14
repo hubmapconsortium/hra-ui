d4f95aef678f3c76ce148a4d6355c865eaca94df
edlu77 <eddd.lu@gmail.com>
2025-01-14 14:57:36 -0500

    Add email back as social option

--- libs/design-system/social-media-button/src/lib/social-media-button.component.ts
+++ libs/design-system/social-media-button/src/lib/social-media-button.component.ts
@@ -3,7 +3,7 @@ import { ChangeDetectionStrategy, Component, computed, input } from '@angular/co
 import { MatIconModule } from '@angular/material/icon';
 
 /** Social media name type */
-export type SocialMediaName = 'x' | 'facebook' | 'instagram' | 'youtube' | 'linkedin' | 'github';
+export type SocialMediaName = 'x' | 'facebook' | 'instagram' | 'youtube' | 'linkedin' | 'email' | 'github';
 /** Button size type */
 export type SocialMediaButtonSize = 'small' | 'large';
 
@@ -14,6 +14,7 @@ export const SOCIAL_LINKS: Record<SocialMediaName, string> = {
   instagram: 'https://www.instagram.com/cns_at_iu/',
   youtube: 'https://www.youtube.com/@CNSCenter/',
   linkedin: 'https://www.linkedin.com/company/cns-indiana-university-bloomington',
+  email: 'mailto:infoccf@iu.edu',
   github: 'https://github.com/hubmapconsortium/hra-ui',
 };
 
