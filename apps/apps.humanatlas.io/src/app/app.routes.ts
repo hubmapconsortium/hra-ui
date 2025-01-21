68120e9a4226f9431f5ec118ca7279eb04b8f3e8
Prashant Channe <prashantchanne121@gmail.com>
2025-01-21 21:10:33 +0000

    created table with predictions data

--- apps/apps.humanatlas.io/src/app/app.routes.ts
+++ apps/apps.humanatlas.io/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Route } from '@angular/router';
 import { CellPopulationPredictorComponent } from './pages/us1/cell-population-predictor/cell-population-predictor.component';
 import { CellPopulationPredictionsComponent } from './pages/us1/cell-population-predictions/cell-population-predictions.component';
+import predictions from './data/predictions';
 
 /** Application routes */
 export const appRoutes: Route[] = [
@@ -11,5 +12,6 @@ export const appRoutes: Route[] = [
   {
     path: 'result',
     component: CellPopulationPredictionsComponent,
+    data: { predictions: predictions },
   },
 ];
--- apps/apps.humanatlas.io/src/app/pages/us1/cell-population-predictions/cell-population-predictions.component.ts
+++ apps/apps.humanatlas.io/src/app/pages/us1/cell-population-predictions/cell-population-predictions.component.ts
@@ -1,12 +1,35 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { WorkflowCardModule } from '@hra-ui/design-system/workflow-card';
+import { MatIconModule } from '@angular/material/icon';
+import { DeleteFileButtonComponent } from '@hra-ui/design-system/delete-file-button';
+import { MatTableModule } from '@angular/material/table';
+
+export interface Prediction {
+  tool: string;
+  modality: string;
+  cell_id: string;
+  cell_label: string;
+  count: number;
+  percentage: number;
+}
 
 @Component({
   selector: 'hra-cell-population-predictions',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, WorkflowCardModule, MatIconModule, DeleteFileButtonComponent, MatTableModule],
   templateUrl: './cell-population-predictions.component.html',
   styleUrl: './cell-population-predictions.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CellPopulationPredictionsComponent {}
+export class CellPopulationPredictionsComponent implements OnInit {
+  predictions: Prediction[] = [];
+  displayedColumns: string[] = ['tool', 'modality', 'percentage', 'count', 'cell_label', 'cell_id'];
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.predictions = this.route.snapshot.data['predictions'] || [];
+  }
+}
