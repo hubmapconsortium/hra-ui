ca5d53bbc91d134c31d5060d0079e7a8d5e64fcf
Daniel Bolin <axbolin@iu.edu>
2025-01-30 16:50:36 -0500

    feat(app:humanatlas.io): Add publications nav button

--- apps/humanatlas.io/src/app/components/page-data/page-data.ts
+++ apps/humanatlas.io/src/app/components/page-data/page-data.ts
@@ -1,5 +1,7 @@
 /** An interface representing the details of the page data */
 export interface PageDataItems {
+  /** Anchor id */
+  id?: string;
   /** Title of the page data */
   heading: string;
   /** Description of the page data */
