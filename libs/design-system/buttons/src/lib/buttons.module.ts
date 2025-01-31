0c3f3670e24342b596d532e6915d37f9e74c89af
Daniel Bolin <axbolin@iu.edu>
2025-01-31 12:31:20 -0500

    fix(global): Scope styles (#943)
    * fix(global): Scope styles
    
    * fix(global): Fix some buttons
    
    * fix(lib:design-system): Do not apply link styles to mat-buttons

--- libs/design-system/buttons/src/lib/buttons.module.ts
+++ libs/design-system/buttons/src/lib/buttons.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { CtaButtonDirective, ButtonSizeDirective, ButtonVariantDirective } from '@hra-ui/design-system/buttons/button';
+import {
+  CtaButtonDirective,
+  ButtonSizeDirective,
+  ButtonVariantDirective,
+  PrimaryButtonVariantDirective,
+  SecondaryButtonVariantDirective,
+} from '@hra-ui/design-system/buttons/button';
 
 /** All re-exported modules, components, directives, etc. */
-const REEXPORTS = [MatButtonModule, CtaButtonDirective, ButtonSizeDirective, ButtonVariantDirective];
+const REEXPORTS = [
+  MatButtonModule,
+  CtaButtonDirective,
+  ButtonSizeDirective,
+  ButtonVariantDirective,
+  PrimaryButtonVariantDirective,
+  SecondaryButtonVariantDirective,
+];
 
 /** Packages up subpackage angular exports for easier use */
 @NgModule({
