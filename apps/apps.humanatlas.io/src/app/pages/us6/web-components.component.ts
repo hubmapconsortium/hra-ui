ec900623d5637bd18c62769d6300379a05157e3b
Gauri Markandey <gauri2029@gmail.com>
2025-01-31 17:17:50 -0500

    added component cards to web components page.

--- apps/apps.humanatlas.io/src/app/pages/us6/web-components.component.ts
+++ apps/apps.humanatlas.io/src/app/pages/us6/web-components.component.ts
@@ -1,15 +1,37 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductLogoComponent } from '@hra-ui/design-system/product-logo';
 import { SoftwareStatusIndicatorComponent } from '@hra-ui/design-system/software-status-indicator';
-import { ButtonModule } from '@hra-ui/design-system/button';
-
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatOption } from '@angular/material/core';
+import { MatSelect } from '@angular/material/select';
+import organs from '../../../../../apps.humanatlas.io/organs.json';
+import components from '../../../../../apps.humanatlas.io/component-defs.json';
+import { WebComponentCardComponent } from '@hra-ui/design-system/web-component-card';
+import { ButtonsModule } from '@hra-ui/design-system/buttons';
 @Component({
   selector: 'hra-web-components',
   standalone: true,
-  imports: [CommonModule, ProductLogoComponent, SoftwareStatusIndicatorComponent, ButtonModule],
+  imports: [
+    CommonModule,
+    WebComponentCardComponent,
+    ProductLogoComponent,
+    SoftwareStatusIndicatorComponent,
+    ButtonsModule,
+    MatFormField,
+    MatOption,
+    MatLabel,
+    MatSelect,
+  ],
   templateUrl: './web-components.component.html',
   styleUrl: './web-components.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WebComponentsComponent {}
+export class WebComponentsComponent implements OnInit {
+  ngOnInit(): void {
+    this.selectedOrgan = this.organs[0];
+  }
+  protected organs = organs;
+  protected components = components;
+  selectedOrgan: any;
+}
