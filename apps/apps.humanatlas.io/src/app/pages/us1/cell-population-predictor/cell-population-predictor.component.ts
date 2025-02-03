9c77550ae68b93e9ab8bc30d93aff316f49cd834
Prashant Channe <prashantchanne121@gmail.com>
2025-02-03 19:41:33 +0000

    updated logic for buttons in the home page

--- apps/apps.humanatlas.io/src/app/pages/us1/cell-population-predictor/cell-population-predictor.component.ts
+++ apps/apps.humanatlas.io/src/app/pages/us1/cell-population-predictor/cell-population-predictor.component.ts
@@ -8,6 +8,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { AssetUrlPipe } from '@hra-ui/cdk/app-href';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { TOOLTIP_POSITION_RIGHT_SIDE } from '@hra-ui/cde-visualization';
+import SAMPLE_DATA from './sample-data.json';
+import { DeleteFileButtonComponent } from '@hra-ui/design-system/delete-file-button';
+import { RouterModule } from '@angular/router';
 
 /**
  * Cell Population Predictor Page Component
@@ -24,25 +27,48 @@ import { TOOLTIP_POSITION_RIGHT_SIDE } from '@hra-ui/cde-visualization';
     WorkflowCardModule,
     TooltipCardComponent,
     OverlayModule,
+    DeleteFileButtonComponent,
+    RouterModule,
   ],
   templateUrl: './cell-population-predictor.component.html',
   styleUrl: './cell-population-predictor.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CellPopulationPredictorComponent {
+  /** File */
+  protected file?: File;
+
   /** Whether to show upload info tooltip */
-  uploadInfoOpen = false;
+  protected uploadInfoOpen = false;
 
   /** Whether to show create info tooltip   */
-  createInfoOpen = false;
+  protected createInfoOpen = false;
 
+  /** Tooltip Position */
   protected readonly tooltipPosition = TOOLTIP_POSITION_RIGHT_SIDE;
 
   /** Tooltip content */
-  readonly tooltip: TooltipContent[] = [
+  protected readonly tooltip: TooltipContent[] = [
     {
       description:
         'An extraction site defines the 3D spatial size, translation, rotation, reference organ (with laterality and sex) and metadata (creator name, creation date) for a tissue block. Importantly, an extraction site also contains a list of anatomical structures that a tissue block collides with, either via bounding-box or mesh-based collision detection. Extraction sites are generated using the Registration User Interface (RUI).',
     },
   ];
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.file = input.files[0];
+      console.log('Selected file:', this.file.name);
+    }
+  }
+
+  cancelLoad(): void {
+    this.file = undefined;
+  }
+
+  onUseSample() {
+    const blob = new Blob([JSON.stringify(SAMPLE_DATA)], { type: 'application/json' });
+    this.file = new File([blob], 'sample.json', { type: 'application/json' });
+  }
 }
