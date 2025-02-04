d506ef551b31584179637765eba7e8a8676d3173
Daniel Bolin <axbolin@iu.edu>
2025-02-04 11:35:59 -0500

    refactor(lib:design-system): Brand mark tweaks

--- libs/design-system/brand/src/lib/brand.module.ts
+++ libs/design-system/brand/src/lib/brand.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrandLogoComponent } from '@hra-ui/design-system/brand/logo';
+import { BrandMarkComponent } from '@hra-ui/design-system/brand/mark';
 
-const REEXPORTS = [BrandLogoComponent];
+const REEXPORTS = [BrandLogoComponent, BrandMarkComponent];
 
 @NgModule({
   imports: REEXPORTS,
