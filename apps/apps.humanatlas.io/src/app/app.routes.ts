98b86f289408c38274359675c664a350016da304
Prashant Channe <prashantchanne121@gmail.com>
2025-02-07 17:02:54 +0000

    updated route with a predictions resolver

--- apps/apps.humanatlas.io/src/app/app.routes.ts
+++ apps/apps.humanatlas.io/src/app/app.routes.ts
@@ -1,14 +1,16 @@
 import { Route } from '@angular/router';
 import { CellPopulationPredictorComponent } from './pages/us1/cell-population-predictor/cell-population-predictor.component';
 import { CellPopulationPredictionsComponent } from './pages/us1/cell-population-predictions/cell-population-predictions.component';
-import predictions from './data/predictions';
+import { resolvePredictions } from './pages/us1/services/predictions.service';
 
 /** Application routes */
 export const appRoutes: Route[] = [
   {
     path: 'us1/result',
     component: CellPopulationPredictionsComponent,
-    data: { predictions: predictions },
+    resolve: {
+      predictions: resolvePredictions,
+    },
   },
   {
     path: 'us1',
