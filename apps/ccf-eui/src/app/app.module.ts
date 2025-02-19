0fc2eb33a079cb4ee2545c28769ef505c3fd4de6
Edward Lu <36934022+edlu77@users.noreply.github.com>
2025-02-19 12:22:20 -0500

    EUI organ select update (#944)
    * Cosmetic and functional updates to results panel
    
    * Fixes and improvements
    
    * Organ select update
    
    * Create separate organ select component
    
    * Fix organ count indicator
    
    * Remove default test file
    
    * Update menu options
    
    * Refactor organ select logic

--- apps/ccf-eui/src/app/app.module.ts
+++ apps/ccf-eui/src/app/app.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -9,30 +7,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideDesignSystem } from '@hra-ui/design-system';
 import { ButtonsModule } from '@hra-ui/design-system/buttons';
+import { BackButtonBarComponent } from '@hra-ui/design-system/buttons/back-button-bar';
 import { ExpansionPanelModule } from '@hra-ui/design-system/expansion-panel';
 import { NavHeaderButtonsComponent } from '@hra-ui/design-system/nav-header-buttons';
-import { BodyUiModule, InfoButtonModule, OrganSelectorModule, TrackingPopupModule } from 'ccf-shared';
+import { BodyUiModule, InfoButtonModule, TrackingPopupModule } from 'ccf-shared';
 
 import { AppWebComponent } from './app-web-component.component';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { FiltersPopoverModule } from './modules/filters/filters-popover/filters-popover.module';
 import { OntologyExplorationModule } from './modules/ontology-exploration/ontology-exploration.module';
+import { ResultsBrowserComponent } from './modules/results-browser/results-browser/results-browser.component';
 import { ButtonToggleModule } from './shared/components/button-toggle/button-toggle.module';
-import { DrawerModule } from './shared/components/drawer/drawer.module';
 import { DualSliderModule } from './shared/components/dual-slider/dual-slider.module';
+import { OrganSelectComponent } from './shared/components/organ-select/organ-select.component';
 import { RunSpatialSearchModule } from './shared/components/run-spatial-search/run-spatial-search.module';
 import { SpinnerOverlayModule } from './shared/components/spinner-overlay/spinner-overlay.module';
-import { ViewerModule } from './shared/components/viewer/viewer.module';
-import { BackButtonBarComponent } from '@hra-ui/design-system/buttons/back-button-bar';
-import { ResultsBrowserComponent } from './modules/results-browser/results-browser/results-browser.component';
 
 @NgModule({
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
     CoreModule,
-    DrawerModule,
     FiltersPopoverModule,
     OntologyExplorationModule,
     MatIconModule,
@@ -40,23 +36,19 @@ import { ResultsBrowserComponent } from './modules/results-browser/results-brows
     ResultsBrowserComponent,
     SpinnerOverlayModule,
     BodyUiModule,
-    OrganSelectorModule,
     InfoButtonModule,
     MatTooltipModule,
-    ViewerModule,
     TrackingPopupModule,
     MatSnackBarModule,
     RunSpatialSearchModule,
-    MatSnackBarModule,
-    MatButtonToggleModule,
     ButtonToggleModule,
 
     NavHeaderButtonsComponent,
     ExpansionPanelModule,
     ButtonsModule,
     MatMenuModule,
-    MatDividerModule,
     BackButtonBarComponent,
+    OrganSelectComponent,
   ],
   declarations: [AppComponent, AppWebComponent],
   providers: [provideDesignSystem()],
