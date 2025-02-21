bdea469ee112e08dcf950b19c0731467c9b727a1
Prashant Channe <prashantchanne121@gmail.com>
2025-02-21 22:18:37 +0000

    updated routes

--- apps/apps.humanatlas.io/src/app/app.routes.ts
+++ apps/apps.humanatlas.io/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Route } from '@angular/router';
 import { CellPopulationPredictorComponent } from './pages/us1/cell-population-predictor/cell-population-predictor.component';
 import { CellPopulationPredictionsComponent } from './pages/us1/cell-population-predictions/cell-population-predictions.component';
+import { TissueOriginPredictorComponent } from './pages/us2/tissue-origin-predictor/tissue-origin-predictor.component';
 import { resolvePredictions } from './pages/us1/services/predictions.service';
 
 /** Application routes */
@@ -15,5 +16,9 @@ export const appRoutes: Route[] = [
   {
     path: 'us1',
     component: CellPopulationPredictorComponent,
+  },
+  {
+    path: 'us2',
+    component: TissueOriginPredictorComponent,
   },
 ];
