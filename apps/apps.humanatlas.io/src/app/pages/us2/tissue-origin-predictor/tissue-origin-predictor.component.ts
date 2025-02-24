593e71ef93d7816b3cfa849d2d8b21a1e85a040a
Prashant Channe <prashantchanne121@gmail.com>
2025-02-24 21:29:23 +0000

    completed basic UI structure for tissue origin predictor page

--- apps/apps.humanatlas.io/src/app/pages/us2/tissue-origin-predictor/tissue-origin-predictor.component.ts
+++ apps/apps.humanatlas.io/src/app/pages/us2/tissue-origin-predictor/tissue-origin-predictor.component.ts
@@ -3,11 +3,26 @@ import { CommonModule } from '@angular/common';
 import { ProductHeaderComponent } from '../../../components/product-header/product-header.component';
 import { WorkflowCardModule } from '@hra-ui/design-system/workflow-card';
 import { ButtonsModule } from '@hra-ui/design-system/buttons';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MicroTooltipDirective } from '@hra-ui/design-system/micro-tooltip';
+import { ErrorIndicatorComponent } from '@hra-ui/design-system/error-indicator';
 
 @Component({
   selector: 'hra-tissue-origin-predictor',
   standalone: true,
-  imports: [CommonModule, ProductHeaderComponent, WorkflowCardModule, ButtonsModule],
+  imports: [
+    CommonModule,
+    ProductHeaderComponent,
+    WorkflowCardModule,
+    ButtonsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MicroTooltipDirective,
+    ErrorIndicatorComponent,
+  ],
   templateUrl: './tissue-origin-predictor.component.html',
   styleUrl: './tissue-origin-predictor.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
