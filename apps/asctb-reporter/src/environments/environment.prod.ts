fdb2e0ef5a5430a97eeaf1af92a1d8e873dfa0ce
Bruce W. Herr II <bherr@iu.edu>
2025-02-25 13:18:45 -0500

    feat(app:asctb-reporter): :sparkles: use sheet configs from hra-api

--- apps/asctb-reporter/src/environments/environment.prod.ts
+++ apps/asctb-reporter/src/environments/environment.prod.ts
@@ -3,4 +3,6 @@ export const environment = {
   tag: '',
   googleAnalyticsId: 'G-WYFW84S56B',
   asctbApiUrl: 'https://apps.humanatlas.io/asctb-api',
+  sheetConfigUrl: 'https://apps.humanatlas.io/api/v1/asctb-sheet-config',
+  omapSheetConfigUrl: 'https://apps.humanatlas.io/api/v1/asctb-omap-sheet-config',
 };
