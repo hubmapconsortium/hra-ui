798bc4844bc5f9b8c9621ecf1fac21df95edfb91
Prashant Channe <prashantchanne121@gmail.com>
2025-03-04 17:52:51 +0000

    created service for tissue origin predictor and updated logic for file buttons

--- apps/apps.humanatlas.io/src/app/pages/us2/tissue-origin-predictor/tissue-origin-predictor.component.ts
+++ apps/apps.humanatlas.io/src/app/pages/us2/tissue-origin-predictor/tissue-origin-predictor.component.ts
@@ -1,16 +1,18 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductHeaderComponent } from '../../../components/product-header/product-header.component';
-import { WorkflowCardModule } from '@hra-ui/design-system/workflow-card';
-import { ButtonsModule } from '@hra-ui/design-system/buttons';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MicroTooltipDirective } from '@hra-ui/design-system/micro-tooltip';
-import { ErrorIndicatorComponent } from '@hra-ui/design-system/error-indicator';
-import { DeleteFileButtonComponent } from '@hra-ui/design-system/buttons/delete-file-button';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { ButtonsModule } from '@hra-ui/design-system/buttons';
+import { DeleteFileButtonComponent } from '@hra-ui/design-system/buttons/delete-file-button';
+import { ErrorIndicatorComponent } from '@hra-ui/design-system/error-indicator';
+import { MicroTooltipDirective } from '@hra-ui/design-system/micro-tooltip';
+import { WorkflowCardModule } from '@hra-ui/design-system/workflow-card';
+import { ProductHeaderComponent } from '../../../components/product-header/product-header.component';
 import { PredictionsService } from '../../us1/services/predictions.service';
+import { TissueOriginService } from '../services/tissue-origin.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'hra-tissue-origin-predictor',
@@ -26,38 +28,52 @@ import { PredictionsService } from '../../us1/services/predictions.service';
     MicroTooltipDirective,
     ErrorIndicatorComponent,
     DeleteFileButtonComponent,
+    FormsModule,
   ],
   templateUrl: './tissue-origin-predictor.component.html',
   styleUrl: './tissue-origin-predictor.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TissueOriginPredictorComponent implements OnInit {
+export class TissueOriginPredictorComponent {
   /** CSV File */
-  protected file: File | null = null;
+  protected file = signal<File | null>(null);
+
+  /** Predictions Service */
+  protected readonly predictionsService = inject(PredictionsService);
+
+  /** Tissue Origin Service */
+  protected readonly tissueOriginService = inject(TissueOriginService);
 
   /** Supported organs */
-  protected readonly predictionService = inject(PredictionsService);
-
-  /** Supported organs */
-  protected supportedOrgans = toSignal(this.predictionService.loadSupportedReferenceOrgans(), {
+  protected supportedOrgans = toSignal(this.predictionsService.loadSupportedReferenceOrgans(), {
     initialValue: [],
   });
 
+  /** Supported tools */
+  protected supportedTools = toSignal(this.tissueOriginService.loadSupportedTools(), {
+    initialValue: [],
+  });
+
+  /** Supported organ value */
+  protected supportedOrgan = '';
+
+  /** Supported tool value */
+  protected supportedTool = '';
+
   /** Use sample CSV File */
-  onUseSampleClicked(): void {
-    // TODO
+  async onUseSampleClicked(): Promise<void> {
+    if (this.tissueOriginService.getFile() === null) {
+      await this.tissueOriginService.setSampleFile();
+    }
+    this.file.set(this.tissueOriginService.getFile());
   }
 
-  /** Truggered when user uploads a file */
+  /** Triggered when user uploads a file */
   onFileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
-      this.file = input.files[0];
-      // this.predictionsService.setFile(this.file);
+      this.file.set(input.files[0]);
+      this.tissueOriginService.setFile(input.files[0]);
     }
-  }
-
-  ngOnInit() {
-    console.log(this.supportedOrgans());
   }
 }
