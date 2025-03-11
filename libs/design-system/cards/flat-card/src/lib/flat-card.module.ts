e3a1827b83a9b277a76a05ca4c91ad30584ea052
Daniel Bolin <axbolin@iu.edu>
2025-03-11 13:24:04 -0400

    docs(lib:design-system): Add missing code docs

--- libs/design-system/cards/flat-card/src/lib/flat-card.module.ts
+++ libs/design-system/cards/flat-card/src/lib/flat-card.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FlatCardActionsComponent, FlatCardComponent } from './flat-card.component';
 
+/** All re-exported modules, components, directives, etc. */
 const REEXPORTS = [FlatCardComponent, FlatCardActionsComponent];
 
 @NgModule({
