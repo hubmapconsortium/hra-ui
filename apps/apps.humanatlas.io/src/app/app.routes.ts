653e220f996674342681527926e3d712a298afc1
Gauri Markandey <gauri2029@gmail.com>
2025-03-17 12:57:45 -0400

    Added new component landing page.

--- apps/apps.humanatlas.io/src/app/app.routes.ts
+++ apps/apps.humanatlas.io/src/app/app.routes.ts
@@ -3,9 +3,14 @@ import { CellPopulationPredictorComponent } from './pages/us1/cell-population-pr
 import { CellPopulationPredictionsComponent } from './pages/us1/cell-population-predictions/cell-population-predictions.component';
 import { resolvePredictions } from './pages/us1/services/predictions.service';
 import { WebComponentsComponent } from './pages/us6/web-components.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
 /** Application routes */
 export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: LandingPageComponent,
+  },
   {
     path: 'us6',
     component: WebComponentsComponent,
