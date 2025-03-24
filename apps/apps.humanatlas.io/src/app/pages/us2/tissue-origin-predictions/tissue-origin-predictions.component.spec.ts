9d01381f8cad8cb7fe8fe57db630bd3851435e30
Prashant Channe <prashantchanne121@gmail.com>
2025-03-24 19:48:05 +0000

    added tests for tissue origin predictions page

--- apps/apps.humanatlas.io/src/app/pages/us2/tissue-origin-predictions/tissue-origin-predictions.component.spec.ts
+++ apps/apps.humanatlas.io/src/app/pages/us2/tissue-origin-predictions/tissue-origin-predictions.component.spec.ts
@@ -1,21 +1,86 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { SnackbarService } from '@hra-ui/design-system/snackbar';
+import { render, screen } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
 import { TissueOriginPredictionsComponent } from './tissue-origin-predictions.component';
 
 describe('TissueOriginPredictionsComponent', () => {
-  let component: TissueOriginPredictionsComponent;
-  let fixture: ComponentFixture<TissueOriginPredictionsComponent>;
+  const providers = [provideHttpClient(), provideHttpClientTesting()];
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [TissueOriginPredictionsComponent],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(TissueOriginPredictionsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    global.ResizeObserver = jest.fn().mockImplementation(() => ({
+      observe: jest.fn(),
+      unobserve: jest.fn(),
+      disconnect: jest.fn(),
+    }));
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create', async () => {
+    const result = render(TissueOriginPredictionsComponent, { providers });
+    await expect(result).resolves.toBeTruthy();
+  });
+
+  it('downloads CSV file when user clicks on download CSV button for Anatomical Structures table', async () => {
+    global.URL.createObjectURL = jest.fn().mockReturnValue('mock-url');
+
+    const { fixture } = await render(TissueOriginPredictionsComponent, { providers });
+
+    const snackbarService = TestBed.inject(SnackbarService);
+    const snackbarSpy = jest.spyOn(snackbarService, 'open');
+
+    const menuButton = screen.getByTestId('anatomical-menu');
+    await userEvent.click(menuButton);
+    fixture.detectChanges();
+
+    const downloadAnatomicalButton = screen.getByTestId('download-anatomical-csv-button');
+    await userEvent.click(downloadAnatomicalButton);
+    fixture.detectChanges();
+
+    expect(snackbarSpy).toHaveBeenCalled();
+
+    const snackbar = screen.getByTestId('snackbar');
+    expect(snackbar.textContent).toBe('File downloaded');
+  });
+
+  it('downloads CSV file when user clicks on download CSV button for Similar Datasets table', async () => {
+    global.URL.createObjectURL = jest.fn().mockReturnValue('mock-url');
+
+    const { fixture } = await render(TissueOriginPredictionsComponent, { providers });
+
+    const snackbarService = TestBed.inject(SnackbarService);
+    const snackbarSpy = jest.spyOn(snackbarService, 'open');
+
+    const menuButton = screen.getByTestId('similar-datasets-menu');
+    await userEvent.click(menuButton);
+    fixture.detectChanges();
+
+    const downloadAnatomicalButton = screen.getByTestId('download-similar-dataset-csv-button');
+    await userEvent.click(downloadAnatomicalButton);
+    fixture.detectChanges();
+
+    expect(snackbarSpy).toHaveBeenCalled();
+
+    const snackbar = screen.getByTestId('snackbar');
+    expect(snackbar.textContent).toBe('File downloaded');
+  });
+
+  it('downloads JSON file when user clicks on download JSON-LD button', async () => {
+    global.URL.createObjectURL = jest.fn().mockReturnValue('mock-url');
+
+    const { fixture } = await render(TissueOriginPredictionsComponent, { providers });
+
+    const snackbarService = TestBed.inject(SnackbarService);
+    const snackbarSpy = jest.spyOn(snackbarService, 'open');
+
+    const downloadJsonButton = screen.getByTestId('download-json-button');
+    await userEvent.click(downloadJsonButton);
+    fixture.detectChanges();
+
+    expect(snackbarSpy).toHaveBeenCalled();
+
+    const snackbar = screen.getByTestId('snackbar');
+    expect(snackbar.textContent).toBe('File downloaded');
   });
 });
