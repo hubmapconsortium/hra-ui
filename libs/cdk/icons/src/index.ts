7f38185ae1668a9c53e44f0298689514d0ed136b
Daniel Bolin <axbolin@iu.edu>
2025-03-25 16:00:35 -0400

    refactor(lib:cdk): Update icon resolver logic

--- libs/cdk/icons/src/index.ts
+++ libs/cdk/icons/src/index.ts
@@ -1,2 +1,3 @@
-export * from './lib/providers';
-export * from './lib/resolvers';
+export { provideIcons } from './lib/providers';
+export { SvgIconNamespaceService } from './lib/svg-icons/namespace.service';
+export { ICONS_CONFIG, IconsConfig } from './lib/tokens';
---
+++ libs/cdk/icons/src/lib/svg-icons/namespace.service.ts
@@ -0,0 +1,49 @@
+import { inject, Injectable } from '@angular/core';
+import { APP_ASSETS_HREF } from '@hra-ui/common';
+import { ICONS_CONFIG } from '../tokens';
+import { Location } from '@angular/common';
+
+/** Default icon directory if not provided in the icons config */
+export const DEFAULT_SVG_DIRECTORY = 'assets/icons/';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SvgIconNamespaceService {
+  /** Icons configuration */
+  private readonly config = inject(ICONS_CONFIG, { optional: true });
+  /** Current assets href */
+  private readonly assetsHref = inject(APP_ASSETS_HREF);
+  /** Mapping from a namespace to a directory */
+  private readonly namespaceDirectories = new Map<string, string>();
+
+  /**
+   * Set an icon namespace's svg directory
+   *
+   * @param namespace Namespace
+   * @param directory Relative directory
+   * @returns `this` for chaining
+   */
+  setNamespaceDirectory(namespace: string, directory: string): this {
+    this.namespaceDirectories.set(namespace, directory);
+    return this;
+  }
+
+  /**
+   * Resolves the namespace's directory using the registered directory and the assets href
+   *
+   * @param namespace Namespace
+   * @returns A resolved path
+   */
+  resolveNamespaceDirectory(namespace: string): string {
+    const { joinWithSlash } = Location;
+    const assetsHref = this.assetsHref();
+    const directory = this.namespaceDirectories.get(namespace);
+    if (directory !== undefined) {
+      return joinWithSlash(assetsHref, directory);
+    }
+
+    const defaultDirectory = this.config?.svgDirectory ?? DEFAULT_SVG_DIRECTORY;
+    return joinWithSlash(assetsHref, joinWithSlash(defaultDirectory, namespace));
+  }
+}
