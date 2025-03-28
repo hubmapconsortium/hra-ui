d90979020e87dc340098eb76476f1a2181516dd2
Prashant Channe <prashantchanne121@gmail.com>
2025-03-28 21:12:56 +0000

    fixed a test in product header

--- apps/apps.humanatlas.io/src/app/components/product-header/product-header.component.spec.ts
+++ apps/apps.humanatlas.io/src/app/components/product-header/product-header.component.spec.ts
@@ -3,7 +3,12 @@ import { render } from '@testing-library/angular';
 
 describe('ProductHeaderComponent', () => {
   it('should create', async () => {
-    const result = render(ProductHeaderComponent);
-    expect(result).resolves.toBeTruthy();
+    const result = render(ProductHeaderComponent, {
+      componentInputs: {
+        imageUrl: '',
+        title: 'Test Product Header',
+      },
+    });
+    await expect(result).resolves.toBeTruthy();
   });
 });
