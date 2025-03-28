e4b5b0420c904f9741f313586cf2f01491b92bed
edlu77 <eddd.lu@gmail.com>
2025-03-28 16:02:42 -0400

    Remove changes in humanatlas.io

--- apps/humanatlas.io/src/app/app.component.ts
+++ apps/humanatlas.io/src/app/app.component.ts
@@ -1,12 +1,7 @@
-import { CommonModule } from '@angular/common';
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { ProductLogoComponent } from '@hra-ui/design-system/brand/product-logo';
-import { PageSectionComponent } from '@hra-ui/design-system/content-template/page-section';
+import { Component } from '@angular/core';
 import { NavigationModule } from '@hra-ui/design-system/navigation';
 
 import { CarouselComponent } from './components-v2/carousel/carousel.component';
-import { ButtonsModule } from '@hra-ui/design-system/buttons';
 import { SectionCardsComponent } from './components-v2/section-cards/section-cards.component';
 
 /**
@@ -14,21 +9,11 @@ import { SectionCardsComponent } from './components-v2/section-cards/section-car
  */
 @Component({
   selector: 'ccf-root',
-  imports: [
-    CommonModule,
-    NavigationModule,
-    CarouselComponent,
-    SectionCardsComponent,
-    PageSectionComponent,
-    ProductLogoComponent,
-    MatIconModule,
-    ButtonsModule,
-  ],
+  imports: [NavigationModule, CarouselComponent, SectionCardsComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   host: {
     class: 'hra-app',
   },
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppComponent {}
