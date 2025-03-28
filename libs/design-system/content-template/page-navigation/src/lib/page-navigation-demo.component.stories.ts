60b3361720a318820c7d4a78b6b4050ac2c29e41
edlu77 <eddd.lu@gmail.com>
2025-03-28 06:15:27 -0400

    Create page navigation demo in storybook

---
+++ libs/design-system/content-template/page-navigation/src/lib/page-navigation-demo.component.stories.ts
@@ -0,0 +1,26 @@
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideDesignSystem } from '@hra-ui/design-system';
+import { applicationConfig, Meta, StoryObj } from '@storybook/angular';
+
+import { PageNavigationDemoComponent } from './page-navigation-demo.component';
+
+const meta: Meta<PageNavigationDemoComponent> = {
+  component: PageNavigationDemoComponent,
+  title: 'Design System/Content Template/PageNavigation',
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/gQEMLugLjweDvbsNNUVffD/HRA-Design-System-Repository?node-id=7632-22566&t=hb4zN0Dq78X9iuuM-4',
+    },
+  },
+  decorators: [
+    applicationConfig({
+      providers: [provideAnimations(), provideDesignSystem()],
+    }),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<PageNavigationDemoComponent>;
+
+export const Primary: Story = {};
--- libs/design-system/content-template/page-navigation/src/lib/page-navigation-demo.component.ts
+++ libs/design-system/content-template/page-navigation/src/lib/page-navigation-demo.component.ts
@@ -1,11 +1,47 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { ProductLogoComponent } from '@hra-ui/design-system/brand/product-logo';
+import { ButtonsModule } from '@hra-ui/design-system/buttons';
+import { PageSectionComponent } from '@hra-ui/design-system/content-template/page-section';
+
+import { PageNavigationComponent, Section } from './page-navigation.component';
+
+const TEST_SECTIONS: Section[] = [
+  {
+    name: 'Page label',
+    children: [{ name: 'Page label 2' }, { name: 'Page label 3' }, { name: 'Page label 4' }],
+  },
+  {
+    name: 'Page label 5',
+    children: [
+      {
+        name: 'Page label 6',
+        children: [{ name: 'Page label 7' }, { name: 'Page label 8' }],
+      },
+      {
+        name: 'Page label 9',
+        children: [{ name: 'Page label 10' }, { name: 'Page label 11' }],
+      },
+    ],
+  },
+];
 
 @Component({
   selector: 'hra-page-navigation-demo',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    PageSectionComponent,
+    ProductLogoComponent,
+    MatIconModule,
+    ButtonsModule,
+    PageNavigationComponent,
+  ],
   templateUrl: './page-navigation-demo.component.html',
   styleUrl: './page-navigation-demo.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class PageNavigationDemoComponent {}
+export class PageNavigationDemoComponent {
+  readonly data: Section[] = TEST_SECTIONS;
+}
