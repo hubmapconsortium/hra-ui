61cd01c5983bed588a3123e1fde1b485cbe7c7cb
Saai Vignesh P <127758744+saaivignesh20@users.noreply.github.com>
2025-04-04 16:55:54 -0400

    API Pages (#1010)
    * create api component
    
    * add comment for the new component
    
    * splash header done
    
    * build(app:apps.humanatlas.io): :package: Install rapidoc
    
    * Configured scss for rapidoc, started styling
    
    * refactor(app:apps.humanatlas.io): :construction: rapidoc styles
    
    added styling for request/response elements
    
    * docs(app:apps.humanatlas.io): :memo: style file comments
    
    * fix: update package lock
    
    * feat(app:apps.humanatlas.io): :lipstick: rapidoc-ui-update
    
    switched from radio server selector to mat-select
    
    * docs(app:apps.humanatlas.io): :memo: add comments for components
    
    * improve test coverage by improving comments
    
    * refactor(app:apps.humanatlas.io): use route resolvers to select spec and endpoint
    
    * feat(app:apps.humanatlas.io): :sparkles: scroll to api section
    
    * docs(app:apps.humanatlas.io): review changes
    
    * fix tests check
    
    * pass compodoc tests
    
    * test(app:apps.humanatlas.io): :white_check_mark: tests for api component
    
    * docs(app:apps.humanatlas.io): :memo: full doc coverage done, updated project.json to prevent rapidoc global style injection.
    
    * feat(app:apps.humanatlas.io): config breadcrumbs, fix build budget, refactor code
    
    * docs(app:apps.humanatlas.io): :memo: complete doc for breadcrumb data impl.
    
    * fix(app:apps.humanatlas.io): :white_check_mark: pass server selector test temp
    
    * style(app:apps.humanatlas.io): :lipstick: rapidoc textbox border styles
    
    * refactor(app:apps.humanatlas.io): :recycle: refactor code and complete review changes
    
    * style(app:apps.humanatlas.io): :lipstick: fix scroll margin for api container
    
    * fix(app:apps.humanatlas.io): Addressed pr comments
    
    ---------
    
    Co-authored-by: Daniel Bolin <axbolin@iu.edu>

--- apps/apps.humanatlas.io/src/app/app.routes.ts
+++ apps/apps.humanatlas.io/src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { CellPopulationPredictorComponent } from './pages/us1/cell-population-pr
 import { CellPopulationPredictionsComponent } from './pages/us1/cell-population-predictions/cell-population-predictions.component';
 import { resolvePredictions } from './pages/us1/services/predictions.service';
 import { WebComponentsComponent } from './pages/us6/web-components.component';
+import { ApiComponent } from './pages/api/api.component';
+import { serverIdResolver } from './resolvers/server-id/server-id-resolver.resolver';
+import { BreadcrumbItem } from '@hra-ui/design-system/buttons/breadcrumbs';
 
 /** Application routes */
 export const appRoutes: Route[] = [
@@ -21,5 +24,28 @@ export const appRoutes: Route[] = [
   {
     path: 'us1',
     component: CellPopulationPredictorComponent,
+  },
+  {
+    path: 'api',
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: ApiComponent,
+        data: {
+          serverId: 'prod',
+        },
+      },
+      {
+        path: ':serverId',
+        component: ApiComponent,
+        resolve: {
+          serverId: serverIdResolver,
+        },
+      },
+    ],
+    data: {
+      crumbs: [{ name: 'Apps', route: '/' }, { name: 'API' }] satisfies BreadcrumbItem[],
+    },
   },
 ];
