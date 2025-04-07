26d930fe2849543f841226a41878e1e436ce636c
Daniel Bolin <axbolin@iu.edu>
2025-04-07 17:07:09 -0400

    docs(app:ccf-rui): :memo: Increase rui doc coverage

--- apps/ccf-rui/src/app/modules/right-sidebar/right-sidebar.component.ts
+++ apps/ccf-rui/src/app/modules/right-sidebar/right-sidebar.component.ts
@@ -18,24 +18,37 @@ import { MetadataService } from '../metadata/metadata.service';
   standalone: false,
 })
 export class RightSidebarComponent {
+  /** Emits when the registration is expanded */
   readonly registrationExpanded = output<boolean>();
 
+  /** Model state */
   protected readonly model = inject(ModelState);
+  /** Registration state */
   protected readonly registration = inject(RegistrationState);
+  /** Page state */
   protected readonly page = inject(PageState);
+  /** AS tags state */
   protected readonly astags = inject(AnatomicalStructureTagState);
+  /** Metadata modal service */
   protected readonly metadata = inject(MetadataService);
 
+  /** Model position */
   protected readonly position$ = this.model.position$.pipe(
     map((p) => ({ x: Math.floor(p.x), y: Math.floor(p.y), z: Math.floor(p.z) })),
   );
 
+  /** As tags */
   protected readonly tags = toSignal(this.astags.tags$, { initialValue: [] });
+  /** All tags */
   protected readonly allTags = toSignal(this.astags.entitiesArray$, { initialValue: [] });
+  /** Added tags */
   protected readonly addedTags = computed(() => this.tags().filter((tag) => tag.type === 'added'));
+  /** Assigned tags */
   protected readonly assignedTags = computed(() => this.tags().filter((tag) => tag.type === 'assigned'));
+  /** Removed tags */
   protected readonly removedTags = computed(() => this.allTags().filter((tag) => tag.type === 'removed'));
 
+  /** Set the model default position */
   setDefaultPosition() {
     if (this.registration.snapshot.initialRegistration) {
       this.registration.resetPosition();
