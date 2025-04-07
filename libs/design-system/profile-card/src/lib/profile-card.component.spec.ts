ddc5cfa97c585afca7009ce14862ff82eb1ea158
Saai Vignesh P <127758744+saaivignesh20@users.noreply.github.com>
2025-04-07 17:36:59 -0400

    feat(#816): :sparkles: add profile card to design system storybook

---
+++ libs/design-system/profile-card/src/lib/profile-card.component.spec.ts
@@ -0,0 +1,18 @@
+import { ProfileCardComponent } from './profile-card.component';
+import { render } from '@testing-library/angular';
+
+describe('ProfileCardComponent', () => {
+  it('should render', async () => {
+    const promise = render(ProfileCardComponent, {
+      inputs: {
+        alignment: 'left',
+        pictureUrl: 'assets/ui-images/placeholder.png',
+        name: 'Firstname Lastname',
+        description: 'Occupation, Company',
+        actionUrl: 'https://google.com/',
+      },
+    });
+
+    await expect(promise).resolves.toBeDefined();
+  });
+});
---
+++ libs/design-system/profile-card/src/lib/profile-card.component.ts
@@ -0,0 +1,43 @@
+import { Component, input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { HraCommonModule } from '@hra-ui/common';
+import { TextHyperlinkDirective } from '@hra-ui/design-system/buttons/text-hyperlink';
+
+/** Alignment options */
+export type Alignment = 'left' | 'center';
+
+/**
+ * Profile Card for About Page
+ */
+@Component({
+  selector: 'hra-profile-card',
+  imports: [HraCommonModule, MatIconModule, TextHyperlinkDirective],
+  templateUrl: './profile-card.component.html',
+  styleUrl: './profile-card.component.scss',
+})
+export class ProfileCardComponent {
+  /**
+   * Field for alignment option
+   */
+  readonly alignment = input.required<Alignment>();
+
+  /**
+   * Field for profile picture URL
+   */
+  readonly pictureUrl = input.required<string>();
+
+  /**
+   * Field for profile name
+   */
+  readonly name = input.required<string>();
+
+  /**
+   * Field for description
+   */
+  readonly description = input.required<string>();
+
+  /**
+   * Field for action URL
+   */
+  readonly actionUrl = input.required<string>();
+}
