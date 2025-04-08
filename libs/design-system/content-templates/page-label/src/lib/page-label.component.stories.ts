9607fd2f9c15b48643d778588f6bcc0cfda265ea
Edward Lu <36934022+edlu77@users.noreply.github.com>
2025-04-08 11:07:05 -0400

    Design system page section (#1009)
    * Add page section to Design System
    
    * Update title-large font and rename body-splash font to body-xl
    
    * Add section link to Design System
    
    * Documentation coverage
    
    * Update product icons
    
    * Add background to product logo
    
    * Add navigation tree
    
    * Use HraCommonModule
    
    * Update body typography letter spacing
    
    * Remove page navigation component from this branch
    
    * Add showDivider option
    
    * Remove changes in humanatlas.io
    
    * refactor(lib:design-system): :fire: Remove duplicate assets
    
    * refactor(lib:design-system): :truck: Rename content-template -> content-templates
    
    * refactor(lib:design-system): Turn content-templates into an entrypoint
    
    * Add missing documentation
    
    * Add organ logos component
    
    * Refactor section link and page section
    
    * Add page label component
    
    * Add website category logos
    
    * Documentation and tweaks
    
    * Tweaks
    
    ---------
    
    Co-authored-by: Daniel Bolin <axbolin@iu.edu>

---
+++ libs/design-system/content-templates/page-label/src/lib/page-label.component.stories.ts
@@ -0,0 +1,61 @@
+import { OrganLogoComponent } from '@hra-ui/design-system/brand/organ-logo';
+import { ProductLogoComponent } from '@hra-ui/design-system/brand/product-logo';
+import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+
+import { PageLabelComponent } from './page-label.component';
+
+const meta: Meta<PageLabelComponent> = {
+  component: PageLabelComponent,
+  title: 'Design System/Content Templates/PageLabel',
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/BCEJn9KCIbBJ5MzqnojKQp/HRA-Components?node-id=2769-16877',
+    },
+  },
+  decorators: [
+    moduleMetadata({
+      imports: [ProductLogoComponent, OrganLogoComponent],
+    }),
+  ],
+  args: {
+    tagline: 'Page label',
+  },
+  argTypes: {
+    includeLink: {
+      control: 'boolean',
+    },
+  },
+  render: (args) => ({
+    props: args,
+    styles: ['.hra-app { margin: 0 2rem; }', 'ul { margin: 0;  margin-bottom: 1.5rem;}'],
+  }),
+};
+
+export default meta;
+type Story = StoryObj<PageLabelComponent>;
+
+export const App: Story = {
+  args: {
+    app: 'ftu',
+  },
+};
+
+export const Organ: Story = {
+  args: {
+    organ: 'bladder',
+  },
+};
+
+export const WebsiteCategory: Story = {
+  args: {
+    category: 'data',
+  },
+};
+
+export const Multiple: Story = {
+  args: {
+    app: 'ftu',
+    organ: 'bladder',
+  },
+};
