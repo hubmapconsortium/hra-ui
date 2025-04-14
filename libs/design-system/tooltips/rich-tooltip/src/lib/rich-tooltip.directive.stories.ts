2379917690cff036347326324e248f13b6dd0502
Saai Vignesh P <127758744+saaivignesh20@users.noreply.github.com>
2025-04-14 13:25:04 -0400

    refactor(lib:design-system): :wastebasket: remove unnecessary imports

--- libs/design-system/tooltips/rich-tooltip/src/lib/rich-tooltip.directive.stories.ts
+++ libs/design-system/tooltips/rich-tooltip/src/lib/rich-tooltip.directive.stories.ts
@@ -2,7 +2,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { ButtonsModule } from '@hra-ui/design-system/buttons';
 import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
 import { RichTooltipDirective } from './rich-tooltip.directive';
-import { RichTooltipContainerComponent, RichTooltipTaglineComponent } from './content/rich-tooltip-content.component';
 import { RichTooltipModule } from './rich-tooltip.module';
 
 const meta: Meta<RichTooltipDirective> = {
@@ -11,7 +10,7 @@ const meta: Meta<RichTooltipDirective> = {
   parameters: {
     design: {
       type: 'figma',
-      url: 'https://www.figma.com/design/BCEJn9KCIbBJ5MzqnojKQp/HRA-Components?node-id=1720-14192', // TODO: replace with actual design URL
+      url: 'https://www.figma.com/design/BCEJn9KCIbBJ5MzqnojKQp/HRA-Components?node-id=1782-202',
     },
   },
   decorators: [
