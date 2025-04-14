3211865e4d2016194cb39cbd7c9fa704078ad622
rchavan10 <96959435+rchavan10@users.noreply.github.com>
2025-04-14 12:08:06 -0400

    Initial app setup (#1047)
    * Initial app setup
    
    * updated packages
    
    * update docs humanatlas setup
    
    * add doc coverage
    
    * update config files
    
    * chore(global:config): Remove unwanted changes to package.json & nx.json
    
    ---------
    
    Co-authored-by: Daniel Bolin <axbolin@iu.edu>

---
+++ apps/docs.humanatlas.io/jest.config.ts
@@ -0,0 +1,21 @@
+export default {
+  displayName: 'docs.humanatlas.io',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts', '@testing-library/jest-dom'],
+  coverageDirectory: '../../coverage/apps/docs.humanatlas.io',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
