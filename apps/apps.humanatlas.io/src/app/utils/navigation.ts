80abc046046f69d0d8f6ef0f7087ef5799bfd9d6
Saai Vignesh P <127758744+saaivignesh20@users.noreply.github.com>
2025-04-15 17:09:01 -0400

    refactor(app:apps.humanatlas.io): :adhesive_bandage: delay progress bar for route loading and introduce in memory scrolling in router

--- apps/apps.humanatlas.io/src/app/utils/navigation.ts
+++ apps/apps.humanatlas.io/src/app/utils/navigation.ts
@@ -1,7 +1,12 @@
 import { inject, Signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { EventType, Router } from '@angular/router';
-import { scan, distinctUntilChanged } from 'rxjs';
+import { Event, EventType, Router } from '@angular/router';
+import { deriveLoading } from 'ngxtension/derive-loading';
+import { filter, switchMap } from 'rxjs';
+
+function isNavigationFinishEvent(event: Event): boolean {
+  return [EventType.NavigationEnd, EventType.NavigationCancel, EventType.NavigationError].includes(event.type);
+}
 
 /**
  * Navigation utility function to show progress bar
@@ -9,22 +14,14 @@ import { scan, distinctUntilChanged } from 'rxjs';
  */
 export function isNavigating(): Signal<boolean> {
   const router = inject(Router);
-  const isNavigating$ = router.events.pipe(
-    scan((state, event) => {
-      switch (event.type) {
-        case EventType.NavigationStart:
-          return true;
-        case EventType.NavigationEnd:
-        case EventType.NavigationCancel:
-        case EventType.NavigationError:
-          return false;
-
-        default:
-          return state;
-      }
-    }, false),
-    distinctUntilChanged(),
+  const navigationEnd$ = router.events.pipe(
+    filter((event) => isNavigationFinishEvent(event)),
+    deriveLoading(),
+  );
+  const navigationStart$ = router.events.pipe(
+    filter((event) => event.type === EventType.NavigationStart),
+    switchMap(() => navigationEnd$),
   );
 
-  return toSignal(isNavigating$, { initialValue: false });
+  return toSignal(navigationStart$, { initialValue: false });
 }
