149bf39316e81b8df2fbc274c427b177b9982afd
Saai Vignesh P <127758744+saaivignesh20@users.noreply.github.com>
2025-04-15 16:33:05 -0400

    fix(app:apps.humanatlas.io): :children_crossing: breadcrumbs vertical align fix, close menu on navigation

--- libs/design-system/navigation/header/src/lib/header.component.ts
+++ libs/design-system/navigation/header/src/lib/header.component.ts
@@ -24,6 +24,9 @@ import { DesktopMenuComponent } from './desktop-menu/desktop-menu.component';
 import { MobileMenuComponent } from './mobile-menu/mobile-menu.component';
 import { HUBMAP_MENU, MENUS } from './static-data/parsed';
 import { Menu } from './types/menus.schema';
+import { NavigationEnd, NavigationSkipped, Router } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { filter } from 'rxjs';
 
 /** Call to action configuration */
 export interface CtaConfig {
@@ -114,6 +117,8 @@ export class HeaderComponent {
   private readonly mobileMenuOverlay = viewChild('mobileMenuOverlay', { read: CdkConnectedOverlay });
   /** Currently open menu or undefined */
   private readonly activeMenu = signal<Menu | 'main' | undefined>(undefined);
+  /** Reference to the application router */
+  private readonly router = inject(Router);
 
   /** Initialize the header */
   constructor() {
@@ -125,6 +130,15 @@ export class HeaderComponent {
     });
 
     explicitEffect([this.menuOffsetPx], () => this.updateMenuPositions(), { defer: true });
+
+    this.router.events
+      .pipe(
+        takeUntilDestroyed(),
+        filter(
+          (navigationEvent) => navigationEvent instanceof NavigationEnd || navigationEvent instanceof NavigationSkipped,
+        ),
+      )
+      .subscribe(() => this.closeMenu());
   }
 
   /**
