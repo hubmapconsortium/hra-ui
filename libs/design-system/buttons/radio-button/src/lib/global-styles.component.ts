05f32ca714dc1912c707b336040916e3aa3e197f
Prashant Channe <prashantchanne121@gmail.com>
2025-04-16 19:04:26 +0000

    added radio button example to the design system

---
+++ libs/design-system/buttons/radio-button/src/lib/global-styles.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'hra-radion-button-global-styles',
+  standalone: true,
+  template: '',
+  styleUrls: ['./global-styles.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RadioButtonGlobalStylesComponent {}
