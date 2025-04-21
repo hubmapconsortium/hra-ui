edb02ce7671101a02680de5bc6ff316ba55d2c1d
edlu77 <eddd.lu@gmail.com>
2025-04-21 17:14:57 -0400

    Fixes for tooltip width/overlapping

--- libs/design-system/tooltips/plain-tooltip/src/plain-tooltip.directive.ts
+++ libs/design-system/tooltips/plain-tooltip/src/plain-tooltip.directive.ts
@@ -21,6 +21,9 @@ export class PlainTooltipDirective {
   /** Size of the tooltip */
   readonly size = input<PlainTooltipSize>('medium', { alias: 'hraPlainTooltipSize' });
 
+  /** Optional tooltip name to set tooltip class for styling */
+  readonly tooltipName = input<string>('');
+
   /** Instance of MatTooltip */
   protected readonly tooltip = inject(MatTooltip);
 
@@ -29,7 +32,7 @@ export class PlainTooltipDirective {
     registerStyleComponents([PlainTooltipStylesComponent]);
 
     effect(() => {
-      this.tooltip.tooltipClass = ['hra-plain-tooltip', `hra-plain-tooltip-${this.size()}`];
+      this.tooltip.tooltipClass = ['hra-plain-tooltip', `hra-plain-tooltip-${this.size()}`, this.tooltipName()];
     });
   }
 }
