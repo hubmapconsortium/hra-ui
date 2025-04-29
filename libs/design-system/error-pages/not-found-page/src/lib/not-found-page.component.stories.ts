b388bc5f01fce9a0119fc1fa87f3120200d348f9
Saai Vignesh P <127758744+saaivignesh20@users.noreply.github.com>
2025-04-29 11:49:26 -0400

    refactor(lib:design-system): :recycle: review changes done

--- libs/design-system/error-pages/not-found-page/src/lib/not-found-page.component.stories.ts
+++ libs/design-system/error-pages/not-found-page/src/lib/not-found-page.component.stories.ts
@@ -1,33 +1,15 @@
-import { applicationConfig, moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
-import { provideDesignSystem } from '@hra-ui/design-system';
-import { ErrorPagesModule } from '@hra-ui/design-system/error-pages';
-import { provideRouter, Routes } from '@angular/router';
-
-/** Route that immediately cancels all navigation attempts */
-const ROUTES: Routes = [
-  {
-    path: '**',
-    canActivate: [() => false],
-    loadComponent: () => Promise.reject(''),
-  },
-];
+import { type Meta, type StoryObj } from '@storybook/angular';
+import { NotFoundPageComponent } from './not-found-page.component';
 
 const meta: Meta = {
   title: 'Design System/Error Pages/Not Found Page',
-  decorators: [
-    moduleMetadata({
-      imports: [ErrorPagesModule],
-    }),
-    applicationConfig({
-      providers: [provideDesignSystem(), provideRouter(ROUTES)],
-    }),
-  ],
-  render: (args) => ({
-    props: args,
-    template: `
-      <hra-not-found-page></hra-not-found-page>
-    `,
-  }),
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/BCEJn9KCIbBJ5MzqnojKQp/HRA-Components?node-id=3287-71',
+    },
+  },
+  component: NotFoundPageComponent,
 };
 export default meta;
 type Story = StoryObj;
--- libs/design-system/error-pages/src/lib/error-pages.module.ts
+++ libs/design-system/error-pages/src/lib/error-pages.module.ts
@@ -2,15 +2,10 @@ import { NgModule } from '@angular/core';
 import { NotFoundPageComponent } from '@hra-ui/design-system/error-pages/not-found-page';
 
 /**
- * All re-exported modules, components, directives, etc.
- */
-const REEXPORTS = [NotFoundPageComponent];
-
-/**
  * Error Pages Module - Contains components for displaying error pages
  */
 @NgModule({
-  imports: REEXPORTS,
-  exports: REEXPORTS,
+  imports: [NotFoundPageComponent],
+  exports: [NotFoundPageComponent],
 })
 export class ErrorPagesModule {}
