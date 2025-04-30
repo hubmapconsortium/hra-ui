06bc95e4048105f7dd16fb8fa9f11f6dcb688490
Prashant Channe <prashantchanne121@gmail.com>
2025-04-30 19:24:01 +0000

    updated test

--- apps/humanatlas.io/src/app/pages/content-page/content-page.component.spec.ts
+++ apps/humanatlas.io/src/app/pages/content-page/content-page.component.spec.ts
@@ -1,21 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { render } from '@testing-library/angular';
 import { ContentPageComponent } from './content-page.component';
 
 describe('ContentPageComponent', () => {
-  let component: ContentPageComponent;
-  let fixture: ComponentFixture<ContentPageComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ContentPageComponent],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(ContentPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
   it('should create', () => {
-    expect(component).toBeTruthy();
+    const result = render(ContentPageComponent);
+    expect(result).toBeTruthy();
   });
 });
