0b9464a4a012fa4c12e1fcc171ac711bcfb9a136
Gauri Markandey <68295389+gauri2029@users.noreply.github.com>
2025-05-01 16:24:05 -0400

    Upgrading 3rd party angular packages to latest versions. (#1125)
    * Upgraded third party packages to version 19
    
    * resolved PR comments
    
    ---------
    
    Co-authored-by: Daniel Bolin <axbolin@iu.edu>

--- libs/cde-visualization/src/index.ts
+++ libs/cde-visualization/src/index.ts
@@ -1,9 +1,7 @@
 import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideDesignSystem } from '@hra-ui/design-system';
 import { InputProps, createCustomElement } from '@hra-ui/webcomponents';
-import { ColorPickerModule } from 'ngx-color-picker';
 import { CdeVisualizationComponent } from './lib/cde-visualization/cde-visualization.component';
 
 export * from './lib/cde-visualization/cde-visualization.component';
@@ -22,5 +20,5 @@ export type CdeVisualizationElementProps = InputProps<CdeVisualizationComponent>
 
 /** Custom element definition for CdeVisualizationComponent */
 export const CdeVisualizationElement = createCustomElement('cde-visualization', CdeVisualizationComponent, {
-  providers: [provideHttpClient(), provideAnimations(), importProvidersFrom(ColorPickerModule), provideDesignSystem()],
+  providers: [provideHttpClient(), provideAnimations(), provideDesignSystem()],
 });
