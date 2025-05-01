11ebebfe4410a232d7fb918eeda7e8da102eb3b6
Saai Vignesh P <127758744+saaivignesh20@users.noreply.github.com>
2025-05-01 11:17:51 -0400

    test(lib:design-system): :white_check_mark: fix tests for server error component

--- libs/design-system/error-pages/server-error-page/src/lib/server-error-page.component.spec.ts
+++ libs/design-system/error-pages/server-error-page/src/lib/server-error-page.component.spec.ts
@@ -3,7 +3,11 @@ import { ServerErrorPageComponent } from './server-error-page.component';
 
 describe('ServerErrorPageComponent', () => {
   it('should render', async () => {
-    const promise = render(ServerErrorPageComponent, {});
+    const promise = render(ServerErrorPageComponent, {
+      inputs: {
+        reportIssueLink: '/report-issue',
+      },
+    });
     await expect(promise).resolves.toBeTruthy();
   });
 });
