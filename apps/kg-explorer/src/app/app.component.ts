22f236a93c5a92d475147213650b03ef5e55a15d
Raj Chavan <rajchavan1099@gmail.com>
2025-05-02 17:59:25 -0400

    add doc coverage

--- apps/kg-explorer/src/app/app.component.ts
+++ apps/kg-explorer/src/app/app.component.ts
@@ -2,6 +2,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavigationModule } from '@hra-ui/design-system/navigation';
 
+/**
+ * Main application component
+ */
 @Component({
   imports: [RouterModule, NavigationModule],
   selector: 'hra-kg-explorer-root',
