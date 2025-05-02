0dbfe4fafec1168c35275cab47911ec43fe03ee3
Daniel Bolin <axbolin@iu.edu>
2025-05-02 17:06:31 -0400

    test(lib:design-system): Add page section & related component tests

--- libs/design-system/layouts/table-of-contents/src/lib/table-of-contents-layout.component.spec.ts
+++ libs/design-system/layouts/table-of-contents/src/lib/table-of-contents-layout.component.spec.ts
@@ -1,21 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { render } from '@testing-library/angular';
 import { TableOfContentsLayoutComponent } from './table-of-contents-layout.component';
 
 describe('TableOfContentsLayoutComponent', () => {
-  let component: TableOfContentsLayoutComponent;
-  let fixture: ComponentFixture<TableOfContentsLayoutComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [TableOfContentsLayoutComponent],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(TableOfContentsLayoutComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create', async () => {
+    const promise = render(TableOfContentsLayoutComponent);
+    await expect(promise).resolves.toBeTruthy();
   });
 });
