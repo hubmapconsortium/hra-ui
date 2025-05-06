bd9dedab1f20ad63223cc564421076622496f520
Daniel Bolin <axbolin@iu.edu>
2025-05-06 17:55:15 -0400

    refactor(app:humanatlas.io): Add table-of-contents-layout to content-page

--- apps/humanatlas.io/src/app/pages/content-page/content-page.component.ts
+++ apps/humanatlas.io/src/app/pages/content-page/content-page.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { ContentTemplateOutletDirective } from '@hra-ui/cdk/content-template';
 import { PageSectionComponent } from '@hra-ui/design-system/content-templates/page-section';
+import { TableOfContentsLayoutModule } from '@hra-ui/design-system/layouts/table-of-contents';
 import { ContentPageData } from './types/content-page.schema';
 
 /**
@@ -9,7 +10,7 @@ import { ContentPageData } from './types/content-page.schema';
  */
 @Component({
   selector: 'hra-content-page',
-  imports: [CommonModule, ContentTemplateOutletDirective, PageSectionComponent],
+  imports: [CommonModule, ContentTemplateOutletDirective, PageSectionComponent, TableOfContentsLayoutModule],
   templateUrl: './content-page.component.html',
   styleUrl: './content-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
