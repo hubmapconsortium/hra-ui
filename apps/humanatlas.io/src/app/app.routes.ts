84be4532ff1c54cd9ecfb0487dfa7937d8f04dd7
edlu77 <eddd.lu@gmail.com>
2025-05-08 16:55:51 -0400

    Create overview data page

--- apps/humanatlas.io/src/app/app.routes.ts
+++ apps/humanatlas.io/src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { Route } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { landingPageResolver } from './resolvers/landing-page/landing-page.resolver';
 import { ContentPageComponent } from './pages/content-page/content-page.component';
-import { createYamlSpecResolver } from './resolvers/yaml-spec/yaml-spec.resolver';
 import { ContentPageDataSchema } from './pages/content-page/types/content-page.schema';
+import { createYamlSpecResolver } from './resolvers/yaml-spec/yaml-spec.resolver';
 
 /** Application routes */
 export const appRoutes: Route[] = [
@@ -21,6 +21,13 @@ export const appRoutes: Route[] = [
     component: ContentPageComponent,
     resolve: {
       data: createYamlSpecResolver('assets/content/3d-reference-library-page/data.yaml', ContentPageDataSchema),
+    },
+  },
+  {
+    path: 'overview-data',
+    component: ContentPageComponent,
+    resolve: {
+      data: createYamlSpecResolver('assets/content/data-overview-page/data.yaml', ContentPageDataSchema),
     },
   },
   {
