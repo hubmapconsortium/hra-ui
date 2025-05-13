b3f4494c0cf770f199559f9fdf569be2953a21be
Gauri Markandey <gauri2029@gmail.com>
2025-05-13 12:22:19 -0400

    Added count card component to landing page

--- apps/humanatlas.io/src/app/pages/landing-page/landing-page.component.ts
+++ apps/humanatlas.io/src/app/pages/landing-page/landing-page.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { HraCommonModule } from '@hra-ui/common';
 import { CarouselComponent } from '../../components-v2/carousel/carousel.component';
-import { CountInfoComponent } from '../../components-v2/count-info/count-info.component';
+import { CountCardComponent } from '@hra-ui/design-system/cards/count-card';
 import { SectionCardsComponent } from '../../components-v2/section-cards/section-cards.component';
 import { LandingPageData } from '../../resolvers/landing-page/landing-page.schema';
 
@@ -10,7 +10,7 @@ import { LandingPageData } from '../../resolvers/landing-page/landing-page.schem
  */
 @Component({
   selector: 'hra-landing-page',
-  imports: [HraCommonModule, CarouselComponent, CountInfoComponent, SectionCardsComponent],
+  imports: [HraCommonModule, CarouselComponent, CountCardComponent, SectionCardsComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- libs/design-system/cards/count-card/src/lib/count-card.component.ts
+++ libs/design-system/cards/count-card/src/lib/count-card.component.ts
@@ -1,15 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { CategoryLogoComponent, toCategoryLogoId } from '@hra-ui/design-system/brand/category-logo';
+import { ProductLogoComponent, toProductLogoId } from '@hra-ui/design-system/brand/product-logo';
+import { OrganLogoComponent, toOrganLogoId } from '@hra-ui/design-system/brand/organ-logo';
 
 /**
  * Component representing a count card.
  * Displays a count, a label, and a category icon.
  */
-
 @Component({
   selector: 'hra-count-card',
-  imports: [CommonModule, CategoryLogoComponent],
+  imports: [CommonModule, CategoryLogoComponent, ProductLogoComponent, OrganLogoComponent],
   templateUrl: './count-card.component.html',
   styleUrl: './count-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -24,6 +25,22 @@ export class CountCardComponent {
   /** Label text*/
   readonly label = input.required<string>();
 
+  /** Icon type */
+  readonly iconType = input<string>();
+
+  /** Icon */
+  readonly icon = input.required<string>();
+
   /** Category icon */
-  readonly categoryIcon = input.required({ transform: toCategoryLogoId });
+  protected readonly categoryIcon = computed(() =>
+    this.iconType() === 'category' ? toCategoryLogoId(this.icon()) : undefined,
+  );
+
+  /** Product icon */
+  protected readonly productIcon = computed(() =>
+    this.iconType() === 'product' ? toProductLogoId(this.icon()) : undefined,
+  );
+
+  /** Organ icon */
+  protected readonly organIcon = computed(() => (this.iconType() === 'organ' ? toOrganLogoId(this.icon()) : undefined));
 }
