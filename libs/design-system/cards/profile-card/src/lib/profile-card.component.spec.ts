60f55fd2c27bb864b4494eef218bd0a2c6c545a8
edlu77 <eddd.lu@gmail.com>
2025-05-15 15:30:42 -0400

    Fix failing test

--- libs/design-system/cards/profile-card/src/lib/profile-card.component.spec.ts
+++ libs/design-system/cards/profile-card/src/lib/profile-card.component.spec.ts
@@ -1,7 +1,10 @@
+import { provideMarkdown } from 'ngx-markdown';
 import { ProfileCardComponent } from './profile-card.component';
 import { render } from '@testing-library/angular';
 
 describe('ProfileCardComponent', () => {
+  const providers = [provideMarkdown()];
+
   it('should render', async () => {
     const promise = render(ProfileCardComponent, {
       inputs: {
@@ -10,6 +13,7 @@ describe('ProfileCardComponent', () => {
         name: 'Firstname Lastname',
         description: 'Occupation, Company',
       },
+      providers,
     });
 
     await expect(promise).resolves.toBeDefined();
