bf23d0354005f5a63631b71ddfed63d9a1fbe970
Saai Vignesh P <127758744+saaivignesh20@users.noreply.github.com>
2025-05-15 13:49:03 -0400

    test(lib:design-system): :white_check_mark: Fix tests after refactoring table component

--- libs/design-system/table/src/lib/table/table.component.spec.ts
+++ libs/design-system/table/src/lib/table/table.component.spec.ts
@@ -1,23 +1,48 @@
 import { render, screen } from '@testing-library/angular';
 import { TableComponent } from './table.component';
-import { TableRow } from '../types/page-table.schema';
+import { TableColumn, TableRow } from '../types/page-table.schema';
+import { provideHttpClient } from '@angular/common/http';
+import { provideMarkdown } from 'ngx-markdown';
 
 describe('Table  Component', () => {
-  const TABLE_DATA: TableRow[] = [
+  const TABLE_COLUMNS: TableColumn[] = [
     {
-      name: 'Test Name',
-      value: 100,
+      column: 'serial_no',
+      label: '#',
+      type: 'text',
+    },
+    {
+      column: 'name',
+      label: 'Name',
+      type: 'markdown',
+    },
+    {
+      column: 'age',
+      label: 'Age',
+      type: 'numeric',
+    },
+    {
+      column: 'download',
+      label: 'Download Data',
+      type: {
+        type: 'link',
+        urlColumn: 'dataUrl',
+      },
     },
   ];
+  const TABLE_ROWS: TableRow[] = [
+    { serial_no: 1, name: '**Peter Parker**', age: 30, download: 'https://example.com' },
+    { serial_no: 2, name: '**Mary Jane**', age: 28, download: 'https://example.com' },
+  ];
   it('It should render the table data', async () => {
-    const { detectChanges } = await render(TableComponent, {
+    await render(TableComponent, {
       inputs: {
-        rows: TABLE_DATA,
-        columns: ['name', 'value'],
+        rows: TABLE_ROWS,
+        columns: TABLE_COLUMNS,
       },
+      providers: [provideHttpClient(), provideMarkdown()],
     });
-    // detectChanges();
-    expect(screen.getByText('Test Name')).toBeInTheDocument();
-    expect(screen.getByText(100)).toBeInTheDocument();
+    expect(screen.getByText('Mary Jane')).toBeInTheDocument();
+    expect(screen.getByText(30)).toBeInTheDocument();
   });
 });
