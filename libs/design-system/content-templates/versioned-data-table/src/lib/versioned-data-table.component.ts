ce2ae603c8b37489027644d8d45a4543f13b562f
Raj Chavan <rajchavan1099@gmail.com>
2025-05-19 11:15:21 -0400

    refactor(lib:design-system): :sparkles: update version table inputs
    Instead of specifying columns every time for new release version in the yaml, this change now makes you to declare the columns once for all release versions if the columns are the same

--- libs/design-system/content-templates/versioned-data-table/src/lib/versioned-data-table.component.ts
+++ libs/design-system/content-templates/versioned-data-table/src/lib/versioned-data-table.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, computed, input, model } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
-import { TableComponent } from '@hra-ui/design-system/table';
+import { TableColumn, TableComponent, TableVariant } from '@hra-ui/design-system/table';
 import { VersionedData } from './types/versioned-data-table.schema';
 
 /**
@@ -27,6 +27,14 @@ export class VersionedDataTableComponent {
 
   /** The initial selection */
   readonly selection = model(0);
+
+  readonly columns = input<TableColumn[]>();
+
+  readonly style = input<TableVariant>('alternating');
+
+  readonly enableSort = input(false);
+
+  readonly verticalDividers = input(false);
 
   /** Item with the selected key */
   protected readonly item = computed(() => {
