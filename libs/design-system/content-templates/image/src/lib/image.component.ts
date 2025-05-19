5a80180c0bfe60e14247a016fd545f343b867deb
Daniel Bolin <axbolin@iu.edu>
2025-05-19 11:52:57 -0400

    refactor(lib:common): Tweak assetUrl pipe to handle absolute urls

--- libs/design-system/content-templates/image/src/lib/image.component.ts
+++ libs/design-system/content-templates/image/src/lib/image.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { AssetUrlPipe } from '@hra-ui/common';
 
 /**
  * Component for displaying an image in the content template
  */
 @Component({
   selector: 'hra-image',
-  imports: [CommonModule],
-  template: '<img [src]="src()" [alt]="alt()"/>',
+  imports: [CommonModule, AssetUrlPipe],
+  template: '<img [src]="src() | assetUrl" [alt]="alt()"/>',
   styleUrl: './image.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
