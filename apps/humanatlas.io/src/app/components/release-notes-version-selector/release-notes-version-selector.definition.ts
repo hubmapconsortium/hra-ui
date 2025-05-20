fd8beb01eda6e782ed46b40f6b962bad4f76bdfc
Saai Vignesh P <127758744+saaivignesh20@users.noreply.github.com>
2025-05-20 17:30:58 -0400

    test(app:humanatlas.io): :white_check_mark: fix compodoc tests

--- apps/humanatlas.io/src/app/components/release-notes-version-selector/release-notes-version-selector.definition.ts
+++ apps/humanatlas.io/src/app/components/release-notes-version-selector/release-notes-version-selector.definition.ts
@@ -2,6 +2,7 @@ import { ContentTemplateDef } from '@hra-ui/cdk/content-template';
 import { ReleaseNotesVersionSelectorComponent } from './release-notes-version-selector.component';
 import { ReleaseNotesVersionSelectorSchema } from './release-notes-version-selector.schema';
 
+/** Definition for Release Notes Version Selector Component */
 export const ReleaseNotesVersionSelectorDef: ContentTemplateDef<ReleaseNotesVersionSelectorComponent> = {
   component: ReleaseNotesVersionSelectorComponent,
   spec: ReleaseNotesVersionSelectorSchema,
