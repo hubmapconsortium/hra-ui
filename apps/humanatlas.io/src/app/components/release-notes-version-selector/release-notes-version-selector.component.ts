5e21461c0dbc645276de0414ce6ecef0a43731e4
Saai Vignesh P <127758744+saaivignesh20@users.noreply.github.com>
2025-05-20 15:03:04 -0400

    feat(app:humanatlas.io): :sparkles: release notes pages completed

--- apps/humanatlas.io/src/app/components/release-notes-version-selector/release-notes-version-selector.component.ts
+++ apps/humanatlas.io/src/app/components/release-notes-version-selector/release-notes-version-selector.component.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { injectParams } from 'ngxtension/inject-params';
 import { injectRouteData } from 'ngxtension/inject-route-data';
 import { ReleaseNotesVersions } from '../../schemas/release-notes-version/release-notes-version.schema';
+import { MatSelectModule } from '@angular/material/select';
+import { Router } from '@angular/router';
 
 /**
  * Dropdown menu for selecting release notes versions
  */
 @Component({
   selector: 'hra-release-notes-version-selector',
-  imports: [CommonModule],
+  imports: [CommonModule, MatSelectModule],
   templateUrl: './release-notes-version-selector.component.html',
   styleUrl: './release-notes-version-selector.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,4 +21,12 @@ export class ReleaseNotesVersionSelectorComponent {
   readonly version = injectParams((params) => (params['version'] as string | undefined) ?? '');
   /** List of release note versions */
   readonly versions = injectRouteData((data) => (data['versions'] as ReleaseNotesVersions | undefined)?.versions ?? []);
+
+  /** Router */
+  private readonly router = inject(Router);
+
+  /** Navigates to the selected release version */
+  protected navigateToVersion(version: string): void {
+    this.router.navigate(['release-notes', version]);
+  }
 }
