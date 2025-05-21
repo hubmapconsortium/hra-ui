da5af02b6941cf701e3bb23e49740bd7f7f0ccb7
Gauri Markandey <gauri2029@gmail.com>
2025-05-21 13:31:27 -0400

    Added OMAP content page

--- apps/humanatlas.io/src/app/app.routes.ts
+++ apps/humanatlas.io/src/app/app.routes.ts
@@ -141,6 +141,13 @@ export const appRoutes: Route[] = [
       data: createYamlSpecResolver('assets/content/vccf-page/data.yaml', ContentPageDataSchema),
     },
   },
+  {
+    path: 'omap',
+    component: ContentPageComponent,
+    resolve: {
+      data: createYamlSpecResolver('assets/content/omap-page/data.yaml', ContentPageDataSchema),
+    },
+  },
 
   // Error pages and redirects
   {
