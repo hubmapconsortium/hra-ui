b7ef289607d29f6b6cafaa3a3056b8a7431b3070
Raj Chavan <rajchavan1099@gmail.com>
2025-05-22 16:43:43 -0400

    docs(app:humanatlas.io): add comments

--- apps/humanatlas.io/src/app/controllers/versioned-table-param-sync/versioned-table-param-sync.service.ts
+++ apps/humanatlas.io/src/app/controllers/versioned-table-param-sync/versioned-table-param-sync.service.ts
@@ -3,14 +3,21 @@ import { ContentTemplateController, Controller } from '@hra-ui/cdk/content-templ
 import { VersionedDataTableComponent } from '@hra-ui/design-system/content-templates/versioned-data-table';
 import { linkedQueryParam } from 'ngxtension/linked-query-param';
 
+/**
+ * Injectable service that synchronizes the version of a versioned data table with a URL query parameter.
+ */
 @Injectable()
 export class VersionedTableParamSyncControllerService implements ContentTemplateController {
+  /** The controller ID */
   static readonly id = 'VersionedTableParamSync';
 
+  /** The version from URL params */
   private readonly version = linkedQueryParam('version');
 
+  /** The component reference for the versioned data table */
   private readonly componentRef = signal<ComponentRef<VersionedDataTableComponent> | undefined>(undefined);
 
+  /** constructor */
   constructor() {
     effect(() => {
       const version = this.version();
@@ -25,6 +32,7 @@ export class VersionedTableParamSyncControllerService implements ContentTemplate
     });
   }
 
+  /** attach function that sets the correct version in the URL*/
   attach(componentRef: ComponentRef<unknown>, options: Controller): void {
     const { instance } = componentRef;
     if (instance instanceof VersionedDataTableComponent) {
@@ -38,6 +46,7 @@ export class VersionedTableParamSyncControllerService implements ContentTemplate
     }
   }
 
+  /** detach function that is empty */
   detach(): void {
     // Intentionally empty
   }
