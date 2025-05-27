9ba43cad43ae19a4db364b944062e32f1bbbb9b3
Gauri Markandey <gauri2029@gmail.com>
2025-05-27 17:15:21 -0400

    added developer documentation for the introduction page.

--- apps/docs.humanatlas.io/src/app/app.routes.ts
+++ apps/docs.humanatlas.io/src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Route } from '@angular/router';
+import { ContentPageComponent, ContentPageDataSchema } from '@hra-ui/design-system/content-templates/content-page';
+import { createYamlSpecResolver } from '@hra-ui/design-system/content-templates/resolvers';
 import { NotFoundPageComponent } from '@hra-ui/design-system/error-pages/not-found-page';
 
 /** Application routes */
@@ -7,6 +9,13 @@ export const appRoutes: Route[] = [
   // Please try to keep sorted in alphabetical order
 
   // TODO: add content pages here!
+  {
+    path: 'introduction',
+    component: ContentPageComponent,
+    resolve: {
+      data: createYamlSpecResolver('assets/content/introduction-page/data.yaml', ContentPageDataSchema),
+    },
+  },
 
   // Error pages and redirects
   {
