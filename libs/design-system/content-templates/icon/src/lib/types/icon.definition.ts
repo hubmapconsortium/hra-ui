a2d8d2e3215b5667f48181032f3ead61fc8d782f
Raj Chavan <rajchavan1099@gmail.com>
2025-05-28 12:31:50 -0400

    docs(lib:design-system): add doc coverage

--- libs/design-system/content-templates/icon/src/lib/types/icon.definition.ts
+++ libs/design-system/content-templates/icon/src/lib/types/icon.definition.ts
@@ -2,6 +2,9 @@ import { MatIcon } from '@angular/material/icon';
 import { ContentTemplateDef } from '@hra-ui/cdk/content-template';
 import { IconSchema } from './icon.schema';
 
+/**
+ * Content template definition for MatIcon
+ */
 export const IconDef: ContentTemplateDef<MatIcon> = {
   component: MatIcon,
   spec: IconSchema,
--- libs/design-system/content-templates/icon/src/lib/types/icon.schema.ts
+++ libs/design-system/content-templates/icon/src/lib/types/icon.schema.ts
@@ -1,8 +1,17 @@
 import { ContentTemplateSchema } from '@hra-ui/cdk/content-template';
 import { z } from 'zod';
 
+/**
+ * Type for Icon
+ */
 export type Icon = z.infer<typeof IconSchema>;
 
+/**
+ * Schema for Icon
+ *
+ * This schema extends the ContentTemplateSchema and adds additional properties
+ * specific to the icon component.
+ */
 export const IconSchema = ContentTemplateSchema.extend({
   component: z.literal('Icon'),
   fontIcon: z.string().optional(),
