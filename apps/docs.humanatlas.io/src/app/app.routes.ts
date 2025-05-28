dd2f412d623b873207cce675385cad5720b53c43
Saai Vignesh P <127758744+saaivignesh20@users.noreply.github.com>
2025-05-28 17:55:01 -0400

    feat(app:docs.humanatlas.io): :sparkles: partially completed docs web component pages

--- apps/docs.humanatlas.io/src/app/app.routes.ts
+++ apps/docs.humanatlas.io/src/app/app.routes.ts
@@ -24,6 +24,14 @@ export const appRoutes: Route[] = [
     },
   },
 
+  {
+    path: 'web-components',
+    component: ContentPageComponent,
+    resolve: {
+      data: createYamlSpecResolver('assets/content/web-components-page/data.yaml', ContentPageDataSchema),
+    },
+  },
+
   // Error pages and redirects
   {
     path: '404',
