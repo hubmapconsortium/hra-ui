5694ca8b97e2445c1fcae28f38cbd5792b529df1
Gauri Markandey <gauri2029@gmail.com>
2025-05-29 16:22:37 -0400

    added initial placeholder markdown for design system dev doc

--- apps/docs.humanatlas.io/src/app/app.routes.ts
+++ apps/docs.humanatlas.io/src/app/app.routes.ts
@@ -9,6 +9,13 @@ export const appRoutes: Route[] = [
   // Please try to keep sorted in alphabetical order
 
   {
+    path: 'design-system',
+    component: ContentPageComponent,
+    resolve: {
+      data: createYamlSpecResolver('assets/content/design-system-page/data.yaml', ContentPageDataSchema),
+    },
+  },
+  {
     path: 'knowledge-graph',
     component: ContentPageComponent,
     resolve: {
