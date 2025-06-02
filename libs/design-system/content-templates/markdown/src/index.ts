c51eff6b61ae3b8a57b88c427cc6a8a99df2a9e8
Saai Vignesh P <127758744+saaivignesh20@users.noreply.github.com>
2025-06-02 11:36:57 -0400

    fix(lib:design-system): :bug: fix imports

--- libs/design-system/content-templates/markdown/src/index.ts
+++ libs/design-system/content-templates/markdown/src/index.ts
@@ -1,4 +1,4 @@
 export { MarkdownDef, HraMarkdownDef } from './lib/markdown.definition';
-export { Markdown, MarkdownSchema, HraMarkdown, HraMarkdownSchema } from './lib/markdown.schema';
+export { Markdown, MarkdownSchema } from './lib/markdown.schema';
 
 export * from './lib/markdown.component';
