e372076221a06bf8b8136d4a0c19ec4a9d6c0d1a
Raj Chavan <rajchavan1099@gmail.com>
2025-06-03 15:43:02 -0400

    update tests for icon component

--- libs/design-system/icons/src/lib/icon/icon.component.spec.ts
+++ libs/design-system/icons/src/lib/icon/icon.component.spec.ts
@@ -1,13 +1,40 @@
+import {} from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { Provider } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { FakeMatIconRegistry, IconType, MatIconHarness } from '@angular/material/icon/testing';
 import { render } from '@testing-library/angular';
 import { IconComponent } from './icon.component';
 
 describe('IconComponent', () => {
+  const providers: Provider[] = [
+    {
+      provide: MatIconRegistry,
+      useClass: FakeMatIconRegistry,
+    },
+  ];
+
   it('should create', async () => {
     const component = await render(IconComponent, {
+      providers,
       componentInputs: {
-        icon: { svgIcon: 'product:ftu' },
+        icon: { fontIcon: 'arrow_left_alt' },
       },
     });
+
     expect(component).toBeTruthy();
+  });
+
+  it('should render svg icon', async () => {
+    const { fixture } = await render(IconComponent, {
+      providers,
+      componentInputs: {
+        svgIcon: 'product:ftu',
+      },
+    });
+    const loader = TestbedHarnessEnvironment.loader(fixture);
+    const harness = await loader.getHarness(MatIconHarness);
+    const type = await harness.getType();
+    expect(type).toBe(IconType.SVG);
   });
 });
