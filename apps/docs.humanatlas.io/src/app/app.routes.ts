bc686e8ffb77693a022916c1e63fadd4ea36b821
Saai Vignesh Premanand <127758744+saaivignesh20@users.noreply.github.com>
2025-06-03 15:46:28 -0400

    Dev Docs - Web Components (#1228)
    * feat(app:docs.humanatlas.io): :sparkles: create content page for docs - kg page
    
    * feat(app:docs.humanatlas.io): :sparkles: write initial content for kg page
    
    * feat(app:docs.humanatlas.io): :sparkles: add md pre styles and added example for sparql kg
    
    * feat(lib:design-system): :sparkles: create custom component to load md from assets
    
    * test(lib:design-system): :white_check_mark: write tests for hra-markdown
    
    * feat(app:docs.humanatlas.io): :sparkles: partially completed docs web component pages
    
    * feat(app:docs.humanatlas.io): :sparkles: content writing and code examples done - illustrations update pending
    
    * merge develop into content-pages-docs-changelog
    
    * refactor(app:docs.humanatlas.io): :recycle: moved code to hra-code-block for syntax highlighting
    
    * style(lib:design-system): :bento: update ui-images assets for web components
    
    * refactor(lib:design-system): :recycle: PR review changes done
    
    * fix(lib:design-system): :bug: fix imports
    
    * review changes done
    
    * test(lib:design-system): :white_check_mark: fix tests
    
    ---------
    
    Co-authored-by: Daniel Bolin <axbolin@iu.edu>

--- apps/docs.humanatlas.io/src/app/app.routes.ts
+++ apps/docs.humanatlas.io/src/app/app.routes.ts
@@ -37,6 +37,13 @@ export const appRoutes: Route[] = [
       data: createYamlSpecResolver('assets/content/knowledge-graph-page/data.yaml', ContentPageDataSchema),
     },
   },
+  {
+    path: 'web-components',
+    component: ContentPageComponent,
+    resolve: {
+      data: createYamlSpecResolver('assets/content/web-components-page/data.yaml', ContentPageDataSchema),
+    },
+  },
 
   // Error pages and redirects
   {
