2b071c2491984fdc676650074c8f694a1eb5c499
edlu77 <eddd.lu@gmail.com>
2025-06-09 16:46:25 -0400

    Update carousel content and Explore section cards

--- apps/humanatlas.io/src/app/components/section-cards/section-cards.component.ts
+++ apps/humanatlas.io/src/app/components/section-cards/section-cards.component.ts
@@ -3,13 +3,15 @@ import { RouterModule } from '@angular/router';
 import { HraCommonModule } from '@hra-ui/common';
 
 import { SectionCardItem } from './section-cards.schema';
+import { ActionCardActionComponent, ActionCardComponent } from '@hra-ui/design-system/cards/action-card';
+import { TextHyperlinkDirective } from '@hra-ui/design-system/buttons/text-hyperlink';
 
 /**
  * Section card that links to another page
  */
 @Component({
   selector: 'hra-section-cards',
-  imports: [HraCommonModule, RouterModule],
+  imports: [HraCommonModule, RouterModule, ActionCardComponent, ActionCardActionComponent, TextHyperlinkDirective],
   templateUrl: './section-cards.component.html',
   styleUrl: './section-cards.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
