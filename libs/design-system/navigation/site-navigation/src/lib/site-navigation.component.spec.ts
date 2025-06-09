05877e83761a27744230ac4ec12403a0be0d11d9
Saai Vignesh P <127758744+saaivignesh20@users.noreply.github.com>
2025-06-09 11:36:53 -0400

    test(lib:design-system): :white_check_mark: fix tests for site-navigation component

--- libs/design-system/navigation/site-navigation/src/lib/site-navigation.component.spec.ts
+++ libs/design-system/navigation/site-navigation/src/lib/site-navigation.component.spec.ts
@@ -1,9 +1,14 @@
 import { render } from '@testing-library/angular';
 import { SiteNavigationComponent } from './site-navigation.component';
+import { DOCS_NAVIGATION_MENU } from './static-data/parsed';
 
 describe('SiteNavigationComponent', () => {
   it('should render', async () => {
-    const promise = render(SiteNavigationComponent);
+    const promise = render(SiteNavigationComponent, {
+      inputs: {
+        navigationMenu: DOCS_NAVIGATION_MENU,
+      },
+    });
     await expect(promise).resolves.toBeTruthy();
   });
 });
