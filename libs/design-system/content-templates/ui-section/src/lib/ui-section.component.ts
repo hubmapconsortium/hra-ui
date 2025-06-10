0b070ab4de67cdc29639593e1215a056eb562b74
Raj Chavan <rajchavan1099@gmail.com>
2025-06-10 14:06:01 -0400

    fix(app:apps.humanatlas.io): :bug: update logic for links
    If its a http (external) link it will open on a new tab but if its a relative link (internal) it will open on the same tab.

--- libs/design-system/content-templates/ui-section/src/lib/ui-section.component.ts
+++ libs/design-system/content-templates/ui-section/src/lib/ui-section.component.ts
@@ -3,12 +3,13 @@ import { HraCommonModule } from '@hra-ui/common';
 import { ButtonsModule } from '@hra-ui/design-system/buttons';
 import { AppLabelComponent } from '@hra-ui/design-system/content-templates/app-label';
 import { IconsModule } from '@hra-ui/design-system/icons';
+import { RouterModule } from '@angular/router';
 import { SoftwareStatus } from '@hra-ui/design-system/software-status-indicator';
 
 /** UI Section component for displaying app information and status */
 @Component({
   selector: 'hra-ui-section',
-  imports: [AppLabelComponent, ButtonsModule, IconsModule, HraCommonModule],
+  imports: [AppLabelComponent, ButtonsModule, IconsModule, HraCommonModule, RouterModule],
   templateUrl: './ui-section.component.html',
   styleUrl: './ui-section.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
