5a20ba3e180d194df87b8fde5afaeae8771a7e5f
Raj Chavan <rajchavan1099@gmail.com>
2025-06-25 14:55:19 -0400

    add documentation

--- apps/ftu-ui/src/app/pages/landing-page/landing.component.ts
+++ apps/ftu-ui/src/app/pages/landing-page/landing.component.ts
@@ -5,8 +5,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { IconsModule } from '@hra-ui/design-system/icons';
 import { RouterModule } from '@angular/router';
 
+/**
+ * Default publication link for the FTU landing page.
+ */
 const PUBLICATION_LINK = 'https://doi.org/10.1038/s41467-024-54591-6';
 
+/**
+ * Landing Component for the FTU application.
+ */
 @Component({
   selector: 'ftu-landing',
   imports: [HraCommonModule, ButtonsModule, MatIconModule, IconsModule, RouterModule],
@@ -15,7 +21,13 @@ const PUBLICATION_LINK = 'https://doi.org/10.1038/s41467-024-54591-6';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LandingComponent {
+  /**
+   * Publication link
+   */
   readonly publicationLink = input<string>();
 
+  /**
+   * Computed property that returns the publication link or a default value if not provided.
+   */
   protected readonly publicationLinkDefault = computed(() => this.publicationLink() ?? PUBLICATION_LINK);
 }
