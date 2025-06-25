a52247911bbde70185440e745fe71ac93646937b
Raj Chavan <rajchavan1099@gmail.com>
2025-06-25 11:42:55 -0400

    remove links

--- apps/ftu-ui/src/app/pages/landing-page/landing.component.ts
+++ apps/ftu-ui/src/app/pages/landing-page/landing.component.ts
@@ -5,9 +5,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { IconsModule } from '@hra-ui/design-system/icons';
 import { RouterModule } from '@angular/router';
 
-const EXPLORE_FTU_LINK =
-  'https://apps.humanatlas.io/ftu-explorer/#/ftu?id=https:%2F%2Fpurl.humanatlas.io%2F2d-ftu%2Fkidney-ascending-thin-loop-of-henle';
-
 const PUBLICATION_LINK = 'https://doi.org/10.1038/s41467-024-54591-6';
 
 @Component({
@@ -18,10 +15,6 @@ const PUBLICATION_LINK = 'https://doi.org/10.1038/s41467-024-54591-6';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LandingComponent {
-  readonly exampleFtuId = input.required<string>(); // Set in route data 'https://purl.humanatlas.io/2d-ftu/kidney-ascending-thin-loop-of-henle';
-
-  protected readonly exploreLink = EXPLORE_FTU_LINK;
-
   readonly publicationLink = input<string>();
 
   protected readonly publicationLinkDefault = computed(() => this.publicationLink() ?? PUBLICATION_LINK);
