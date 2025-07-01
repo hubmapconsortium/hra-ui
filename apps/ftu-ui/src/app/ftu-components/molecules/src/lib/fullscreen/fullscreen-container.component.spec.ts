b1868b3d62b22b4747213b78967b6a8ef2a61287
Raj Chavan <rajchavan1099@gmail.com>
2025-07-01 13:58:36 -0400

    update selector and imports for fullscreen container molecule

---
+++ apps/ftu-ui/src/app/ftu-components/molecules/src/lib/fullscreen/fullscreen-container.component.spec.ts
@@ -0,0 +1,43 @@
+import { Component, Input } from '@angular/core';
+import { Shallow } from 'shallow-render';
+import { FullscreenContainerComponent } from './fullscreen-container.component';
+import { FullscreenContentComponent } from './fullscreen-content.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TestBed } from '@angular/core/testing';
+
+@Component({
+  selector: 'ftu-test',
+  standalone: true,
+  imports: [FullscreenContainerComponent, FullscreenContentComponent],
+  template: `
+    <ftu-fullscreen-container [fullscreen]="fullscreen">
+      Some content
+      <ftu-fullscreen-content> Other content </ftu-fullscreen-content>
+    </ftu-fullscreen-container>
+  `,
+})
+class TestComponent {
+  @Input() fullscreen = false;
+}
+
+describe('FullscreenContainerComponent', () => {
+  let shallow: Shallow<TestComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [BrowserAnimationsModule],
+    });
+    shallow = new Shallow(TestComponent).dontMock(FullscreenContainerComponent, FullscreenContentComponent);
+  });
+
+  it('should create', async () => {
+    await expect(shallow.render()).resolves.toBeDefined();
+  });
+
+  it('should update fullscreen mode of child components', async () => {
+    const { findComponent } = await shallow.render({ bind: { fullscreen: true } });
+    const child = findComponent(FullscreenContentComponent);
+    expect(child).toHaveFoundOne();
+    expect(child.isFullScreen).toBeTruthy();
+  });
+});
