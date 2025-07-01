5980d1b9a46fada7879d3f72b95960f59fd4d59e
Raj Chavan <rajchavan1099@gmail.com>
2025-07-01 15:12:39 -0400

    update selector and imports for empty biomarker atom component

---
+++ apps/ftu-ui/src/app/ftu-components/atoms/src/lib/empty-biomarker/empty-biomarker.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MarkdownModule } from 'ngx-markdown';
+
+/**
+ *  Component for any empty biomaker cell
+ *  to inform about the empty data and has
+ *  button to navigate to HRA Team.
+ */
+@Component({
+  selector: 'ftu-empty-biomarker',
+  imports: [CommonModule, MatButtonModule, MarkdownModule],
+  templateUrl: './empty-biomarker.component.html',
+  styleUrls: ['./empty-biomarker.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EmptyBiomarkerComponent {
+  /**
+   * Input  buttonon text of empty biomarker component.
+   */
+  @Input() collaborateText = '';
+
+  /**
+   * Input  message markdown of empty biomarker component.
+   */
+  @Input() message = '';
+
+  /**
+   * An event emitter that emits the user button click event
+   */
+  @Output() readonly collaborateClick = new EventEmitter();
+}
