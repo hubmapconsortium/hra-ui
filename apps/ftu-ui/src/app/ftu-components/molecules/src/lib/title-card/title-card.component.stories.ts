6ac56da5ec5638c59f43dba24defc8cc7b695e9b
Raj Chavan <rajchavan1099@gmail.com>
2025-07-01 13:44:07 -0400

    update selector and imports for title card molecule

---
+++ apps/ftu-ui/src/app/ftu-components/molecules/src/lib/title-card/title-card.component.stories.ts
@@ -0,0 +1,30 @@
+import { moduleMetadata, StoryFn, Meta } from '@storybook/angular';
+import { TitleCardComponent } from './title-card.component';
+
+export default {
+  title: 'TitleCardComponent',
+  component: TitleCardComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [],
+    }),
+  ],
+} as Meta<TitleCardComponent>;
+
+const Template: StoryFn<TitleCardComponent> = (args) => ({
+  props: args,
+});
+
+export const Primary = {
+  render: Template,
+
+  args: {
+    title: 'Welcome to the Functional Tissue Unit Explorer',
+
+    description: `
+    Explore functional tissue units (FTUs) featuring experimental datasets and Human Reference Atlas (HRA) technologies.<br><br>
+    Read more about this effort at <a href='' target='_blank'>HRA Portal: FTU Explorer</a>.<br><br>
+    A special thanks to our partners: <a href='' target='_blank'>Kidney Precision Medicine Project</a> and <a href='' target='_blank'>European Bioinformatics Institute</a>.
+    `,
+  },
+};
