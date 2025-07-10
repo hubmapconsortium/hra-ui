f59b504cc93953a9f4528ecb683d9bf386ebade4
Raj Chavan <rajchavan1099@gmail.com>
2025-07-10 13:17:07 -0400

    add doc coverage

--- libs/design-system/bottom-sheet/src/bottom-sheet.service.ts
+++ libs/design-system/bottom-sheet/src/bottom-sheet.service.ts
@@ -2,10 +2,19 @@ import { inject, Injectable } from '@angular/core';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { BottomSheetComponent, BottomSheetData } from './lib/bottom-sheet.component';
 
+/**
+ * Service to handle bottom sheet operations.
+ * Provides a method to open a bottom sheet with specified data.
+ */
 @Injectable({ providedIn: 'root' })
 export class BottomSheetService {
+  /** Injects the MatBottomSheet service to open bottom sheets */
   private readonly _bottomSheet = inject(MatBottomSheet);
 
+  /**
+   * Opens a bottom sheet with the provided data.
+   * @param data - The data to be passed to the bottom sheet component.
+   */
   openBottomSheet(data: BottomSheetData) {
     return this._bottomSheet.open(BottomSheetComponent, { data });
   }
