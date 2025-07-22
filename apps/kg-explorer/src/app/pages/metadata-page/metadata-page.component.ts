708bbf6b132785db9cd5990fe0154bf4c211b511
edlu77 <eddd.lu@gmail.com>
2025-07-22 11:44:34 -0400

    Layout updates

--- apps/kg-explorer/src/app/pages/metadata-page/metadata-page.component.ts
+++ apps/kg-explorer/src/app/pages/metadata-page/metadata-page.component.ts
@@ -1,32 +1,50 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { watchBreakpoint } from '@hra-ui/cdk/breakpoints';
 import { HraCommonModule } from '@hra-ui/common';
 import { ButtonsModule } from '@hra-ui/design-system/buttons';
 import { PageSectionComponent } from '@hra-ui/design-system/content-templates/page-section';
-import { MetadataLayoutModule } from '@hra-ui/design-system/layouts/metadata';
-import { TableColumn, TableComponent, TableRow } from '@hra-ui/design-system/table';
+import { IconsModule } from '@hra-ui/design-system/icons';
+import { TableColumn, TableComponent } from '@hra-ui/design-system/table';
 import { MarkdownComponent } from 'ngx-markdown';
+
+import { MetadataLayoutModule } from '../../metadata-layout/metadata-layout.module';
 
 interface MetadataPageData {
   type: string;
   organ: string;
-  title: string;
-  description: string;
-  publisher: string;
-  funders: string[];
-  license: string;
+  title: string; //was_derived_from.title
+  description: string; //was_derived_from.description
+  publisher: {
+    name: string; //publisher
+    url: string;
+  };
+  funders: {
+    //was_derived_from.funders
+    funder: string;
+    awardNumber: string;
+  }[];
+  license: string; //license
   tags: string[];
 
-  creators: string[];
-  leads: string[];
-  reviewers: string[];
-  doi: string;
-  hubmapId: string;
-  dateCreated: string;
+  creators: {
+    name: string; //was_derived_from.creators.label
+    id: string; //was_derived_from.creators.id
+  }[];
+  leads: {
+    name: string; //was_derived_from.project_leads.label
+    id: string; //was_derived_from.project_leads.id
+  }[];
+  reviewers: {
+    name: string; //was_derived_from.reviewers.label
+    id: string; //was_derived_from.reviewers.id
+  }[];
+  doi: string; //was_derived_from.doi
+  hubmapId: string; //was_derived_from.hubmapId
+  dateCreated: string; //creation_date
   dateModified: string;
 
-  image?: string;
+  image?: string; //was_derived_from.distributions[?].downloadUrl
 }
 
 const testData: MetadataPageData = {
@@ -35,14 +53,30 @@ const testData: MetadataPageData = {
   title: '2D Outer Medullary Collecting Duct FTU for Kidney',
   description:
     'This functional tissue unit (FTU) illustration includes cell types related to the outer medullary collecting duct FTU in the ASCT+B Table [Kidney v1.2](https://doi.org/10.48539/HBM248.CBJV.556). This illustration was inspired by the [Kidney Tissue Atlas Explorer](https://atlas.kpmp.org/explorer) created by the Kidney Precision Medicine Project (KPMP). Multiple histology atlases, especially *Human Microscopic Anatomy* (Krstić 1991) and *Histology: A Text and Atlas* (Wojciech Pawlina and Michael H. Ross 2019) were referenced. Cell types and metrics were primarily defined by (Hu, McDonough, and Layton 2021; Layton and Layton 2019). Uberon describes this FTU as follows: "The outer medullary collecting duct is the portion of the collecting duct that lies in the renal outer medulla." \n\n**Bibliography**:\n\n* Hu, Rui, Alicia A. McDonough, and Anita T. Layton. 2021. “Sex Differences in Solute and Water Handling in the Human Kidney: Modeling and Functional Implications.” *iScience* 24 (6): 102667. https://doi.org/10.1016/j.isci.2021.102667.\n* Krstić, Radivoj V. 1991. Human Microscopic Anatomy. Berlin, Heidelberg: *Springer*. https://doi.org/10.1007/978-3-662-02676-2.\n* Layton, Anita T., and Harold E. Layton. 2019. “A Computational Model of Epithelial Solute and Water Transport along a Human Nephron.” Edited by Daniel A Beard. *PLOS Computational Biology* 15 (2): e1006108. https://doi.org/10.1371/journal.pcbi.1006108.\n* Wojciech Pawlina and Michael H. Ross. 2019. “Histology: A Text and Atlas: With Correlated Cell and Molecular Biology. Eighth Edition, 2018 Authors: Wojciech Pawlina; Michael H. Ross.” *Morphologia* 13 (4): 76–89. https://doi.org/10.26641/1997-9665.2019.4.76-89.',
-  publisher: 'HuBMAP',
-  funders: ['National Institutes of Health', 'National Science Foundation'],
+  publisher: {
+    name: 'HuBMAP',
+    url: 'https://hubmapconsortium.org/',
+  },
+  funders: [
+    {
+      funder: 'National Institutes of Health',
+      awardNumber: 'OT2OD033756',
+    },
+    {
+      funder: 'National Institutes of Health',
+      awardNumber: 'OT2OD026671',
+    },
+  ],
   license: 'Creative Commons Attribution 4.0 International ([CC BY 4.0](https://creativecommons.org/licenses/by/4.0/))',
   tags: ['Functional Tissue Unit', 'Kidney'],
 
-  creators: ['Rachel Bajema'],
-  leads: ['Katy Börner'],
-  reviewers: ['Sanjay Jain', 'Matthias Kretzler', 'M. Todd Valerius'],
+  creators: [{ name: 'Rachel Bajema', id: 'https://orcid.org/0000-0002-3775-8574' }],
+  leads: [{ name: 'Katy Börner', id: 'https://orcid.org/0000-0002-3321-6137' }],
+  reviewers: [
+    { name: 'Sanjay Jain', id: 'https://orcid.org/0000-0003-2804-127X' },
+    { name: 'Matthias Kretzler', id: 'https://orcid.org/0000-0003-4064-0582' },
+    { name: 'M. Todd Valerius', id: 'https://orcid.org/0000-0001-8143-9231' },
+  ],
   doi: 'https://doi.org/10.48539/HBM724.KFSK.483',
   hubmapId: 'HBM724.KFSK.483',
   dateCreated: '2025-06-12',
@@ -51,6 +85,8 @@ const testData: MetadataPageData = {
   image:
     'https://cdn.humanatlas.io/digital-objects/2d-ftu/kidney-outer-medullary-collecting-duct/v1.2/assets/2d-ftu-kidney-outer-medullary-collecting-duct.svg',
 };
+
+const testIcons = ['product:ftu', 'organ:kidneys'];
 
 const columns: TableColumn[] = [
   {
@@ -65,22 +101,6 @@ const columns: TableColumn[] = [
   },
 ];
 
-const symbolUrl = 'https://google.com';
-
-/** Example data */
-const rows: TableRow[] = [
-  { provenance: 'Creator(s)', metadata: `[Rachel Bajema](${symbolUrl})` },
-  { provenance: 'Project lead', metadata: `[Katy Börner](${symbolUrl})` },
-  {
-    provenance: 'Reviewer(s)',
-    metadata: `\n* [Sanjay Jain](${symbolUrl})\n* [Matthias Kretzler](${symbolUrl})\n* [M. Todd Valerius](${symbolUrl})`,
-  },
-  { provenance: 'DOI', metadata: `[https://doi.org/10.48539/HBM489.KLNJ.323](${symbolUrl})` },
-  { provenance: 'HuBMAP ID', metadata: 'HBM283.BMNR.744' },
-  { provenance: 'Date created', metadata: '2023-12-15' },
-  { provenance: 'Date last modified', metadata: '2023-12-15' },
-];
-
 @Component({
   selector: 'hra-metadata-page',
   imports: [
@@ -91,6 +111,7 @@ const rows: TableRow[] = [
     MetadataLayoutModule,
     MarkdownComponent,
     TableComponent,
+    IconsModule,
   ],
   templateUrl: './metadata-page.component.html',
   styleUrl: './metadata-page.component.scss',
@@ -98,10 +119,33 @@ const rows: TableRow[] = [
 export class MetadataPageComponent {
   readonly metadata = input<MetadataPageData>(testData);
 
-  rows = rows;
-  columns = columns;
+  readonly icons = input<string[]>(testIcons);
 
-  protected isWideScreen = watchBreakpoint('(min-width: 1100px)');
+  readonly columns = input<TableColumn[]>(columns);
 
-  protected isSmallScreen = watchBreakpoint('(max-width: 639.9999px)');
+  readonly rows = computed(() => [
+    { provenance: 'Creator(s)', metadata: this.createMarkdownList(this.metadata().creators) },
+    { provenance: 'Project lead', metadata: this.createMarkdownList(this.metadata().leads) },
+    {
+      provenance: 'Reviewer(s)',
+      metadata: this.createMarkdownList(this.metadata().reviewers),
+    },
+    { provenance: 'DOI', metadata: `[${this.metadata().doi}](${this.metadata().doi})` },
+    { provenance: 'HuBMAP ID', metadata: this.metadata().hubmapId },
+    { provenance: 'Date created', metadata: this.metadata().dateCreated },
+    { provenance: 'Date last modified', metadata: this.metadata().dateModified },
+  ]);
+
+  protected isWMediumScreen = watchBreakpoint('(min-width: 1100px), (max-width: 639.9999px)');
+
+  createMarkdownLink(text: string, url: string): string {
+    return `[${text}](${url})`;
+  }
+
+  createMarkdownList(items: { name: string; id: string }[]): string {
+    if (items.length === 1) {
+      return this.createMarkdownLink(items[0].name, items[0].id);
+    }
+    return items.map((item) => `\n* ${this.createMarkdownLink(item.name, item.id)}`).join();
+  }
 }
--- libs/design-system/layouts/metadata/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { MetadataLayoutComponent, MetadataLayoutHeaderComponent } from './lib/metadata-layout.component';
-export { MetadataLayoutModule } from './lib/metadata-layout.module';
