b96585450a18d06c3579a9d13e77eff2acba0b0f
Gauri Markandey <gauri2029@gmail.com>
2025-07-23 15:55:53 -0400

    added documentation to switch component

--- libs/design-system/switch-button/src/lib/switch.component.ts
+++ libs/design-system/switch-button/src/lib/switch.component.ts
@@ -2,6 +2,9 @@ import { CommonModule } from '@angular/common';
 import { Component, input, model } from '@angular/core';
 import { MatSlideToggleChange, MatSlideToggleModule } from '@angular/material/slide-toggle';
 
+/**
+ * Component representing a toggle button that allows users to switch between two states.
+ */
 @Component({
   selector: 'hra-switch-button',
   standalone: true,
@@ -10,10 +13,14 @@ import { MatSlideToggleChange, MatSlideToggleModule } from '@angular/material/sl
   styleUrl: './switch.component.scss',
 })
 export class SwitchComponent {
+  /** Determines whether toggle is checked or not */
   readonly isChecked = model.required<boolean>();
+  /** Determines if the toggle is disabled or not */
   readonly isDisabled = input<boolean>();
+  /** Determines if the icon should be hidden */
   readonly hideIcon = input<boolean>(false);
 
+  /** Event emitted when the toggle state changes */
   onSwitchToggle(event: MatSlideToggleChange) {
     this.isChecked.set(event.checked);
   }
