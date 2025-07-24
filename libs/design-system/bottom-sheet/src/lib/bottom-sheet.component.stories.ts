30ba124a0ca352278d492ea30699df9992b26c4e
Raj Chavan <rajchavan1099@gmail.com>
2025-07-24 16:38:51 -0400

    update stories

--- libs/design-system/bottom-sheet/src/lib/bottom-sheet.component.stories.ts
+++ libs/design-system/bottom-sheet/src/lib/bottom-sheet.component.stories.ts
@@ -1,18 +1,17 @@
 import { Component, inject, input } from '@angular/core';
-import { BottomSheetData, BottomSheetVariant } from './bottom-sheet.component';
 import { applicationConfig, type Meta, type StoryObj } from '@storybook/angular';
 import { TableColumn, TableRow } from '@hra-ui/design-system/table';
 import { provideDesignSystem } from '@hra-ui/design-system';
 import { BottomSheetService } from '../bottom-sheet.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  selector: 'hra-bottom-sheet-demo',
+  selector: 'hra-table-bottom-sheet-demo',
   standalone: true,
-  template: ` <button mat-flat-button (click)="openBottomSheet()">Open Bottom Sheet</button> `,
+  imports: [MatButtonModule],
+  template: ` <button mat-flat-button (click)="openTableBottomSheet()">Open Table Bottom Sheet</button> `,
 })
-class BottomSheetDemoComponent {
-  readonly variant = input<BottomSheetVariant>('table');
-  readonly tagline = input<string>('Demo tagline');
+class TableBottomSheetDemoComponent {
   readonly rows = input<TableRow[]>([
     { name: 'Alice', age: 30 },
     { name: 'Bob', age: 25 },
@@ -24,24 +23,31 @@ class BottomSheetDemoComponent {
 
   private readonly service = inject(BottomSheetService);
 
-  openBottomSheet() {
-    const data: BottomSheetData = {
-      variant: this.variant(),
-      tagline: this.tagline(),
-      rows: this.variant() === 'table' ? this.rows() : undefined,
-      columns: this.variant() === 'table' ? this.columns() : undefined,
-    };
-
-    this.service.openBottomSheet(data);
+  openTableBottomSheet() {
+    this.service.openTableBottomSheet(this.rows(), this.columns());
   }
 }
 
-const meta: Meta<BottomSheetDemoComponent> = {
-  component: BottomSheetDemoComponent,
-  title: 'BottomSheetDemoComponent',
+@Component({
+  selector: 'hra-page-section-bottom-sheet-demo',
+  standalone: true,
+  imports: [MatButtonModule],
+  template: ` <button mat-flat-button (click)="openPageSectionBottomSheet()">Open Page Section Bottom Sheet</button> `,
+})
+class PageSectionBottomSheetDemoComponent {
+  readonly tagline = input<string>('Demo tagline');
+
+  private readonly service = inject(BottomSheetService);
+
+  openPageSectionBottomSheet() {
+    this.service.openPageSectionBottomSheet(this.tagline());
+  }
+}
+
+const tableMeta: Meta<TableBottomSheetDemoComponent> = {
+  component: TableBottomSheetDemoComponent,
+  title: 'BottomSheet/Table',
   args: {
-    variant: 'table',
-    tagline: 'Demo Bottom Sheet',
     rows: [
       { name: 'Alice', age: 30 },
       { name: 'Bob', age: 25 },
@@ -52,13 +58,6 @@ const meta: Meta<BottomSheetDemoComponent> = {
     ],
   },
   argTypes: {
-    variant: {
-      control: 'radio',
-      options: ['table', 'page-section'],
-    },
-    tagline: {
-      control: 'text',
-    },
     rows: {
       control: 'object',
     },
@@ -78,20 +77,61 @@ const meta: Meta<BottomSheetDemoComponent> = {
     }),
   ],
 };
-export default meta;
-type Story = StoryObj<BottomSheetDemoComponent>;
 
-export const Default: Story = {
+const pageSectionMeta: Meta<PageSectionBottomSheetDemoComponent> = {
+  component: PageSectionBottomSheetDemoComponent,
+  title: 'BottomSheet/PageSection',
   args: {
-    variant: 'table',
-    tagline: 'Demo Bottom Sheet',
+    tagline: 'This is a page section demo',
+  },
+  argTypes: {
+    tagline: {
+      control: 'text',
+    },
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/BCEJn9KCIbBJ5MzqnojKQp/HRA-Components?node-id=4132-1621&p=f&t=j4a0ZyhA3Dkh4YFX-0',
+    },
+  },
+  decorators: [
+    applicationConfig({
+      providers: [provideDesignSystem()],
+    }),
+  ],
+};
+
+export default tableMeta;
+export { pageSectionMeta };
+
+type TableStory = StoryObj<TableBottomSheetDemoComponent>;
+type PageSectionStory = StoryObj<PageSectionBottomSheetDemoComponent>;
+
+export const TableBottomSheet: TableStory = {
+  args: {
     rows: [
       { name: 'Alice', age: 30 },
       { name: 'Bob', age: 25 },
+      { name: 'Charlie', age: 35 },
     ],
     columns: [
       { column: 'name', label: 'Name', type: 'text' },
       { column: 'age', label: 'Age', type: 'numeric' },
     ],
+  },
+};
+
+export const PageSectionBottomSheet: PageSectionStory = {
+  parameters: {
+    ...pageSectionMeta.parameters,
+  },
+  decorators: pageSectionMeta.decorators,
+  render: (args) => ({
+    component: PageSectionBottomSheetDemoComponent,
+    props: args,
+  }),
+  args: {
+    tagline: 'This is a page section bottom sheet demo',
   },
 };
