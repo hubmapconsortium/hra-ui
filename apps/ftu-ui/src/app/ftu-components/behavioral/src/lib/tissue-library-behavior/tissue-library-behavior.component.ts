99080ae7d9f2b1249b7a594c1f7b25a9142688bd
Saai Vignesh P <127758744+saaivignesh20@users.noreply.github.com>
2025-07-30 17:42:13 -0400

    feat(app:ftu-ui): :sparkles: implement more menu

--- apps/ftu-ui/src/app/ftu-components/behavioral/src/lib/tissue-library-behavior/tissue-library-behavior.component.ts
+++ apps/ftu-ui/src/app/ftu-components/behavioral/src/lib/tissue-library-behavior/tissue-library-behavior.component.ts
@@ -1,19 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
+import { MatDivider } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { LinkDirective } from '@hra-ui/cdk';
 import { dispatch, select$, selectSnapshot } from '@hra-ui/cdk/injectors';
 import { LinkRegistryActions } from '@hra-ui/cdk/state';
+import { ButtonsModule } from '@hra-ui/design-system/buttons';
+import { MenuDemoOption } from '@hra-ui/design-system/menu';
+import { PlainTooltipDirective } from '@hra-ui/design-system/tooltips/plain-tooltip';
+import { Tissue } from '@hra-ui/services';
+import { ActiveFtuSelectors, DownloadActions, DownloadSelectors, LinkIds, TissueLibrarySelectors } from '@hra-ui/state';
 import { LabelBoxComponent } from '../../../../atoms/src';
 import { TissueTreeListComponent } from '../../../../molecules/src';
-import { Tissue } from '@hra-ui/services';
-import { ActiveFtuSelectors, TissueLibrarySelectors } from '@hra-ui/state';
-import { MenuDemoComponent, MenuDemoOption } from '@hra-ui/design-system/menu';
-import { MatDivider } from '@angular/material/divider';
 /**
  * Component for Tissue Library Behavior
  */
 @Component({
   selector: 'ftu-tissue-library-behavior',
-  imports: [CommonModule, LabelBoxComponent, TissueTreeListComponent, MenuDemoComponent, MatDivider],
+  imports: [
+    CommonModule,
+    LabelBoxComponent,
+    TissueTreeListComponent,
+    MatDivider,
+    MatMenuModule,
+    MatIconModule,
+    ButtonsModule,
+    LinkDirective,
+    PlainTooltipDirective,
+  ],
   templateUrl: './tissue-library-behavior.component.html',
   styleUrls: ['./tissue-library-behavior.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -27,7 +42,7 @@ export class TissueLibraryBehaviorComponent {
    * Reference to the TissueTreeListComponent.
    */
   @ViewChild('list', { static: true })
-  readonly list?: TissueTreeListComponent<never, never>;
+  private readonly list?: TissueTreeListComponent<never, never>;
 
   /**
    * Input for tissues data
@@ -37,12 +52,22 @@ export class TissueLibraryBehaviorComponent {
   /**
    * Selected  of tissue library behavior component
    */
-  selected?: Tissue;
+  protected selected?: Tissue;
 
   /**
    * Navigates to a tissue page
    */
-  navigate = dispatch(LinkRegistryActions.Navigate);
+  protected navigate = dispatch(LinkRegistryActions.Navigate);
+
+  /** Data for Menus */
+  /** Illustration Metadata */
+  protected readonly illustrationMetadata = LinkIds.Illustration;
+
+  /** Available Download Formats */
+  protected readonly downloadFormats = selectSnapshot(DownloadSelectors.formats);
+
+  /** Download Action Dispatcher */
+  protected readonly download = dispatch(DownloadActions.Download);
 
   /**
    * Sets the TissueItem instance as undefined if the url is undefined
