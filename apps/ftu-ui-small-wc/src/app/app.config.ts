b6d548713ff4506f785953ee5c01d7407b593690
Raj Chavan <rajchavan1099@gmail.com>
2025-07-30 15:22:37 -0400

    application setup

---
+++ apps/ftu-ui-small-wc/src/app/app.config.ts
@@ -0,0 +1,53 @@
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideAppInitializer,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
+import { CdkStateModule } from '@hra-ui/cdk/state';
+import { FTU_DATA_IMPL_ENDPOINTS, HraServiceModule } from '@hra-ui/services';
+import { HraStateModule, MouseTrackerModule } from '@hra-ui/state';
+import { ReplaySubject } from 'rxjs';
+import { initFactory } from './app.init';
+import { NavigationLessRouter } from './routing/simple-router.service';
+import { provideDesignSystem } from '@hra-ui/design-system';
+import { provideStore } from '@ngxs/store';
+import { provideNothrowPlatformLocation } from '@hra-ui/cdk/platform-location';
+import { provideMarkdown } from 'ngx-markdown';
+import { provideTabs } from '@hra-ui/design-system/tabs';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideAppInitializer(() => {
+      const initializerFn = initFactory();
+      return initializerFn();
+    }),
+
+    provideAnimations(),
+    provideTabs(),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideNothrowPlatformLocation(),
+    provideDesignSystem(),
+    provideExperimentalZonelessChangeDetection(),
+    provideMarkdown({ loader: HttpClient }),
+    provideStore([]),
+    importProvidersFrom(CdkStateModule, HraServiceModule, HraStateModule, MouseTrackerModule),
+    {
+      // Replace full routing with a partial implementation
+      provide: Router,
+      useExisting: NavigationLessRouter,
+    },
+    {
+      provide: FTU_DATA_IMPL_ENDPOINTS,
+      useValue: new ReplaySubject(1),
+    },
+    {
+      provide: MatDialogRef,
+      useValue: {},
+    },
+  ],
+};
