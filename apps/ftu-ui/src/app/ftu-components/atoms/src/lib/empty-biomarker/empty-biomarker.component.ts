2bdb44c6851070e4fd63f14db18b3b6d32b66314
Saai Vignesh P <127758744+saaivignesh20@users.noreply.github.com>
2025-07-30 12:31:55 -0400

    feat(app:ftu-ui): :sparkles: add behavior to copy email

--- apps/ftu-ui/src/app/ftu-components/atoms/src/lib/empty-biomarker/empty-biomarker.component.ts
+++ apps/ftu-ui/src/app/ftu-components/atoms/src/lib/empty-biomarker/empty-biomarker.component.ts
@@ -1,7 +1,9 @@
+import { CdkCopyToClipboard } from '@angular/cdk/clipboard';
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { SnackbarService } from '@hra-ui/design-system/snackbar';
 import { MarkdownModule } from 'ngx-markdown';
 
 /**
@@ -11,31 +13,17 @@ import { MarkdownModule } from 'ngx-markdown';
  */
 @Component({
   selector: 'ftu-empty-biomarker',
-  imports: [CommonModule, MatButtonModule, MarkdownModule, MatIconModule],
+  imports: [CommonModule, MatButtonModule, MarkdownModule, MatIconModule, CdkCopyToClipboard],
   templateUrl: './empty-biomarker.component.html',
   styleUrls: ['./empty-biomarker.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmptyBiomarkerComponent {
-  /**
-   * Input button on text of empty biomarker component.
-   */
-  @Input() collaborateText = '';
+  /** Snackbar service */
+  private snackbar = inject(SnackbarService);
 
-  /**
-   * Input  message markdown of empty biomarker component.
-   */
-  @Input() message = `
-  <p>
-    We currently do not have cell type by biomarker data for the selected biomarker.
-  </p>
-  <p>
-  Please email the Human Reference Atlas team at infoccf@iu.edu about your dataset.
-  </p>
-`;
-
-  /**
-   * An event emitter that emits the user button click event
-   */
-  @Output() readonly collaborateClick = new EventEmitter();
+  /** Shows the copied snackbar message */
+  protected showCopiedMessage() {
+    this.snackbar.open('Email copied', '');
+  }
 }
