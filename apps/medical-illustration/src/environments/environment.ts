042af57f3029737d1706ae7ab64a603ffc3b70e2
Raj Chavan <rajchavan1099@gmail.com>
2025-08-05 10:17:02 -0400

    add and update enviroments for medical-illustration

---
+++ apps/medical-illustration/src/environments/environment.ts
@@ -0,0 +1,7 @@
+/**
+ * Environment configuration
+ */
+export const environment = {
+  googleAnalyticsToken: 'G-1WRJHN9FM6',
+  illustrationsUrl: 'https://apps.humanatlas.io/api--staging/v1/ftu-illustrations',
+};
