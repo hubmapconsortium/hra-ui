e51347d0c72547636fe9260b06c54279a5d7077a
Raj Chavan <rajchavan1099@gmail.com>
2025-08-07 15:48:11 -0400

    update error font to metropolis

--- libs/design-system/input/src/lib/input.component.stories.ts
+++ libs/design-system/input/src/lib/input.component.stories.ts
@@ -79,6 +79,11 @@ export const RequiredInputWithValidation: Story = {
       emailFormControl: new FormControl('', [Validators.email, Validators.required]),
     },
     template: `
+    <style>
+        mat-error {
+          font-family: 'Metropolis';
+        }
+      </style>
       <mat-form-field>
         <mat-label>Input</mat-label>
         <input type="email" matInput [formControl]="emailFormControl" placeholder="Enter email">
