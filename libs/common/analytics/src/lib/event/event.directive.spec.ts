5bfaa3ec0a6cb149869b2606d6c5cfc5e86c25af
Saai Vignesh P <saaivigneshpremanand@gmail.com>
2025-08-20 16:53:22 -0400

    test(lib:common): :white_check_mark: event.directive and asset-url spec files

--- libs/common/analytics/src/lib/event/event.directive.spec.ts
+++ libs/common/analytics/src/lib/event/event.directive.spec.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { createEvent, EventCategory } from '@hra-ui/common/analytics/events';
+import { By } from '@angular/platform-browser';
+import { createEvent, EventCategory } from '../../../events/src/lib/event';
 import { EventDirective } from './event.directive';
 
 // Mock event type for testing
@@ -8,7 +9,14 @@ const TestEvent = createEvent('test-event', EventCategory.Necessary);
 
 describe('EventDirective', () => {
   @Component({
-    template: `<button [hraEvent]="event" [hraEventProps]="eventProps">Test Button</button>`,
+    template: `<button
+      [hraEvent]="event"
+      [hraEventProps]="eventProps"
+      hraEventTriggerOn="click"
+      data-testid="test-button"
+    >
+      Test Button
+    </button>`,
     imports: [EventDirective],
   })
   class TestComponent {
@@ -19,5 +27,18 @@ describe('EventDirective', () => {
   it('should create', () => {
     const fixture = TestBed.createComponent(TestComponent);
     expect(() => fixture.detectChanges()).not.toThrow();
+  });
+
+  it('should call logEvent when triggered', () => {
+    const fixture = TestBed.createComponent(TestComponent);
+
+    const directiveDebugElement = fixture.debugElement.query(By.directive(EventDirective));
+    const directiveInstance = directiveDebugElement.injector.get(EventDirective);
+    jest.spyOn(directiveInstance, 'logEvent');
+    fixture.detectChanges();
+
+    directiveDebugElement.nativeElement.click();
+
+    expect(directiveInstance.logEvent).toHaveBeenCalledWith('click', expect.any(Event));
   });
 });
--- libs/common/src/lib/assets-href/asset-url.spec.ts
+++ libs/common/src/lib/assets-href/asset-url.spec.ts
@@ -1,10 +1,11 @@
+import { isSignal } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { assetsUrl, buildAssetUrl } from './asset-url';
 import { APP_ASSETS_HREF } from './tokens';
-import { isSignal } from '@angular/core';
 
 const base = 'foo';
 const path = 'bar';
+const pathGetter = () => path;
 const url = `${base}/${path}`;
 const cssUrl = `url("${url}")`;
 
@@ -26,6 +27,13 @@ describe('assetsUrl(path, type)', () => {
   it('returns a signal with the built url', () => {
     TestBed.inject(APP_ASSETS_HREF).set(base);
     const result = TestBed.runInInjectionContext(() => assetsUrl(path, 'css'));
+    expect(isSignal(result)).toBeTruthy();
+    expect(result()).toEqual(cssUrl);
+  });
+
+  it('returns a signal with the built url when using a path getter', () => {
+    TestBed.inject(APP_ASSETS_HREF).set(base);
+    const result = TestBed.runInInjectionContext(() => assetsUrl(pathGetter, 'css'));
     expect(isSignal(result)).toBeTruthy();
     expect(result()).toEqual(cssUrl);
   });
