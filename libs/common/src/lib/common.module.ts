acaf6acd31f3367212bd356ef59c2cfb96ec9ebf
Raj Chavan <rajchavan1099@gmail.com>
2025-08-25 16:10:48 -0400

    update common module

--- libs/common/src/lib/common.module.ts
+++ libs/common/src/lib/common.module.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { AssetUrlPipe } from './url/url-pipes';
 
 /**
  * Provides common directives and pipes.
  * Also reexports Angular's common module for ease of use.
  */
 @NgModule({
-  imports: [AssetUrlPipe],
-  exports: [CommonModule, AssetUrlPipe],
+  imports: [],
+  exports: [CommonModule],
 })
 export class HraCommonModule {}
