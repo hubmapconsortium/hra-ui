d4acb7e8cbb2abe23179a90e3a7e897baf2c16a3
Raj Chavan <rajchavan1099@gmail.com>
2025-08-25 15:35:32 -0400

    update index

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,7 +1,24 @@
 export { DOCUMENT } from '@angular/common';
-export { assetsUrl } from './lib/assets-href/asset-url';
-export { AssetUrlPipe } from './lib/assets-href/asset-url.pipe';
-export { APP_ASSETS_HREF, getDefaultAssetsHref } from './lib/assets-href/tokens';
+export {
+  assetUrl as assetsUrl,
+  appUrl,
+  pageUrl,
+  cssUrl,
+  resolveUrl,
+  resolveUrlSignal,
+  UrlResolverService,
+  provideUrlResolver,
+  provideAssetHref,
+  provideAppHref,
+  providePageHref,
+  UrlConfiguration,
+  UrlType,
+  injectUrlConfiguration,
+  provideUrlConfiguration,
+  getDefaultAssetsHref,
+} from './lib/url/url-configuration';
+export { AssetUrlPipe, ResolveUrlPipe, AppUrlPipe, PageUrlPipe, CssUrlPipe } from './lib/url/url-pipes';
+export { ASSET_HREF as APP_ASSETS_HREF, APP_HREF, BASE_HREF } from './lib/url/url-configuration';
 export { HraCommonModule } from './lib/common.module';
 export { INTERSECTION_OBSERVER, WINDOW } from './lib/injection-tokens';
 export { monitorHeight } from './lib/utils/monitor-height';
