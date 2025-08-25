8796ca6b978c3a2df2d8280dacf271d701313f51
Raj Chavan <rajchavan1099@gmail.com>
2025-08-25 16:12:24 -0400

    update exports for index

--- libs/common/url/src/index.ts
+++ libs/common/url/src/index.ts
@@ -1,3 +1,17 @@
-export * from '../../src/lib/url/url-configuration';
-export * from '../../src/lib/url/url-pipes';
-export * from '../../src/lib/url/utils';
+export type { UrlType, UrlConfiguration } from './lib/url-configuration';
+
+export { injectUrlConfiguration, provideUrlConfiguration, getDefaultAssetsHref } from './lib/url-configuration';
+
+export { resolveUrl, resolveUrlSignal, assetUrl, appUrl, pageUrl, cssUrl } from './lib/url-configuration';
+
+export {
+  UrlResolverService,
+  provideUrlResolver,
+  provideAssetHref,
+  provideAppHref,
+  providePageHref,
+} from './lib/url-configuration';
+
+export { ResolveUrlPipe, AssetUrlPipe, AppUrlPipe, PageUrlPipe, CssUrlPipe } from './lib/url-pipes';
+
+export { parseUrl } from './lib/utils';
