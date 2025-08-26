47c72522c69098c580ad1fdb0b0f276bfe275c4d
Raj Chavan <rajchavan1099@gmail.com>
2025-08-26 11:20:04 -0400

    update index

--- libs/common/url/src/index.ts
+++ libs/common/url/src/index.ts
@@ -1,17 +1,13 @@
-export type { UrlType, UrlConfiguration } from './lib/url-configuration';
-
-export { injectUrlConfiguration, provideUrlConfiguration, getDefaultAssetsHref } from './lib/url-configuration';
-
-export { resolveUrl, resolveUrlSignal, assetUrl, appUrl, pageUrl, cssUrl } from './lib/url-configuration';
-
 export {
-  UrlResolverService,
-  provideUrlResolver,
-  provideAssetHref,
+  injectAppHref,
+  injectAssetHref,
+  injectBaseHref,
   provideAppHref,
-  providePageHref,
-} from './lib/url-configuration';
-
-export { ResolveUrlPipe, AssetUrlPipe, AppUrlPipe, PageUrlPipe, CssUrlPipe } from './lib/url-pipes';
-
-export { parseUrl } from './lib/utils';
+  provideAssetHref,
+  provideBaseHref,
+} from './lib/href-tokens';
+export { appUrl, assetUrl, cssUrl, pageUrl, resolveUrl } from './lib/url-resolver.functions';
+export { AppUrlPipe, AssetUrlPipe, CssUrlPipe, PageUrlPipe, ResolveUrlPipe } from './lib/url-resolver.pipe';
+export { provideUrlResolver, UrlResolverService, UrlType } from './lib/url-resolver.service';
+export { UrlModule } from './lib/url.module';
+export { parseUrl } from './lib/util/parse-url';
