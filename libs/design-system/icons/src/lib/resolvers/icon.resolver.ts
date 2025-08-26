02f8aef123cdc4a10c08893a5b96956ebb171baa
Raj Chavan <rajchavan1099@gmail.com>
2025-08-26 12:03:50 -0400

    update icon resolver

--- libs/design-system/icons/src/lib/resolvers/icon.resolver.ts
+++ libs/design-system/icons/src/lib/resolvers/icon.resolver.ts
@@ -2,7 +2,7 @@ import { Location } from '@angular/common';
 import { assertInInjectionContext, inject } from '@angular/core';
 import { IconResolver } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
-import { APP_ASSETS_HREF } from '@hra-ui/common';
+import { injectAssetHref } from '@hra-ui/common/url';
 import { SVG_ICON_DIRECTORY } from '../utils/tokens';
 
 /**
@@ -16,11 +16,11 @@ export function createSvgIconResolver(): IconResolver {
   assertInInjectionContext(createSvgIconResolver);
 
   const { joinWithSlash } = Location;
-  const assetsHref = inject(APP_ASSETS_HREF);
+  const assetHref = injectAssetHref();
   const directory = inject(SVG_ICON_DIRECTORY);
   const sanitizer = inject(DomSanitizer);
   return (name, namespace) => {
-    let path = joinWithSlash(assetsHref(), directory);
+    let path = joinWithSlash(assetHref(), directory);
     path = joinWithSlash(path, namespace);
     path = joinWithSlash(path, `${name}.svg`);
     return sanitizer.bypassSecurityTrustResourceUrl(path);
