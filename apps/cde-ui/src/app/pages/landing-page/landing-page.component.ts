e674707c0cb8880387a1d69dd6c81d558aecaee9
Gauri Markandey <gauri2029@gmail.com>
2025-09-03 11:37:45 -0400

    refactor(app:cde-ui): :recycle: removed unused code from cde-ui landing page

--- apps/cde-ui/src/app/pages/landing-page/landing-page.component.ts
+++ apps/cde-ui/src/app/pages/landing-page/landing-page.component.ts
@@ -1,28 +1,17 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { YouTubePlayerModule } from '@angular/youtube-player';
-import { HraCommonModule, routeData } from '@hra-ui/common';
+import { HraCommonModule } from '@hra-ui/common';
 import { ButtonsModule } from '@hra-ui/design-system/buttons';
 import { NavigationModule } from '@hra-ui/design-system/navigation';
 import { VisualCard, VisualCardComponent } from '../../components/visual-card/visual-card.component';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatDividerModule } from '@angular/material/divider';
 
 /**
  * Landing Page Component
  */
 @Component({
   selector: 'cde-landing-page',
-  imports: [
-    HraCommonModule,
-    VisualCardComponent,
-    MatIconModule,
-    ButtonsModule,
-    YouTubePlayerModule,
-    NavigationModule,
-    MatMenuModule,
-    MatDividerModule,
-  ],
+  imports: [HraCommonModule, VisualCardComponent, MatIconModule, ButtonsModule, YouTubePlayerModule, NavigationModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -30,9 +19,4 @@ import { MatDividerModule } from '@angular/material/divider';
 export class LandingPageComponent {
   /** Visual cards to display on the landing page */
   readonly cards = input<VisualCard[]>([]);
-
-  /**
-   * Route data of app component
-   */
-  private readonly data = routeData();
 }
