1b98b03f1bdddb6b9eec601b7f04b22e1e1b5d15
Daniel Bolin <axbolin@iu.edu>
2025-09-05 19:45:47 -0400

    refactor(global): Remove unused dependencies

--- jest.preset.js
+++ jest.preset.js
@@ -3,15 +3,15 @@ const { createCjsPreset } = require('jest-preset-angular/presets');
 
 module.exports = {
   ...nxPreset,
-  ...createCjsPreset({
-    testEnvironment: 'jest-preset-angular/environments/jest-jsdom-env',
-  }),
+  ...createCjsPreset(),
 
+  testEnvironment: 'jest-preset-angular/environments/jest-jsdom-env',
   moduleNameMapper: {
     '.*import-meta': 'libs/common/import-meta/src/import-meta.mock.ts',
   },
 
   collectCoverage: true,
+  coverageReporters: ['html', 'lcov'],
   coveragePathIgnorePatterns: ['/node_modules/', 'index.ts', '.*-routing.module.ts'],
   coverageThreshold: {
     global: {
