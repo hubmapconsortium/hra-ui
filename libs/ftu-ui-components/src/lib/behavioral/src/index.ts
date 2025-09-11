f282308924bc2b84061992f74a55e6e88dba2dba
rchavan10 <96959435+rchavan10@users.noreply.github.com>
2025-09-11 12:56:24 -0400

    Post upgrade cleanup (#1674)
    * refactor(lib:state): :fire: Cleanup contact code as not used anymore
    
    * refactor(lib:services): :fire: Delete contact service
    
    * refactor(app:ftu-ui): :fire: remove title card
    
    * refactor(app:ftu-ui): :fire: delete old code - landing page in depth, landing page intro and metrics components
    
    * refactor(app:ftu-ui): :fire: remove old code - info modal
    
    * refactor(app:ftu-ui): :fire: remove old code - landing page intro wc component
    
    * refactor(app:ftu-ui): :fire: remove old code - footer and header
    
    * refactor(app:ftu-ui): :fire: remove old code - contact modal component
    
    * remove landing page intro and landing page content old code
    
    * remove header and footer behavior components
    
    * remove contact behavior component
    
    * refactor(app:ftu-ui): :fire: remove unused code - required input and required textbox
    
    * remove email input component
    
    * refactor(app:ftu-ui): :recycle: update index: ftu-ui molecules
    
    * refactor(app:ftu-ui): :recycle: update index: ftu-ui atoms
    
    * refactor(app:ftu-ui): :recycle: update index: ftu-ui behavioral
    
    * update inputs and components
    
    * update tests
    
    * update package
    
    * test(app:ftu-ui): :white_check_mark: Add tests for legends
    
    * update tests - remove old tests
    
    * test(lib:state): :white_check_mark: Improve test coverage - add tests for state analytics plugins

--- libs/ftu-ui-components/src/lib/behavioral/src/index.ts
+++ libs/ftu-ui-components/src/lib/behavioral/src/index.ts
@@ -1,14 +1,6 @@
 export * from './lib/biomarker-details/biomarker-details.component';
-export * from './lib/contact-behavior/contact-behavior.component';
-export * from './lib/header-behavior/header-behavior.component';
-export * from './lib/footer-behavior/footer-behavior.component';
 export * from './lib/medical-illustration-behavior/medical-illustration-behavior.component';
-export * from './lib/landing-page-content/landing-page-content.component';
 export * from './lib/tissue-library-behavior/tissue-library-behavior.component';
 export * from './lib/screen-notice-behavior/screen-notice-behavior.component';
-
 export * from './lib/biomarker-details-wc/biomarker-details-wc.component';
-
 export * from './lib/ftu-fullscreen-service/ftu-fullscreen.service';
-
-export * from './lib/ftu-landing-page-intro-wc-behaviour/ftu-landing-page-intro-wc-behaviour.component';
--- libs/services/src/index.ts
+++ libs/services/src/index.ts
@@ -1,13 +1,9 @@
 export * from './lib/ftu-data/ftu-data.model';
 export { Iri, Url } from './lib/shared/common.model';
-
-export * from './lib/contact/contact.mock';
-export * from './lib/contact/contact.service';
 export * from './lib/ftu-data/ftu-data.impl';
 export * from './lib/ftu-data/ftu-data.mock';
 export * from './lib/ftu-data/ftu-data.service';
 export * from './lib/service.module';
 export * from './lib/shared/common.model';
-
 export * as FtuDataSchemas from './lib/ftu-data/ftu-data.model';
 export * from './lib/ftu-data/ftu-data.transformers';
