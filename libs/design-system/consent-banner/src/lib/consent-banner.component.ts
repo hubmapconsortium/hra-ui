2d982247eecaac2035a73d15d2147f66fda22ddf
Raj Chavan <rajchavan1099@gmail.com>
2025-09-16 13:28:51 -0400

    add imports

--- libs/design-system/consent-banner/src/lib/consent-banner.component.ts
+++ libs/design-system/consent-banner/src/lib/consent-banner.component.ts
@@ -3,10 +3,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { BrandModule } from '@hra-ui/design-system/brand';
 import { ButtonsModule } from '@hra-ui/design-system/buttons';
+import { TextHyperlinkComponent } from '@hra-ui/design-system/buttons/text-hyperlink';
 
 @Component({
   selector: 'hra-consent-banner',
-  imports: [CommonModule, MatIconModule, BrandModule, ButtonsModule],
+  imports: [CommonModule, MatIconModule, BrandModule, ButtonsModule, TextHyperlinkComponent],
   templateUrl: './consent-banner.component.html',
   styleUrl: './consent-banner.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
