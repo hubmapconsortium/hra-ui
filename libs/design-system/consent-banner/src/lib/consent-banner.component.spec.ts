562038f0c99a72d35729175a53a715b0b09ae79e
Raj Chavan <rajchavan1099@gmail.com>
2025-09-16 14:20:50 -0400

    update providers

--- libs/design-system/consent-banner/src/lib/consent-banner.component.spec.ts
+++ libs/design-system/consent-banner/src/lib/consent-banner.component.spec.ts
@@ -1,3 +1,4 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ConsentBannerComponent } from './consent-banner.component';
 import { render } from '@testing-library/angular';
 
@@ -5,7 +6,7 @@ describe('ConsentBannerComponent', () => {
   it('should create the component', async () => {
     const component = await render(ConsentBannerComponent, {
       imports: [],
-      providers: [],
+      providers: [provideHttpClient()],
     });
     expect(component).toBeTruthy();
   });
