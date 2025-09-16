7a5420ecd3f2eaf712588cfeb388f738cdea6b9b
Raj Chavan <rajchavan1099@gmail.com>
2025-09-16 14:07:31 -0400

    add figma link to story and fullscreen layout

--- libs/design-system/consent-banner/src/lib/consent-banner.component.stories.ts
+++ libs/design-system/consent-banner/src/lib/consent-banner.component.stories.ts
@@ -5,6 +5,13 @@ import { applicationConfig, Meta, StoryObj } from '@storybook/angular';
 const meta: Meta<ConsentBannerComponent> = {
   component: ConsentBannerComponent,
   title: 'Design System/Consent Banner',
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/BCEJn9KCIbBJ5MzqnojKQp/HRA-Components?node-id=4497-1340&t=wmPJnsrT7daOVyJv-1',
+    },
+    layout: 'fullscreen',
+  },
   decorators: [
     applicationConfig({
       providers: [provideDesignSystem()],
