f4d3c37824157a410d3d22810ef3ae732b222cc6
Gauri Markandey <gauri2029@gmail.com>
2025-09-19 13:06:05 -0400

    updated storybook title and imports in modal

--- libs/design-system/privacy-preferences-modal/src/lib/privacy-preferences-modal.component.stories.ts
+++ libs/design-system/privacy-preferences-modal/src/lib/privacy-preferences-modal.component.stories.ts
@@ -1,22 +1,16 @@
-import { provideDesignSystem } from '@hra-ui/design-system';
-import { applicationConfig, type Meta, type StoryObj } from '@storybook/angular';
+import { type Meta, type StoryObj } from '@storybook/angular';
 
 import { PrivacyPreferencesModalComponent } from './privacy-preferences-modal.component';
 
 const meta: Meta = {
   component: PrivacyPreferencesModalComponent,
-  title: 'PrivacyPreferencesModalComponent',
+  title: 'Design System/PrivacyPreferencesModalComponent',
   parameters: {
     design: {
       type: 'figma',
       url: 'https://www.figma.com/design/BCEJn9KCIbBJ5MzqnojKQp/HRA-Components?node-id=4497-850&p=f',
     },
   },
-  decorators: [
-    applicationConfig({
-      providers: [provideDesignSystem()],
-    }),
-  ],
 };
 export default meta;
 type Story = StoryObj<PrivacyPreferencesModalComponent>;
