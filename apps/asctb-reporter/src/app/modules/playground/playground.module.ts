86c7400e2905add2271f85fb8cc922335758127c
Saai Vignesh P <saaivigneshpremanand@gmail.com>
2025-09-23 15:15:21 -0400

    feat(app:asctb-reporter): :sparkles: add click and model change analytics to some more components

--- apps/asctb-reporter/src/app/modules/playground/playground.module.ts
+++ apps/asctb-reporter/src/app/modules/playground/playground.module.ts
@@ -1,10 +1,10 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatTabsModule } from '@angular/material/tabs';
+import { HraCommonModule } from '@hra-ui/common';
 import { ErrorComponent } from '../../components/error/error.component';
 import { FileUploadComponent } from '../../components/file-upload/file-upload.component';
 import { UploadComponent } from '../../components/upload/upload.component';
@@ -14,7 +14,7 @@ import { PlaygroundComponent } from './playground.component';
 @NgModule({
   declarations: [PlaygroundComponent],
   imports: [
-    CommonModule,
+    HraCommonModule,
     MatTabsModule,
     TreeModule,
     MatFormFieldModule,
