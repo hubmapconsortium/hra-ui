3d3498d89c73185a2c90856ee1919e55ab9ed0e6
Daniel Bolin <axbolin@iu.edu>
2025-09-24 14:18:16 -0400

    fix(lib:common): Delay event directive cleanup

--- libs/common/src/lib/slugify/slugify.pipe.spec.ts
+++ libs/common/src/lib/slugify/slugify.pipe.spec.ts
@@ -4,6 +4,8 @@ describe('SlugifyPipe', () => {
   it('should slugify strings', () => {
     const pipe = new SlugifyPipe();
 
+    expect(pipe.transform('')).toEqual('');
+
     const value = pipe.transform('  Abc d!e@F   ghi ');
     expect(value).toEqual('abc-def-ghi');
   });
