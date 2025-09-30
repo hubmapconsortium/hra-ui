46f88308a4ee2ff367d837ff2f2c750b654e5b2e
Raj Chavan <rajchavan1099@gmail.com>
2025-09-30 11:43:19 -0400

    add privacy preference service to the humanatlas.io app

--- apps/humanatlas.io/src/app/app.component.ts
+++ apps/humanatlas.io/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { HraCommonModule, monitorHeight } from '@hra-ui/common';
 import { CustomScrollService } from '@hra-ui/common/custom-scroll';
 import { NavigationModule } from '@hra-ui/design-system/navigation';
 import { CtaConfig, HeaderComponent } from '@hra-ui/design-system/navigation/header';
+import { PrivacyPreferencesService } from '@hra-ui/design-system/privacy';
 import { routeData } from './utils/route-data';
 
 /** Padding when scrolling to an anchor in px */
@@ -49,9 +50,13 @@ export class AppComponent {
   constructor() {
     inject(CustomScrollService);
     const scroller = inject(ViewportScroller);
+    const privacyPreferences = inject(PrivacyPreferencesService);
+
     effect(() => {
       const yOffset = this.headerHeight() + ANCHOR_SCROLL_PADDING;
       scroller.setOffset([0, yOffset]);
     });
+
+    privacyPreferences.launch();
   }
 }
