8705f963166280d8cf7b5346e32aa6d96ef8350d
Daniel Bolin <axbolin@iu.edu>
2025-09-30 18:20:31 -0400

    refactor(lib:common): Rename and export ConsentCategories from common/analytics

--- libs/common/analytics/src/index.ts
+++ libs/common/analytics/src/index.ts
@@ -1,7 +1,7 @@
 export { AnalyticsErrorHandlerConfig } from './lib/analytics-features/error-handler';
 export { AnalyticsModule } from './lib/analytics.module';
 export { AnalyticsService, injectLogEvent } from './lib/analytics/analytics.service';
-export { ConsentService } from './lib/consent/consent.service';
+export { ConsentCategories, ConsentService } from './lib/consent/consent.service';
 export {
   ClickEventDirective,
   DoubleClickEventDirective,
