af5e1a9828efca119aac306985e6fc93d5b09d40
Raj Chavan <rajchavan1099@gmail.com>
2025-09-30 15:24:32 -0400

    feat(lib:design-system): add categories component
    this breaks out of the larger privacy preferences component

---
+++ libs/design-system/privacy/privacy-preferences/src/lib/categories/categories.component.ts
@@ -0,0 +1,27 @@
+import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { ChangeDetectionStrategy, Component, model } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { HraCommonModule } from '@hra-ui/common';
+import { Categories } from '@hra-ui/common/analytics';
+import { EventCategory } from '@hra-ui/common/analytics/events';
+import { ButtonsModule } from '@hra-ui/design-system/buttons';
+import { IconsModule } from '@hra-ui/design-system/icons';
+import { CATEGORY_DEFS } from './category-defs';
+
+@Component({
+  selector: 'hra-categories',
+  imports: [HraCommonModule, CdkAccordionModule, ButtonsModule, IconsModule, MatDividerModule, MatSlideToggleModule],
+  templateUrl: './categories.component.html',
+  styleUrl: './categories.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CategoriesComponent {
+  readonly categories = model.required<Categories>();
+
+  protected readonly categoryDefs = CATEGORY_DEFS;
+
+  toggleCategory(id: EventCategory, checked: boolean): void {
+    this.categories.update((current) => ({ ...current, [id]: checked }));
+  }
+}
