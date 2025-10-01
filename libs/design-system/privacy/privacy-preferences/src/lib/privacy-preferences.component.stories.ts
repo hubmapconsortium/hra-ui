6d1daf4a5aa52ae0156984f3826d968594e4988d
Raj Chavan <rajchavan1099@gmail.com>
2025-10-01 11:47:45 -0400

    update stories for the privacy preferences component

--- libs/design-system/privacy/privacy-preferences/src/lib/privacy-preferences.component.stories.ts
+++ libs/design-system/privacy/privacy-preferences/src/lib/privacy-preferences.component.stories.ts
@@ -1,6 +1,8 @@
-import { type Meta, type StoryObj } from '@storybook/angular';
+import { applicationConfig, type Meta, type StoryObj } from '@storybook/angular';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { EventCategory } from '@hra-ui/common/analytics/events';
 
-import { PrivacyPreferencesComponent } from './privacy-preferences.component';
+import { PrivacyPreferencesComponent, type PrivacyPreferencesData } from './privacy-preferences.component';
 
 const meta: Meta<PrivacyPreferencesComponent> = {
   component: PrivacyPreferencesComponent,
@@ -11,6 +13,24 @@ const meta: Meta<PrivacyPreferencesComponent> = {
       url: 'https://www.figma.com/design/BCEJn9KCIbBJ5MzqnojKQp/HRA-Components?node-id=4497-850&p=f',
     },
   },
+  decorators: [
+    applicationConfig({
+      providers: [
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {
+            categories: {
+              [EventCategory.Necessary]: true,
+              [EventCategory.Statistics]: false,
+              [EventCategory.Preferences]: false,
+              [EventCategory.Marketing]: false,
+            },
+            tab: 'consent',
+          } satisfies PrivacyPreferencesData,
+        },
+      ],
+    }),
+  ],
 };
 export default meta;
 type Story = StoryObj<PrivacyPreferencesComponent>;
