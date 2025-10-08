75c609a25533b86607c0290643519a6f6924a5c9
Raj Chavan <rajchavan1099@gmail.com>
2025-10-08 13:36:54 -0500

    update imports and add inputs

--- libs/design-system/buttons/info-button/src/lib/info-button.component.ts
+++ libs/design-system/buttons/info-button/src/lib/info-button.component.ts
@@ -1,10 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { PlainTooltipDirective } from '@hra-ui/design-system/tooltips/plain-tooltip';
+import { RichTooltipDirective } from '@hra-ui/design-system/tooltips/rich-tooltip';
 
 @Component({
   selector: 'hra-info-button',
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, PlainTooltipDirective, RichTooltipDirective],
   templateUrl: './info-button.component.html',
   styleUrl: './info-button.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class InfoButtonComponent {}
+export class InfoButtonComponent {
+  readonly richTooltipTagline = input.required<string>();
+
+  readonly richTooltipDescription = input.required<string>();
+}
