59a26281cc1aba69ff09098516dffd2c499579cb
Saai Vignesh P <saaivigneshpremanand@gmail.com>
2025-10-09 13:15:27 -0400

    refactor(lib:design-system): :recycle: remove redundant code

--- libs/design-system/indicators/progress-bar/src/lib/progress-bar.component.spec.ts
+++ libs/design-system/indicators/progress-bar/src/lib/progress-bar.component.spec.ts
@@ -1,24 +1,13 @@
 import { render } from '@testing-library/angular';
-import { ProgressSpinnerComponent } from './progress-spinner.component';
+import { ProgressBarComponent } from './progress-bar.component';
 
 describe('ProgressSpinnerComponent', () => {
   it('should render', async () => {
-    const promise = render(ProgressSpinnerComponent, {
+    const promise = render(ProgressBarComponent, {
       inputs: {
-        size: 'small',
         color: 'color',
       },
     });
     await expect(promise).resolves.toBeTruthy();
-  });
-
-  it('should check for the size large', async () => {
-    const { fixture } = await render(ProgressSpinnerComponent, {
-      inputs: {
-        size: 'large',
-        color: 'color',
-      },
-    });
-    await expect(fixture.componentInstance.size()).toBe('large');
   });
 });
--- libs/design-system/indicators/progress-bar/src/lib/progress-bar.component.ts
+++ libs/design-system/indicators/progress-bar/src/lib/progress-bar.component.ts
@@ -1,12 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 
-/** Alignment options */
-export type SpinnerSize = 'small' | 'large';
-
 /** Color options */
-export type SpinnerColor = 'dark' | 'light' | 'color';
+export type ProgressBarColor = 'dark' | 'light' | 'color';
 
 /**
  * HRA Progress Bar Component
@@ -23,19 +20,7 @@ export type SpinnerColor = 'dark' | 'light' | 'color';
 })
 export class ProgressBarComponent {
   /**
-   * Field for variant option
-   */
-  readonly size = input<SpinnerSize>('large');
-
-  /**
    * Field for color option
    */
-  readonly color = input.required<SpinnerColor>();
-
-  /**
-   * Computed field for the diameter of the spinner.
-   */
-  readonly diameter = computed(() => {
-    return this.size() === 'small' ? 24 : 48;
-  });
+  readonly color = input.required<ProgressBarColor>();
 }
