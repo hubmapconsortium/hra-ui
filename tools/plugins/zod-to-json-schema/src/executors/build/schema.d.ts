f48801ca80c1b9db249a6dc7ce7a3ea4233c1325
Raj Chavan <rajchavan1099@gmail.com>
2025-10-14 15:54:06 -0400

    update schema

--- tools/plugins/zod-to-json-schema/src/executors/build/schema.d.ts
+++ tools/plugins/zod-to-json-schema/src/executors/build/schema.d.ts
@@ -1,1 +1,8 @@
-export interface BuildExecutorSchema {} // eslint-disable-line
+export interface BuildExecutorSchema {
+  /** Glob pattern to match schema files */
+  pattern?: string;
+  /** Output file extension */
+  outputExtension?: string;
+  /** Path to JSON Schema specification */
+  schemaRefPath?: string;
+}
