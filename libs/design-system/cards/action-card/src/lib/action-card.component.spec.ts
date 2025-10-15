1418ea7e09de2a0f076b4e73f37e921615cb3c9f
edlu77 <eddd.lu@gmail.com>
2025-10-15 17:36:22 -0400

    Remove spacer

--- libs/design-system/cards/action-card/src/lib/action-card.component.spec.ts
+++ libs/design-system/cards/action-card/src/lib/action-card.component.spec.ts
@@ -1,6 +1,5 @@
-import { render, screen } from '@testing-library/angular';
-import { ActionCardComponent, ActionCardActionComponent } from './action-card.component';
-import { ButtonsModule } from '@hra-ui/design-system/buttons';
+import { render } from '@testing-library/angular';
+import { ActionCardComponent } from './action-card.component';
 
 describe('ActionCardComponent', () => {
   it('should create', async () => {
@@ -12,31 +11,5 @@ describe('ActionCardComponent', () => {
     });
 
     await expect(promise).resolves.toBeTruthy();
-  });
-
-  it('should set the correct alignment for the action', async () => {
-    await render(
-      `<hra-action-card variant="elevated" tagline="Title">
-        <hra-action-card-action>
-          <button mat-button>
-            Action 1
-          </button>
-        </hra-action-card-action>
-        <hra-action-card-action alignment="right">
-          <button mat-flat-button>
-            Action 2
-          </button>
-        </hra-action-card-action>
-      </hra-action-card>`,
-      {
-        imports: [ActionCardComponent, ActionCardActionComponent, ButtonsModule],
-      },
-    );
-
-    const button1 = screen.getByRole('button', { name: 'Action 1' });
-    expect(button1.parentElement?.nextElementSibling).toHaveClass('spacer');
-
-    const button2 = screen.getByRole('button', { name: 'Action 2' });
-    expect(button2.parentElement?.previousElementSibling).toHaveClass('spacer');
   });
 });
