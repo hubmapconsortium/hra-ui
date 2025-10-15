c37be224181481e7c5a51ab8b8327d0f24c01dca
Raj Chavan <rajchavan1099@gmail.com>
2025-10-15 16:56:02 -0400

    make changes to info button

--- libs/design-system/buttons/info-button/src/lib/info-button.component.ts
+++ libs/design-system/buttons/info-button/src/lib/info-button.component.ts
@@ -1,24 +1,29 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { PlainTooltipDirective } from '@hra-ui/design-system/tooltips/plain-tooltip';
-import { RichTooltipDirective, RichTooltipContainerComponent } from '@hra-ui/design-system/tooltips/rich-tooltip';
+import { RichTooltipModule } from '@hra-ui/design-system/tooltips/rich-tooltip';
+
+@Component({
+  selector: 'hra-info-button-tooltip-tagline',
+  template: '<ng-content />',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InfoButtonTooltipTaglineComponent {}
+
+@Component({
+  selector: 'hra-info-button-tooltip-content',
+  template: '<ng-content />',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InfoButtonTooltipContentComponent {}
 
 /** Info Button Component */
 @Component({
   selector: 'hra-info-button',
-  imports: [MatIconModule, MatButtonModule, PlainTooltipDirective, RichTooltipDirective],
+  imports: [MatIconModule, MatButtonModule, PlainTooltipDirective, RichTooltipModule],
   templateUrl: './info-button.component.html',
   styleUrl: './info-button.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class InfoButtonComponent {
-  /** Custom rich tooltip container for advanced usage with action buttons */
-  readonly richTooltipContent = input<RichTooltipContainerComponent>();
-
-  /** Tagline for simple rich tooltip variant */
-  readonly richTooltipTagline = input<string>('');
-
-  /** Description for simple rich tooltip variant */
-  readonly richTooltipDescription = input<string>('');
-}
+export class InfoButtonComponent {}
