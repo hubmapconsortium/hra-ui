6ada157ef5cfcaf124bf6e63b8c2d03d4c3d41d9
Saai Vignesh P <saaivigneshpremanand@gmail.com>
2025-10-16 15:41:15 -0400

    refactor(lib:design-system): :recycle: minor tweaks done

--- libs/design-system/indicators/no-results-indicator/src/lib/no-results-indicator.component.stories.ts
+++ libs/design-system/indicators/no-results-indicator/src/lib/no-results-indicator.component.stories.ts
@@ -1,5 +1,4 @@
-import { applicationConfig, Meta, StoryObj } from '@storybook/angular';
-import { provideDesignSystem } from '../../../../src';
+import { Meta, StoryObj } from '@storybook/angular';
 import { NoResultsIndicatorComponent } from './no-results-indicator.component';
 
 const meta: Meta = {
@@ -11,11 +10,6 @@ const meta: Meta = {
       url: 'https://www.figma.com/design/BCEJn9KCIbBJ5MzqnojKQp/HRA-Components?node-id=4958-37',
     },
   },
-  decorators: [
-    applicationConfig({
-      providers: [provideDesignSystem()],
-    }),
-  ],
 };
 export default meta;
 type Story = StoryObj;
