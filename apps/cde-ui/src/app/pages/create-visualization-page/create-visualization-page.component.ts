313e757d00c4903f443e98bfae188c885159a66a
Raj Chavan <rajchavan1099@gmail.com>
2025-10-16 11:55:44 -0400

    update info button on create visualization page

--- apps/cde-ui/src/app/pages/create-visualization-page/create-visualization-page.component.ts
+++ apps/cde-ui/src/app/pages/create-visualization-page/create-visualization-page.component.ts
@@ -13,7 +13,7 @@ import {
   DEFAULT_COLOR_MAP_VALUE_KEY,
 } from '@hra-ui/cde-visualization';
 import { HraCommonModule } from '@hra-ui/common';
-import { InfoButtonComponent } from '@hra-ui/design-system/buttons/info-button';
+import { InfoButtonComponent, InfoButtonTooltipContentComponent } from '@hra-ui/design-system/buttons/info-button';
 import { CsvFileLoaderOptions, CsvFileLoaderService } from '@hra-ui/common/fs';
 import { ButtonsModule } from '@hra-ui/design-system/buttons';
 import { ErrorIndicatorComponent } from '@hra-ui/design-system/indicators/error-indicator';
@@ -62,6 +62,7 @@ function optionalValue<T>(): T | null {
     HraCommonModule,
     ButtonsModule,
     InfoButtonComponent,
+    InfoButtonTooltipContentComponent,
     DeprecatedWorkflowCardComponent,
     FileUploadComponent,
     MarkEmptyFormControlDirective,
