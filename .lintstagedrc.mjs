9808acc8abd016d0e485cbaaa5a774b57f91c0b6
Daniel Bolin <axbolin@iu.edu>
2025-10-17 11:42:53 -0400

    perf(global:config): Update lint staged commands

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,5 @@
-const lint = 'npx nx affected:lint --fix --files';
-const format = 'npx nx format:write --files';
+const lint = 'eslint --fix';
+const format = 'prettier --write';
 
 export default {
   '*.{js,mjs,cjs,ts,mts,cts,html}': [lint, format],
