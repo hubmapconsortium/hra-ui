703833869cead4f940c752283d06d8f2a72744f1
Prashant Channe <prashantchanne121@gmail.com>
2025-10-20 14:42:00 -0400

    updated tag story

--- libs/design-system/buttons/src/lib/stories/chips.stories.ts
+++ libs/design-system/buttons/src/lib/stories/chips.stories.ts
@@ -73,21 +73,10 @@ export const Group: Story = {
 export const WithoutCloseButton: Story = {
   render: () => ({
     template: `
-      <mat-chip-set aria-label="Chip selection">
-        <mat-chip>Option 1</mat-chip>
-        <mat-chip>Option 2</mat-chip>
-        <mat-chip>Option 3</mat-chip>
-      </mat-chip-set>
-    `,
-  }),
-};
-
-export const WithoutCloseButtonDisabled: Story = {
-  render: () => ({
-    template: `
-      <mat-chip-set aria-label="Disabled chips">
-        <mat-chip disabled>Disabled Chip</mat-chip>
-        <mat-chip>Enabled Chip</mat-chip>
+      <mat-chip-set aria-label="Category tags">
+        <mat-chip class="stateless-chip stateless-chip-small">Small</mat-chip>
+        <mat-chip class="stateless-chip stateless-chip-medium">Medium</mat-chip>
+        <mat-chip class="stateless-chip stateless-chip-large">Large</mat-chip>
       </mat-chip-set>
     `,
   }),
