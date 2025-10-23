f95c5e19fccfde752839268d4dffb2ac305080d1
edlu77 <eddd.lu@gmail.com>
2025-10-23 17:06:03 -0400

    Minor tweaks

--- libs/design-system/cards/content-button/src/lib/content-button.schema.ts
+++ libs/design-system/cards/content-button/src/lib/content-button.schema.ts
@@ -1,5 +1,5 @@
 import { ContentTemplateSchema } from '@hra-ui/cdk/content-template';
-import { z } from 'zod';
+import * as z from 'zod';
 
 /** Content button action card type */
 export type ContentButton = z.infer<typeof ContentButtonSchema>;
@@ -7,7 +7,7 @@ export type ContentButton = z.infer<typeof ContentButtonSchema>;
 /** Schema for content button action card data */
 export const ContentButtonSchema = ContentTemplateSchema.extend({
   component: z.literal('ContentButton'),
-  image: z.string().optional(),
+  image: z.string(),
   date: z.string(),
   tagline: z.string(),
   tags: z.array(z.string()).optional(),
