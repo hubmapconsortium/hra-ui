8c1823558a77a75bd6dbef1e2f8218a2e34b7a65
Saai Vignesh P <saaivigneshpremanand@gmail.com>
2025-10-23 14:31:57 -0400

    build(lib:design-system): :green_heart: commit package.json and corrected comments

--- libs/design-system/density/src/lib/density.directive.ts
+++ libs/design-system/density/src/lib/density.directive.ts
@@ -1,15 +1,17 @@
 import { Directive, input } from '@angular/core';
 import { ValueOf } from 'type-fest';
 
+/** Density levels */
 export type Density = ValueOf<typeof Densities>;
 
+/** Density level enum */
 export const Densities = {
   Compact0: 'compact-0',
   Compact2: 'compact-2',
   Compact4: 'compact-4',
 } as const;
 
-/** Style a list to a specific density level */
+/** Style a component to a specific density level */
 @Directive({
   selector: '[hraDensity]',
   standalone: true,
@@ -18,6 +20,6 @@ export const Densities = {
   },
 })
 export class DensityDirective {
-  /** Density level of the list */
+  /** Density level of the component */
   readonly density = input.required<Density>({ alias: 'hraDensity' });
 }
