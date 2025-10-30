b70f17a624520d49f0f7c93238859f6a2fa128cc
Saai Vignesh P <saaivigneshpremanand@gmail.com>
2025-10-30 18:27:52 -0400

    feat: :sparkles: 1714 - replace local instance of info button with DS equivalent, refactor info button components to directives and export accordingly

--- libs/design-system/buttons/info-button/src/lib/info-button.component.ts
+++ libs/design-system/buttons/info-button/src/lib/info-button.component.ts
@@ -1,31 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Directive } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { PlainTooltipDirective } from '@hra-ui/design-system/tooltips/plain-tooltip';
 import { RichTooltipModule } from '@hra-ui/design-system/tooltips/rich-tooltip';
 
-/** Tagline component for info button rich tooltip */
-@Component({
-  selector: 'hra-info-button-tooltip-tagline',
-  standalone: true,
-  template: '<ng-content />',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class InfoButtonTooltipTaglineComponent {}
+@Directive({ selector: '[hraInfoButtonTagline]' })
+export class InfoButtonTaglineDirective {}
 
-/** Content component for info button rich tooltip */
-@Component({
-  selector: 'hra-info-button-tooltip-content',
-  standalone: true,
-  template: '<ng-content />',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class InfoButtonTooltipContentComponent {}
+@Directive({ selector: '[hraInfoButtonActions]' })
+export class InfoButtonActionsDirective {}
 
 /** Info Button Component */
 @Component({
   selector: 'hra-info-button',
-  standalone: true,
   imports: [MatIconModule, MatButtonModule, PlainTooltipDirective, RichTooltipModule],
   templateUrl: './info-button.component.html',
   styleUrl: './info-button.component.scss',
