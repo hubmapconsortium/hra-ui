de9aa6e10d08f2b2b5cc6dcb900f57df44a5ac9e
rchavan10 <96959435+rchavan10@users.noreply.github.com>
2025-11-03 13:04:10 -0500

    Update page label - add optional features (#1841)
    * add inputs for breadcrumbs, date and tags
    
    * add inputs
    
    * add optional features
    
    * update scss
    
    * update tests
    
    * update stories
    
    * update paths
    
    * update html
    
    * update inputs on components

--- libs/design-system/content-templates/page-label/src/lib/page-label.component.spec.ts
+++ libs/design-system/content-templates/page-label/src/lib/page-label.component.spec.ts
@@ -1,9 +1,52 @@
-import { render } from '@testing-library/angular';
+import { render, screen, RenderComponentOptions } from '@testing-library/angular';
 import { PageLabelComponent } from './page-label.component';
 
 describe('PageLabelComponent', () => {
+  async function setup(inputs: RenderComponentOptions<PageLabelComponent>['inputs']) {
+    return await render(PageLabelComponent, { inputs });
+  }
+
   it('should render', async () => {
-    const promise = render(PageLabelComponent, { inputs: { tagline: 'Hello' } });
+    const promise = setup({ tagline: 'Hello' });
     await expect(promise).resolves.toBeTruthy();
+  });
+
+  it('should display breadcrumbs when provided', async () => {
+    await setup({
+      tagline: 'Page Label',
+      breadcrumbs: [{ name: 'Home', route: '/' }, { name: 'Current Page' }],
+    });
+
+    expect(screen.getByText('Home')).toBeTruthy();
+    expect(screen.getByText('Current Page')).toBeTruthy();
+  });
+
+  it('should display date when provided', async () => {
+    await setup({
+      tagline: 'Page Label',
+      date: 'October 2, 2025',
+    });
+
+    expect(screen.getByText('October 2, 2025')).toBeTruthy();
+  });
+
+  it('should display tags when provided', async () => {
+    await setup({
+      tagline: 'Page Label',
+      tags: ['Tag1', 'Tag2', 'Tag3'],
+    });
+
+    expect(screen.getByText('Tag1')).toBeTruthy();
+    expect(screen.getByText('Tag2')).toBeTruthy();
+    expect(screen.getByText('Tag3')).toBeTruthy();
+  });
+
+  it('should not display optional elements when not provided', async () => {
+    await setup({
+      tagline: 'Page Label',
+    });
+
+    expect(screen.queryByText('Home')).toBeFalsy();
+    expect(screen.queryByText('October 2, 2025')).toBeFalsy();
   });
 });
--- libs/design-system/content-templates/page-label/src/lib/page-label.component.ts
+++ libs/design-system/content-templates/page-label/src/lib/page-label.component.ts
@@ -1,12 +1,14 @@
-import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input, numberAttribute } from '@angular/core';
+import { MatChipsModule } from '@angular/material/chips';
+import { HraCommonModule } from '@hra-ui/common';
+import { BreadcrumbItem, BreadcrumbsComponent } from '@hra-ui/design-system/buttons/breadcrumbs';
 import { SectionLinkComponent } from '@hra-ui/design-system/content-templates/section-link';
 import { coerceIconList, IconsModule } from '@hra-ui/design-system/icons';
 
 /** Label for a page section. Can also be used standalone */
 @Component({
   selector: 'hra-page-label',
-  imports: [CommonModule, IconsModule, SectionLinkComponent],
+  imports: [HraCommonModule, IconsModule, SectionLinkComponent, BreadcrumbsComponent, MatChipsModule],
   templateUrl: './page-label.component.html',
   styleUrl: './page-label.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -23,4 +25,13 @@ export class PageLabelComponent {
 
   /** Anchor id of this label */
   readonly anchor = input<string>();
+
+  /** Breadcrumbs to display above the label */
+  readonly breadcrumbs = input<BreadcrumbItem[]>();
+
+  /** Date to display below the label */
+  readonly date = input<string>();
+
+  /** Tags/labels to display below the date */
+  readonly tags = input<string[]>([]);
 }
