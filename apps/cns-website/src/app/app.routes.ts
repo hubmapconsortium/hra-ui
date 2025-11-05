6bdd5311b2ca120a1e3eec392fcd205cbe87403e
Gauri Markandey <gauri2029@gmail.com>
2025-11-05 13:37:33 -0500

    feat: :sparkles: added jobs page to new cns website

--- apps/cns-website/src/app/app.routes.ts
+++ apps/cns-website/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Route } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page.component';
+import { createYamlSpecResolver } from '@hra-ui/design-system/content-templates/resolvers';
+import { ContentPageComponent, ContentPageDataSchema } from '@hra-ui/design-system/content-templates/content-page';
 
 /** Application routes */
 export const appRoutes: Route[] = [
@@ -7,5 +9,15 @@ export const appRoutes: Route[] = [
     path: '',
     pathMatch: 'full',
     component: LandingPageComponent,
+  },
+
+  // Content pages
+  // Please try to keep sorted in alphabetical order
+  {
+    path: 'jobs',
+    component: ContentPageComponent,
+    resolve: {
+      data: createYamlSpecResolver('assets/content/jobs-page/data.yaml', ContentPageDataSchema),
+    },
   },
 ];
