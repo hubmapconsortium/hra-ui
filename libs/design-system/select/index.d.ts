cf789024b40f91276424a31f6ec21cb018e2d9ab
axdanbol <axdanbol@users.noreply.github.com>
2025-11-11 23:09:30 +0000

    Staging deploy -- Design System improvements - Update slider and select (#1925)
    * remove select entry point and move stories
    
    * remove application config and update title
    
    * remove slider entry point and move stories
    
    * move slider to material demos
    
    * update slider import 73a8802ca5c149c89f3cae682ed6542ac54a93a5

--- libs/design-system/select/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { StoryObj } from '@storybook/angular';
-
-type Story = StoryObj;
-declare const Default: Story;
-declare const RequiredSelect: Story;
-declare const SelectWithNoneOption: Story;
-
-export { Default, RequiredSelect, SelectWithNoneOption };
