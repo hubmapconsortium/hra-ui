e53832f54edfafa2ab7f9ecb190a496e209d5243
Raj Chavan <rajchavan1099@gmail.com>
2025-11-10 19:20:28 -0500

    use hraLink for header component

--- libs/design-system/navigation/header/src/lib/header.component.ts
+++ libs/design-system/navigation/header/src/lib/header.component.ts
@@ -15,8 +15,9 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressBarModule, ProgressBarMode } from '@angular/material/progress-bar';
-import { EventType, Router } from '@angular/router';
+import { EventType } from '@angular/router';
 import { Breakpoints, watchBreakpoint } from '@hra-ui/cdk/breakpoints';
+import { injectRouter } from '@hra-ui/common/router-ext';
 import { BrandModule } from '@hra-ui/design-system/brand';
 import { ButtonsModule } from '@hra-ui/design-system/buttons';
 import { BreadcrumbItem } from '@hra-ui/design-system/buttons/breadcrumbs';
@@ -133,7 +134,7 @@ export class HeaderComponent {
 
     explicitEffect([this.menuOffsetPx], () => this.updateMenuPositions(), { defer: true });
 
-    inject(Router)
+    injectRouter({ optional: true })
       ?.events.pipe(
         takeUntilDestroyed(),
         filter((navigationEvent) =>
--- libs/design-system/navigation/header/src/lib/menu-content/menu-content.component.ts
+++ libs/design-system/navigation/header/src/lib/menu-content/menu-content.component.ts
@@ -1,8 +1,8 @@
 import { Location } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Directive, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Directive, input } from '@angular/core';
 import { MatDivider } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
-import { Router, RouterModule, UrlTree } from '@angular/router';
+import { LinkDirective } from '@hra-ui/common/router-ext';
 import { isAbsolute as isAbsoluteUrl } from '@hra-ui/common/url';
 import { ButtonsModule } from '@hra-ui/design-system/buttons';
 import { Menu, MenuGroup, MenuItem, MenuSubGroup } from '../types/menus.schema';
@@ -58,7 +58,7 @@ export class MenuSubGroupDirective {
   selector: 'hra-menu-content',
   imports: [
     HraCommonModule,
-    RouterModule,
+    LinkDirective,
     MatDivider,
     MatIconModule,
     ButtonsModule,
@@ -81,29 +81,20 @@ export class MenuContentComponent {
   /** Base url - Menu urls starting with this will be converted into router links */
   readonly baseUrl = input.required<string | undefined>();
 
-  /** Reference to the router if available */
-  private readonly router = inject(Router, { optional: true });
-
   /**
    * Resolves an url against the baseUrl
    *
    * @param url Raw url
-   * @returns Whether the url is absolute along with the resolved url
+   * @returns Whether the url is external and the resolved url
    */
-  resolveUrl(url: string, forceExternal = false): { isAbsolute: boolean; value: string | UrlTree } {
-    const { router } = this;
+  resolveUrl(url: string, forceExternal = false): { isExternal: boolean; value: string } {
     const baseUrl = Location.stripTrailingSlash(this.baseUrl() ?? '') + '/';
-    let isAbsolute = forceExternal || isAbsoluteUrl(url);
+    let isExternal = forceExternal || isAbsoluteUrl(url);
     if (!forceExternal && baseUrl && url.startsWith(baseUrl)) {
-      isAbsolute = false;
+      isExternal = false;
       url = url.slice(baseUrl.length);
     }
 
-    let value: string | UrlTree = url;
-    if (!isAbsolute && router) {
-      value = router.parseUrl(Location.stripTrailingSlash(url));
-    }
-
-    return { isAbsolute, value };
+    return { isExternal, value: url };
   }
 }
