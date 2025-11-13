50e22e4dba507b06c12206d3cafd3ab1dc7e1b87
Raj Chavan <rajchavan1099@gmail.com>
2025-11-13 15:41:35 -0500

    add hraTextButton Directive

--- libs/design-system/buttons/text-hyperlink/src/lib/directives/text-hyperlink.directive.ts
+++ libs/design-system/buttons/text-hyperlink/src/lib/directives/text-hyperlink.directive.ts
@@ -18,3 +18,22 @@ import { ClickEventDirective } from '@hra-ui/common/analytics';
   },
 })
 export class TextHyperlinkDirective {}
+
+/**
+ * Applies hyperlink styles when placed on a <button> tag.
+ * Styles the button as a text hyperlink with underline.
+ * Also attaches a click event for analytics.
+ */
+@Directive({
+  selector: 'button[hraTextButton]',
+  hostDirectives: [
+    {
+      directive: ClickEventDirective,
+      inputs: ['hraClickEvent: hraTextButton'],
+    },
+  ],
+  host: {
+    class: 'hra-text-hyperlink-button',
+  },
+})
+export class TextHyperlinkButtonDirective {}
