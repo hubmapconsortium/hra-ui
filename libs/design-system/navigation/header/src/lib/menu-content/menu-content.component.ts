66566d57b651fc1d0349314807e428973dcd3c16
Raj Chavan <rajchavan1099@gmail.com>
2025-11-13 12:09:47 -0500

    remove resolveUrl function

--- libs/design-system/navigation/header/src/lib/menu-content/menu-content.component.ts
+++ libs/design-system/navigation/header/src/lib/menu-content/menu-content.component.ts
@@ -1,9 +1,7 @@
-import { Location } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Directive, input } from '@angular/core';
 import { MatDivider } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { LinkDirective } from '@hra-ui/common/router-ext';
-import { isAbsolute as isAbsoluteUrl } from '@hra-ui/common/url';
 import { ButtonsModule } from '@hra-ui/design-system/buttons';
 import { Menu, MenuGroup, MenuItem, MenuSubGroup } from '../types/menus.schema';
 import { HraCommonModule } from '@hra-ui/common';
@@ -80,21 +78,4 @@ export class MenuContentComponent {
   readonly menu = input.required<Menu>();
   /** Base url - Menu urls starting with this will be converted into router links */
   readonly baseUrl = input.required<string | undefined>();
-
-  /**
-   * Resolves an url against the baseUrl
-   *
-   * @param url Raw url
-   * @returns Whether the url is external and the resolved url
-   */
-  resolveUrl(url: string, forceExternal = false): { isExternal: boolean; value: string } {
-    const baseUrl = Location.stripTrailingSlash(this.baseUrl() ?? '') + '/';
-    let isExternal = forceExternal || isAbsoluteUrl(url);
-    if (!forceExternal && baseUrl && url.startsWith(baseUrl)) {
-      isExternal = false;
-      url = url.slice(baseUrl.length);
-    }
-
-    return { isExternal, value: url };
-  }
 }
