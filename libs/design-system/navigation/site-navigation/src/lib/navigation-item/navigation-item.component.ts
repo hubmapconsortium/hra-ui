c50878831a007ae544c9ba93528901e3cf17b7b4
Raj Chavan <rajchavan1099@gmail.com>
2025-11-13 12:41:39 -0500

    update to have baseUrl

--- libs/design-system/navigation/site-navigation/src/lib/navigation-item/navigation-item.component.ts
+++ libs/design-system/navigation/site-navigation/src/lib/navigation-item/navigation-item.component.ts
@@ -18,6 +18,9 @@ export class NavigationItemComponent {
   /** Navigation Item Data */
   readonly navigationItem = input.required<DocsNavigationItem>();
 
+  /** Base URL for the appliation */
+  readonly baseUrl = input.required<string>();
+
   /** Resolved URL for the navigation item */
   protected readonly url = computed(() => this.navigationItem().url);
 }
