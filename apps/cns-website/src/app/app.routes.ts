7ed13d4532f0272a0f4c38abebae8cacb19f667d
Prashant Channe <prashantchanne121@gmail.com>
2025-11-13 15:07:40 -0500

    updated a route and a picture

--- apps/cns-website/src/app/app.routes.ts
+++ apps/cns-website/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Route } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page.component';
+import { PeopleProfileComponent } from './pages/people-profile/people-profile.component';
 
 /** Application routes */
 export const appRoutes: Route[] = [
@@ -8,4 +9,15 @@ export const appRoutes: Route[] = [
     pathMatch: 'full',
     component: LandingPageComponent,
   },
+  {
+    path: 'people/katy-borner',
+    component: PeopleProfileComponent,
+  },
+  // Dynamic route for future profile pages
+  // When you create a team page, clicking on a person will navigate to /people/:slug
+  // You'll need to create a component that loads profile data based on the slug
+  // {
+  //   path: 'people/:slug',
+  //   component: PeopleProfileComponent,
+  // },
 ];
