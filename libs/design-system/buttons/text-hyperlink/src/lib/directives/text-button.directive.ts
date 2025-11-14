835ab38c19139c1fbf0a6d691926b31a8ef23f7b
Raj Chavan <rajchavan1099@gmail.com>
2025-11-14 10:21:54 -0500

    update documentation

--- libs/design-system/buttons/text-hyperlink/src/lib/directives/text-button.directive.ts
+++ libs/design-system/buttons/text-hyperlink/src/lib/directives/text-button.directive.ts
@@ -1,6 +1,10 @@
 import { Directive } from '@angular/core';
 import { ClickEventDirective } from '@hra-ui/common/analytics';
 
+/**
+ * Applies text hyperlink styles when placed on a <button> tag.
+ * Also attaches a click event for analytics.
+ */
 @Directive({
   selector: 'button[hraTextButton]',
   host: {
