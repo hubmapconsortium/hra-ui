33f4d1551079c5ab4238e0b103c575a2a0dd2071
Saai Vignesh P <saaivigneshpremanand@gmail.com>
2025-11-18 15:12:44 -0500

    feat(lib:design-system): :sparkles: make brand mark component have it's own injection token for brand marks

--- libs/design-system/brand/mark/src/lib/mark.component.ts
+++ libs/design-system/brand/mark/src/lib/mark.component.ts
@@ -2,8 +2,7 @@ import { ChangeDetectionStrategy, Component, computed, input } from '@angular/co
 import { HraCommonModule } from '@hra-ui/common';
 import { findOrThrow } from '@hra-ui/common/array-util';
 import { InlineSVGModule, SVGScriptEvalMode } from 'ng-inline-svg-2';
-import { MARKS } from './static-data/parsed';
-import { BrandMarkVariant } from './types/marks.schema';
+import { BrandMarkVariant, injectBrandMarks } from './brand-marks';
 
 /**
  * HRA brandmark component
@@ -19,9 +18,12 @@ export class BrandMarkComponent {
   /** Mark variant */
   readonly variant = input<BrandMarkVariant>('default');
 
+  /** Marks from injection token */
+  readonly marks = injectBrandMarks();
+
   /** SVG script eval mode */
   protected readonly NEVER_EVAL_SCRIPTS = SVGScriptEvalMode.NEVER;
 
   /** Mark data */
-  protected readonly data = computed(() => findOrThrow(MARKS, ({ variant }) => variant === this.variant()));
+  protected readonly data = computed(() => findOrThrow(this.marks, ({ variant }) => variant === this.variant()));
 }
