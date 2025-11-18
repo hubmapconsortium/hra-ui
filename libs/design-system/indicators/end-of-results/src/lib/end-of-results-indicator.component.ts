7bf8f4dce20f0b1a1d5c2a7dfd11331f79061467
Raj Chavan <rajchavan1099@gmail.com>
2025-11-18 13:00:04 -0500

    add documentation

--- libs/design-system/indicators/end-of-results/src/lib/end-of-results-indicator.component.ts
+++ libs/design-system/indicators/end-of-results/src/lib/end-of-results-indicator.component.ts
@@ -1,6 +1,9 @@
 import { ChangeDetectionStrategy, Component, input, numberAttribute } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+/**
+ * End of Results Indicator Component
+ */
 @Component({
   selector: 'hra-end-of-results-indicator',
   imports: [CommonModule],
@@ -9,9 +12,12 @@ import { CommonModule } from '@angular/common';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EndOfResultsIndicatorComponent {
+  /** Number of filtered results */
   readonly resultCount = input.required({ transform: numberAttribute });
 
+  /** Label text for results count */
   readonly resultsLabel = input<string>('Results:');
 
+  /** Label text for end message */
   readonly endLabel = input<string>('End of results');
 }
