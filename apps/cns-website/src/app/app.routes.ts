308ad9fbf69d8e8206706f3a25fe6a4ec39107a5
Gauri Markandey <gauri2029@gmail.com>
2025-11-19 11:45:37 -0500

    removed some extra comments in app routes

--- apps/cns-website/src/app/app.routes.ts
+++ apps/cns-website/src/app/app.routes.ts
@@ -20,9 +20,6 @@ export const appRoutes: Route[] = [
       data: createYamlSpecResolver('assets/content/jobs-page/data.yaml', ContentPageDataSchema),
     },
   },
-
-  // Content pages
-  // Please try to keep sorted in alphabetical order
   {
     path: 'visitor-info',
     component: ContentPageComponent,
