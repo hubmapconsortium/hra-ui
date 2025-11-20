4e4f4fb4e8c0893f858e8e26dbcf9a4d934c8ae2
Raj Chavan <rajchavan1099@gmail.com>
2025-11-20 10:46:49 -0500

    update tests for text-hyperlink

--- libs/design-system/buttons/text-hyperlink/src/lib/text-hyperlink.component.spec.ts
+++ libs/design-system/buttons/text-hyperlink/src/lib/text-hyperlink.component.spec.ts
@@ -26,4 +26,19 @@ describe('TextHyperlinkComponent', () => {
     const link = screen.getByRole('link');
     expect(link).not.toHaveAttribute('target', '_blank');
   });
+
+  it('should open external links in a new tab', async () => {
+    await render(TextHyperlinkComponent, {
+      inputs: {
+        text: 'test',
+        url: 'https://example.com',
+        external: true,
+      },
+    });
+
+    const link = screen.getByRole('link');
+    expect(link).toHaveAttribute('href', 'https://example.com');
+    expect(link).toHaveAttribute('target', '_blank');
+    expect(link).toHaveAttribute('rel', 'noopener noreferrer');
+  });
 });
