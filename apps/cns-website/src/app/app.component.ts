2af3d8cc10b59dea53d9a7dcee3b81ddbc5e0279
Saai Vignesh P <saaivigneshpremanand@gmail.com>
2025-11-24 17:47:17 -0500

    feat(app:cns-website): :sparkles: add 404 page to route and fix header in hierarchy

--- apps/cns-website/src/app/app.component.ts
+++ apps/cns-website/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BaseApplicationComponent } from '@hra-ui/application';
 import { HraCommonModule } from '@hra-ui/common';
+import { HeaderComponent } from './components/header/header.component';
 
 /**
  * Main application component
  */
 @Component({
   selector: 'cns-website',
-  imports: [HraCommonModule, RouterModule],
+  imports: [HraCommonModule, RouterModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
