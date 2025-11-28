2bd2a3f0e4eba17affa8e6031a2659bf7c8657e2
Raj Chavan <rajchavan1099@gmail.com>
2025-11-28 17:58:17 -0500

    add header to all pages

--- apps/cns-website/src/app/app.component.ts
+++ apps/cns-website/src/app/app.component.ts
@@ -2,13 +2,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BaseApplicationComponent } from '@hra-ui/application';
 import { HraCommonModule } from '@hra-ui/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 /**
  * Main application component
  */
 @Component({
   selector: 'cns-website',
-  imports: [HraCommonModule, RouterModule],
+  imports: [HraCommonModule, RouterModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
