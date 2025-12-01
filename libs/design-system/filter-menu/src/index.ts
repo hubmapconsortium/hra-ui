972bff3ff7c41d8bcfe1b127a784fabb206518ab
edlu77 <eddd.lu@gmail.com>
2025-12-01 11:07:23 -0500

    Update index.ts

--- libs/design-system/filter-menu/src/index.ts
+++ libs/design-system/filter-menu/src/index.ts
@@ -1,7 +1,3 @@
 export * from './lib/filter-menu.component';
 
-export * from './lib/filter-menu-customize/filter-menu-customize.component';
-
-export * from './lib/filter-list-flyout/filter-list-flyout.component';
-
 export { FilterContainerComponent, type FilterChip } from './lib/filter-container/filter-container.component';
