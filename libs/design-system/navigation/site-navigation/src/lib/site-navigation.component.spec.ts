3472995cef1e7dd640b94ac8739cca07cf22f48c
Raj Chavan <rajchavan1099@gmail.com>
2025-12-03 11:22:06 -0500

    update test coverage for site navigation

--- libs/design-system/navigation/site-navigation/src/lib/site-navigation.component.spec.ts
+++ libs/design-system/navigation/site-navigation/src/lib/site-navigation.component.spec.ts
@@ -10,4 +10,31 @@ describe('SiteNavigationComponent', () => {
     });
     await expect(promise).resolves.toBeTruthy();
   });
+
+  it('should set expanded category when changeExpandedCategory is called with true', async () => {
+    const { fixture } = await render(SiteNavigationComponent, {
+      inputs: {
+        baseUrl: 'https://docs.humanatlas.io',
+      },
+    });
+
+    const component = fixture.componentInstance;
+    const categoryName = 'Test Category';
+
+    component.changeExpandedCategory(true, categoryName);
+
+    expect(component.expandedCategory()).toBe(categoryName);
+  });
+
+  it('should update expanded category in constructor', async () => {
+    const { fixture } = await render(SiteNavigationComponent, {
+      inputs: {
+        baseUrl: 'https://docs.humanatlas.io',
+      },
+    });
+
+    const component = fixture.componentInstance;
+
+    expect(component.expandedCategory()).toBeDefined();
+  });
 });
