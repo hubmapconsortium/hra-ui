7010d57845d0ad74f9ad582128ff13e355df943d
edlu77 <eddd.lu@gmail.com>
2025-12-10 20:05:14 -0500

    Gallery view stories

--- libs/design-system/content-templates/gallery-view/src/lib/gallery-view.component.stories.ts
+++ libs/design-system/content-templates/gallery-view/src/lib/gallery-view.component.stories.ts
@@ -1,62 +1,155 @@
 import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
-import { GalleryViewComponent } from './gallery-view.component';
-import { ButtonsModule } from '@hra-ui/design-system/buttons';
-import { CardsModule } from '@hra-ui/design-system/cards';
+import { GalleryItem, GalleryViewComponent } from './gallery-view.component';
 
 const PICTURE_URL = 'assets/ui-images/placeholder.png';
 
+const GALLERY_CARDS: GalleryItem[] = [
+  {
+    name: 'A',
+    imageSrc: PICTURE_URL,
+    date: 'March 15, 2024',
+    link: 'https://humanatlas.io',
+    external: true,
+    tags: ['Research', 'HRA'],
+  },
+  {
+    name: 'AB',
+    imageSrc: PICTURE_URL,
+    date: 'March 15, 2024',
+    link: 'https://humanatlas.io',
+    external: true,
+    tags: ['Research', 'HRA', 'Test'],
+  },
+  {
+    name: 'ABC',
+    imageSrc: PICTURE_URL,
+    date: 'June 15, 2024',
+    link: 'https://humanatlas.io',
+    external: true,
+    tags: ['Research', 'HRA', 'Label'],
+  },
+  {
+    name: 'ABCD',
+    imageSrc: PICTURE_URL,
+    date: 'May 15, 2024',
+    link: 'https://humanatlas.io',
+    external: true,
+    tags: ['HRA'],
+  },
+  {
+    name: 'ABCDE',
+    imageSrc: PICTURE_URL,
+    date: 'April 15, 2024',
+    link: 'https://humanatlas.io',
+    external: true,
+    tags: ['Research', 'HRA'],
+  },
+];
+
+const PROFILE_CARDS = [
+  {
+    name: 'Katy Börner',
+    imageSrc: PICTURE_URL,
+    link: 'https://humanatlas.io',
+    description: 'Faculty, Center Director',
+    actionText: 'Learn more',
+  },
+  {
+    name: 'Andreas Bueckle',
+    imageSrc: PICTURE_URL,
+    link: 'https://humanatlas.io',
+    description: 'Research Lead, Faculty',
+    actionText: 'Learn more',
+  },
+  {
+    name: 'Bruce W. Herr II',
+    imageSrc: PICTURE_URL,
+    link: 'https://humanatlas.io',
+    description: 'Technical Director',
+    actionText: 'Learn more',
+  },
+  {
+    name: 'Lisel Record',
+    imageSrc: PICTURE_URL,
+    link: 'https://humanatlas.io',
+    description: 'Associate Director',
+    actionText: 'Learn more',
+  },
+  {
+    name: 'Daniel Bolin',
+    imageSrc: PICTURE_URL,
+    link: 'https://humanatlas.io',
+    description: 'Senior Software Developer',
+    actionText: 'Learn more',
+  },
+  {
+    name: 'Mike Gallant',
+    imageSrc: PICTURE_URL,
+    link: 'https://humanatlas.io',
+    description: 'Assistant Director of IT',
+    actionText: 'Learn more',
+  },
+  {
+    name: 'Michael Ginda',
+    imageSrc: PICTURE_URL,
+    link: 'https://humanatlas.io',
+    description: 'Senior Research Analyst',
+    actionText: 'Learn more',
+  },
+  {
+    name: 'Yashvardhan Jain',
+    imageSrc: PICTURE_URL,
+    link: 'https://humanatlas.io',
+    description: 'Research Software Engineer - Machine Learning',
+    actionText: 'Learn more',
+  },
+];
+
 const meta: Meta = {
   title: 'Design System/Content Templates/Gallery View',
-  decorators: [
-    moduleMetadata({
-      imports: [GalleryViewComponent, ButtonsModule, CardsModule],
-    }),
-  ],
   parameters: {
     design: {
       type: 'figma',
       url: 'https://www.figma.com/design/gQEMLugLjweDvbsNNUVffD/HRA-Design-System-Repository?node-id=12642-57693&t=Hhug8Q6yf6QcFAL9-4',
     },
   },
+  decorators: [
+    moduleMetadata({
+      imports: [GalleryViewComponent],
+    }),
+  ],
+  render: (args) => ({
+    props: args,
+    template: `
+        <button (click)="gallery.sidenav().toggle()">Toggle</button>
+        <hra-gallery-view [galleryCards]="galleryCards" [variant]="variant" #gallery/>
+    `,
+    styles: [
+      `.hra-app {
+        height: calc(100vh - 3rem);
+      }
+        button {
+          position: absolute;
+          right: 0;
+          z-index: 2;
+      }`,
+    ],
+  }),
 };
 
 export default meta;
 type Story = StoryObj;
 
-export const ProfileCard: Story = {
-  args: { dataSource: [1, 2, 3, 4, 5, 6, 7, 8, 9] },
-  render: (args) => ({
-    props: { ...args, pictureUrl: PICTURE_URL },
-    template: `
-      <hra-gallery-view>
-        @for (i of dataSource; track i) {
-          <hra-profile-card
-            [name]="'Person ' + (i + 1)"
-            [description]="'Description ' + (i + 1)"
-            [pictureUrl]="pictureUrl"
-          />
-        }
-      </hra-gallery-view>
-    `,
-  }),
+export const Gallery: Story = {
+  args: {
+    galleryCards: GALLERY_CARDS,
+    variant: 'gallery',
+  },
 };
 
-export const GalleryCard: Story = {
-  args: { dataSource: [1, 2, 3, 4, 5, 6, 7, 8, 9] },
-  render: (args) => ({
-    props: { ...args, pictureUrl: PICTURE_URL },
-    template: `
-      <hra-gallery-view>
-        @for (i of dataSource; track i) {
-          <hra-gallery-card
-            [imageSrc]="pictureUrl"
-            date="September 20, 2025"
-            tagline="Card tagline that is less than 100 characters or truncated."
-            [tags]="['Label', 'Longer label']"
-            link="https://google.com"
-          ></hra-gallery-card>
-        }
-      </hra-gallery-view>
-    `,
-  }),
+export const Profile: Story = {
+  args: {
+    galleryCards: PROFILE_CARDS,
+    variant: 'profile',
+  },
 };
--- libs/design-system/content-templates/gallery-view/src/lib/gallery-view.component.ts
+++ libs/design-system/content-templates/gallery-view/src/lib/gallery-view.component.ts
@@ -1,22 +1,88 @@
-import { ChangeDetectionStrategy, Component, model } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, computed, input, signal, viewChild } from '@angular/core';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { watchBreakpoint } from '@hra-ui/cdk/breakpoints';
 import { HraCommonModule } from '@hra-ui/common';
+import { ButtonsModule } from '@hra-ui/design-system/buttons';
+import { TextHyperlinkDirective } from '@hra-ui/design-system/buttons/text-hyperlink';
 import { CardsModule } from '@hra-ui/design-system/cards';
 import { GridContainerComponent } from '@hra-ui/design-system/content-templates/grid-container';
+import { FilterMenuComponent } from '@hra-ui/design-system/filter-menu';
+import { IconsModule } from '@hra-ui/design-system/icons';
+import { ScrollingModule } from '@hra-ui/design-system/scrolling';
 import { SearchFilterComponent } from '@hra-ui/design-system/search-filter';
 
+/** Gallery item interface */
+export interface GalleryItem {
+  /** Name of item */
+  name: string;
+  /** Path to image */
+  imageSrc: string;
+  /** Link */
+  link: string;
+  /** Date to show on card (for gallery cards) */
+  date?: string;
+  /** If the link is external (for gallery cards) */
+  external?: boolean;
+  /** Tags (for gallery cards) */
+  tags?: string[];
+  /** Job description (for profile cards) */
+  description?: string;
+  /** Action button text (for profile cards) */
+  actionText?: string;
+}
+
+/** Gallery view variant type */
+type GalleryVariant = 'gallery' | 'profile';
+
+/**
+ * Gallery view component for displaying gallery and profile cards
+ */
 @Component({
   selector: 'hra-gallery-view',
-  imports: [HraCommonModule, GridContainerComponent, CardsModule, SearchFilterComponent],
+  imports: [
+    HraCommonModule,
+    GridContainerComponent,
+    CardsModule,
+    SearchFilterComponent,
+    FilterMenuComponent,
+    MatSidenavModule,
+    IconsModule,
+    TextHyperlinkDirective,
+    ScrollingModule,
+    ButtonsModule,
+  ],
   templateUrl: './gallery-view.component.html',
   styleUrl: './gallery-view.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GalleryViewComponent {
-  readonly totalCount = model(0);
-  readonly viewingCount = model(0);
+  /** Reference to sidenav element */
+  readonly sidenav = viewChild<MatSidenav>('sidenav');
 
-  search(event: any) {
-    console.log(event);
+  /** List of gallery cards */
+  readonly galleryCards = input<GalleryItem[]>([]);
+
+  /** Gallery view variant */
+  readonly variant = input<GalleryVariant>('gallery');
+
+  /** Current search bar value */
+  readonly search = signal<string>('');
+
+  /** Filtered items (after typing in search bar) */
+  readonly filteredGalleryItems = computed(() => this.doGallerySearch());
+
+  /** Total number of cards */
+  readonly totalCount = computed(() => this.galleryCards().length);
+
+  /** Number of cards after filtering */
+  readonly viewingCount = computed(() => this.filteredGalleryItems().length);
+
+  /** Whether the user is on a wide screen */
+  protected readonly isWideScreen = watchBreakpoint('(min-width: 1100px)');
+
+  /** Filters items according to the search bar value */
+  private doGallerySearch() {
+    const searchTerm = this.search().toLowerCase();
+    return this.galleryCards().filter((option) => option.name.toLowerCase().includes(searchTerm));
   }
 }
