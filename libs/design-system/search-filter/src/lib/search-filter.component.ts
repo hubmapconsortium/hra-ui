89fa59cdd74486fb2c6a0d0f57589973c52027f4
Prashant Channe <prashantchanne121@gmail.com>
2025-12-15 10:10:33 -0500

    updated search filter

--- libs/design-system/search-filter/src/lib/search-filter.component.ts
+++ libs/design-system/search-filter/src/lib/search-filter.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { HraCommonModule } from '@hra-ui/common';
 import { ResultsIndicatorComponent } from '@hra-ui/design-system/indicators/results-indicator';
 
@@ -11,7 +12,15 @@ import { ResultsIndicatorComponent } from '@hra-ui/design-system/indicators/resu
  */
 @Component({
   selector: 'hra-search-filter',
-  imports: [HraCommonModule, FormsModule, MatFormFieldModule, MatInputModule, MatIconModule, ResultsIndicatorComponent],
+  imports: [
+    HraCommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    ResultsIndicatorComponent,
+  ],
   standalone: true,
   templateUrl: './search-filter.component.html',
   styleUrl: './search-filter.component.scss',
