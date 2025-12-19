f5197c1bc205e7fcf00e0e025e108bc69c227ea1
Daniel Bolin <axbolin@iu.edu>
2025-12-19 16:05:54 -0500

    test(app:cns-website): Set coverage levels

--- apps/cns-website/jest.config.ts
+++ apps/cns-website/jest.config.ts
@@ -3,4 +3,13 @@ export default {
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../../coverage/apps/cns-website',
+  // TODO increase to 85%!
+  coverageThreshold: {
+    global: {
+      statements: 18,
+      branches: 7,
+      lines: 17,
+      functions: 18,
+    },
+  },
 };
