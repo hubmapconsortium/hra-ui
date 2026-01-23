f4bcc6673e777027e1d45ec51ac6c60bb6460264
Raj Chavan <rajchavan1099@gmail.com>
2026-01-23 13:23:45 -0500

    add doc coverage for the portal outlet group directive

--- libs/cdk/portal-outlet-group/src/lib/portal-outlet-group.directive.ts
+++ libs/cdk/portal-outlet-group/src/lib/portal-outlet-group.directive.ts
@@ -2,39 +2,69 @@ import { Directive, contentChildren, effect, input, Signal } from '@angular/core
 import { Portal } from '@angular/cdk/portal';
 import { HraPortalOutletNameDirective } from './portal-outlet-name.directive';
 
+/**
+ * Directive that manages a group of named portal outlets.
+ * It takes portal content and can dynamically switch which outlet the content is attached to.
+ *
+ * @example
+ * ```html
+ * <div hraPortalOutletGroup [portal]="myPortal" [activeOutlet]="'main'">
+ *   <div hraPortalOutletName="main"><ng-template cdkPortalOutlet /></div>
+ *   <div hraPortalOutletName="secondary"><ng-template cdkPortalOutlet /></div>
+ * </div>
+ * ```
+ */
 @Directive({
   selector: '[hraPortalOutletGroup]',
   standalone: true,
 })
 export class HraPortalOutletGroupDirective {
+  /** The portal content to be attached to the active outlet */
   readonly portal = input<Portal<unknown> | null>(null);
 
+  /** The name of the active outlet where the portal should be attached */
   readonly activeOutlet = input<string>('');
 
+  /** Query all registered portal outlet name directives within this group */
   private readonly outlets: Signal<readonly HraPortalOutletNameDirective[]> = contentChildren(
     HraPortalOutletNameDirective,
     { descendants: true },
   );
 
+  /** Map of outlet names to their corresponding directives */
   private readonly outletMap = new Map<string, HraPortalOutletNameDirective>();
 
+  /** Currently active outlet directive */
   private currentOutlet: HraPortalOutletNameDirective | null = null;
 
   constructor() {
+    /** Effect to update the outlet map when outlets change */
     effect(() => {
       this.updateOutletMap();
     });
 
+    /** Effect to handle portal attachment when portal or activeOutlet changes */
     effect(() => {
       this.attachPortalToActiveOutlet();
     });
   }
 
+  /**
+   * Registers an outlet with this group
+   *
+   * @param name The name of the outlet
+   * @param outlet The outlet directive to register
+   */
   registerOutlet(name: string, outlet: HraPortalOutletNameDirective): void {
     this.outletMap.set(name, outlet);
     this.attachPortalToActiveOutlet();
   }
 
+  /**
+   * Unregisters an outlet from this group
+   *
+   * @param name The name of the outlet to unregister
+   */
   unregisterOutlet(name: string): void {
     const outlet = this.outletMap.get(name);
     if (outlet === this.currentOutlet) {
@@ -43,6 +73,9 @@ export class HraPortalOutletGroupDirective {
     this.outletMap.delete(name);
   }
 
+  /**
+   * Updates the outlet map with the current content children
+   */
   private updateOutletMap(): void {
     const outlets = this.outlets();
     this.outletMap.clear();
@@ -55,10 +88,14 @@ export class HraPortalOutletGroupDirective {
     }
   }
 
+  /**
+   * Attaches the portal to the currently active outlet
+   */
   private attachPortalToActiveOutlet(): void {
     const portal = this.portal();
     const activeOutletName = this.activeOutlet();
 
+    /** Detach from current outlet if switching */
     this.detachCurrentPortal();
 
     if (!portal || !activeOutletName) {
@@ -72,6 +109,9 @@ export class HraPortalOutletGroupDirective {
     }
   }
 
+  /**
+   * Detaches the portal from the current outlet
+   */
   private detachCurrentPortal(): void {
     if (this.currentOutlet) {
       this.currentOutlet.detachPortal();
