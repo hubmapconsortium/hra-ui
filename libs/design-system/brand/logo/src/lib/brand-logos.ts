70b12103c2084d60d77873f461a26b2b8c1b3c14
Raj Chavan <rajchavan1099@gmail.com>
2026-01-28 12:32:43 -0500

    update brand logos injection token

--- libs/design-system/brand/logo/src/lib/brand-logos.ts
+++ libs/design-system/brand/logo/src/lib/brand-logos.ts
@@ -1,5 +1,30 @@
 import { createInjectionToken } from 'ngxtension/create-injection-token';
 
+/** Brand logo size */
+export type BrandLogoSize = 'regular' | 'small';
+
+/** Brand logo */
+export interface BrandLogo {
+  /** Logo size */
+  size: BrandLogoSize;
+  /** Logo source url */
+  src: string;
+  /** Logo width */
+  width: number;
+  /** Logo height */
+  height: number;
+}
+
+/** Brand logos configuration */
+export interface BrandLogosConfig {
+  /** Label for the brand */
+  label: string;
+  /** URL for the brand website */
+  url: string;
+  /** Array of brand logos */
+  logos: BrandLogo[];
+}
+
 /** Default logos for applications */
 export const DEFAULT_LOGOS: BrandLogo[] = [
   {
@@ -16,26 +41,18 @@ export const DEFAULT_LOGOS: BrandLogo[] = [
   },
 ];
 
-/** Brand logo size */
-export type BrandLogoSize = 'regular' | 'small';
-
-/** Brand logo */
-export interface BrandLogo {
-  /** Logo size */
-  size: BrandLogoSize;
-  /** Logo source url */
-  src: string;
-  /** Logo width */
-  width: number;
-  /** Logo height */
-  height: number;
-}
+/** Default brand logos configuration */
+export const DEFAULT_BRAND_LOGOS_CONFIG: BrandLogosConfig = {
+  label: 'Human Reference Atlas',
+  url: 'https://humanatlas.io',
+  logos: DEFAULT_LOGOS,
+};
 
 /** Injection token for brand logos configuration */
-const BRAND_LOGOS = createInjectionToken(() => DEFAULT_LOGOS);
+const BRAND_LOGOS = createInjectionToken(() => DEFAULT_BRAND_LOGOS_CONFIG);
 
 /** Inject the brand logos configuration */
 export const injectBrandLogos = BRAND_LOGOS[0];
 
 /** Set the brand logos configuration */
-export const provideBrandLogos = BRAND_LOGOS[1] as (logos: BrandLogo[]) => ReturnType<(typeof BRAND_LOGOS)[1]>;
+export const provideBrandLogos = BRAND_LOGOS[1] as (config: BrandLogosConfig) => ReturnType<(typeof BRAND_LOGOS)[1]>;
