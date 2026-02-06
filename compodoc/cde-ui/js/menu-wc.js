ca68a61ef9c8f4fdb5181ec961e841278e135fe6
axdanbol <axdanbol@users.noreply.github.com>
2026-02-06 17:06:54 +0000

    Staging deploy -- Cde improvments (mostly study page) (#2117)
    * fix(app:cde-ui): Fix scrolling & anchors
    
    * refactor(app:cde-ui): Improve breadcrumbs
    
    * refactor(lib:cde-visualization): Hide data source and timestamp when not present in metadata
    
    * chore(global): Tell copilot to import zod correctly
    
    * refactor(app:cde-ui): :coffin: Remove webcomponent build for cde app
    
    * refactor(app:cde-ui): Big routes refactor
    
    * docs(app:cde-ui): :memo: Add code docs
    
    * test(app:cde-ui): Increase test coverage & fix breaking tests
    
    * test(app:cde-ui): Add more tests
    
    * test(app:cde-ui): Even more tests
    
    * test(lib:cde-visualization): Increase branch coverage 0bc474ab35fef7a78abfbc22108d209885e25d76

--- compodoc/cde-ui/js/menu-wc.js
+++ compodoc/cde-ui/js/menu-wc.js
@@ -85,6 +85,19 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 </li>
                             </ul>
                         </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#classes-links"' :
+                            'data-bs-target="#xs-classes-links"' }>
+                            <span class="icon ion-ios-paper"></span>
+                            <span>Classes</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="classes-links"' : 'id="xs-classes-links"' }>
+                            <li class="link">
+                                <a href="classes/NotFoundError.html" data-type="entity-link" >NotFoundError</a>
+                            </li>
+                        </ul>
+                    </li>
                         <li class="chapter">
                             <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#injectables-links"' :
                                 'data-bs-target="#xs-injectables-links"' }>
