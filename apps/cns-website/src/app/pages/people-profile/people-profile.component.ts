e09ff7ba84a4cc8eeabf58352c37191f23cfb40d
Daniel Bolin <axbolin@iu.edu>
2026-02-26 13:36:32 -0500

    Profile page fixes (#2161)
    * fix(app:cns-website): Remove duplicate background block
    
    * refactor(app:cns-website): Refactor profile page layout
    
    * refactor(app:cns-website): Add override for breadcrumbs font
    
    Needs to be fixed in the design system
    
    * fix(app:cns-website): Profile page's contact info tweaks

--- apps/cns-website/src/app/pages/people-profile/people-profile.component.ts
+++ apps/cns-website/src/app/pages/people-profile/people-profile.component.ts
@@ -1,17 +1,19 @@
 import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
+import { Breakpoints, watchBreakpoint } from '@hra-ui/cdk/breakpoints';
 import { HraCommonModule } from '@hra-ui/common';
+import { BreadcrumbItem } from '@hra-ui/design-system/buttons/breadcrumbs';
 import { ChipsModule } from '@hra-ui/design-system/chips';
+import { MarkdownComponent } from '@hra-ui/design-system/content-templates/markdown';
 import { PageSectionComponent } from '@hra-ui/design-system/content-templates/page-section';
 import {
   TableOfContentsLayoutComponent,
   TableOfContentsLayoutHeaderComponent,
 } from '@hra-ui/design-system/layouts/table-of-contents';
-import { MarkdownModule } from 'ngx-markdown';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { PeopleItem } from '../../schemas/people.schema';
-import { ContactInfo, ContactInfoComponent } from './contact-info/contact-info.component';
 import { getRefinedRoleTypeLabel, refineRoleType } from '../../utils/refined-roles';
-import { BreadcrumbItem } from '@hra-ui/design-system/buttons/breadcrumbs';
+import { ContactInfoComponent } from './contact-info/contact-info.component';
 
 /** Profile section data */
 interface ProfileSection {
@@ -33,7 +35,10 @@ interface ProfileSection {
     ChipsModule,
     ContactInfoComponent,
     FooterComponent,
-    MarkdownModule,
+    MarkdownComponent,
+    MatSidenav,
+    MatSidenavContainer,
+    MatSidenavContent,
     PageSectionComponent,
     TableOfContentsLayoutComponent,
     TableOfContentsLayoutHeaderComponent,
@@ -48,6 +53,9 @@ export class PeopleProfileComponent {
    */
   readonly data = input.required<PeopleItem>();
 
+  /** Whether the current screen size matches mobile breakpoint */
+  protected readonly isMobile = watchBreakpoint(Breakpoints.Mobile);
+
   /** Breadcrumbs computed for navigation */
   protected readonly breadcrumbs = computed((): BreadcrumbItem[] => [
     { name: 'Home', route: '/' },
@@ -58,11 +66,20 @@ export class PeopleProfileComponent {
   /** Primary role computed from profile data */
   protected readonly primaryRole = computed(() => this.data().roles?.[0]);
 
-  /** Contact info computed for the sidebar */
-  protected readonly contactInfo = computed<ContactInfo>(() => ({
-    image: this.data().image || '',
-    role: this.primaryRole(),
-  }));
+  /** Whether contact info is available for display */
+  protected readonly hasContactInfo = computed(() => {
+    if (this.data().image) {
+      return true;
+    }
+
+    const role = this.primaryRole();
+    if (!role || role.type !== 'member') {
+      return false;
+    }
+
+    const { email, fax, office, phone } = role;
+    return !!(email || fax || office || phone);
+  });
 
   /** Role tags computed for display */
   protected readonly tags = computed(() => {
