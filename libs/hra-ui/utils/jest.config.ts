f7e156ac80aa355311c977b681e069ae5f8485ee
Daniel Bolin <axbolin@iu.edu>
2026-03-04 14:14:29 -0500

    feat(lib:utils): Generate hra-ui utils library
    Different from deprecated libs/shared/utils

---
+++ libs/hra-ui/utils/jest.config.ts
@@ -0,0 +1,6 @@
+export default {
+  displayName: 'utils',
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory: '../../../coverage/libs/hra-ui/utils',
+};
