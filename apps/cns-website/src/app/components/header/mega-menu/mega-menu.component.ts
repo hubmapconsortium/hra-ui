6b896b32c327657f261f75c096fdfc52084bd6e4
Daniel Bolin <axbolin@iu.edu>
2026-03-16 11:34:34 -0400

    fix(app:cns-website): :bug: Fix external links in header

--- apps/cns-website/src/app/components/header/mega-menu/mega-menu.component.ts
+++ apps/cns-website/src/app/components/header/mega-menu/mega-menu.component.ts
@@ -25,5 +25,16 @@ export class MegaMenuComponent {
   readonly socials = input(CNS_SOCIAL_IDS);
 
   /** Scrollbar store for managing viewport scrolling */
-  protected readonly scrollbarStore = inject(ScrollbarStore);
+  private readonly scrollbarStore = inject(ScrollbarStore);
+
+  /**
+   * Scrolls to the top of the page if the menu item is not an external link
+   *
+   * @param item Link item
+   */
+  maybeScrollToTop(item: { external?: boolean }): void {
+    if (!item.external) {
+      this.scrollbarStore.scrollToTop();
+    }
+  }
 }
