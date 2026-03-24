62a26a4e21c6b5e8e21c3d18d99d5c02cf7863be
edlu77 <eddd.lu@gmail.com>
2026-03-24 16:23:03 -0400

    Update tests

--- apps/asctb-reporter/src/app/components/legend/legend.component.spec.ts
+++ apps/asctb-reporter/src/app/components/legend/legend.component.spec.ts
@@ -2,16 +2,19 @@ import { render, screen } from '@testing-library/angular';
 import { Subject } from 'rxjs';
 import { BimodalData, BMNode } from '../../models/bimodal.model';
 import { Legend } from '../../models/legend.model';
+import { Error } from '../../models/response.model';
 import { CompareData } from '../../models/sheet.model';
 import { TNode } from '../../models/tree.model';
 import { LegendService } from '../../services/legend/legend.service';
 import { LegendComponent } from './legend.component';
 
 describe('LegendComponent', () => {
+  const mockError: Error = { hasError: false, msg: '' };
+
   class MockLegendService {
     private readonly subject = new Subject<Legend[]>();
     readonly legendData$ = this.subject.asObservable();
-    makeLegendData = jest.fn((tree: TNode[], nodes: BMNode[], _compare?: CompareData[]) => {
+    makeLegendData = jest.fn((tree: TNode[], nodes: BMNode[]) => {
       // simple fake that constructs Legend items from BM nodes
       const legends = nodes.map(
         (n, i) =>
@@ -31,32 +34,50 @@ describe('LegendComponent', () => {
   }
 
   const createMockService = () => new MockLegendService();
+  const emptyBimodalData = {
+    nodes: [],
+    links: [],
+    config: { CT: { sort: '', size: '' }, BM: { sort: '', size: '', type: '' } },
+  } as BimodalData;
 
-  it('renders legend items when service emits data (view-driven)', async () => {
+  const setup = async (
+    inputs: Partial<{
+      treeData: TNode[];
+      bimodalData: BimodalData;
+      compareData: CompareData[];
+      error: Error;
+    }> = {},
+  ) => {
     const mockService = createMockService();
 
-    await render(LegendComponent, {
+    const renderResult = await render(LegendComponent, {
+      inputs: {
+        treeData: [],
+        bimodalData: emptyBimodalData,
+        compareData: [],
+        error: mockError,
+        ...inputs,
+      },
       providers: [{ provide: LegendService, useValue: mockService }],
-      componentProperties: { treeData: [], bimodalData: undefined, compareData: [] },
     });
+
+    return { ...renderResult, mockService };
+  };
+
+  it('renders legend items when service emits data (view-driven)', async () => {
+    const { mockService } = await setup();
 
     // trigger generation of legends via the mock
     mockService.makeLegendData(
       [{ color: '#E41A1C', isNew: false } as TNode],
       [{ type: 'BM', bType: 'gene' } as BMNode],
-      [],
     );
 
     expect(await screen.findByText('Gene Biomarkers')).toBeTruthy();
   });
 
   it('calls makeLegendData when inputs have data', async () => {
-    const mockService = createMockService();
-
-    const { rerender } = await render(LegendComponent, {
-      providers: [{ provide: LegendService, useValue: mockService }],
-      componentProperties: { treeData: [], bimodalData: undefined, compareData: [] },
-    });
+    const { rerender, mockService } = await setup();
 
     const treeData = [{ color: '#000', isNew: false } as TNode];
     const bimodalData = {
@@ -65,28 +86,20 @@ describe('LegendComponent', () => {
       config: { CT: { sort: '', size: '' }, BM: { sort: '', size: '', type: '' } },
     } as BimodalData;
 
-    await rerender({ componentProperties: { treeData, bimodalData, compareData: [] } });
+    await rerender({ inputs: { treeData, bimodalData, compareData: [], error: mockError } });
 
     expect(mockService.makeLegendData).toHaveBeenCalledWith(treeData, bimodalData.nodes, []);
   });
 
   it('does not call makeLegendData when inputs are empty', async () => {
-    const mockService = createMockService();
-
-    const { rerender } = await render(LegendComponent, {
-      providers: [{ provide: LegendService, useValue: mockService }],
-      componentProperties: { treeData: [], bimodalData: undefined, compareData: [] },
-    });
+    const { rerender, mockService } = await setup();
 
     await rerender({
-      componentProperties: {
+      inputs: {
         treeData: [],
-        bimodalData: {
-          nodes: [],
-          links: [],
-          config: { CT: { sort: '', size: '' }, BM: { sort: '', size: '', type: '' } },
-        } as BimodalData,
+        bimodalData: emptyBimodalData,
         compareData: [],
+        error: mockError,
       },
     });
 
