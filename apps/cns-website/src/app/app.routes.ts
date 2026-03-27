1d4d8e59aaaab76d56e109bf5190c4e32b4afff8
edlu77 <eddd.lu@gmail.com>
2026-03-27 02:10:53 -0400

    Add UCSD map of science page

--- apps/cns-website/src/app/app.routes.ts
+++ apps/cns-website/src/app/app.routes.ts
@@ -59,6 +59,13 @@ export const appRoutes: Route[] = [
   // Content pages
   // Please try to keep sorted in alphabetical order
   {
+    path: '2012-ucsdmap',
+    component: ContentPageComponent,
+    resolve: {
+      data: createYamlSpecResolver('assets/content/2012-ucsdmap/data.yaml', ContentPageDataSchema),
+    },
+  },
+  {
     path: 'about',
     component: ContentPageComponent,
     resolve: {
@@ -131,6 +138,10 @@ export const appRoutes: Route[] = [
   },
 
   // Redirects
+  {
+    path: '2012-UCSDMap.html',
+    redirectTo: '/2012-ucsdmap',
+  },
   {
     path: 'all_news.html',
     redirectTo: '/research?category=news',
