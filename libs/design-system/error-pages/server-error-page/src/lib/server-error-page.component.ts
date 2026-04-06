c841880df7bf051975e71cb918758a79ab8af41a
Daniel Bolin <axbolin@iu.edu>
2026-04-06 15:57:31 -0400

    fix(lib:design-system): :bug: Change buttons to proper links

--- libs/design-system/error-pages/server-error-page/src/lib/server-error-page.component.ts
+++ libs/design-system/error-pages/server-error-page/src/lib/server-error-page.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterModule } from '@angular/router';
 import { HraCommonModule } from '@hra-ui/common';
+import { RouterExtModule } from '@hra-ui/common/router-ext';
 import { ButtonsModule } from '@hra-ui/design-system/buttons';
 
 /**
@@ -10,7 +10,7 @@ import { ButtonsModule } from '@hra-ui/design-system/buttons';
  */
 @Component({
   selector: 'hra-server-error-page',
-  imports: [HraCommonModule, ButtonsModule, MatIconModule, RouterModule],
+  imports: [HraCommonModule, ButtonsModule, MatIconModule, RouterExtModule],
   templateUrl: './server-error-page.component.html',
   styleUrl: './server-error-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
