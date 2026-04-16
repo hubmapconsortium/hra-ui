fafd3e1c717c1afed9ddb725aa8be7c829ebb6e7
Daniel Bolin <axbolin@iu.edu>
2026-04-16 17:23:36 -0400

    test(app:cde-ui): Update a failing test

--- apps/cde-ui/src/app/app.component.spec.ts
+++ apps/cde-ui/src/app/app.component.spec.ts
@@ -2,11 +2,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { Shallow } from 'shallow-render';
 import { AppComponent } from './app.component';
+import { PrivacyPreferencesService } from '@hra-ui/design-system/privacy';
 
 describe('AppComponent', () => {
   let shallow: Shallow<AppComponent>;
   beforeEach(async () => {
-    shallow = new Shallow(AppComponent).import(HttpClientModule, HttpClientTestingModule);
+    shallow = new Shallow(AppComponent)
+      .import(HttpClientModule, HttpClientTestingModule)
+      .mock(PrivacyPreferencesService, { launch: jest.fn() });
   });
 
   it(`should have as title 'cde-ui'`, async () => {
