3368837a3fc28ddd1ea75d6e63f3326466f63f96
Daniel Bolin <axbolin@iu.edu>
2026-04-17 11:11:34 -0400

    Siteimprove fixes (#2232)
    * fix(app:cns-website): Fix privacy policy link in consent banner
    
    * fix(app:cns-website): Correct the aria-label for jsmf
    
    * fix(lib:design-system): :wheelchair: Improve section link accessability
    
    * docs(lib:design-system): :memo: Add missing code docs
    
    * fix(lib:design-system): Fix consent banner layout issues
    
    * refactor(lib:design-system): Use cdk overlay instead of dialog for consent banner
    
    * test(app:cde-ui): Update a failing test
    
    * fix(app:cns-website): Hide mobile only button in mega menu

--- libs/design-system/privacy/consent-banner/src/lib/consent-banner.component.ts
+++ libs/design-system/privacy/consent-banner/src/lib/consent-banner.component.ts
@@ -1,9 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, output } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { HraCommonModule } from '@hra-ui/common';
+import { RouterExtModule } from '@hra-ui/common/router-ext';
 import { BrandModule } from '@hra-ui/design-system/brand';
 import { ButtonsModule } from '@hra-ui/design-system/buttons';
+import { createInjectionToken } from 'ngxtension/create-injection-token';
+
+/** Consent banner configuration */
+export interface ConsentBannerConfig {
+  /** URL to the privacy policy */
+  privacyPolicyUrl?: string;
+}
 
 /** Result of the consent banner */
 export type ConsentBannerResult = 'allow-all' | 'allow-necessary' | 'customize';
@@ -14,17 +22,37 @@ export const CONSENT_BANNER_PANEL_CLASS = 'hra-consent-banner-panel';
 /** Aria labelledby id of consent banner component */
 export const CONSENT_BANNER_ARIA_LABELLEDBY_ID = 'consentBannerDialogTitle';
 
+/** Default configuration for the consent banner */
+const DEFAULT_CONSENT_BANNER_CONFIG: Required<ConsentBannerConfig> = {
+  privacyPolicyUrl: 'https://humanatlas.io/privacy-policy',
+};
+
+/** Injection token for providing and injecting ConsentBannerConfig */
+const CONSENT_BANNER_CONFIG_TOKEN = createInjectionToken((): ConsentBannerConfig => DEFAULT_CONSENT_BANNER_CONFIG);
+
+/** Injection function for obtaining the consent banner configuration */
+export const injectConsentBannerConfig = CONSENT_BANNER_CONFIG_TOKEN[0];
+
+/** Provider function for supplying new ConsentBannerConfig */
+export const provideConsentBannerConfig = CONSENT_BANNER_CONFIG_TOKEN[1];
+
 /** Consent Banner Component */
 @Component({
   selector: 'hra-consent-banner',
-  imports: [HraCommonModule, MatDialogModule, MatIconModule, BrandModule, ButtonsModule],
+  imports: [HraCommonModule, MatDialogModule, MatIconModule, BrandModule, ButtonsModule, RouterExtModule],
   templateUrl: './consent-banner.component.html',
   styleUrl: './consent-banner.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ConsentBannerComponent {
+  /** Emits when one of the actions is clicked */
+  readonly buttonClick = output<ConsentBannerResult>();
+
   /**
    * Aria labelledby id
    */
   readonly ariaLabelledbyId = CONSENT_BANNER_ARIA_LABELLEDBY_ID;
+
+  /** Banner configuration */
+  protected readonly config = { ...DEFAULT_CONSENT_BANNER_CONFIG, ...injectConsentBannerConfig() };
 }
