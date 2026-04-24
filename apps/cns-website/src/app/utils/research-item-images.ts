18a17aa8723e3ab145b6bcde0d0c84621b5bb16a
edlu77 <eddd.lu@gmail.com>
2026-04-24 13:50:30 -0400

    Add additional placeholder images

--- apps/cns-website/src/app/utils/research-item-images.ts
+++ apps/cns-website/src/app/utils/research-item-images.ts
@@ -1,23 +1,4 @@
-import { ResearchTypeId } from '../schemas/research-type.schema';
 import { ResearchItem } from '../schemas/research.schema';
-
-const EVENT_PLACEHOLDER_TYPES = ['conference', 'meeting', 'other', 'presentation', 'tutorial', 'visit', 'workshop'];
-
-const PUBLICATION_PLACEHOLDER_TYPES = [
-  'article-journal',
-  'book',
-  'broadcast',
-  'chapter',
-  'manuscript',
-  'paper-conference',
-  'patent',
-  'periodical',
-  'report',
-  'thesis',
-  'unknown',
-];
-
-const PLACEHOLDER_TYPES = [...EVENT_PLACEHOLDER_TYPES, ...PUBLICATION_PLACEHOLDER_TYPES] as ResearchTypeId[];
 
 /**
  * Gets image url and uses the appropriate placeholder if none is provided.
@@ -30,9 +11,7 @@ export function getImageUrl(item: ResearchItem): string {
   }
   const url = `assets/placeholder-images/placeholder-${item.category}`;
   if (item.category === 'publication' || item.category === 'event') {
-    if (PLACEHOLDER_TYPES.includes(item.type)) {
-      return `${url}-${item.type}.png`;
-    }
+    return `${url}-${item.type}.png`;
   }
   return `${url}.png`;
 }
